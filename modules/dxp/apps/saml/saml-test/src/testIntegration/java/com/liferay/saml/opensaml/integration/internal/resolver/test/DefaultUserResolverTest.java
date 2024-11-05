1d7c3a22e7eeb17b4fa98ff2038448936c4bb842
Jorge García Jiménez <jorge.garcia@liferay.com>
2024-11-05 12:40:21 +0100

    LPD-40838 add integration tests

---
+++ modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/opensaml/integration/internal/resolver/test/DefaultUserResolverTest.java
@@ -0,0 +1,152 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.saml.opensaml.integration.internal.resolver.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.saml.opensaml.integration.field.expression.handler.registry.UserFieldExpressionHandlerRegistry;
+import com.liferay.saml.opensaml.integration.processor.UserProcessor;
+import com.liferay.saml.opensaml.integration.processor.factory.UserProcessorFactory;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jorge García Jiménez
+ */
+@RunWith(Arquillian.class)
+public class DefaultUserResolverTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_user = UserTestUtil.addUser();
+
+		_userGroup = UserGroupTestUtil.addUserGroup();
+
+		_userGroupLocalService.addUserUserGroup(_user.getUserId(), _userGroup);
+	}
+
+	@Test
+	public void testUserAddsUserGroupsWhenMappedUserGroupComesInTheAssertion()
+		throws Exception {
+
+		User processedUser = _processUser(
+			HashMapBuilder.put(
+				"emailAddress", new String[] {_user.getEmailAddress()}
+			).put(
+				"firstName", new String[] {_user.getFirstName()}
+			).put(
+				"lastName", new String[] {_user.getLastName()}
+			).put(
+				"membership:userGroups", new String[] {_userGroup.getName()}
+			).put(
+				"screenName", new String[] {_user.getScreenName()}
+			).build());
+
+		Assert.assertEquals(
+			1,
+			_userGroupLocalService.getUserUserGroupsCount(
+				processedUser.getUserId()));
+	}
+
+	@Test
+	public void testUserKeepsUserGroupsWhenMappingIsNotSet() throws Exception {
+		User processedUser = _processUser(
+			HashMapBuilder.put(
+				"emailAddress", new String[] {_user.getEmailAddress()}
+			).put(
+				"firstName", new String[] {_user.getFirstName()}
+			).put(
+				"lastName", new String[] {_user.getLastName()}
+			).put(
+				"screenName", new String[] {_user.getScreenName()}
+			).build());
+
+		Assert.assertEquals(
+			1,
+			_userGroupLocalService.getUserUserGroupsCount(
+				processedUser.getUserId()));
+	}
+
+	@Test
+	public void testUserNotKeepsUserGroupsWhenMappedUserGroupNotComesInTheAssertion()
+		throws Exception {
+
+		User processedUser = _processUser(
+			HashMapBuilder.put(
+				"emailAddress", new String[] {_user.getEmailAddress()}
+			).put(
+				"firstName", new String[] {_user.getFirstName()}
+			).put(
+				"lastName", new String[] {_user.getLastName()}
+			).put(
+				"membership:userGroups", new String[0]
+			).put(
+				"screenName", new String[] {_user.getScreenName()}
+			).build());
+
+		Assert.assertEquals(
+			0,
+			_userGroupLocalService.getUserUserGroupsCount(
+				processedUser.getUserId()));
+	}
+
+	private User _processUser(Map<String, String[]> attributesMap)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_user.getCompanyId(), _user.getGroupId(), _user.getUserId());
+
+		UserProcessor userProcessor = _userProcessorFactory.create(
+			_user, _userFieldExpressionHandlerRegistry);
+
+		for (Map.Entry<String, String[]> attribute : attributesMap.entrySet()) {
+			userProcessor.setValueArray(
+				attribute.getKey(), attribute.getValue());
+		}
+
+		return userProcessor.process(serviceContext);
+	}
+
+	@DeleteAfterTestRun
+	private User _user;
+
+	@Inject
+	private UserFieldExpressionHandlerRegistry
+		_userFieldExpressionHandlerRegistry;
+
+	@DeleteAfterTestRun
+	private UserGroup _userGroup;
+
+	@Inject
+	private UserGroupLocalService _userGroupLocalService;
+
+	@Inject
+	private UserProcessorFactory _userProcessorFactory;
+
+}
