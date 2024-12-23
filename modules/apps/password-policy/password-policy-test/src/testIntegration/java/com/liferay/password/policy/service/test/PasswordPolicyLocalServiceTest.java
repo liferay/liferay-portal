22dcb2d0132effcaaf99dbca87a436c1445560cb
ChrisKian <christopher.kian@liferay.com>
2024-12-23 10:25:23 -0800

    LPD-36309 Move test to correct module and package

--- modules/apps/password-policy/password-policy-test/src/testIntegration/java/com/liferay/password/policy/service/test/PasswordPolicyLocalServiceTest.java
+++ modules/apps/password-policy/password-policy-test/src/testIntegration/java/com/liferay/password/policy/service/test/PasswordPolicyLocalServiceTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.password.policies.admin.internal.exportimport.data.handler.test;
+package com.liferay.password.policy.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.lang.SafeCloseable;
