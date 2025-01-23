ead208910e6b632c74308f82904ca5691adcc118
Rafael Uen <rafael.uen@liferay.com>
2025-01-23 10:24:06 -0300

    LRSD-7973 SF

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/LMS/fragments/lms-lesson-display-page-layout/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/LMS/fragments/lms-lesson-display-page-layout/index.js
@@ -1,23 +1,38 @@
-//import "lms"
-const copyToClipboardButtons = document.querySelectorAll(".copy-to-clipboard-button");
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+const copyToClipboardButtons = document.querySelectorAll(
+	'.copy-to-clipboard-button'
+);
 
 copyToClipboardButtons.forEach((button) => {
-  button.addEventListener("click", function (event) {
-    const codeToolbar = button.closest('.code-toolbar');
+	button.addEventListener('click', () => {
+		const codeToolbar = button.closest('.code-toolbar');
 
-    if (codeToolbar) {
-      const codeText = codeToolbar.querySelector('code.language-bash').innerText;
+		if (codeToolbar) {
+			const codeText =
+				codeToolbar.querySelector('code.language-bash').innerText;
 
-      if (codeText) {
-        navigator.clipboard.writeText(codeText)
-          .then(function () {
-            button.setAttribute('data-copy-state', 'copy-success');
+			if (codeText) {
+				navigator.clipboard
+					.writeText(codeText)
+					.then(() => {
+						button.setAttribute('data-copy-state', 'copy-success');
 
-            setTimeout(function () {
-              button.setAttribute('data-copy-state', 'copy');
-            }, 3000);
-          });
-      }
-    }
-  });
+						setTimeout(() => {
+							button.setAttribute('data-copy-state', 'copy');
+						}, 3000);
+					})
+					.catch(() => {
+						button.setAttribute('data-copy-state', 'copy-failure');
+
+						setTimeout(() => {
+							button.setAttribute('data-copy-state', 'copy');
+						}, 3000);
+					});
+			}
+		}
+	});
 });
