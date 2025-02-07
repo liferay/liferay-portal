d345adb1197bfdbf19059969ffb215fda3119b87
Pedro Tavares <pedro.tavares@liferay.com>
2025-02-07 14:17:40 -0300

    LPD-41877 Create ObjectDefinitionSettingConstants

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/constants/ObjectDefinitionSettingConstants.java
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.constants;
+
+/**
+ * @author Pedro Tavares
+ */
+public class ObjectDefinitionSettingConstants {
+
+	public static final String NAME_ACCEPT_ALL_GROUPS = "acceptAllGroups";
+
+	public static final String NAME_ACCEPTED_GROUP_ERCS = "acceptedGroupERCs";
+
+	public static final String NAME_ACCEPTED_GROUP_IDS = "acceptedGroupIds";
+
+}
