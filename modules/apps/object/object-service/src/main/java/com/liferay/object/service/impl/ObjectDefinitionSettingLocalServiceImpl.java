53fbef46154be0b6f90de4a153f1408e2b45f57f
Pedro Tavares <pedro.tavares@liferay.com>
2025-02-10 14:51:41 -0300

    LPD-41877 Create method to add object definition setting

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionSettingLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionSettingLocalServiceImpl.java
@@ -7,11 +7,16 @@ package com.liferay.object.service.impl;
 
 import com.liferay.object.model.ObjectDefinitionSetting;
 import com.liferay.object.service.base.ObjectDefinitionSettingLocalServiceBaseImpl;
+import com.liferay.object.service.persistence.ObjectDefinitionPersistence;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
@@ -24,11 +29,42 @@ public class ObjectDefinitionSettingLocalServiceImpl
 	extends ObjectDefinitionSettingLocalServiceBaseImpl {
 
 	@Override
+	public ObjectDefinitionSetting addObjectDefinitionSetting(
+			long userId, long objectDefinitionId, String name, String value)
+		throws PortalException {
+
+		_objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
+
+		ObjectDefinitionSetting objectDefinitionSetting =
+			objectDefinitionSettingPersistence.create(
+				counterLocalService.increment());
+
+		User user = _userLocalService.getUser(userId);
+
+		objectDefinitionSetting.setCompanyId(user.getCompanyId());
+		objectDefinitionSetting.setUserId(user.getUserId());
+		objectDefinitionSetting.setUserName(user.getFullName());
+
+		objectDefinitionSetting.setObjectDefinitionId(objectDefinitionId);
+		objectDefinitionSetting.setName(name);
+		objectDefinitionSetting.setValue(value);
+
+		return objectDefinitionSettingPersistence.update(
+			objectDefinitionSetting);
+	}
+
+	@Override
 	public List<ObjectDefinitionSetting> getObjectDefinitionSettings(
 		long objectDefinitionId) {
 
 		return objectDefinitionSettingPersistence.findByObjectDefinitionId(
 			objectDefinitionId);
 	}
+
+	@Reference
+	private ObjectDefinitionPersistence _objectDefinitionPersistence;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
