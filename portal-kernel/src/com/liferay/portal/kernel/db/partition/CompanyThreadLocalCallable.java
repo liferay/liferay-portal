7dfe48679c15361dac69de715f7bab6e77777b41
IstvanD <istvan.dezsi@liferay.com>
2025-02-14 14:36:38 +0100

    LPD-47689 Introducing CompanyThreadLocalCallable and CompanyThreadLocalRunnable to propagate companyId to child thread

---
+++ portal-kernel/src/com/liferay/portal/kernel/db/partition/CompanyThreadLocalCallable.java
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.db.partition;
+
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author István András Dézsi
+ */
+public class CompanyThreadLocalCallable<T> implements Callable<T> {
+
+	public CompanyThreadLocalCallable(Callable<T> callable) {
+		_callable = callable;
+
+		_companyId = CompanyThreadLocal.getCompanyId();
+	}
+
+	@Override
+	public T call() throws Exception {
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(_companyId)) {
+
+			return _callable.call();
+		}
+	}
+
+	private final Callable<T> _callable;
+	private final Long _companyId;
+
+}
