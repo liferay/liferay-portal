6e958de291221267ab24b5633dc11d75affc6d3a
shuyangzhou <shuyang.zhou@liferay.com>
2025-02-14 18:20:32 -0800

    LPD-X Update module jspc support

--- modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
+++ modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
@@ -78,14 +78,14 @@ public class JspCPlugin implements Plugin<Project> {
 
 	private void _addDependenciesJspC(Project project) {
 		GradleUtil.addDependency(
-			project, CONFIGURATION_NAME, "javax.servlet.jsp.jstl",
-			"javax.servlet.jsp.jstl-api", "1.2.1");
+			project, CONFIGURATION_NAME, "jakarta.servlet.jsp.jstl",
+			"jakarta.servlet.jsp.jstl-api", "3.0.2");
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "org.apache.tomcat", "tomcat-jasper",
-			"9.0.102");
+			"10.1.39");
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "org.glassfish.web",
-			"javax.servlet.jsp.jstl", "1.2.3", false);
+			"jakarta.servlet.jsp.jstl", "3.0.1", false);
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "org.osgi", "osgi.core", "6.0.0");
 
