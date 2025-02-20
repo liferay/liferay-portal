07cc454be6a4d653c189cfda953bc88ce8ae98e0
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:33 +0100

    LPD-47925 @liferay-headless - modules/apps/staging/staging-processes-web

--- modules/apps/staging/staging-processes-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/staging/staging-processes-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,11 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {
-	getCheckedCheckboxes,
-	openConfirmModal,
-	postForm,
-} from 'frontend-js-web';
+import {openConfirmModal} from 'frontend-js-components-web';
+import {getCheckedCheckboxes, postForm} from 'frontend-js-web';
 
 export function StagingProcessesWebToolbarPropsTransformer({
 	portletNamespace,
