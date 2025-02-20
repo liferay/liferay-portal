0ef395859f8c1a1e3c59fd055fcf2fc806f3983b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:30 +0100

    LPD-47925 @liferay-headless - modules/apps/dispatch/dispatch-web

--- modules/apps/dispatch/dispatch-web/src/main/resources/META-INF/resources/js/DispatchTriggerManagementToolbarPropsTransformer.js
+++ modules/apps/dispatch/dispatch-web/src/main/resources/META-INF/resources/js/DispatchTriggerManagementToolbarPropsTransformer.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openConfirmModal} from 'frontend-js-web';
+import {openConfirmModal} from 'frontend-js-components-web';
 
 export default function propsTransformer({
 	additionalProps: {deleteEntriesURL, inputId, inputValue},
