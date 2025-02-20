5651d2fce8b0a743cc024eee581036fe532c44f2
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:24 +0100

    LPD-47925 @liferay-database-infra - modules/apps/portal-instances/portal-instances-web

--- modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/AddInstance.js
+++ modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/AddInstance.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {fetch, getOpener, openToast} from 'frontend-js-web';
+import {openToast} from 'frontend-js-components-web';
+import {fetch, getOpener} from 'frontend-js-web';
 
 export default function ({namespace}) {
 	const form = document.getElementById(`${namespace}fm`);
--- modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/openDeleteCompanyModal.js
+++ modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/openDeleteCompanyModal.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openModal, sub} from 'frontend-js-web';
+import {openModal} from 'frontend-js-components-web';
+import {sub} from 'frontend-js-web';
 
 export default function openDeleteAssetEntryListModal({onDelete}) {
 	openModal({
