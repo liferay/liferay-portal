753f47c8613b03f8171322f106dc3d323397265a
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-02-25 06:46:56 +0100

    LPD-49417 Move to suitable package

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/validator/LayoutValidator.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/validator/LayoutValidator.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.layout.util.validator;
+package com.liferay.layout.validator;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
