87c24a519b493ddb908ad06427a8ac86d8896a75
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-02-25 23:28:19 -0300

    LPD-49213 Just renaming files from js to ts

--- modules/apps/analytics/analytics-client-js/src/queues/identityMessageQueue.ts
+++ modules/apps/analytics/analytics-client-js/src/queues/identityMessageQueue.ts
@@ -3,11 +3,18 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import Analytics from '../analytics';
 import {STORAGE_KEY_MESSAGE_IDENTITY} from '../utils/constants';
 import BaseSendMessageQueue from './baseSendMessageQueue';
 
 class IdentityMessageQueue extends BaseSendMessageQueue {
-	constructor({analyticsInstance, name = STORAGE_KEY_MESSAGE_IDENTITY}) {
+	constructor({
+		analyticsInstance,
+		name = STORAGE_KEY_MESSAGE_IDENTITY,
+	}: {
+		analyticsInstance: Analytics;
+		name?: string;
+	}) {
 		super({
 			analyticsInstance,
 			flushTo: analyticsInstance.config.identityEndpoint,
