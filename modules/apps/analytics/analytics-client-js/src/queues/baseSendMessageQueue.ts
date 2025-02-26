287c36bc37ddb3cb059bd6304ac93d6efb45f13b
Adriano Interaminense <adriano.interaminense@gmail.com>
2025-02-25 23:28:51 -0300

    LPD-49213 Convert baseSendMessageQueue to typescript

--- modules/apps/analytics/analytics-client-js/src/queues/baseSendMessageQueue.ts
+++ modules/apps/analytics/analytics-client-js/src/queues/baseSendMessageQueue.ts
@@ -3,11 +3,22 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import Analytics from '../analytics';
 import ClientAdapter from '../clientAdapter';
 import BaseQueue from './baseQueue';
 
 class BaseSendMessageQueue extends BaseQueue {
-	constructor({analyticsInstance, flushTo, name}) {
+	clientAdapter: ClientAdapter;
+
+	constructor({
+		analyticsInstance,
+		flushTo,
+		name,
+	}: {
+		analyticsInstance: Analytics;
+		flushTo: string;
+		name: string;
+	}) {
 		super({analyticsInstance, name});
 
 		this.clientAdapter = new ClientAdapter({
@@ -17,13 +28,13 @@ class BaseSendMessageQueue extends BaseQueue {
 	}
 
 	onFlush() {
-		return this.getItems().map((message) =>
+		return this.getItems().map((message: {id: string}) =>
 			this.clientAdapter
 				.sendWithTimeout(message)
 				.then(() => {
 					this._dequeue(message.id);
 				})
-				.catch((error) => {
+				.catch((error: {status: number}) => {
 					if (error.status === 400) {
 						this._dequeue(message.id);
 					}
