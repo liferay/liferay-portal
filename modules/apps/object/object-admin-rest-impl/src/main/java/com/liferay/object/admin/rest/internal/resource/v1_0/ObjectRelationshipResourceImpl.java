b9ddfb8c0a8ab0f6cad18fca6f1f1f7c4c6f7eed
Carlos Correa <carlos.correa@liferay.com>
2025-03-12 18:12:55 +0100

    Revert "LPD-49994 Skip reindex to avoid the conflicts of indexes during batch import, this default object definition is actually a place holder of the real object definition which should be populated later in the same batch import, thus it is safe to skip indexing it as a index will be triggered when the real object definition populated. The long term fix would be to restructure the order of importing relationship to avoid populating this default object definition as a place holder."
    This reverts commit 86ea1da681c812b364139098a1c8236e9e20d297.

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
@@ -23,7 +23,6 @@ import com.liferay.object.service.ObjectFolderLocalService;
 import com.liferay.object.service.ObjectRelationshipService;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.IndexStatusManagerThreadLocal;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -325,26 +324,16 @@ public class ObjectRelationshipResourceImpl
 			_objectFolderLocalService.getOrAddDefaultObjectFolder(
 				contextCompany.getCompanyId());
 
-		boolean indexReadOnly = IndexStatusManagerThreadLocal.isIndexReadOnly();
-
-		IndexStatusManagerThreadLocal.setIndexReadOnly(true);
-
-		try {
-			return _objectDefinitionLocalService.addObjectDefinition(
-				objectRelationship.getObjectDefinitionExternalReferenceCode2(),
-				contextUser.getUserId(),
-				defaultObjectFolder.getObjectFolderId(),
-				GetterUtil.get(
-					objectRelationship.getObjectDefinitionModifiable2(), true),
-				GetterUtil.get(
-					objectRelationship.getObjectDefinitionScope2(),
-					ObjectDefinitionConstants.SCOPE_COMPANY),
-				GetterUtil.get(
-					objectRelationship.getObjectDefinitionSystem2(), false));
-		}
-		finally {
-			IndexStatusManagerThreadLocal.setIndexReadOnly(indexReadOnly);
-		}
+		return _objectDefinitionLocalService.addObjectDefinition(
+			objectRelationship.getObjectDefinitionExternalReferenceCode2(),
+			contextUser.getUserId(), defaultObjectFolder.getObjectFolderId(),
+			GetterUtil.get(
+				objectRelationship.getObjectDefinitionModifiable2(), true),
+			GetterUtil.get(
+				objectRelationship.getObjectDefinitionScope2(),
+				ObjectDefinitionConstants.SCOPE_COMPANY),
+			GetterUtil.get(
+				objectRelationship.getObjectDefinitionSystem2(), false));
 	}
 
 	private ObjectRelationship _toObjectRelationship(
