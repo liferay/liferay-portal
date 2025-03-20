ded394f9b46775e084be9d11526b7fb24e7dfa5f
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2025-03-20 18:28:05 -0300

    LPD-51511 Use containerId to validate whether to send the default language id or the editing one

--- modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form/FormView.es.js
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form/FormView.es.js
@@ -255,6 +255,7 @@ const usePublicAPI = ({apiRef, containerRef, unstable_onEventRef}) => {
 			dispatch(
 				formValidate({
 					activePage,
+					containerId,
 					defaultLanguageId,
 					editingLanguageId,
 					formId: containerRef.current
