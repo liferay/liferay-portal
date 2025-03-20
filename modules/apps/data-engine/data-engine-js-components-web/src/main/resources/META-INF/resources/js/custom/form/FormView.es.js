2f5d0a9bb3406fc172acb6c0ae0aa62ebfae09ab
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2025-03-20 18:29:47 -0300

    LPD-51511 Create function to call the locales dropdown dispatch and call it

--- modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form/FormView.es.js
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/custom/form/FormView.es.js
@@ -16,6 +16,7 @@ import React, {
 import {DndProvider} from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
 
+import {EVENT_TYPES} from '../../core/actions/eventTypes.es';
 import Pages from '../../core/components/Pages.es';
 import {INITIAL_CONFIG_STATE} from '../../core/config/initialConfigState.es';
 import {INITIAL_STATE} from '../../core/config/initialState.es';
@@ -283,6 +284,20 @@ const usePublicAPI = ({apiRef, containerRef, unstable_onEventRef}) => {
 		]
 	);
 
+	/**
+	 * Switches the LocalesDropdown back to the default language id.
+	 * This is necessary within objects entries context since the
+	 * entry is only required in the default locale.
+	 */
+
+	const updateLocalesDropdownToDefaultLanguage = () =>
+		dispatch({
+			payload: {
+				editingLanguageId: defaultLanguageId,
+			},
+			type: EVENT_TYPES.LANGUAGE.LOCALES_DROPDOWN_CHANGE,
+		});
+
 	useEffect(() => {
 		Liferay.component(
 			containerId,
@@ -377,6 +392,7 @@ const usePublicAPI = ({apiRef, containerRef, unstable_onEventRef}) => {
 					readOnly,
 				})
 			),
+		updateLocalesDropdownToDefaultLanguage,
 		validate,
 	}));
 };
