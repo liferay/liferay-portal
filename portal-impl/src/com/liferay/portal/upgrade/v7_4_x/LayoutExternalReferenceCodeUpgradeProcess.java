25d02827cb0353251bc9c90cbbcec519c0f9f765
Javier Moral <javier.moral@liferay.com>
2025-03-27 16:39:49 +0100

    LPD-52199 As uuids might not be unique for a site, make sure that we are generating unique ERCs

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/LayoutExternalReferenceCodeUpgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/LayoutExternalReferenceCodeUpgradeProcess.java
@@ -5,17 +5,63 @@
 
 package com.liferay.portal.upgrade.v7_4_x;
 
-import com.liferay.portal.kernel.upgrade.BaseExternalReferenceCodeUpgradeProcess;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+import com.liferay.portal.model.impl.LayoutModelImpl;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Rubén Pulido
  */
-public class LayoutExternalReferenceCodeUpgradeProcess
-	extends BaseExternalReferenceCodeUpgradeProcess {
+public class LayoutExternalReferenceCodeUpgradeProcess extends UpgradeProcess {
 
 	@Override
-	protected String[][] getTableAndPrimaryKeyColumnNames() {
-		return new String[][] {{"Layout", "plid"}};
+	protected void doUpgrade() throws Exception {
+		if (!hasColumn(LayoutModelImpl.TABLE_NAME, "externalReferenceCode")) {
+			alterTableAddColumn(
+				LayoutModelImpl.TABLE_NAME, "externalReferenceCode",
+				"VARCHAR(75)");
+		}
+
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			StringBundler selectSB = new StringBundler(4);
+
+			selectSB.append("select plid from ");
+			selectSB.append(LayoutModelImpl.TABLE_NAME);
+			selectSB.append(" where externalReferenceCode is null or ");
+			selectSB.append("externalReferenceCode = ''");
+
+			StringBundler updateSB = new StringBundler(3);
+
+			updateSB.append("update ");
+			updateSB.append(LayoutModelImpl.TABLE_NAME);
+			updateSB.append(" set externalReferenceCode = ? where plid = ?");
+
+			try (PreparedStatement preparedStatement1 =
+					connection.prepareStatement(selectSB.toString());
+				ResultSet resultSet = preparedStatement1.executeQuery();
+				PreparedStatement preparedStatement2 =
+					AutoBatchPreparedStatementUtil.autoBatch(
+						connection, updateSB.toString())) {
+
+				while (resultSet.next()) {
+					long plid = resultSet.getLong(1);
+
+					preparedStatement2.setString(1, PortalUUIDUtil.generate());
+
+					preparedStatement2.setLong(2, plid);
+
+					preparedStatement2.addBatch();
+				}
+
+				preparedStatement2.executeBatch();
+			}
+		}
 	}
 
 }
