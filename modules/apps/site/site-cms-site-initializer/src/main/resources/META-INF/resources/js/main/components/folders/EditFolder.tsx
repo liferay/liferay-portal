cd04af5eb277f3e074ac45ce56da3d9dec590e92
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-04-22 18:42:06 +0200

    LPD-47341 Use a ClayToolbar as a header

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/folders/EditFolder.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/folders/EditFolder.tsx
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayToolbar from '@clayui/toolbar';
 import React from 'react';
 
 interface EditFolderProps {
@@ -13,11 +15,47 @@ interface EditFolderProps {
 
 const EditFolder: React.FC<EditFolderProps> = ({description, name, space}) => {
 	return (
-		<h1>
-			{' '}
+		<div className="edit-folder">
+			<ClayToolbar className="container-fluid" light>
+				<ClayToolbar.Nav>
+					<ClayToolbar.Item>
+						<ClayButtonWithIcon
+							aria-label={Liferay.Language.get('back')}
+							borderless
+							displayType="secondary"
+							monospaced
+							size="sm"
+							symbol="angle-left"
+							title={Liferay.Language.get('back')}
+						/>
+					</ClayToolbar.Item>
 
-			Edit Folder Page {name} {description} {space}{' '}
-		</h1>
+					<ClayToolbar.Item className="text-left" expand>
+						<h2 className="font-weight-semi-bold m-0 text-5">
+							{name}
+						</h2>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayButton
+							borderless
+							displayType="secondary"
+							size="sm"
+						>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+					</ClayToolbar.Item>
+
+					<ClayToolbar.Item>
+						<ClayButton displayType="primary" size="sm">
+							{Liferay.Language.get('save')}
+						</ClayButton>
+					</ClayToolbar.Item>
+				</ClayToolbar.Nav>
+			</ClayToolbar>
+
+			Edit Folder Page {name} {description} {space}
+		</div>
 	);
 };
 
