649726500714b7324955b02727e948ea7ffe9c1e
Roberto Díaz <roberto.diaz@liferay.com>
2025-05-13 13:09:25 +0200

    LPD-55576 Create dummy react component for the New Space page

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -27,6 +27,7 @@ export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
 export {default as SpaceSticker} from './main/components/SpaceSticker';
 export {default as EditFolder} from './main/components/folders/EditFolder';
+export {default as NewSpace} from './main/components/spaces/NewSpace';
 export {default as ViewDashboard} from './main/dashboard/ViewDashboard';
 export {default as SpacesNavigation} from './main/spaces_navigation/SpacesNavigation';
 
