ec32bc98d1041032844ec87e560637efd95b3eb9
Roberto Díaz <roberto.diaz@liferay.com>
2025-05-13 13:05:08 +0200

    LPD-55576 Create fragment renderer for New Space page

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/NewSpaceFragmentRenderer.java
@@ -0,0 +1,83 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
+
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.frontend.taglib.react.servlet.taglib.ComponentTag;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.taglib.servlet.PageContextFactoryUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = FragmentRenderer.class)
+public class NewSpaceFragmentRenderer extends BaseSectionFragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "sections";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "new-space");
+	}
+
+	@Override
+	public void render(
+			FragmentRendererContext fragmentRendererContext,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		try {
+			PrintWriter printWriter = httpServletResponse.getWriter();
+
+			printWriter.write("<div><span aria-hidden=\"true\" class=\"");
+			printWriter.write("loading-animation\"></span>");
+
+			ComponentTag componentTag = new ComponentTag();
+
+			componentTag.setModule("{NewSpace} from site-cms-site-initializer");
+			componentTag.setPageContext(
+				PageContextFactoryUtil.create(
+					httpServletRequest, httpServletResponse));
+
+			componentTag.setServletContext(_servletContext);
+
+			componentTag.doStartTag();
+
+			componentTag.doEndTag();
+
+			printWriter.write("</div>");
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	@Reference
+	private Language _language;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.cms.site.initializer)"
+	)
+	private ServletContext _servletContext;
+
+}
