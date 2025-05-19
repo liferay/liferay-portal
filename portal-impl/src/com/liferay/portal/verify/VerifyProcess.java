b992929613fea986b3dc764fbc25077881eb0275
Jorge Díaz <jorge.diaz@liferay.com>
2025-05-19 10:58:36 +0200

    LPS-1 Update wrong javadoc in VerifyProcess

--- portal-impl/src/com/liferay/portal/verify/VerifyProcess.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProcess.java
@@ -30,10 +30,7 @@ import java.util.regex.Pattern;
 
 /**
  * This abstract class should be extended for startup processes that verify the
- * integrity of the database. They can be added as part of
- * <code>VerifyProcessSuite</code> or be executed independently by being set in
- * the portal.properties file. Each of these processes should not cause any
- * problems if run multiple times.
+ * integrity of the database.
  *
  * @author Alexander Chow
  * @author Hugo Huijser
