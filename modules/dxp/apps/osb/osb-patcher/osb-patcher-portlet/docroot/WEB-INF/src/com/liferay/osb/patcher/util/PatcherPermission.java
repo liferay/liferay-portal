4e64b81a60c35e29f5faf7f6069dfafb4bc1a230
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-05-20 18:40:05 +0200

    LPD-57803 Copyright

--- modules/dxp/apps/osb/osb-patcher/osb-patcher-portlet/docroot/WEB-INF/src/com/liferay/osb/patcher/util/PatcherPermission.java
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-portlet/docroot/WEB-INF/src/com/liferay/osb/patcher/util/PatcherPermission.java
@@ -1,15 +1,6 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.osb.patcher.util;
