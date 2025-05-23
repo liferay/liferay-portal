07388d931f86c54ea8ccf3560da7b01ac820bdf1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-05-23 10:31:27 +0200

    LPD-57803 Patcher Fix Packs - Create new finders and methods in service impl class

--- modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherFixPackLocalServiceImpl.java
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherFixPackLocalServiceImpl.java
@@ -5,8 +5,14 @@
 
 package com.liferay.osb.patcher.service.impl;
 
+import com.liferay.osb.patcher.model.PatcherFixPack;
 import com.liferay.osb.patcher.service.base.PatcherFixPackLocalServiceBaseImpl;
+import com.liferay.osb.patcher.util.comparator.PatcherFixPackVersionComparator;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,4 +25,78 @@ import org.osgi.service.component.annotations.Component;
 )
 public class PatcherFixPackLocalServiceImpl
 	extends PatcherFixPackLocalServiceBaseImpl {
+
+	@Override
+	public PatcherFixPack fetchPatcherFixPack(
+		long patcherProjectVersionId, String name) {
+
+		return patcherFixPackPersistence.fetchByPFCI_N(
+			patcherProjectVersionId, name);
+	}
+
+	@Override
+	public PatcherFixPack fetchPatcherFixPackByPatcherBuildId(
+		long patcherBuildId) {
+
+		return patcherFixPackPersistence.fetchByPatcherBuildId(patcherBuildId);
+	}
+
+	@Override
+	public PatcherFixPack getPatcherFixPack(
+			long patcherProjectVersionId, String name)
+		throws PortalException {
+
+		return patcherFixPackPersistence.findByPFCI_N(
+			patcherProjectVersionId, name);
+	}
+
+	@Override
+	public PatcherFixPack getPatcherFixPackByPatcherBuildId(long patcherBuildId)
+		throws PortalException {
+
+		return patcherFixPackPersistence.findByPatcherBuildId(patcherBuildId);
+	}
+
+	@Override
+	public List<PatcherFixPack> getPatcherFixPacks(
+		long patcherFixComponentId, int version) {
+
+		return patcherFixPackPersistence.findByPFCI_V(
+			patcherFixComponentId, version);
+	}
+
+	@Override
+	public List<PatcherFixPack> getPatcherFixPacks(
+		long patcherFixComponentId, long patcherProjectVersionId, int version,
+		boolean older) {
+
+		if (older) {
+			return patcherFixPackPersistence.findByPFCI_PPVI_LtV(
+				patcherFixComponentId, patcherProjectVersionId, version,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				PatcherFixPackVersionComparator.getInstance(false));
+		}
+
+		return patcherFixPackPersistence.findByPFCI_PPVI_GtV(
+			patcherFixComponentId, patcherProjectVersionId, version,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+			PatcherFixPackVersionComparator.getInstance(true));
+	}
+
+	@Override
+	public List<PatcherFixPack> getPatcherFixPacksByPatcherFixComponentId(
+		long patcherFixComponentId) {
+
+		return patcherFixPackPersistence.findByPatcherFixComponentId(
+			patcherFixComponentId);
+	}
+
+	@Override
+	public List<PatcherFixPack> getPatcherFixPacksByStatus(
+		long patcherProjectVersionId, int status) {
+
+		return patcherFixPackPersistence.findByPFCI_S(
+			patcherProjectVersionId, status);
+	}
+
 }
