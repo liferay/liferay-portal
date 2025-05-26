c04ace1e620d8ff70c3e3c777ebeb4ba6d35d20a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-05-26 12:33:17 +0200

    LPD-57803 Patcher Fixes - Create new finders and methods in service impl class

--- modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherFixLocalServiceImpl.java
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherFixLocalServiceImpl.java
@@ -5,8 +5,15 @@
 
 package com.liferay.osb.patcher.service.impl;
 
+import com.liferay.osb.patcher.model.PatcherFix;
 import com.liferay.osb.patcher.service.base.PatcherFixLocalServiceBaseImpl;
+import com.liferay.osb.patcher.util.comparator.PatcherFixKeyVersionComparator;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.Date;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -18,4 +25,73 @@ import org.osgi.service.component.annotations.Component;
 	service = AopService.class
 )
 public class PatcherFixLocalServiceImpl extends PatcherFixLocalServiceBaseImpl {
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		Date modifiedDate, int[] type, boolean notified, int status) {
+
+		return patcherFixPersistence.findByLtM_T_N_S(
+			modifiedDate, type, notified, status);
+	}
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		long patcherProjectVersionId, int type, boolean latestFix) {
+
+		return patcherFixPersistence.findByP_T_L(
+			patcherProjectVersionId, type, latestFix);
+	}
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		long patcherProjectVersionId, int type, boolean latestFix, int status) {
+
+		if (status == WorkflowConstants.STATUS_ANY) {
+			return patcherFixPersistence.findByP_NotT_L(
+				patcherProjectVersionId, type, latestFix);
+		}
+
+		return patcherFixPersistence.findByP_NotT_L_S(
+			patcherProjectVersionId, type, latestFix, status);
+	}
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		long patcherProjectVersionId, String name, int type,
+		boolean latestFix) {
+
+		return patcherFixPersistence.findByP_N_NotT_L(
+			patcherProjectVersionId, name, type, latestFix);
+	}
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		String key, double keyVersion, int type, boolean older) {
+
+		if (older) {
+			return patcherFixPersistence.findByK_LtKV_NotT(
+				key, keyVersion, type, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				PatcherFixKeyVersionComparator.getInstance(false));
+		}
+
+		return patcherFixPersistence.findByK_GtKV_NotT(
+			key, keyVersion, type, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+			PatcherFixKeyVersionComparator.getInstance(true));
+	}
+
+	@Override
+	public List<PatcherFix> getPatcherFixes(
+		String key, int type, boolean latestFix) {
+
+		return patcherFixPersistence.findByK_NotT_L(key, type, latestFix);
+	}
+
+	@Override
+	public int getPatcherFixesCountByPatcherProjectVersionId(
+		long patcherProjectVersionId) {
+
+		return patcherFixPersistence.countByPatcherProjectVersionId(
+			patcherProjectVersionId);
+	}
+
 }
