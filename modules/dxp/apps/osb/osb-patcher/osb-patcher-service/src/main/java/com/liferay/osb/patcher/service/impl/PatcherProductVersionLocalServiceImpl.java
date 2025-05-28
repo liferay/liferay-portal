472b14c88cafe82554053b2bd4622a44899ac31d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-05-28 15:37:05 +0100

    LPD-57803 Patcher Product Versions - Adds new service methods to add, update, delete, ... entities

--- modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherProductVersionLocalServiceImpl.java
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/service/impl/PatcherProductVersionLocalServiceImpl.java
@@ -5,15 +5,24 @@
 
 package com.liferay.osb.patcher.service.impl;
 
+import com.liferay.osb.patcher.constants.PatcherProductVersionConstants;
 import com.liferay.osb.patcher.model.PatcherProductVersion;
 import com.liferay.osb.patcher.service.base.PatcherProductVersionLocalServiceBaseImpl;
 import com.liferay.osb.patcher.util.comparator.PatcherProductVersionNameComparator;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.util.Date;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -24,6 +33,35 @@ import org.osgi.service.component.annotations.Component;
 )
 public class PatcherProductVersionLocalServiceImpl
 	extends PatcherProductVersionLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public PatcherProductVersion addPatcherProductVersion(
+			long userId, String name, int fixDeliveryMethod,
+			String moduleFolderName)
+		throws PortalException {
+
+		_validateDuplicateName(0, name);
+		_validateFixDeliveryMethod(fixDeliveryMethod, moduleFolderName);
+
+		PatcherProductVersion patcherProductVersion =
+			patcherProductVersionPersistence.create(
+				counterLocalService.increment());
+
+		User user = _userLocalService.getUser(userId);
+
+		patcherProductVersion.setCompanyId(user.getCompanyId());
+		patcherProductVersion.setUserId(user.getUserId());
+		patcherProductVersion.setUserName(user.getFullName());
+
+		patcherProductVersion.setCreateDate(new Date());
+		patcherProductVersion.setModifiedDate(new Date());
+		patcherProductVersion.setName(name);
+		patcherProductVersion.setFixDeliveryMethod(fixDeliveryMethod);
+		patcherProductVersion.setModuleFolderName(moduleFolderName);
+
+		return patcherProductVersionPersistence.update(patcherProductVersion);
+	}
 
 	@Override
 	public PatcherProductVersion fetchPatcherProductVersion(String name) {
@@ -44,5 +82,67 @@ public class PatcherProductVersionLocalServiceImpl
 		return patcherProductVersionPersistence.findByFixDeliveryMethod(
 			fixDeliveryMethod);
 	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public PatcherProductVersion updatePatcherProductVersion(
+			long patcherProductVersionId, String name, int fixDeliveryMethod,
+			String moduleFolderName)
+		throws PortalException {
+
+		_validateDuplicateName(patcherProductVersionId, name);
+		_validateFixDeliveryMethod(fixDeliveryMethod, moduleFolderName);
+
+		PatcherProductVersion patcherProductVersion =
+			patcherProductVersionPersistence.findByPrimaryKey(
+				patcherProductVersionId);
+
+		patcherProductVersion.setModifiedDate(new Date());
+		patcherProductVersion.setName(name);
+		patcherProductVersion.setFixDeliveryMethod(fixDeliveryMethod);
+		patcherProductVersion.setModuleFolderName(moduleFolderName);
+
+		return patcherProductVersionPersistence.update(patcherProductVersion);
+	}
+
+	private void _validateDuplicateName(
+			long patcherProductVersionId, String name)
+		throws PortalException {
+
+		if (Validator.isNull(name)) {
+			throw new PortalException("the-product-version-name-is-invalid");
+		}
+
+		PatcherProductVersion patcherProductVersion =
+			patcherProductVersionPersistence.fetchByName(name);
+
+		if ((patcherProductVersion != null) &&
+			(patcherProductVersion.getPatcherProductVersionId() !=
+				patcherProductVersionId)) {
+
+			throw new PortalException(
+				"the-product-version-name-already-exists");
+		}
+	}
+
+	private void _validateFixDeliveryMethod(
+			int fixDeliveryMethod, String moduleFolderName)
+		throws PortalException {
+
+		if (fixDeliveryMethod < 0) {
+			throw new PortalException("the-fix-delivery-method-is-invalid");
+		}
+		else if ((fixDeliveryMethod ==
+					PatcherProductVersionConstants.
+						TYPE_FIX_DELIVERY_METHOD_MARKETPLACE_RELEASE) &&
+				 Validator.isNull(moduleFolderName)) {
+
+			throw new PortalException(
+				"the-module-folder-name-is-required-for-marketplace-apps");
+		}
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
