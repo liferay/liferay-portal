74aee131528f542bd0e040d20de7784bde130da1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-05-30 12:12:18 +0100

    LPD-57803 Patcher Project Versions - Adds necessary MVCActionCommands and MVCRenderCommands

---
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-web/src/main/java/com/liferay/osb/patcher/web/internal/portlet/action/DeleteProjectVersionsMVCActionCommand.java
@@ -0,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.patcher.web.internal.portlet.action;
+
+import com.liferay.osb.patcher.constants.PatcherPortletKeys;
+import com.liferay.osb.patcher.service.PatcherProjectVersionLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import jakarta.portlet.ActionRequest;
+import jakarta.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {
+		"jakarta.portlet.name=" + PatcherPortletKeys.PATCHER,
+		"mvc.command.name=/patcher/delete_project_versions"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteProjectVersionsMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long patcherProjectVersionId = ParamUtil.getLong(
+			actionRequest, "patcherProjectVersionId");
+
+		_patcherProjectVersionLocalService.deletePatcherProjectVersion(
+			patcherProjectVersionId);
+	}
+
+	@Reference
+	private PatcherProjectVersionLocalService
+		_patcherProjectVersionLocalService;
+
+}
---
+++ modules/dxp/apps/osb/osb-patcher/osb-patcher-web/src/main/java/com/liferay/osb/patcher/web/internal/portlet/action/EditProjectVersionsMVCRenderCommand.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.patcher.web.internal.portlet.action;
+
+import com.liferay.osb.patcher.constants.PatcherPortletKeys;
+import com.liferay.osb.patcher.exception.NoSuchPatcherProjectVersionException;
+import com.liferay.osb.patcher.service.PatcherProjectVersionLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import jakarta.portlet.PortletException;
+import jakarta.portlet.RenderRequest;
+import jakarta.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = {
+		"jakarta.portlet.name=" + PatcherPortletKeys.PATCHER,
+		"mvc.command.name=/patcher/edit_project_versions"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditProjectVersionsMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		long patcherProjectVersionId = ParamUtil.getLong(
+			renderRequest, "patcherProjectVersionId");
+
+		try {
+			_patcherProjectVersionLocalService.getPatcherProjectVersion(
+				patcherProjectVersionId);
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchPatcherProjectVersionException) {
+				SessionErrors.add(renderRequest, exception.getClass());
+
+				return "/osb_patcher/views/error.jsp";
+			}
+
+			throw new PortletException(exception);
+		}
+
+		return "/osb_patcher/views/project_versions/edit.jsp";
+	}
+
+	@Reference
+	private PatcherProjectVersionLocalService
+		_patcherProjectVersionLocalService;
+
+}
