e546862c9bcca7fc91070ef4e426ffac48ed4b5e
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-06-09 09:29:44 +0200

    LPD-56399 Add event for keydown taking into account the enter and escape keys

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/getEditorProcessor.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/getEditorProcessor.ts
@@ -45,6 +45,11 @@ const INITIAL_STATE = {
 	eventHandlers: [],
 };
 
+const KEYCODES = {
+	ENTER: 13,
+	ESCAPE: 27,
+};
+
 export default function getEditorProcessor(
 	editorType: EditorType,
 	getEditorWrapper = defaultGetEditorWrapper,
@@ -109,6 +114,22 @@ export default function getEditorProcessor(
 			onBlurEditor(state.editor);
 		};
 
+		const onKeydownEditor = (
+			event: Event,
+			data: KeyboardEvent & {keyCode: number}
+		) => {
+			if (!state.editor) {
+				return;
+			}
+
+			if (data.keyCode === KEYCODES.ESCAPE) {
+				onBlurEditor(state.editor);
+			}
+			else if (data.keyCode === KEYCODES.ENTER) {
+				data.stopPropagation();
+			}
+		};
+
 		return [
 			{
 				callback: onClickOutside,
@@ -122,6 +143,11 @@ export default function getEditorProcessor(
 				callback: onFocusEditor,
 				emitter: state.editor.ui.focusTracker,
 				event: 'change:isFocused',
+			},
+			{
+				callback: onKeydownEditor,
+				emitter: state.editor.editing.view.document,
+				event: 'keydown',
 			},
 		];
 	};
