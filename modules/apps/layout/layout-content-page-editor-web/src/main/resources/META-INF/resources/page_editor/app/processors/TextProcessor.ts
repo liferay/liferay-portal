4d42301387c18a8a7e7249bed9d59e3df6284a83
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-06-11 12:52:37 +0200

    LPD-57528 Add feature flag to use getEditorProcessor in text editables

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/TextProcessor.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/TextProcessor.ts
@@ -8,9 +8,14 @@ import {isNullOrUndefined} from '@liferay/layout-js-components-web';
 import {EditableConfig} from '../../types/editables/EditableValue';
 import {getAlloyEditorProcessor} from '../js-index';
 import {getEditableLinkValue} from '../utils/getEditableLinkValue';
+import getEditorProcessor from './getEditorProcessor';
 import {getLinkableEditableEditorWrapper} from './getLinkableEditableEditorWrapper';
 
-export default getAlloyEditorProcessor(
+const processor = Liferay.FeatureFlags['LPD-11235']
+	? getEditorProcessor
+	: getAlloyEditorProcessor;
+
+export default processor(
 	'text',
 	getLinkableEditableEditorWrapper,
 	(
