cec70ff9ad596e58538076a64cca61e24c365672
agnosmelo <aguinus.santana@liferay.com>
2025-06-11 10:46:54 -0300

    LRSD-1334 Refactor confirmation message component

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/attachment-uploader/pages/AttachmentUploadConfirmation/AttachmentUploadConfirmation.tsx
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/attachment-uploader/pages/AttachmentUploadConfirmation/AttachmentUploadConfirmation.tsx
@@ -3,14 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayIcon from '@clayui/icon';
-import i18n from '~/utils/I18n';
-
-import './AttachmentUploadConfirmation.css';
-
 import {useLocation} from 'react-router-dom';
 import {useAppPropertiesContext} from '~/contexts/AppPropertiesContext';
+import i18n from '~/utils/I18n';
 import routerPath from '~/utils/routerPath';
+
+import {AttachmentMessages} from '../../components';
 
 const AttachmentUploadConfirmation = () => {
 	const {state} = useLocation();
@@ -18,48 +16,26 @@ const AttachmentUploadConfirmation = () => {
 	const {helpCenterURL} = useAppPropertiesContext();
 
 	return (
-		<div className="uploader-confirmation-container">
-			<div className="uploader-confirmation-box-containter">
-				<div className="d-flex justify-content-center pb-4">
-					<div className="uploader-icon">
-						<ClayIcon symbol="check-square" />
-					</div>
-				</div>
+		<AttachmentMessages
+			icon="check-square"
+			state={state}
+			subtitle="x-was-uploaded-successfully"
+			title="upload-confirmation"
+		>
+			<a
+				className="btn btn-secondary mr-2 uploader-secondary-button"
+				href={`${pageRoutes.project(state?.uploadAccountKey)}/attachments`}
+			>
+				{i18n.translate('go-to-attachments')}
+			</a>
 
-				<div className="d-flex justify-content-center pb-4 text-neutral-10">
-					<h3>{i18n.translate('upload-confirmation')}</h3>
-				</div>
-
-				<div className="d-flex justify-content-center pb-5">
-					<p
-						className="text-center"
-						dangerouslySetInnerHTML={{
-							__html: i18n.sub(`x-was-uploaded-successfully`, [
-								`<strong>${state?.attachmentName}</strong> <br>`,
-							]),
-						}}
-					/>
-				</div>
-
-				<div>
-					<div className="d-flex justify-content-center">
-						<a
-							className="btn btn-secondary mr-2 uploader-attachments-button"
-							href={`${pageRoutes.project(state?.uploadAccountKey)}/attachments`}
-						>
-							{i18n.translate('go-to-attachments')}
-						</a>
-
-						<a
-							className="btn btn-primary uploader-ticket-button"
-							href={`${helpCenterURL}/hc/requests/${state?.ticketId}`}
-						>
-							{i18n.translate('return-to-ticket')}
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+			<a
+				className="btn btn-primary uploader-primary-button"
+				href={`${helpCenterURL}/hc/requests/${state?.ticketId}`}
+			>
+				{i18n.translate('return-to-ticket')}
+			</a>
+		</AttachmentMessages>
 	);
 };
 
