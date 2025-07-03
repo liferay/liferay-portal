f946b0f20d935900dee6bcd3c21f52ccb0cf1b64
Luis Ortiz <luis.ortiz@liferay.com>
2025-07-03 17:07:32 +0200

    LPD-53131 Adding new preugrade data clean up process for cleaning entries with not existing companyId values. It is not strictly needed to have a separate class at this moment, but we will need it later for the system settings that will allow to execute the processes manually once the portal is started

---
+++ portal-impl/src/com/liferay/portal/upgrade/data/cleanup/CompanyIdDataCleanupPreupgradeProcess.java
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.data.cleanup;
+
+import com.liferay.portal.kernel.upgrade.data.cleanup.BaseOrphanReferencesDataCleanupPreupgradeProcess;
+
+/**
+ * @author Luis Ortiz
+ */
+public class CompanyIdDataCleanupPreupgradeProcess
+	extends BaseOrphanReferencesDataCleanupPreupgradeProcess {
+
+	public CompanyIdDataCleanupPreupgradeProcess() {
+		super("companyId", "Company");
+	}
+
+}
