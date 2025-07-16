4cd46df11d145c7420c25dd34a522c55bcc3209e
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-07-16 13:31:58 +0200

    LPD-60283 SF

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/constants/CMSSiteInitializerFDSNames.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/constants/CMSSiteInitializerFDSNames.java
@@ -36,12 +36,12 @@ public class CMSSiteInitializerFDSNames {
 	public static final String FILES_SECTION =
 		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME + "-filesSection";
 
+	public static final String SHARED_WITH_ME =
+		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME + "-sharedWithMe";
+
 	public static final String SPACE_CONTENTS_SUMMARY_SECTION =
 		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME +
 			"-spaceContentsSummarySection";
-
-	public static final String SHARED_WITH_ME =
-		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME + "-sharedWithMe";
 
 	public static final String SPACE_FILES_SUMMARY_SECTION =
 		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME +
