7ea47d6d17e0cb6895f11d8e676a3733311b6343
marcoscv-work <marcos.castro@liferay.com>
2025-08-13 12:09:10 +0200

    LPD-55626 - Remove Unused className

--- modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/code_mirror_keyboard_message/CodeMirrorKeyboardMessage.tsx
+++ modules/apps/frontend-js/frontend-js-components-web/src/main/resources/META-INF/resources/code_mirror_keyboard_message/CodeMirrorKeyboardMessage.tsx
@@ -3,28 +3,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import classNames from 'classnames';
 import {sub} from 'frontend-js-web';
 import React from 'react';
 
 import './CodeMirrorKeyboardMessage.scss';
 
 interface IProps {
-	className?: string;
 	keyIsEnabled: boolean;
 }
 
 export default function CodeMirrorKeyboardMessage({
-	className,
 	keyIsEnabled = false,
 }: IProps) {
 	return (
-		<div
-			className={classNames(
-				className,
-				'keyboard-message popover px-2 py-1'
-			)}
-		>
+		<div className="keyboard-message popover px-2 py-1">
 			<span className="c-kbd-sm">
 				{`${sub(
 					Liferay.Language.get('x-tab-key-using'),
