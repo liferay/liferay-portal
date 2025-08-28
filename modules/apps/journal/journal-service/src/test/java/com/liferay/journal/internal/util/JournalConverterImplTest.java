84000c4033a2a84ed907589cce335388130ec251
Chaitanya Sammetla <chaitanya.sammetla@liferay.com>
2025-08-28 15:19:18 +0530

    LPD-63825 Add Test

--- modules/apps/journal/journal-service/src/test/java/com/liferay/journal/internal/util/JournalConverterImplTest.java
+++ modules/apps/journal/journal-service/src/test/java/com/liferay/journal/internal/util/JournalConverterImplTest.java
@@ -6,10 +6,12 @@
 package com.liferay.journal.internal.util;
 
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
 import com.liferay.dynamic.data.mapping.storage.Field;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
@@ -37,6 +39,8 @@ public class JournalConverterImplTest {
 		String value = RandomTestUtil.randomString();
 
 		_testUpdateContentDynamicElement(value, value);
+
+		_testUpdateContentDynamicElementWithOptions();
 	}
 
 	private DDMFormField _createDDMFormField(
@@ -73,6 +77,55 @@ public class JournalConverterImplTest {
 			0, ddmFormField, rootElement, field);
 
 		Assert.assertEquals(expectedValue, rootElement.getStringValue());
+	}
+
+	private void _testUpdateContentDynamicElementWithOptions() {
+		DDMFormField ddmFormField = _createDDMFormField(
+			"string", true, "field2", "text");
+
+		DDMFormFieldOptions ddmFormFieldOptions = new DDMFormFieldOptions();
+
+		String optionLabel1 = RandomTestUtil.randomString();
+		String optionReference1 = RandomTestUtil.randomString();
+		String optionValue1 = RandomTestUtil.randomString();
+
+		ddmFormFieldOptions.addOption(optionValue1);
+		ddmFormFieldOptions.addOptionLabel(
+			optionValue1, LocaleUtil.US, optionLabel1);
+		ddmFormFieldOptions.addOptionReference(optionValue1, optionReference1);
+
+		String optionLabel2 = RandomTestUtil.randomString();
+		String optionValue2 = RandomTestUtil.randomString();
+		String optionReference2 = RandomTestUtil.randomString();
+
+		ddmFormFieldOptions.addOption(optionValue2);
+
+		ddmFormFieldOptions.addOptionLabel(
+			optionValue2, LocaleUtil.US, optionLabel2);
+		ddmFormFieldOptions.addOptionReference(optionValue2, optionReference2);
+
+		ddmFormField.setDDMFormFieldOptions(ddmFormFieldOptions);
+
+		SAXReaderImpl saxReaderImpl = new SAXReaderImpl();
+
+		Document document = saxReaderImpl.createDocument();
+
+		Element rootElement = document.addElement("root");
+
+		Field field = new Field(
+			RandomTestUtil.randomLong(), ddmFormField.getName(), optionValue2);
+
+		ReflectionTestUtil.invoke(
+			new JournalConverterImpl(), "_updateContentDynamicElement",
+			new Class<?>[] {
+				int.class, DDMFormField.class, Element.class, Field.class
+			},
+			0, ddmFormField, rootElement, field);
+
+		String xmlValue = ddmFormFieldOptions.getOptionReference(
+			rootElement.getStringValue());
+
+		Assert.assertEquals( optionReference2, xmlValue);
 	}
 
 }
