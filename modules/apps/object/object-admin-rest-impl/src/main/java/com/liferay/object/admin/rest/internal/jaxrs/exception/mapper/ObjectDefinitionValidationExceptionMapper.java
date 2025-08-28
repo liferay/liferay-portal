fd7b6be4949d03bffd5cc53faa503c3b7dde9fce
Caio Farias <caio.farias@liferay.com>
2025-08-28 10:42:49 -0300

    LPD-59867 add object definition validation exception and object definition validation exception mapper

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectDefinitionValidationExceptionMapper.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectDefinitionValidationException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Caio Farias
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectDefinitionValidationExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ObjectDefinitionValidationExceptionMapper
+	extends BaseExceptionMapper<ObjectDefinitionValidationException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectDefinitionValidationException
+			objectDefinitionValidationException) {
+
+		return new Problem(
+			objectDefinitionValidationException.getDetail(),
+			Response.Status.BAD_REQUEST,
+			ObjectDefinitionValidationException.class.getName(),
+			ObjectDefinitionValidationException.class.getName());
+	}
+
+}
