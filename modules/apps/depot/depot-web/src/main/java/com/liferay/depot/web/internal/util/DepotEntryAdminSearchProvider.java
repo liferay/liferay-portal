36f55055d0e29e81d54f25479a9c5c97cfdc04c1
Roberto Díaz <roberto.diaz@liferay.com>
2025-09-05 12:10:40 +0200

    LPD-63562 Rename DepotAdminGroupSearchProvider to DepotEntryAdminSearchProvider and refactor to use new search logic for depot entries

---
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/util/DepotEntryAdminSearchProvider.java
@@ -0,0 +1,192 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.depot.web.internal.util;
+
+import com.liferay.depot.constants.DepotConstants;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryService;
+import com.liferay.depot.web.internal.item.selector.DepotGroupItemSelectorCriterion;
+import com.liferay.depot.web.internal.search.DepotEntrySearch;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.portlet.PortletRequest;
+import jakarta.portlet.PortletResponse;
+import jakarta.portlet.PortletURL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = DepotEntryAdminSearchProvider.class)
+public class DepotEntryAdminSearchProvider {
+
+	public DepotEntrySearch getDepotEntrySearch(
+			DepotGroupItemSelectorCriterion depotGroupItemSelectorCriterion,
+			PortletRequest portletRequest, PortletResponse portletResponse,
+			PortletURL portletURL)
+		throws PortalException {
+
+		if (Validator.isNull(ParamUtil.getString(portletRequest, "keywords")) &&
+			!depotGroupItemSelectorCriterion.isIncludeAllVisibleGroups()) {
+
+			return _getGroupConnectedDepotEntrySearch(
+				depotGroupItemSelectorCriterion.getDepotEntryType(),
+				portletRequest, portletResponse, portletURL);
+		}
+
+		return _getDepotEntrySearch(
+			depotGroupItemSelectorCriterion.getDepotEntryType(), portletRequest,
+			portletResponse, portletURL);
+	}
+
+	public DepotEntrySearch getDepotEntrySearch(
+			int depotEntryType, PortletRequest portletRequest,
+			PortletResponse portletResponse, PortletURL portletURL)
+		throws PortalException {
+
+		return _getDepotEntrySearch(
+			depotEntryType, portletRequest, portletResponse, portletURL);
+	}
+
+	private DepotEntrySearch _getDepotEntrySearch(
+			int depotEntryType, PortletRequest portletRequest,
+			PortletResponse portletResponse, PortletURL portletURL)
+		throws PortalException {
+
+		DepotEntrySearch depotEntrySearch = new DepotEntrySearch(
+			portletRequest, portletResponse, portletURL, "depotEntries");
+
+		depotEntrySearch.setEmptyResultsMessage(
+			_language.get(
+				portletRequest.getLocale(), "no-asset-libraries-were-found"));
+
+		depotEntrySearch.setResultsAndTotal(
+			() -> _getResults(
+				depotEntryType, portletRequest, depotEntrySearch.getStart(),
+				depotEntrySearch.getEnd()),
+			_getTotal(depotEntryType, portletRequest));
+
+		return depotEntrySearch;
+	}
+
+	private DepotEntrySearch _getGroupConnectedDepotEntrySearch(
+			int depotEntryType, PortletRequest portletRequest,
+			PortletResponse portletResponse, PortletURL portletURL)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		DepotEntrySearch depotEntrySearch = new DepotEntrySearch(
+			portletRequest, portletResponse, portletURL, "depotEntries");
+
+		depotEntrySearch.setEmptyResultsMessage(
+			_language.get(
+				portletRequest.getLocale(),
+				(depotEntryType == DepotConstants.TYPE_SPACE) ?
+					"no-spaces-were-found" : "no-asset-libraries-were-found"));
+
+		depotEntrySearch.setResultsAndTotal(
+			() -> _depotEntryService.getGroupConnectedDepotEntries(
+				themeDisplay.getScopeGroupId(), depotEntryType,
+				depotEntrySearch.getStart(), depotEntrySearch.getEnd()),
+			_depotEntryService.getGroupConnectedDepotEntriesCount(
+				themeDisplay.getScopeGroupId(), depotEntryType));
+
+		return depotEntrySearch;
+	}
+
+	private List<DepotEntry> _getResults(
+			int depotEntryType, PortletRequest portletRequest, int start,
+			int end)
+		throws PortalException {
+
+		List<DepotEntry> depotEntries = new ArrayList<>();
+
+		Indexer<Object> indexer = IndexerRegistryUtil.getIndexer(
+			DepotEntry.class.getName());
+
+		Hits hits = indexer.search(
+			_getSearchContext(depotEntryType, portletRequest, start, end));
+
+		for (Document document : hits.getDocs()) {
+			long classPK = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
+
+			DepotEntry depotEntry = _depotEntryService.getDepotEntry(classPK);
+
+			if (depotEntry != null) {
+				depotEntries.add(depotEntry);
+			}
+		}
+
+		return depotEntries;
+	}
+
+	private SearchContext _getSearchContext(
+		int depotEntryType, PortletRequest portletRequest, int start, int end) {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setAttribute(Field.TYPE, depotEntryType);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		searchContext.setCompanyId(themeDisplay.getCompanyId());
+
+		searchContext.setEnd(end);
+
+		String keywords = ParamUtil.getString(portletRequest, "keywords");
+
+		if (Validator.isNotNull(keywords)) {
+			searchContext.setKeywords(keywords);
+		}
+
+		searchContext.setStart(start);
+
+		return searchContext;
+	}
+
+	private int _getTotal(int depotEntryType, PortletRequest portletRequest)
+		throws SearchException {
+
+		Indexer<Object> indexer = IndexerRegistryUtil.getIndexer(
+			DepotEntry.class.getName());
+
+		return (int)indexer.searchCount(
+			_getSearchContext(depotEntryType, portletRequest, start, end));
+	}
+
+	@Reference
+	private DepotEntryService _depotEntryService;
+
+	@Reference
+	private Language _language;
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED)
+	private ModuleServiceLifecycle _moduleServiceLifecycle;
+
+}
