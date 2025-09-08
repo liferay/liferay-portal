82f83e33de9e057c23dc4a8d34b6f9c93ed06d85
Jeff Wu <jeffwu0724@gmail.com>
2025-09-08 12:52:13 -0700

    LPD-58844 migrate functional test to integration test

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/mail/test/MailServiceTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/mail/test/MailServiceTest.java
@@ -16,6 +16,7 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsValues;
@@ -43,6 +44,45 @@ public class MailServiceTest {
 		new LiferayIntegrationTestRule();
 
 	@Test
+	public void testEnablePOPServerNotificationsIsolatedPerCompany()
+		throws Exception {
+
+		long companyId1 = RandomTestUtil.randomLong();
+		long companyId2 = RandomTestUtil.randomLong();
+
+		_setCompanyPortletPreferences(
+			companyId1, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED, "false");
+		_setCompanyPortletPreferences(
+			companyId2, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED, "false");
+
+		Assert.assertFalse(
+			PrefsPropsUtil.getBoolean(
+				companyId1, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED));
+		Assert.assertFalse(
+			PrefsPropsUtil.getBoolean(
+				companyId2, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED));
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.addParameter(
+			"popServerNotificationsEnabled", "true");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_updateMail",
+			new Class<?>[] {ActionRequest.class, PortletPreferences.class},
+			mockLiferayPortletActionRequest,
+			PrefsPropsUtil.getPreferences(companyId1));
+
+		Assert.assertTrue(
+			PrefsPropsUtil.getBoolean(
+				companyId1, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED));
+		Assert.assertFalse(
+			PrefsPropsUtil.getBoolean(
+				companyId2, PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED));
+	}
+
+	@Test
 	public void testGetSessionWithCompanyId() {
 		long companyId = RandomTestUtil.randomLong();
 		String smtpHost = "test.local";
@@ -67,6 +107,18 @@ public class MailServiceTest {
 		Assert.assertEquals(
 			PropsValues.MAIL_SESSION_MAIL_SMTP_HOST,
 			session.getProperty("mail.smtp.host"));
+	}
+
+	private void _setCompanyPortletPreferences(
+			long companyId, String key, String value)
+		throws Exception {
+
+		PortletPreferences portletPreferences = PrefsPropsUtil.getPreferences(
+			companyId);
+
+		portletPreferences.setValue(key, value);
+
+		portletPreferences.store();
 	}
 
 	@Inject
