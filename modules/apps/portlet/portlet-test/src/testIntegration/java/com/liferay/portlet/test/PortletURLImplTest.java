c410e1abf3360e0c10b81c39846bc9bb01971688
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2025-10-02 20:18:18 +0200

    LPD-67042 Fix PortletURLImpl encryption, add test
    LPS-73238 (02c80c37ce35f9b052b9a261ecda03a5d408abf1) removed doAsUserId encryption which caused LPS-90474, this API is used only from export-import-web: `<liferay-portlet:actionURL doAsUserId="<%= user.getUserId() %>" name="/export_import/export_import">`

--- modules/apps/portlet/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletURLImplTest.java
+++ modules/apps/portlet/portlet-test/src/testIntegration/java/com/liferay/portlet/test/PortletURLImplTest.java
@@ -8,16 +8,22 @@ package com.liferay.portlet.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.encryptor.EncryptorUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.LiferayPortletURL;
 import com.liferay.portal.kernel.portlet.PortletURLFactory;
+import com.liferay.portal.kernel.security.ChecksumUtil;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -45,6 +51,66 @@ public class PortletURLImplTest {
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testDoAsUserId() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Layout layout = LayoutTestUtil.addTypePortletLayout(_group);
+
+		themeDisplay.setLayout(layout);
+		themeDisplay.setPlid(layout.getPlid());
+
+		themeDisplay.setPortalURL("http://localhost:8080");
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		Company company = CompanyLocalServiceUtil.getCompany(
+			TestPropsValues.getCompanyId());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.COMPANY_ID, company.getCompanyId());
+
+		String doAsUserIdString = StringUtil.bytesToHexString(
+			ChecksumUtil.appendChecksum(
+				EncryptorUtil.encryptUnencoded(company.getKeyObj(), "1")));
+
+		LiferayPortletURL liferayPortletURL = _portletURLFactory.create(
+			mockHttpServletRequest, PortletKeys.LOGIN, themeDisplay.getPlid(),
+			PortletRequest.RENDER_PHASE);
+
+		themeDisplay.setDoAsUserId(doAsUserIdString);
+
+		String url = liferayPortletURL.toString();
+
+		Assert.assertTrue(url.contains("doAsUserId=" + doAsUserIdString));
+
+		ReflectionTestUtil.invoke(
+			liferayPortletURL, "clearCache", new Class<?>[0]);
+
+		long doAsUserId = 2;
+
+		doAsUserIdString = StringUtil.bytesToHexString(
+			ChecksumUtil.appendChecksum(
+				EncryptorUtil.encryptUnencoded(
+					company.getKeyObj(), String.valueOf(doAsUserId))));
+
+		themeDisplay.setDoAsUserId(null);
+
+		liferayPortletURL.setDoAsUserId(doAsUserId);
+
+		url = liferayPortletURL.toString();
+
+		Assert.assertTrue(url.contains("doAsUserId=" + doAsUserIdString));
+	}
 
 	@Test
 	public void testToStringShouldNotReplicateExistingParamValues()
