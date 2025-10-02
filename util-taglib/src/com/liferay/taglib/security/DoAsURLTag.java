481720dbf134a70e42db52eb18e0091f3bb43bc5
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2025-10-02 17:38:46 +0200

    LPD-67042 Add checksum to doAsUserId and switch Base64 for HEX

--- util-taglib/src/com/liferay/taglib/security/DoAsURLTag.java
+++ util-taglib/src/com/liferay/taglib/security/DoAsURLTag.java
@@ -8,10 +8,12 @@ package com.liferay.taglib.security;
 import com.liferay.portal.kernel.encryptor.EncryptorUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.ChecksumUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -49,8 +51,10 @@ public class DoAsURLTag extends TagSupport {
 			doAsUserId = guestUser.getUserId();
 		}
 
-		String encDoAsUserId = EncryptorUtil.encrypt(
-			company.getKeyObj(), String.valueOf(doAsUserId));
+		String encDoAsUserId = StringUtil.bytesToHexString(
+			ChecksumUtil.appendChecksum(
+				EncryptorUtil.encryptUnencoded(
+					company.getKeyObj(), String.valueOf(doAsUserId))));
 
 		return HttpComponentsUtil.addParameter(
 			doAsURL, "doAsUserId", encDoAsUserId);
