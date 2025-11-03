475cba27ec563b4e4eb9eb444fa037a0966a533d
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-11-03 23:06:51 +0100

    LPD-70456 Import the configuration field values of the types URL and Video

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/layout/structure/item/importer/util/FragmentConfigurationFieldValuesUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/layout/structure/item/importer/util/FragmentConfigurationFieldValuesUtil.java
@@ -19,6 +19,7 @@ import com.liferay.headless.admin.site.dto.v1_0.ColorPaletteValue;
 import com.liferay.headless.admin.site.dto.v1_0.ColorPickerFragmentConfigurationFieldValue;
 import com.liferay.headless.admin.site.dto.v1_0.ContextualMenuNavigationMenuValue;
 import com.liferay.headless.admin.site.dto.v1_0.FragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.HrefURLValue;
 import com.liferay.headless.admin.site.dto.v1_0.ItemExternalReference;
 import com.liferay.headless.admin.site.dto.v1_0.ItemFragmentConfigurationFieldValue;
 import com.liferay.headless.admin.site.dto.v1_0.ItemValue;
@@ -27,17 +28,24 @@ import com.liferay.headless.admin.site.dto.v1_0.NavigationMenuFragmentConfigurat
 import com.liferay.headless.admin.site.dto.v1_0.NavigationMenuValue;
 import com.liferay.headless.admin.site.dto.v1_0.SelectFragmentConfigurationFieldValue;
 import com.liferay.headless.admin.site.dto.v1_0.SiteMenuNavigationMenuValue;
+import com.liferay.headless.admin.site.dto.v1_0.SitePageURLValue;
 import com.liferay.headless.admin.site.dto.v1_0.SitePagesNavigationMenuValue;
 import com.liferay.headless.admin.site.dto.v1_0.TemplateReference;
 import com.liferay.headless.admin.site.dto.v1_0.TextFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.URLFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.URLValue;
+import com.liferay.headless.admin.site.dto.v1_0.VideoFragmentConfigurationFieldValue;
+import com.liferay.headless.admin.site.dto.v1_0.VideoValue;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.CollectionUtil;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.ContextualMenuTypeUtil;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.FragmentConfigurationFieldValueTypeUtil;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.InfoItemUtil;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.ItemScopeUtil;
+import com.liferay.headless.admin.site.internal.dto.v1_0.util.LayoutUtil;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.LocalizedValueUtil;
 import com.liferay.headless.admin.site.internal.resource.v1_0.layout.structure.item.importer.context.LayoutStructureItemImporterContext;
 import com.liferay.headless.admin.site.internal.util.LogUtil;
+import com.liferay.item.selector.criteria.VideoEmbeddableHTMLItemSelectorReturnType;
 import com.liferay.petra.function.UnsafeFunction;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
@@ -289,6 +297,39 @@ public class FragmentConfigurationFieldValuesUtil {
 				fragmentConfigurationField.isLocalizable(),
 				textFragmentConfigurationFieldValue.getValue(),
 				textFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.URL)) {
+
+			URLFragmentConfigurationFieldValue
+				urlFragmentConfigurationFieldValue =
+					(URLFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationJSONObject(
+				fragmentConfigurationField.isLocalizable(),
+				urlValue -> _getURLJSONObject(
+					layoutStructureItemImporterContext, urlValue),
+				urlFragmentConfigurationFieldValue.getValue(),
+				urlFragmentConfigurationFieldValue.getValue_i18n());
+		}
+
+		if (Objects.equals(
+				fragmentConfigurationFieldValue.getType(),
+				FragmentConfigurationFieldValue.Type.VIDEO)) {
+
+			VideoFragmentConfigurationFieldValue
+				videoFragmentConfigurationFieldValue =
+					(VideoFragmentConfigurationFieldValue)
+						fragmentConfigurationFieldValue;
+
+			return _getConfigurationJSONObject(
+				fragmentConfigurationField.isLocalizable(),
+				videoValue -> _getVideoJSONObject(videoValue),
+				videoFragmentConfigurationFieldValue.getValue(),
+				videoFragmentConfigurationFieldValue.getValue_i18n());
 		}
 
 		return null;
@@ -674,6 +715,54 @@ public class FragmentConfigurationFieldValuesUtil {
 			Objects.equals(
 				sitePagesNavigationMenuValue.getPageSetType(),
 				SitePagesNavigationMenuValue.PageSetType.PRIVATE_PAGES)
+		);
+	}
+
+	private static JSONObject _getURLJSONObject(
+			LayoutStructureItemImporterContext
+				layoutStructureItemImporterContext,
+			URLValue urlValue)
+		throws Exception {
+
+		if (urlValue == null) {
+			return null;
+		}
+
+		if (Objects.equals(urlValue.getUrlType(), URLValue.UrlType.HREF)) {
+			HrefURLValue hrefURLValue = (HrefURLValue)urlValue;
+
+			return JSONUtil.put("href", hrefURLValue.getHref());
+		}
+
+		SitePageURLValue sitePageURLValue = (SitePageURLValue)urlValue;
+
+		ItemExternalReference itemExternalReference =
+			sitePageURLValue.getSitePage();
+
+		if (itemExternalReference == null) {
+			return null;
+		}
+
+		return JSONUtil.put(
+			"layout",
+			LayoutUtil.getMappedLayoutJSONObject(
+				layoutStructureItemImporterContext.getCompanyId(),
+				itemExternalReference.getExternalReferenceCode(),
+				itemExternalReference.getScope(),
+				layoutStructureItemImporterContext.getGroupId()));
+	}
+
+	private static JSONObject _getVideoJSONObject(VideoValue videoValue) {
+		if ((videoValue == null) || Validator.isNull(videoValue.getHtml())) {
+			return null;
+		}
+
+		return JSONUtil.put(
+			"html", videoValue.getHtml()
+		).put(
+			"title", videoValue.getTitle()
+		).put(
+			"type", VideoEmbeddableHTMLItemSelectorReturnType.class.getName()
 		);
 	}
 
