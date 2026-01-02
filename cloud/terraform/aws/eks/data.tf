6ba9d147791fe3d8ee8715a9817fc1fcd07b8d6a
Gregory Amerson <gregory.amerson@liferay.com>
2026-01-02 14:30:39 -0600

    LCD-49360 add s3 csi driver with access to s3 buckets for extension mountpoints

--- cloud/terraform/aws/eks/data.tf
+++ cloud/terraform/aws/eks/data.tf
@@ -6,6 +6,10 @@ data "aws_availability_zones" "available" {
 }
 data "aws_caller_identity" "current" {
 }
+data "aws_eks_addon_version" "s3_csi" {
+  addon_name="aws-mountpoint-s3-csi-driver"
+  kubernetes_version=module.eks.cluster_version
+}
 data "aws_eks_cluster_versions" "available" {
 	region=var.region
 	version_status="STANDARD_SUPPORT"
--- cloud/terraform/aws/eks/eks.tf
+++ cloud/terraform/aws/eks/eks.tf
@@ -55,6 +55,13 @@ module "eks" {
 	version="21.3.1"
 	vpc_id=module.vpc.vpc_id
 }
+resource "aws_eks_addon" "s3_csi" {
+	addon_name="aws-mountpoint-s3-csi-driver"
+	addon_version=data.aws_eks_addon_version.s3_csi.version
+	cluster_name=module.eks.cluster_name
+	resolve_conflicts_on_update="OVERWRITE"
+	service_account_role_arn=aws_iam_role.s3_csi_driver.arn
+}
 resource "aws_iam_role" "ebs_csi_driver" {
 	assume_role_policy=jsonencode(
 		{
@@ -101,6 +108,52 @@ resource "aws_iam_role" "irsa" {
 	)
 	force_detach_policies=true
 	name="${var.deployment_name}-irsa"
+}
+resource "aws_iam_role" "s3_csi_driver" {
+	assume_role_policy=jsonencode(
+    {
+			Statement=[
+				{
+					Action="sts:AssumeRoleWithWebIdentity"
+					Condition={
+						StringEquals={
+							"${module.eks.oidc_provider}:sub"="system:serviceaccount:kube-system:s3-csi-controller-sa"
+							"${module.eks.oidc_provider}:aud"="sts.amazonaws.com"
+						}
+					}
+					Effect="Allow"
+					Principal={
+						Federated=local.oidc_provider_arn
+					}
+				}
+			]
+			Version="2012-10-17"
+		}
+	)
+  force_detach_policies=true
+  name="${var.deployment_name}-s3_csi_driver"
+}
+resource "aws_iam_role_policy" "s3_csi_driver" {
+	policy=jsonencode(
+		{
+			Statement=[
+				{
+					Action=[
+						"s3:GetObject",
+						"s3:ListBucket",
+					]
+					Effect="Allow"
+					Resource=[
+						"arn:${var.arn_partition}:s3:::${var.deployment_name}-extensions-*",
+						"arn:${var.arn_partition}:s3:::${var.deployment_name}-extensions-*/*"
+					]
+					Sid="AllowS3BucketOperations"
+				}
+			]
+			Version="2012-10-17"
+		}
+	)
+	role=aws_iam_role.s3_csi_driver.id
 }
 resource "aws_iam_role_policy" "this" {
 	count = length(var.ecr_repositories) > 0 ? 1 : 0
