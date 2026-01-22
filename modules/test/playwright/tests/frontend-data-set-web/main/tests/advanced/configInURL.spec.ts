5be89eb1eaf67b48bb60f310cf26c305bcabdfab
antonio-ortega <antonio.ortega@liferay.com>
2026-01-22 15:52:44 +0100

    LPD-72727 SF

--- modules/test/playwright/tests/frontend-data-set-web/main/tests/advanced/configInURL.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-web/main/tests/advanced/configInURL.spec.ts
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 import JsonURL from '@jsonurl/jsonurl';
 import {Page, expect, mergeTests} from '@playwright/test';
 
