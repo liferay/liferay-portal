d3afd922cf53f299570292491d2dbbf588b312c0
Joshua Cords <joshua.cords@liferay.com>
2026-01-26 17:23:37 -0800

    LPD-71796 portal-search-api: rename package
    https://liferay.atlassian.net/browse/LPD-71796

--- modules/apps/portal-search/portal-search-api/src/test/java/com/liferay/portal/search/ml/embedding/text/helper/TextEmbeddingContentHelperTest.java
+++ modules/apps/portal-search/portal-search-api/src/test/java/com/liferay/portal/search/ml/embedding/text/helper/TextEmbeddingContentHelperTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.ml.embedding.text.util;
+package com.liferay.portal.search.ml.embedding.text.helper;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
