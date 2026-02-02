80fca75a0992336bfa632ec5a2415e66b3b58fc4
Bryan Engler <bryan.engler@liferay.com>
2026-02-02 12:02:33 -0800

    LPD-78407 commerce-elasticsearch7: Delete the commerce-elasticsearch7 module and move the contributor to commerce-service
    https://liferay.atlassian.net/browse/LPD-78407

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/search/spi/index/configuration/contributor/CommerceCompanyIndexConfigurationContributor.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/search/spi/index/configuration/contributor/CommerceCompanyIndexConfigurationContributor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.commerce.elasticsearch7.internal.index.configuration.contributor;
+package com.liferay.commerce.internal.search.spi.index.configuration.contributor;
 
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.spi.index.configuration.contributor.CompanyIndexConfigurationContributor;
