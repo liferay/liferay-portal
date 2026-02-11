bebf26df8dea493f74bef703daec43f4ed7b6ef4
Mario Leandro <mario.leandro@liferay.com>
2026-02-11 14:57:22 -0300

    LPD-70107 Add JSP Homes for Commerce and Control Panel

---
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/homes/control_panel_home.jsp
@@ -0,0 +1,17 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+HomeDisplayContext homeDisplayContext = new HomeDisplayContext(request);
+%>
+
+<react:component
+	module="{ControlPanelHome} from product-navigation-applications-menu-web"
+	props="<%= homeDisplayContext.getProps() %>"
+/>
---
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/homes/ControlPanelHome.tsx
@@ -0,0 +1,38 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayLayout from '@clayui/layout';
+import React from 'react';
+
+import CategoryCardHorizontal from './components/CategoryCardHorizontal';
+import HomePageLayout from './components/HomePageLayout';
+import {CategoryItemGrouped, HomeProps} from './types';
+
+const ControlPanelHome = ({items, label, logo}: HomeProps) => {
+	return (
+		<HomePageLayout logo={logo} title={label}>
+			{(items as Array<CategoryItemGrouped>).map((group) => (
+				<ClayLayout.ContainerFluid className="mb-4" key={group.label}>
+					<ClayLayout.ContentRow className="mb-2 pb-2">
+						<ClayLayout.ContentCol expand>
+							<p className="font-weight-semi-bold mb-0 text-3 text-secondary text-uppercase">
+								{group.label}
+							</p>
+						</ClayLayout.ContentCol>
+					</ClayLayout.ContentRow>
+
+					<ClayLayout.Row>
+						{group.items.map((app) => (
+							<ClayLayout.Col key={app.id} md={4} sm={6}>
+								<CategoryCardHorizontal item={app} />
+							</ClayLayout.Col>
+						))}
+					</ClayLayout.Row>
+				</ClayLayout.ContainerFluid>
+			))}
+		</HomePageLayout>
+	);
+};
+export default ControlPanelHome;
