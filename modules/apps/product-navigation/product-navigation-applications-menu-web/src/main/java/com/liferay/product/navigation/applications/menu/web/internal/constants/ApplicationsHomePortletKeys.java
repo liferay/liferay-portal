d499e6e274cf9e122bcd31a2b10883c169246db3
Mario Leandro <mario.leandro@liferay.com>
2026-02-12 10:28:16 -0300

    LPD-78636 Add Panel Category, Panel App and Portlet for Applications Home

---
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/java/com/liferay/product/navigation/applications/menu/web/internal/constants/ApplicationsHomePortletKeys.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.product.navigation.applications.menu.web.internal.constants;
+
+/**
+ * @author Mario Leandro
+ */
+public class ApplicationsHomePortletKeys {
+
+	public static final String APPLICATIONS_HOME =
+		"com_liferay_applications_home_web_internal_portlet_" +
+			"ApplicationsHomePortlet";
+
+}
---
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/java/com/liferay/product/navigation/applications/menu/web/internal/portlet/ApplicationsHomePortlet.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.product.navigation.applications.menu.web.internal.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.product.navigation.applications.menu.web.internal.constants.ApplicationsHomePortletKeys;
+
+import jakarta.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mario Leandro
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/admin/css/main.css",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.use-default-template=true",
+		"jakarta.portlet.expiration-cache=0",
+		"jakarta.portlet.init-param.mvc-command-names-default-views=/admin/view",
+		"jakarta.portlet.init-param.portlet-title-based-navigation=true",
+		"jakarta.portlet.init-param.template-path=/META-INF/resources/",
+		"jakarta.portlet.display-name=Home",
+		"jakarta.portlet.init-param.view-template=/homes/applications_home.jsp",
+		"jakarta.portlet.name=" + ApplicationsHomePortletKeys.APPLICATIONS_HOME,
+		"jakarta.portlet.resource-bundle=content.Language",
+		"jakarta.portlet.security-role-ref=administrator",
+		"jakarta.portlet.version=4.0"
+	},
+	service = Portlet.class
+)
+public class ApplicationsHomePortlet extends MVCPortlet {
+}
