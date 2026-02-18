ac12971241bd369b49a4c3698b5dce0a89600b4d
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-02-18 12:39:42 +0100

    LPD-78636 Improve accessibility

--- modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/GlobalMenu.tsx
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/GlobalMenu.tsx
@@ -129,6 +129,7 @@ export default function GlobalMenu({
 			trigger={
 				<ClayButtonWithIcon
 					aria-haspopup="dialog"
+					aria-label={Liferay.Language.get('open-applications-menu')}
 					className="control-menu-nav-link dropdown-toggle lfr-portal-tooltip"
 					data-qa-id="globalMenu"
 					data-title={openButtonTitle}
