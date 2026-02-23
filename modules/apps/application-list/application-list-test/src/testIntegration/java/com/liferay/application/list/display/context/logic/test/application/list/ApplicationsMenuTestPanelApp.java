86aafcbcb6675ae6f45867d5f4bb5823e8ce0e08
Jason Myatt <jason.myatt@liferay.com>
2026-02-23 13:55:56 -0500

    LPD-78636 Adapt tests

--- modules/apps/application-list/application-list-test/src/testIntegration/java/com/liferay/application/list/display/context/logic/test/application/list/ApplicationsMenuTestPanelApp.java
+++ modules/apps/application-list/application-list-test/src/testIntegration/java/com/liferay/application/list/display/context/logic/test/application/list/ApplicationsMenuTestPanelApp.java
@@ -19,7 +19,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	property = {
-		"panel.app.order:Integer=10000",
+		"panel.app.order:Integer=50",
 		"panel.category.key=" + PanelCategoryKeys.APPLICATIONS_MENU_APPLICATIONS
 	},
 	service = PanelApp.class
