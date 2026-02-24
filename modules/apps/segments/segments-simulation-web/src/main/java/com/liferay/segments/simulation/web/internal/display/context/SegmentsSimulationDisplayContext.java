26c29cb8abe8b7b9d4e27a3a2e157c0411538f35
Thiago Buarque <thiago.buarque@liferay.com>
2026-02-24 13:39:11 -0300

    LPD-78402 fetch only Analytics Cloud segments when using the simulation panel

--- modules/apps/segments/segments-simulation-web/src/main/java/com/liferay/segments/simulation/web/internal/display/context/SegmentsSimulationDisplayContext.java
+++ modules/apps/segments/segments-simulation-web/src/main/java/com/liferay/segments/simulation/web/internal/display/context/SegmentsSimulationDisplayContext.java
@@ -6,12 +6,15 @@
 package com.liferay.segments.simulation.web.internal.display.context;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -92,8 +95,18 @@ public class SegmentsSimulationDisplayContext {
 			return _segmentsEntries;
 		}
 
-		_segmentsEntries = SegmentsEntryServiceUtil.getSegmentsEntries(
-			_getStagingAwareGroupId());
+		if (FeatureFlagManagerUtil.isEnabled(
+				CompanyConstants.SYSTEM, "LPD-78863")) {
+
+			_segmentsEntries = SegmentsEntryServiceUtil.getSegmentsEntries(
+				_getStagingAwareGroupId());
+		}
+		else {
+			_segmentsEntries = SegmentsEntryServiceUtil.getSegmentsEntries(
+				_getStagingAwareGroupId(),
+				SegmentsEntryConstants.SOURCE_ASAH_FARO_BACKEND,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+		}
 
 		return _segmentsEntries;
 	}
