1c169af7168529d23043b98b36d9e2d0ab6fab97
Thiago Buarque <thiago.buarque@liferay.com>
2026-02-27 10:27:10 -0300

    LPD-78402 rename

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/dao/search/SegmentsEntrySearchContainerFactory.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/dao/search/SegmentsEntrySearchContainerFactory.java
@@ -119,9 +119,9 @@ public class SegmentsEntrySearchContainerFactory {
 		if (!FeatureFlagManagerUtil.isEnabled(
 				CompanyConstants.SYSTEM, "LPD-78863")) {
 
-			BooleanQuery sourceQuery = new BooleanQueryImpl();
+			BooleanQuery booleanQuery = new BooleanQueryImpl();
 
-			sourceQuery.addTerm(
+			booleanQuery.addTerm(
 				"source",
 				StringUtil.toLowerCase(
 					SegmentsEntryConstants.SOURCE_ASAH_FARO_BACKEND));
@@ -129,7 +129,7 @@ public class SegmentsEntrySearchContainerFactory {
 			searchContext.setBooleanClauses(
 				new BooleanClause[] {
 					BooleanClauseFactoryUtil.create(
-						sourceQuery, BooleanClauseOccur.MUST.getName())
+						booleanQuery, BooleanClauseOccur.MUST.getName())
 				});
 		}
 
--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryLocalServiceImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryLocalServiceImpl.java
@@ -454,9 +454,9 @@ public class SegmentsEntryLocalServiceImpl
 		if (!FeatureFlagManagerUtil.isEnabled(
 				CompanyConstants.SYSTEM, "LPD-78863")) {
 
-			BooleanQuery sourceQuery = new BooleanQueryImpl();
+			BooleanQuery booleanQuery = new BooleanQueryImpl();
 
-			sourceQuery.addTerm(
+			booleanQuery.addTerm(
 				"source",
 				StringUtil.toLowerCase(
 					SegmentsEntryConstants.SOURCE_ASAH_FARO_BACKEND));
@@ -464,7 +464,7 @@ public class SegmentsEntryLocalServiceImpl
 			searchContext.setBooleanClauses(
 				new BooleanClause[] {
 					BooleanClauseFactoryUtil.create(
-						sourceQuery, BooleanClauseOccur.MUST.getName())
+						booleanQuery, BooleanClauseOccur.MUST.getName())
 				});
 		}
 
