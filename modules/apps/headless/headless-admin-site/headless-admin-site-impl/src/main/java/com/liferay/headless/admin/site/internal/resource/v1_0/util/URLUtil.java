95604bfef7b3d422e3248062984af5733fe06be6
Javier Moral <javier.moral@liferay.com>
2026-03-02 16:09:28 +0100

    LPD-80966 Avoid string byte conversion

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/URLUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/util/URLUtil.java
@@ -6,13 +6,15 @@
 package com.liferay.headless.admin.site.internal.resource.v1_0.util;
 
 import com.liferay.exportimport.attachment.ExportImportAttachmentManagerUtil;
+import com.liferay.petra.io.StreamUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.util.Validator;
+
+import java.io.InputStream;
 
 import java.net.URL;
-
-import java.nio.charset.StandardCharsets;
+import java.net.URLConnection;
 
 import java.util.Objects;
 
@@ -24,20 +26,26 @@ public class URLUtil {
 	public static byte[] getByteArray(String urlString) throws Exception {
 		URL url = ExportImportAttachmentManagerUtil.getURL(urlString);
 
-		if (Objects.equals(url.getProtocol(), "file")) {
-			throw new UnsupportedOperationException(
-				StringBundler.concat(
-					"Unable to download file from ", urlString,
-					" because of unsupported protocol ", url.getProtocol()));
+		String protocol = url.getProtocol();
+
+		if (Objects.equals(protocol, Http.HTTP) ||
+			Objects.equals(protocol, Http.HTTPS)) {
+
+			return HttpUtil.URLtoByteArray(url.toString());
 		}
 
-		String string = HttpUtil.URLtoString(url);
+		if (Objects.equals(protocol, "lar")) {
+			URLConnection urlConnection = url.openConnection();
 
-		if (Validator.isNull(string)) {
-			return new byte[0];
+			try (InputStream inputStream = urlConnection.getInputStream()) {
+				return StreamUtil.toByteArray(inputStream);
+			}
 		}
 
-		return string.getBytes(StandardCharsets.UTF_8);
+		throw new UnsupportedOperationException(
+			StringBundler.concat(
+				"Unable to download file from ", urlString,
+				" because of unsupported protocol ", protocol));
 	}
 
 }
