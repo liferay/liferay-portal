eb03f0dffe395449a2912b36774b3515d1a115e8
Francesco Acciaro <francesco.acciaro@liferay.com>
2026-03-16 08:43:39 +0100

    DG-1540 Fixed CSV importer

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/ExamResultsRestController.java
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/ExamResultsRestController.java
@@ -137,7 +137,7 @@ public class ExamResultsRestController extends BaseRestController {
 							LocalDateTime.parse(
 								csvRecord.get(10),
 								DateTimeFormatter.ofPattern(
-									"yyyy-MM-dd H:mm:ss")),
+									"yyyy-MM-dd'T'H:mm:ss[.SSS]")),
 							ZoneOffset.UTC
 						).format(
 							DateTimeFormatter.ISO_INSTANT
@@ -228,7 +228,7 @@ public class ExamResultsRestController extends BaseRestController {
 					csvPrinter.printRecord(
 						jsonObject2.getString("firstName"),
 						jsonObject2.getString("lastName"),
-						jsonObject2.getString("email"),
+						jsonObject2.getString("emailAddress"),
 						jsonObject2.getString("examName"),
 						jsonObject2.getString("date"),
 						jsonObject2.getDouble("score"),
