306599d3af7e57287a93db5492b6bc93a222f104
Alan Huang <alan.huang@liferay.com>
2026-04-04 19:52:01 +0800

    LPD-66061 Add ModifiedServiceMethodCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/ModifiedServiceMethodCheck.java
@@ -0,0 +1,115 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.checkstyle.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.AnnotationUtil;
+
+/**
+ * @author Alan Huang
+ */
+public class ModifiedServiceMethodCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.METHOD_DEF};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		String className = JavaSourceUtil.getClassName(getAbsolutePath());
+
+		if (className.equals("ServiceTrackerCustomizerFactory") ||
+			!AnnotationUtil.containsAnnotation(detailAST, "Override")) {
+
+			return;
+		}
+
+		String methodName = getName(detailAST);
+
+		if (!methodName.equals("modifiedService")) {
+			return;
+		}
+
+		DetailAST modifiersDetailAST = detailAST.findFirstToken(
+			TokenTypes.MODIFIERS);
+
+		if (!modifiersDetailAST.branchContains(TokenTypes.LITERAL_PUBLIC)) {
+			return;
+		}
+
+		DetailAST typeDetailAST = detailAST.findFirstToken(TokenTypes.TYPE);
+
+		DetailAST firstChildDetailAST = typeDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() != TokenTypes.LITERAL_VOID) {
+			return;
+		}
+
+		DetailAST slistDetailAST = detailAST.findFirstToken(TokenTypes.SLIST);
+
+		if (slistDetailAST == null) {
+			return;
+		}
+
+		firstChildDetailAST = slistDetailAST.getFirstChild();
+
+		if ((firstChildDetailAST == null) ||
+			(firstChildDetailAST.getType() != TokenTypes.EXPR)) {
+
+			return;
+		}
+
+		methodName = getMethodName(firstChildDetailAST.getFirstChild());
+
+		if (!StringUtil.equals(methodName, "removedService")) {
+			return;
+		}
+
+		DetailAST nextSiblingDetailAST = firstChildDetailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.SEMI)) {
+
+			return;
+		}
+
+		nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.EXPR)) {
+
+			return;
+		}
+
+		methodName = getMethodName(nextSiblingDetailAST.getFirstChild());
+
+		if (!StringUtil.equals(methodName, "addingService")) {
+			return;
+		}
+
+		nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.SEMI) ||
+			!equals(
+				nextSiblingDetailAST.getNextSibling(),
+				slistDetailAST.getLastChild())) {
+
+			return;
+		}
+
+		log(detailAST, _MSG_INCORRECT_METHOD_CONTENT);
+	}
+
+	private static final String _MSG_INCORRECT_METHOD_CONTENT =
+		"method.content.incorrect";
+
+}
