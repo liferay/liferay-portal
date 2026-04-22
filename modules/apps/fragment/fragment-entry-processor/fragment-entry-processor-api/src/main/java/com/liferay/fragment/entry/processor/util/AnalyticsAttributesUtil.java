e770302c702b1f1410aa1b5b3f571fbbe0bf8cf5
Thiago Buarque <thiago.buarque@liferay.com>
2026-04-22 17:45:14 -0300

    LPD-86068 send vocabulary id of an asset category

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/util/AnalyticsAttributesUtil.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/util/AnalyticsAttributesUtil.java
@@ -237,6 +237,8 @@ public class AnalyticsAttributesUtil {
 					"id", assetCategory.getCategoryId()
 				).put(
 					"name", assetCategory.getTitle(locale)
+				).put(
+					"vocabularyId", assetCategory.getVocabularyId()
 				),
 				_log);
 
