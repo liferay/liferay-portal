b2d5152a38c14f1c4539939594ddc635c16f87e5
Christian Moura <christian.moura@liferay.com>
2026-04-28 13:51:11 -0300

    LPD-86317 emit suppressThirdPartyCookies when GPC signal active

--- modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/servlet/taglib/EnableThirdPartyCookiesBottomJSDynamicInclude.java
+++ modules/apps/cookies/cookies-banner-web/src/main/java/com/liferay/cookies/banner/web/internal/servlet/taglib/EnableThirdPartyCookiesBottomJSDynamicInclude.java
@@ -7,7 +7,10 @@ package com.liferay.cookies.banner.web.internal.servlet.taglib;
 
 import com.liferay.cookies.configuration.CookiesConfigurationProvider;
 import com.liferay.cookies.configuration.CookiesPreferenceHandlingConfiguration;
+import com.liferay.cookies.provider.GlobalPrivacyControlProvider;
 import com.liferay.frontend.js.loader.modules.extender.esm.ESImportUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.cookies.constants.CookiesConstants;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
@@ -42,22 +45,26 @@ public class EnableThirdPartyCookiesBottomJSDynamicInclude
 			HttpServletResponse httpServletResponse, String key)
 		throws IOException {
 
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		CookiesPreferenceHandlingConfiguration
+			cookiesPreferenceHandlingConfiguration;
+
 		try {
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)httpServletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
-			CookiesPreferenceHandlingConfiguration
-				cookiesPreferenceHandlingConfiguration =
-					_cookiesConfigurationProvider.
-						getCookiesPreferenceHandlingConfiguration(themeDisplay);
-
-			if (!cookiesPreferenceHandlingConfiguration.enabled()) {
-				return;
-			}
+			cookiesPreferenceHandlingConfiguration =
+				_cookiesConfigurationProvider.
+					getCookiesPreferenceHandlingConfiguration(themeDisplay);
 		}
 		catch (Exception exception) {
 			_log.error(exception);
+
+			return;
+		}
+
+		if (!cookiesPreferenceHandlingConfiguration.enabled()) {
+			return;
 		}
 
 		AbsolutePortalURLBuilder absolutePortalURLBuilder =
@@ -66,15 +73,49 @@ public class EnableThirdPartyCookiesBottomJSDynamicInclude
 
 		ScriptData scriptData = new ScriptData();
 
-		scriptData.append(
-			_portal.getPortletId(httpServletRequest),
-			new JSFragment(
-				"runThirdPartyCookiesInterval();",
-				Arrays.asList(
-					ESImportUtil.getESImport(
-						absolutePortalURLBuilder,
-						"{runThirdPartyCookiesInterval} from " +
-							"cookies-banner-web/index.js"))));
+		if (_globalPrivacyControlProvider.isSignalActive(httpServletRequest)) {
+			scriptData.append(
+				_portal.getPortletId(httpServletRequest),
+				new JSFragment(
+					StringBundler.concat(
+						"suppressThirdPartyCookies(",
+						cookiesPreferenceHandlingConfiguration.
+							consentRenewalPeriod(),
+						", \"",
+						cookiesPreferenceHandlingConfiguration.
+							consentRenewalPeriodTimeUnit(),
+						"\", ",
+						cookiesPreferenceHandlingConfiguration.
+							dissentRenewalPeriod(),
+						", \"",
+						cookiesPreferenceHandlingConfiguration.
+							dissentRenewalPeriodTimeUnit(),
+						"\", [\"",
+						CookiesConstants.NAME_CONSENT_TYPE_FUNCTIONAL, "\", \"",
+						CookiesConstants.NAME_CONSENT_TYPE_PERFORMANCE,
+						"\", \"",
+						CookiesConstants.NAME_CONSENT_TYPE_PERSONALIZATION,
+						"\"], [\"",
+						CookiesConstants.NAME_CONSENT_TYPE_NECESSARY, "\"], ",
+						cookiesPreferenceHandlingConfiguration.storeConsent(),
+						");"),
+					Arrays.asList(
+						ESImportUtil.getESImport(
+							absolutePortalURLBuilder,
+							"{suppressThirdPartyCookies} from " +
+								"cookies-banner-web/index.js"))));
+		}
+		else {
+			scriptData.append(
+				_portal.getPortletId(httpServletRequest),
+				new JSFragment(
+					"runThirdPartyCookiesInterval();",
+					Arrays.asList(
+						ESImportUtil.getESImport(
+							absolutePortalURLBuilder,
+							"{runThirdPartyCookiesInterval} from " +
+								"cookies-banner-web/index.js"))));
+		}
 
 		scriptData.writeTo(httpServletResponse.getWriter());
 	}
@@ -94,6 +135,9 @@ public class EnableThirdPartyCookiesBottomJSDynamicInclude
 
 	@Reference
 	private CookiesConfigurationProvider _cookiesConfigurationProvider;
+
+	@Reference
+	private GlobalPrivacyControlProvider _globalPrivacyControlProvider;
 
 	@Reference
 	private Portal _portal;
