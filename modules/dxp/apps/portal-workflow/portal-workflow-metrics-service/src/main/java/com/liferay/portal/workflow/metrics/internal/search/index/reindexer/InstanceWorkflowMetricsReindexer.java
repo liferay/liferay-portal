1736153bec3653ba3ed7973f827459ef8f6d5774
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-05-14 10:54:15 -0300

    [LPD-75968] portal-workflow-metrics-service: Migrate InstanceWorkflowMetricsReindexer to dual SPI and remove IndexerHelper2
    https://liferay.atlassian.net/browse/LPD-75968

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/reindexer/InstanceWorkflowMetricsReindexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/reindexer/InstanceWorkflowMetricsReindexer.java
@@ -8,6 +8,7 @@ package com.liferay.portal.workflow.metrics.internal.search.index.reindexer;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.search.capabilities.SearchCapabilities;
@@ -15,6 +16,7 @@ import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.search.index.SyncReindexManager;
 import com.liferay.portal.search.spi.reindexer.IndexReindexer;
+import com.liferay.portal.workflow.kaleo.metrics.integration.helper.IndexerHelper;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
 import com.liferay.portal.workflow.kaleo.model.KaleoInstance;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
@@ -23,6 +25,7 @@ import com.liferay.portal.workflow.metrics.internal.search.index.WorkflowMetrics
 import com.liferay.portal.workflow.metrics.search.background.task.WorkflowMetricsReindexStatusMessageSender;
 import com.liferay.portal.workflow.metrics.search.index.InstanceWorkflowMetricsIndexer;
 import com.liferay.portal.workflow.metrics.search.index.constants.WorkflowMetricsIndexNameConstants;
+import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexer;
 
 import java.util.Collections;
 import java.util.Date;
@@ -35,25 +38,47 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Rafael Praxedes
  */
-@Component(service = IndexReindexer.class)
-public class InstanceWorkflowMetricsReindexer implements IndexReindexer { //rename class
+@Component(service = {IndexReindexer.class, WorkflowMetricsReindexer.class})
+public class InstanceWorkflowMetricsReindexer
+	implements IndexReindexer, WorkflowMetricsReindexer {
 
+	@Override
+	public String getIndexNameSuffix() {
+		return WorkflowMetricsIndexNameConstants.SUFFIX_INSTANCE;
+	}
+
+	@Override
 	public String getKey() {
 		return "instance";
+	}
+
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 *             #reindex(long, ExecutionMode)}
+	 */
+	@Deprecated
+	@Override
+	public void reindex(long companyId) throws PortalException {
+		try {
+			reindex(companyId, ExecutionMode.FULL);
+		}
+		catch (PortalException portalException) {
+			throw portalException;
+		}
+		catch (Exception exception) {
+			throw new PortalException(exception);
+		}
 	}
 
 	@Override
 	public void reindex(long companyId, ExecutionMode executionMode)
 		throws Exception {
 
-		if (!_searchCapabilities.isResultRankingsSupported() ||
+		if (!_searchCapabilities.isWorkflowMetricsSupported() ||
 			(companyId == CompanyConstants.SYSTEM)) {
 
 			return;
 		}
-
-		WorkflowMetricsIndex workflowMetricsIndex =
-				WorkflowMetricsIndex.toWorkflowMetricsIndex(getKey());
 
 		Date date = null;
 
@@ -63,13 +88,16 @@ public class InstanceWorkflowMetricsReindexer implements IndexReindexer { //rena
 			Thread.sleep(1000);
 		}
 		else {
+			WorkflowMetricsIndex workflowMetricsIndex =
+				WorkflowMetricsIndex.toWorkflowMetricsIndex(getKey());
+
 			workflowMetricsIndex.removeIndex(
-				_searchCapabilities, _searchEngineAdapter,
-				_indexNameBuilder, companyId);
+				_searchCapabilities, _searchEngineAdapter, _indexNameBuilder,
+				companyId);
 
 			workflowMetricsIndex.createIndex(
-				_searchCapabilities, _searchEngineAdapter,
-				_indexNameBuilder, companyId);
+				_searchCapabilities, _searchEngineAdapter, _indexNameBuilder,
+				companyId);
 		}
 
 		ActionableDynamicQuery actionableDynamicQuery =
@@ -99,10 +127,10 @@ public class InstanceWorkflowMetricsReindexer implements IndexReindexer { //rena
 				}
 
 				_instanceWorkflowMetricsIndexer.addInstance(
-					_indexerHelper2.createAssetTitleLocalizationMap(
+					_indexerHelper.createAssetTitleLocalizationMap(
 						kaleoInstance.getClassName(),
 						kaleoInstance.getClassPK(), kaleoInstance.getGroupId()),
-					_indexerHelper2.createAssetTypeLocalizationMap(
+					_indexerHelper.createAssetTypeLocalizationMap(
 						kaleoInstance.getClassName(),
 						kaleoInstance.getGroupId()),
 					kaleoInstance.getClassName(), kaleoInstance.getClassPK(),
@@ -147,19 +175,13 @@ public class InstanceWorkflowMetricsReindexer implements IndexReindexer { //rena
 	private static final Snapshot<SyncReindexManager>
 		_syncReindexManagerSnapshot = new Snapshot<>(
 			InstanceWorkflowMetricsReindexer.class, SyncReindexManager.class,
-		null, true);
+			null, true);
+
+	@Reference
+	private IndexerHelper _indexerHelper;
 
 	@Reference
 	private IndexNameBuilder _indexNameBuilder;
-
-	@Reference
-	private SearchCapabilities _searchCapabilities;
-
-	@Reference
-	private SearchEngineAdapter _searchEngineAdapter;
-
-	@Reference
-	private IndexerHelper2 _indexerHelper2;
 
 	@Reference
 	private InstanceWorkflowMetricsIndexer _instanceWorkflowMetricsIndexer;
@@ -170,6 +192,12 @@ public class InstanceWorkflowMetricsReindexer implements IndexReindexer { //rena
 
 	@Reference
 	private KaleoInstanceLocalService _kaleoInstanceLocalService;
+
+	@Reference
+	private SearchCapabilities _searchCapabilities;
+
+	@Reference
+	private SearchEngineAdapter _searchEngineAdapter;
 
 	@Reference
 	private WorkflowMetricsReindexStatusMessageSender
