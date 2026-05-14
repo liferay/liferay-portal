1fae83fdbb58fece188c2662d39cdc2e3102757a
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-05-14 11:08:04 -0300

    [LPD-75968] portal-workflow-metrics-service: Migrate TransitionWorkflowMetricsReindexer to dual SPI
    https://liferay.atlassian.net/browse/LPD-75968

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/reindexer/TransitionWorkflowMetricsReindexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/reindexer/TransitionWorkflowMetricsReindexer.java
@@ -3,21 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.workflow.kaleo.metrics.integration.internal.search.index.reindexer;
+package com.liferay.portal.workflow.metrics.internal.search.index.reindexer;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.module.service.Snapshot;
+import com.liferay.portal.search.capabilities.SearchCapabilities;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.search.index.SyncReindexManager;
+import com.liferay.portal.search.spi.reindexer.IndexReindexer;
 import com.liferay.portal.workflow.kaleo.metrics.integration.helper.IndexerHelper;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
 import com.liferay.portal.workflow.kaleo.model.KaleoTransition;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoTransitionLocalService;
+import com.liferay.portal.workflow.metrics.internal.search.index.WorkflowMetricsIndex;
 import com.liferay.portal.workflow.metrics.search.background.task.WorkflowMetricsReindexStatusMessageSender;
 import com.liferay.portal.workflow.metrics.search.index.TransitionWorkflowMetricsIndexer;
+import com.liferay.portal.workflow.metrics.search.index.constants.WorkflowMetricsIndexNameConstants;
 import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexer;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.osgi.service.component.annotations.Component;
@@ -26,17 +37,68 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Rafael Praxedes
  */
-@Component(service = WorkflowMetricsReindexer.class)
+@Component(service = {IndexReindexer.class, WorkflowMetricsReindexer.class})
 public class TransitionWorkflowMetricsReindexer
-	implements WorkflowMetricsReindexer {
+	implements IndexReindexer, WorkflowMetricsReindexer {
+
+	@Override
+	public String getIndexNameSuffix() {
+		return WorkflowMetricsIndexNameConstants.SUFFIX_TRANSITION;
+	}
 
 	@Override
 	public String getKey() {
 		return "transition";
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
+	 *             #reindex(long, ExecutionMode)}
+	 */
+	@Deprecated
 	@Override
 	public void reindex(long companyId) throws PortalException {
+		try {
+			reindex(companyId, ExecutionMode.FULL);
+		}
+		catch (PortalException portalException) {
+			throw portalException;
+		}
+		catch (Exception exception) {
+			throw new PortalException(exception);
+		}
+	}
+
+	@Override
+	public void reindex(long companyId, ExecutionMode executionMode)
+		throws Exception {
+
+		if (!_searchCapabilities.isWorkflowMetricsSupported() ||
+			(companyId == CompanyConstants.SYSTEM)) {
+
+			return;
+		}
+
+		Date date = null;
+
+		if (_isExecuteSyncReindex(executionMode)) {
+			date = new Date();
+
+			Thread.sleep(1000);
+		}
+		else {
+			WorkflowMetricsIndex workflowMetricsIndex =
+				WorkflowMetricsIndex.toWorkflowMetricsIndex(getKey());
+
+			workflowMetricsIndex.removeIndex(
+				_searchCapabilities, _searchEngineAdapter, _indexNameBuilder,
+				companyId);
+
+			workflowMetricsIndex.createIndex(
+				_searchCapabilities, _searchEngineAdapter, _indexNameBuilder,
+				companyId);
+		}
+
 		ActionableDynamicQuery actionableDynamicQuery =
 			_kaleoTransitionLocalService.getActionableDynamicQuery();
 
@@ -72,10 +134,41 @@ public class TransitionWorkflowMetricsReindexer
 			});
 
 		actionableDynamicQuery.performActions();
+
+		if (_isExecuteSyncReindex(executionMode)) {
+			SyncReindexManager syncReindexManager =
+				_syncReindexManagerSnapshot.get();
+
+			syncReindexManager.deleteStaleDocuments(
+				WorkflowMetricsIndex.getIndexName(
+					_indexNameBuilder,
+					WorkflowMetricsIndexNameConstants.SUFFIX_TRANSITION,
+					companyId),
+				date, Collections.emptySet());
+		}
 	}
+
+	private boolean _isExecuteSyncReindex(ExecutionMode executionMode) {
+		if ((_syncReindexManagerSnapshot.get() != null) &&
+			(executionMode != null) &&
+			executionMode.equals(ExecutionMode.SYNC)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final Snapshot<SyncReindexManager>
+		_syncReindexManagerSnapshot = new Snapshot<>(
+			TransitionWorkflowMetricsReindexer.class, SyncReindexManager.class,
+			null, true);
 
 	@Reference
 	private IndexerHelper _indexerHelper;
+
+	@Reference
+	private IndexNameBuilder _indexNameBuilder;
 
 	@Reference
 	private KaleoDefinitionVersionLocalService
@@ -83,6 +176,12 @@ public class TransitionWorkflowMetricsReindexer
 
 	@Reference
 	private KaleoTransitionLocalService _kaleoTransitionLocalService;
+
+	@Reference
+	private SearchCapabilities _searchCapabilities;
+
+	@Reference
+	private SearchEngineAdapter _searchEngineAdapter;
 
 	@Reference
 	private TransitionWorkflowMetricsIndexer _transitionWorkflowMetricsIndexer;
