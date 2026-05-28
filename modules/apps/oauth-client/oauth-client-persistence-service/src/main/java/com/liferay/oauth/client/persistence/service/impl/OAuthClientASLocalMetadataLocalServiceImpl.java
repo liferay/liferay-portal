1f0f8c97e0d48a120b41c7e93f0d14d233343b72
Christian Moura <christian.moura@liferay.com>
2026-05-28 10:34:49 -0300

    LPD-92475 Stop persisting "null" for missing local AS metadata URIs

--- modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/service/impl/OAuthClientASLocalMetadataLocalServiceImpl.java
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/service/impl/OAuthClientASLocalMetadataLocalServiceImpl.java
@@ -67,12 +67,17 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 
 		return addOAuthClientASLocalMetadata(
 			null, userId,
-			String.valueOf(
-				authorizationServerMetadata.getAuthorizationEndpointURI()),
-			String.valueOf(authorizationServerMetadata.getIssuer()),
-			String.valueOf(authorizationServerMetadata.getJWKSetURI()), false,
-			String.valueOf(
-				authorizationServerMetadata.getRegistrationEndpointURI()),
+			Objects.toString(
+				authorizationServerMetadata.getAuthorizationEndpointURI(),
+				StringPool.BLANK),
+			Objects.toString(
+				authorizationServerMetadata.getIssuer(), StringPool.BLANK),
+			Objects.toString(
+				authorizationServerMetadata.getJWKSetURI(), StringPool.BLANK),
+			false,
+			Objects.toString(
+				authorizationServerMetadata.getRegistrationEndpointURI(),
+				StringPool.BLANK),
 			StringUtil.split(
 				StringUtil.merge(authorizationServerMetadata.getGrantTypes()),
 				StringPool.COMMA),
@@ -82,7 +87,9 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 			StringUtil.split(
 				_getSubjectTypes(authorizationServerMetadata),
 				StringPool.COMMA),
-			String.valueOf(authorizationServerMetadata.getTokenEndpointURI()),
+			Objects.toString(
+				authorizationServerMetadata.getTokenEndpointURI(),
+				StringPool.BLANK),
 			_getUserInfoEndpointURI(authorizationServerMetadata));
 	}
 
@@ -271,12 +278,17 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 
 		return updateOAuthClientASLocalMetadata(
 			oAuthClientASLocalMetadataId,
-			String.valueOf(
-				authorizationServerMetadata.getAuthorizationEndpointURI()),
-			String.valueOf(authorizationServerMetadata.getIssuer()),
-			String.valueOf(authorizationServerMetadata.getJWKSetURI()), false,
-			String.valueOf(
-				authorizationServerMetadata.getRegistrationEndpointURI()),
+			Objects.toString(
+				authorizationServerMetadata.getAuthorizationEndpointURI(),
+				StringPool.BLANK),
+			Objects.toString(
+				authorizationServerMetadata.getIssuer(), StringPool.BLANK),
+			Objects.toString(
+				authorizationServerMetadata.getJWKSetURI(), StringPool.BLANK),
+			false,
+			Objects.toString(
+				authorizationServerMetadata.getRegistrationEndpointURI(),
+				StringPool.BLANK),
 			StringUtil.split(
 				StringUtil.merge(authorizationServerMetadata.getGrantTypes()),
 				StringPool.COMMA),
@@ -286,7 +298,9 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 			StringUtil.split(
 				_getSubjectTypes(authorizationServerMetadata),
 				StringPool.COMMA),
-			String.valueOf(authorizationServerMetadata.getTokenEndpointURI()),
+			Objects.toString(
+				authorizationServerMetadata.getTokenEndpointURI(),
+				StringPool.BLANK),
 			_getUserInfoEndpointURI(authorizationServerMetadata));
 	}
 
@@ -352,17 +366,30 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 			AuthorizationServerMetadata authorizationServerMetadata =
 				new AuthorizationServerMetadata(new Issuer(issuer));
 
-			authorizationServerMetadata.setAuthorizationEndpointURI(
-				new URI(authorizationEndpoint));
+			if (Validator.isNotNull(authorizationEndpoint)) {
+				authorizationServerMetadata.setAuthorizationEndpointURI(
+					new URI(authorizationEndpoint));
+			}
+
 			authorizationServerMetadata.setGrantTypes(
 				TransformUtil.transformToList(
 					supportedGrantTypes, GrantType::parse));
-			authorizationServerMetadata.setJWKSetURI(new URI(jwksURI));
-			authorizationServerMetadata.setRegistrationEndpointURI(
-				new URI(registrationEndpoint));
+
+			if (Validator.isNotNull(jwksURI)) {
+				authorizationServerMetadata.setJWKSetURI(new URI(jwksURI));
+			}
+
+			if (Validator.isNotNull(registrationEndpoint)) {
+				authorizationServerMetadata.setRegistrationEndpointURI(
+					new URI(registrationEndpoint));
+			}
+
 			authorizationServerMetadata.setScopes(new Scope(supportedScopes));
-			authorizationServerMetadata.setTokenEndpointURI(
-				new URI(tokenEndpoint));
+
+			if (Validator.isNotNull(tokenEndpoint)) {
+				authorizationServerMetadata.setTokenEndpointURI(
+					new URI(tokenEndpoint));
+			}
 
 			return String.valueOf(authorizationServerMetadata.toJSONObject());
 		}
@@ -416,15 +443,25 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 						supportedSubjectTypes, SubjectType::parse),
 					new URI(jwksURI));
 
-			oidcProviderMetadata.setAuthorizationEndpointURI(
-				new URI(authorizationEndpoint));
+			if (Validator.isNotNull(authorizationEndpoint)) {
+				oidcProviderMetadata.setAuthorizationEndpointURI(
+					new URI(authorizationEndpoint));
+			}
+
 			oidcProviderMetadata.setGrantTypes(
 				TransformUtil.transformToList(
 					supportedGrantTypes, GrantType::parse));
 			oidcProviderMetadata.setScopes(new Scope(supportedScopes));
-			oidcProviderMetadata.setTokenEndpointURI(new URI(tokenEndpoint));
-			oidcProviderMetadata.setUserInfoEndpointURI(
-				new URI(userInfoEndpoint));
+
+			if (Validator.isNotNull(tokenEndpoint)) {
+				oidcProviderMetadata.setTokenEndpointURI(
+					new URI(tokenEndpoint));
+			}
+
+			if (Validator.isNotNull(userInfoEndpoint)) {
+				oidcProviderMetadata.setUserInfoEndpointURI(
+					new URI(userInfoEndpoint));
+			}
 
 			return String.valueOf(oidcProviderMetadata.toJSONObject());
 		}
@@ -452,8 +489,9 @@ public class OAuthClientASLocalMetadataLocalServiceImpl
 		if (authorizationServerMetadata instanceof
 				OIDCProviderMetadata oidcProviderMetadata) {
 
-			return String.valueOf(
-				oidcProviderMetadata.getUserInfoEndpointURI());
+			return Objects.toString(
+				oidcProviderMetadata.getUserInfoEndpointURI(),
+				StringPool.BLANK);
 		}
 
 		return StringPool.BLANK;
