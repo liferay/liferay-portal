9d06825905ba902a1d7c433e0f61a57a8824f8cf
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-05-29 12:40:11 +0200

    LPD-90027 Add UnlockDraftLayoutMVCActionCommand for the LayoutContentVersion back action

---
+++ modules/apps/layout/layout-content-web/src/main/java/com/liferay/layout/content/web/internal/portlet/action/UnlockDraftLayoutMVCActionCommand.java
@@ -0,0 +1,55 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.content.web.internal.portlet.action;
+
+import com.liferay.layout.content.web.internal.constants.LayoutContentVersionPortletKeys;
+import com.liferay.layout.manager.LayoutLockManager;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.portlet.ActionRequest;
+import jakarta.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	property = {
+		"jakarta.portlet.name=" + LayoutContentVersionPortletKeys.LAYOUT_CONTENT_VERSION,
+		"mvc.command.name=/layout_content_version/unlock_draft_layout"
+	},
+	service = MVCActionCommand.class
+)
+public class UnlockDraftLayoutMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		_layoutLockManager.unlock(
+			_layoutLocalService.getLayout(themeDisplay.getPlid()),
+			themeDisplay.getUserId());
+
+		sendRedirect(actionRequest, actionResponse);
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutLockManager _layoutLockManager;
+
+}
