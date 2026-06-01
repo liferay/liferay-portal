215530775256b1692f45c1857817da013e76c30c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-01 21:27:13 +0200

    LPD-90027 Hide Simulation entry on history mode

--- modules/apps/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/internal/product/navigation/control/menu/SimulationProductNavigationControlMenuEntry.java
+++ modules/apps/product-navigation/product-navigation-simulation-web/src/main/java/com/liferay/product/navigation/simulation/web/internal/product/navigation/control/menu/SimulationProductNavigationControlMenuEntry.java
@@ -165,7 +165,9 @@ public class SimulationProductNavigationControlMenuEntry
 		String layoutMode = ParamUtil.getString(
 			httpServletRequest, "p_l_mode", Constants.VIEW);
 
-		if (layoutMode.equals(Constants.EDIT)) {
+		if (layoutMode.equals(Constants.EDIT) ||
+			layoutMode.equals(Constants.HISTORY)) {
+
 			return false;
 		}
 
