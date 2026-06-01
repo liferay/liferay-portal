dc481698d5d7052ecb1290517ff2b3ed9d97d91e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-01 21:23:32 +0200

    LPD-90027 Add HISTORY constant in Constants

--- portal-kernel/src/com/liferay/portal/kernel/util/Constants.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/Constants.java
@@ -66,6 +66,8 @@ public interface Constants {
 
 	public static final String GET_TEMP = "get_temp";
 
+	public static final String HISTORY = "history";
+
 	public static final String IMPORT = "import";
 
 	public static final String LOCK = "lock";
