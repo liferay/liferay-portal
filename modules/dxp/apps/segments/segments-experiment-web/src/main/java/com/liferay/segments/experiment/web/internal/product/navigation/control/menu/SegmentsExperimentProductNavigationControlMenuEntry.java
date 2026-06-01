be34f32c3cb65f409e65311b2113dafbec57039d
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-01 21:27:17 +0200

    LPD-90027 Hide SegmentsExperiment entry on history mode

--- modules/dxp/apps/segments/segments-experiment-web/src/main/java/com/liferay/segments/experiment/web/internal/product/navigation/control/menu/SegmentsExperimentProductNavigationControlMenuEntry.java
+++ modules/dxp/apps/segments/segments-experiment-web/src/main/java/com/liferay/segments/experiment/web/internal/product/navigation/control/menu/SegmentsExperimentProductNavigationControlMenuEntry.java
@@ -212,7 +212,9 @@ public class SegmentsExperimentProductNavigationControlMenuEntry
 		String layoutMode = ParamUtil.getString(
 			httpServletRequest, "p_l_mode", Constants.VIEW);
 
-		if (layoutMode.equals(Constants.EDIT)) {
+		if (layoutMode.equals(Constants.EDIT) ||
+			layoutMode.equals(Constants.HISTORY)) {
+
 			return false;
 		}
 
