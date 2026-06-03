4da99323128174f936fec263bddbc38bf3e6ae70
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-03 09:33:27 +0200

    LPD-90199 Add PageSpecificationVersionDTOConverter

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationVersionDTOConverter.java
@@ -0,0 +1,71 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
+
+import com.liferay.headless.admin.site.dto.v1_0.PageSpecification;
+import com.liferay.headless.admin.site.dto.v1_0.PageSpecificationVersion;
+import com.liferay.layout.content.model.LayoutContentVersion;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = DTOConverter.class)
+public class PageSpecificationVersionDTOConverter
+	implements DTOConverter<LayoutContentVersion, PageSpecificationVersion> {
+
+	@Override
+	public String getContentType() {
+		return PageSpecificationVersion.class.getSimpleName();
+	}
+
+	@Override
+	public PageSpecificationVersion toDTO(
+		DTOConverterContext dtoConverterContext,
+		LayoutContentVersion layoutContentVersion) {
+
+		return new PageSpecificationVersion() {
+			{
+				setDateCreated(layoutContentVersion::getCreateDate);
+				setDateModified(layoutContentVersion::getModifiedDate);
+				setExternalReferenceCode(
+					layoutContentVersion::getExternalReferenceCode);
+				setName(
+					() -> layoutContentVersion.getName(
+						LocaleUtil.getSiteDefault()));
+				setPageSpecification(
+					() -> {
+						String data = layoutContentVersion.getData();
+
+						if (Validator.isNull(data)) {
+							return null;
+						}
+
+						return PageSpecification.unsafeToDTO(data);
+					});
+				setStatus(
+					() -> {
+						if (layoutContentVersion.getStatus() ==
+								WorkflowConstants.STATUS_APPROVED) {
+
+							return PageSpecificationVersion.Status.APPROVED;
+						}
+
+						return PageSpecificationVersion.Status.DRAFT;
+					});
+				setStatusDate(layoutContentVersion::getStatusDate);
+				setVersion(layoutContentVersion::getVersion);
+			}
+		};
+	}
+
+}
