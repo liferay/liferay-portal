a6dfeda0c594c2fa37725d1e768dc9b28729c18a
Caio Pinheiro <caio.elias@liferay.com>
2026-06-22 13:35:42 -0300

    LPD-94965 Add ProjectUsageHelper for the data source usage report

---
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/helper/ProjectUsageHelper.java
@@ -0,0 +1,98 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.faro.web.internal.helper;
+
+import com.liferay.batch.engine.pagination.Page;
+import com.liferay.batch.engine.pagination.Pagination;
+import com.liferay.osb.faro.constants.FaroProjectConstants;
+import com.liferay.osb.faro.model.FaroProject;
+import com.liferay.osb.faro.service.FaroProjectLocalService;
+import com.liferay.osb.faro.util.DateUtil;
+import com.liferay.osb.faro.web.internal.model.display.contacts.DataSourceUsage;
+import com.liferay.osb.faro.web.internal.model.display.contacts.DataSourceUsageMetric;
+import com.liferay.osb.faro.web.internal.model.display.contacts.DataSourceUsageMetricDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Caio Pinheiro
+ */
+@Component(service = ProjectUsageHelper.class)
+public class ProjectUsageHelper {
+
+	public Page<DataSourceUsageMetricDisplay> getDataSourceUsageMetricDisplays(
+			String endDateString, int page, int pageSize,
+			String startDateString)
+		throws Exception {
+
+		List<DataSourceUsageMetricDisplay> dataSourceUsageMetricDisplays =
+			new ArrayList<>();
+
+		for (FaroProject faroProject :
+				_faroProjectLocalService.getFaroProjects(
+					(page - 1) * pageSize, page * pageSize)) {
+
+			dataSourceUsageMetricDisplays.add(
+				_getDataSourceUsageMetricDisplay(faroProject));
+		}
+
+		return Page.of(
+			dataSourceUsageMetricDisplays, Pagination.of(page, pageSize),
+			_faroProjectLocalService.getFaroProjectsCount());
+	}
+
+	private DataSourceUsageMetricDisplay _getDataSourceUsageMetricDisplay(
+			FaroProject faroProject)
+		throws Exception {
+
+		List<DataSourceUsage> dataSourceUsages = _getDataSourceUsages();
+
+		return new DataSourceUsageMetricDisplay(
+			5, dataSourceUsages.size(), faroProject.getCorpProjectName(),
+			faroProject.getCorpProjectUuid(), dataSourceUsages,
+			DateUtil.formatDate(
+				new Date(faroProject.getLastAccessTime()),
+				DateUtil.PATTERN_DATE),
+			DateUtil.formatDate(
+				faroProject.getLastAnniversaryDate(), DateUtil.PATTERN_DATE),
+			!StringUtil.equals(
+				faroProject.getState(), FaroProjectConstants.STATE_READY),
+			3, faroProject.getWeDeployKey());
+	}
+
+	private List<DataSourceUsageMetric> _getDataSourceUsageMetrics(
+		int dataSourceIndex) {
+
+		return ListUtil.fromArray(
+			new DataSourceUsageMetric(
+				1000 - (dataSourceIndex * 100), "2026-06-17",
+				100000 - (dataSourceIndex * 12000),
+				60 + (dataSourceIndex * 25)),
+			new DataSourceUsageMetric(
+				940 - (dataSourceIndex * 100), "2026-06-16",
+				98230 - (dataSourceIndex * 12000),
+				63 + (dataSourceIndex * 25)));
+	}
+
+	private List<DataSourceUsage> _getDataSourceUsages() {
+		return ListUtil.fromArray(
+			new DataSourceUsage(
+				"10001", "Liferay", _getDataSourceUsageMetrics(0)),
+			new DataSourceUsage(
+				"10002", "Salesforce", _getDataSourceUsageMetrics(1)));
+	}
+
+	@Reference
+	private FaroProjectLocalService _faroProjectLocalService;
+
+}
