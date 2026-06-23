a507441d44d5d8c312ca6f5c77a6d07601409891
IstvanD <istvan.dezsi@liferay.com>
2026-06-23 14:16:56 +0200

    LPD-82361 Move the change tracking logo reference to CompanyInfo

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/reference/CompanyInfoTableReferenceDefinition.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/reference/CompanyInfoTableReferenceDefinition.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -8,43 +8,52 @@ package com.liferay.change.tracking.internal.spi.reference;
 import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
 import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
 import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
+import com.liferay.portal.kernel.model.CompanyInfoTable;
 import com.liferay.portal.kernel.model.CompanyTable;
+import com.liferay.portal.kernel.model.ImageTable;
 import com.liferay.portal.kernel.service.persistence.BasePersistence;
-import com.liferay.portal.kernel.service.persistence.CompanyPersistence;
+import com.liferay.portal.kernel.service.persistence.CompanyInfoPersistence;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Preston Crary
+ * @author István András Dézsi
  */
 @Component(service = TableReferenceDefinition.class)
-public class CompanyTableReferenceDefinition
-	implements TableReferenceDefinition<CompanyTable> {
+public class CompanyInfoTableReferenceDefinition
+	implements TableReferenceDefinition<CompanyInfoTable> {
 
 	@Override
 	public void defineChildTableReferences(
-		ChildTableReferenceInfoBuilder<CompanyTable>
+		ChildTableReferenceInfoBuilder<CompanyInfoTable>
 			childTableReferenceInfoBuilder) {
+
+		childTableReferenceInfoBuilder.singleColumnReference(
+			CompanyInfoTable.INSTANCE.logoId, ImageTable.INSTANCE.imageId);
 	}
 
 	@Override
 	public void defineParentTableReferences(
-		ParentTableReferenceInfoBuilder<CompanyTable>
+		ParentTableReferenceInfoBuilder<CompanyInfoTable>
 			parentTableReferenceInfoBuilder) {
+
+		parentTableReferenceInfoBuilder.singleColumnReference(
+			CompanyInfoTable.INSTANCE.companyId,
+			CompanyTable.INSTANCE.companyId);
 	}
 
 	@Override
 	public BasePersistence<?> getBasePersistence() {
-		return _companyPersistence;
+		return _companyInfoPersistence;
 	}
 
 	@Override
-	public CompanyTable getTable() {
-		return CompanyTable.INSTANCE;
+	public CompanyInfoTable getTable() {
+		return CompanyInfoTable.INSTANCE;
 	}
 
 	@Reference
-	private CompanyPersistence _companyPersistence;
+	private CompanyInfoPersistence _companyInfoPersistence;
 
 }
