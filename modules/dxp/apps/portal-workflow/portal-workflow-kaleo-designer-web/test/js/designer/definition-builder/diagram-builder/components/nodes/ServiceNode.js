dcf8c63a3f631ee8a8318c7fcddcc06e7b7394c9
Davyson Melo <davyson.melo@liferay.com>
2026-06-26 16:04:52 -0300

    LPD-95622 test: add service node render test

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/test/js/designer/definition-builder/diagram-builder/components/nodes/ServiceNode.js
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import '@testing-library/jest-dom';
+import {render} from '@testing-library/react';
+import React from 'react';
+
+import ServiceNode from '../../../../../../../src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/nodes/ServiceNode';
+import MockDefinitionBuilderContext from '../../../../../../mock/MockDefinitionBuilderContext';
+import MockDiagramBuilderContext from '../../../../../../mock/MockDiagramBuilderContext';
+
+describe('The ServiceNode component should', () => {
+	let container;
+
+	it('Be rendered with correct icon and default values for label and description, if they are not set', () => {
+		const renderResult = render(
+			<MockDefinitionBuilderContext>
+				<MockDiagramBuilderContext>
+					<ServiceNode />
+				</MockDiagramBuilderContext>
+			</MockDefinitionBuilderContext>
+		);
+
+		container = renderResult.container;
+
+		const icon = container.querySelector('svg.lexicon-icon-cog');
+
+		expect(icon).toBeTruthy();
+
+		const label = container.querySelector('span.node-label');
+		const description = container.querySelector('span.node-description');
+
+		expect(label).toHaveTextContent('service');
+		expect(description).toHaveTextContent(
+			'execute-custom-business-logic-using-a-java-delegate'
+		);
+	});
+
+	it('Be rendered with custom label and description', () => {
+		const renderResult = render(
+			<MockDefinitionBuilderContext>
+				<MockDiagramBuilderContext>
+					<ServiceNode
+						data={{
+							description: 'test description',
+							label: {en_US: 'test label'},
+						}}
+					/>
+				</MockDiagramBuilderContext>
+			</MockDefinitionBuilderContext>
+		);
+
+		container = renderResult.container;
+
+		const label = container.querySelector('span.node-label');
+		const description = container.querySelector('span.node-description');
+
+		expect(label).toHaveTextContent('test label');
+		expect(description).toHaveTextContent('test description');
+	});
+});
