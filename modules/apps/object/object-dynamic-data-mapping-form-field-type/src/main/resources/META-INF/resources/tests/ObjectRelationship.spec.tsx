85e32c4a34bc7ac92cc9ac74f5eca4314ae2aa10
Nathaly Gomes <nathaly.gomes@liferay.com>
2026-06-26 10:29:29 -0300

    LPD-81459 Add tests

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/tests/ObjectRelationship.spec.tsx
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/tests/ObjectRelationship.spec.tsx
@@ -4,8 +4,104 @@
  */
 
 import '@testing-library/jest-dom';
+import {fireEvent, render, waitFor} from '@testing-library/react';
+import {fetch} from 'frontend-js-web';
+import React from 'react';
 
-import {getLabel} from '../js/ObjectRelationship/ObjectRelationship';
+import ObjectRelationship, {
+	getLabel,
+} from '../js/ObjectRelationship/ObjectRelationship';
+
+import type {Locale} from 'dynamic-data-mapping-form-field-type';
+
+jest.mock('frontend-js-web', () => {
+	const originalModule = jest.requireActual('frontend-js-web');
+
+	return {
+		...originalModule,
+		fetch: jest.fn(),
+	};
+});
+
+const API_URL =
+	'http://localhost:8080/o/headless-admin-user/v1.0/organizations';
+
+const DEFAULT_PROPS = {
+	fieldName: 'relationship',
+	inputName: 'relationship',
+	name: 'relationship',
+	objectDefinitionDefaultLanguageId: 'en_US' as Locale,
+	objectEntryId: '0',
+	objectFieldBusinessType: 'Text',
+	onChange: () => {},
+};
+
+describe('fetchData', () => {
+	it('drops the query string when fetching a selected value by id', async () => {
+		(fetch as jest.Mock)
+			.mockResolvedValueOnce({
+				json: () => Promise.resolve({items: []}),
+			})
+			.mockResolvedValueOnce({
+				json: () => Promise.resolve({id: 123}),
+			});
+
+		render(
+			<ObjectRelationship
+				{...DEFAULT_PROPS}
+				apiURL={`${API_URL}?flatten=true`}
+				value="123"
+			/>
+		);
+
+		await waitFor(() =>
+			expect(fetch).toHaveBeenCalledWith(
+				`${API_URL}/123`,
+				expect.anything()
+			)
+		);
+	});
+
+	it('joins the search term with "&" when the apiURL already has a query string', async () => {
+		(fetch as jest.Mock).mockResolvedValue({
+			json: () => Promise.resolve({items: []}),
+		});
+
+		const apiURL = `${API_URL}?flatten=true`;
+
+		const {getByRole} = render(
+			<ObjectRelationship {...DEFAULT_PROPS} apiURL={apiURL} />
+		);
+
+		fireEvent.change(getByRole('textbox'), {target: {value: 'Org B'}});
+
+		await waitFor(() =>
+			expect(fetch).toHaveBeenCalledWith(
+				`${apiURL}&search=Org%20B`,
+				expect.anything()
+			)
+		);
+	});
+
+	it('joins the search term with "?" when the apiURL has no query string', async () => {
+		(fetch as jest.Mock).mockResolvedValue({
+			json: () => Promise.resolve({items: []}),
+		});
+
+		const {getByRole} = render(
+			<ObjectRelationship {...DEFAULT_PROPS} apiURL={API_URL} />
+		);
+
+		fireEvent.change(getByRole('textbox'), {target: {value: 'Org B'}});
+
+		await waitFor(() =>
+			expect(fetch).toHaveBeenCalledWith(
+				`${API_URL}?search=Org%20B`,
+				expect.anything()
+			)
+		);
+	});
+});
 
 describe('getLabel', () => {
 	it('returns as a string the same boolean value passed in booleanField', () => {
---
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/test/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/object/relationship/ObjectRelationshipDDMFormFieldTemplateContextContributorTest.java
@@ -0,0 +1,187 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.dynamic.data.mapping.form.field.type.internal.object.relationship;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.test.util.BaseDDMFormFieldTemplateContextContributorTestCase;
+import com.liferay.object.dynamic.data.mapping.form.field.type.constants.ObjectDDMFormFieldTypeConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.rest.context.path.RESTContextPathResolver;
+import com.liferay.object.rest.context.path.RESTContextPathResolverRegistry;
+import com.liferay.object.scope.ObjectScopeProvider;
+import com.liferay.object.scope.ObjectScopeProviderRegistry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.system.SystemObjectDefinitionManager;
+import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Nathaly Gomes
+ */
+public class ObjectRelationshipDDMFormFieldTemplateContextContributorTest
+	extends BaseDDMFormFieldTemplateContextContributorTestCase {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_ddmFormField.setDDMForm(getDDMForm());
+
+		ObjectDefinitionLocalService objectDefinitionLocalService =
+			Mockito.mock(ObjectDefinitionLocalService.class);
+
+		Mockito.when(
+			objectDefinitionLocalService.fetchObjectDefinition(
+				Mockito.anyLong())
+		).thenReturn(
+			Mockito.mock(ObjectDefinition.class)
+		);
+
+		ObjectScopeProviderRegistry objectScopeProviderRegistry = Mockito.mock(
+			ObjectScopeProviderRegistry.class);
+		ObjectScopeProvider objectScopeProvider = Mockito.mock(
+			ObjectScopeProvider.class);
+
+		Mockito.when(
+			objectScopeProviderRegistry.getObjectScopeProvider(
+				Mockito.nullable(String.class))
+		).thenReturn(
+			objectScopeProvider
+		);
+
+		Portal portal = Mockito.mock(Portal.class);
+
+		Mockito.when(
+			portal.getPathContext()
+		).thenReturn(
+			StringPool.BLANK
+		);
+
+		Mockito.when(
+			portal.getPortalURL(Mockito.any(HttpServletRequest.class))
+		).thenReturn(
+			_PORTAL_URL
+		);
+
+		RESTContextPathResolver restContextPathResolver = Mockito.mock(
+			RESTContextPathResolver.class);
+
+		Mockito.when(
+			restContextPathResolver.getRESTContextPath(Mockito.anyLong())
+		).thenReturn(
+			_REST_CONTEXT_PATH
+		);
+
+		RESTContextPathResolverRegistry restContextPathResolverRegistry =
+			Mockito.mock(RESTContextPathResolverRegistry.class);
+
+		Mockito.when(
+			restContextPathResolverRegistry.getRESTContextPathResolver(
+				Mockito.nullable(String.class))
+		).thenReturn(
+			restContextPathResolver
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_jsonFactory", new JSONFactoryImpl());
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_objectDefinitionLocalService", objectDefinitionLocalService);
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_objectScopeProviderRegistry", objectScopeProviderRegistry);
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_portal", portal);
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_restContextPathResolverRegistry",
+			restContextPathResolverRegistry);
+		ReflectionTestUtil.setFieldValue(
+			_objectRelationshipDDMFormFieldTemplateContextContributor,
+			"_systemObjectDefinitionManagerRegistry",
+			_systemObjectDefinitionManagerRegistry);
+	}
+
+	@Test
+	public void testGetParametersURL() {
+		String additionalAPIURLParameters = RandomTestUtil.randomString();
+
+		Assert.assertEquals(
+			StringBundler.concat(
+				_PORTAL_URL, _REST_CONTEXT_PATH, StringPool.QUESTION,
+				additionalAPIURLParameters),
+			_getParametersAPIURL(additionalAPIURLParameters));
+
+		Assert.assertEquals(
+			_PORTAL_URL + _REST_CONTEXT_PATH,
+			_getParametersAPIURL(StringPool.BLANK));
+	}
+
+	private String _getParametersAPIURL(String additionalAPIURLParameters) {
+		SystemObjectDefinitionManager systemObjectDefinitionManager =
+			Mockito.mock(SystemObjectDefinitionManager.class);
+
+		Mockito.when(
+			systemObjectDefinitionManager.getAdditionalAPIURLParameters()
+		).thenReturn(
+			additionalAPIURLParameters
+		);
+
+		Mockito.when(
+			_systemObjectDefinitionManagerRegistry.
+				getSystemObjectDefinitionManager(Mockito.nullable(String.class))
+		).thenReturn(
+			systemObjectDefinitionManager
+		);
+
+		return MapUtil.getString(
+			_objectRelationshipDDMFormFieldTemplateContextContributor.
+				getParameters(
+					_ddmFormField, createDDMFormFieldRenderingContext()),
+			"apiURL");
+	}
+
+	private static final String _PORTAL_URL = RandomTestUtil.randomString();
+
+	private static final String _REST_CONTEXT_PATH =
+		RandomTestUtil.randomString();
+
+	private final DDMFormField _ddmFormField = new DDMFormField(
+		RandomTestUtil.randomString(),
+		ObjectDDMFormFieldTypeConstants.OBJECT_RELATIONSHIP);
+	private final ObjectRelationshipDDMFormFieldTemplateContextContributor
+		_objectRelationshipDDMFormFieldTemplateContextContributor =
+			new ObjectRelationshipDDMFormFieldTemplateContextContributor();
+	private final SystemObjectDefinitionManagerRegistry
+		_systemObjectDefinitionManagerRegistry = Mockito.mock(
+			SystemObjectDefinitionManagerRegistry.class);
+
+}
