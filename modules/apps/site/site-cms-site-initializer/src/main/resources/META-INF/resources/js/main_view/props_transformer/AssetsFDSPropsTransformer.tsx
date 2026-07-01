035e9bec3250d594b840cb7d89ffad0ae58bf0d4
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-07-01 23:33:51 +0200

    LPD-90341 Reuse the shared confirmation modal helper for the reset permissions bulk action

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFDSPropsTransformer.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/AssetsFDSPropsTransformer.tsx
@@ -27,6 +27,7 @@ import {
 } from '../../common/utils/constants';
 import {getFormattedLabel} from '../../common/utils/getFormattedText';
 import {getScopeExternalReferenceCode} from '../../common/utils/getScopeExternalReferenceCode';
+import {openBulkActionConfirmationModal} from '../../common/utils/openBulkActionConfirmationModal';
 import {openCMSModal} from '../../common/utils/openCMSModal';
 import EditAssetCategoriesModalContent from '../categorization/modal/EditAssetCategoriesModalContent';
 import EditAssetTagsModalContent from '../categorization/modal/EditAssetTagsModalContent';
@@ -814,13 +815,22 @@ export default function AssetsFDSPropsTransformer({
 				});
 			}
 			else if (action?.data?.id === 'reset-to-default-permissions') {
-				openResetAssetPermissionModal({
-					loadData: () => {
+				openBulkActionConfirmationModal({
+					confirmDisplayType: 'warning',
+					confirmLabel: Liferay.Language.get('confirm'),
+					message: Liferay.Language.get(
+						'are-you-sure-you-want-to-reset-the-permissions-to-the-default-values'
+					),
+					onConfirm: () => {
 						executeResetPermissionObjectBulkSelectionAction({
 							apiURL: bulkActionAPIURL,
 							selectedData,
 						});
 					},
+					status: 'warning',
+					title: Liferay.Language.get(
+						'confirm-reset-to-default-permissions'
+					),
 				});
 			}
 			else if (
