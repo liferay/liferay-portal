a50922cee9939775c8a194163f04a7e697a55d6c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-07-02 11:38:45 +0200

    LPD-90201 buildService

--- modules/apps/layout/layout-content-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/layout/layout-content-service/src/main/resources/META-INF/sql/indexes.sql
@@ -2,4 +2,4 @@ create index IX_4B2F0707 on LayoutContentVersion (groupId, dataHash[$COLUMN_LENG
 create unique index IX_D4C862E2 on LayoutContentVersion (groupId, externalReferenceCode[$COLUMN_LENGTH:75$]);
 create index IX_813BD3E1 on LayoutContentVersion (groupId, status);
 create index IX_A2BB1180 on LayoutContentVersion (plid, status);
-create index IX_DC2DF6AE on LayoutContentVersion (plid, version);
+create unique index IX_DC2DF6AE on LayoutContentVersion (plid, version);
