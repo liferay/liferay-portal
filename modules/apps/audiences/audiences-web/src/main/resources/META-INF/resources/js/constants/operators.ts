49f7fbce3bc64f218aa6345e177cb8664c9fbcb0
Georgel Pop <georgel.pop@liferay.com>
2026-07-02 18:35:05 +0200

    LPD-94450 Fix the operator labels rendering as raw keys

--- modules/apps/audiences/audiences-web/src/main/resources/META-INF/resources/js/constants/operators.ts
+++ modules/apps/audiences/audiences-web/src/main/resources/META-INF/resources/js/constants/operators.ts
@@ -3,25 +3,25 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const DATE_OPERATOR_LANGUAGE_KEYS: Record<string, string> = {
-	gt: 'is-after',
-	gte: 'is-on-or-after',
-	lt: 'is-before',
-	lte: 'is-on-or-before',
+const DATE_OPERATOR_LABELS: Record<string, string> = {
+	gt: Liferay.Language.get('is-after'),
+	gte: Liferay.Language.get('is-on-or-after'),
+	lt: Liferay.Language.get('is-before'),
+	lte: Liferay.Language.get('is-on-or-before'),
 };
 
-const NUMBER_OPERATOR_LANGUAGE_KEYS: Record<string, string> = {
-	gt: 'is-greater-than',
-	gte: 'is-greater-than-or-equal-to',
-	lt: 'is-less-than',
-	lte: 'is-less-than-or-equal-to',
+const NUMBER_OPERATOR_LABELS: Record<string, string> = {
+	gt: Liferay.Language.get('is-greater-than'),
+	gte: Liferay.Language.get('is-greater-than-or-equal-to'),
+	lt: Liferay.Language.get('is-less-than'),
+	lte: Liferay.Language.get('is-less-than-or-equal-to'),
 };
 
-const SHARED_OPERATOR_LANGUAGE_KEYS: Record<string, string> = {
-	eq: 'is',
-	includes: 'contains',
-	not_eq: 'is-not',
-	not_includes: 'does-not-contain',
+const SHARED_OPERATOR_LABELS: Record<string, string> = {
+	eq: Liferay.Language.get('is'),
+	includes: Liferay.Language.get('contains'),
+	not_eq: Liferay.Language.get('is-not'),
+	not_includes: Liferay.Language.get('does-not-contain'),
 };
 
 const EQUALITY_OPERATORS = ['eq', 'not_eq'];
@@ -33,13 +33,13 @@ const SET_OPERATORS = ['includes', 'not_includes'];
 const TEXT_OPERATORS = ['eq', 'includes', 'not_eq', 'not_includes'];
 
 export function getOperatorLabel(operator: string, inputType: string): string {
-	const languageKey =
-		SHARED_OPERATOR_LANGUAGE_KEYS[operator] ||
+	return (
+		SHARED_OPERATOR_LABELS[operator] ||
 		(inputType === 'date'
-			? DATE_OPERATOR_LANGUAGE_KEYS[operator]
-			: NUMBER_OPERATOR_LANGUAGE_KEYS[operator]);
-
-	return languageKey ? Liferay.Language.get(languageKey) : operator;
+			? DATE_OPERATOR_LABELS[operator]
+			: NUMBER_OPERATOR_LABELS[operator]) ||
+		operator
+	);
 }
 
 export function getOperators(inputType: string, type: string): string[] {
