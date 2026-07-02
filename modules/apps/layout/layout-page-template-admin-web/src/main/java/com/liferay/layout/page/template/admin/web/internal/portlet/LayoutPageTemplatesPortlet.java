64d7272912ce9463e7be523d817da88274161cd5
Vendel Töreki <vendel.toreki@liferay.com>
2026-07-02 21:37:56 +0200

    LPD-96982 Fix staging publish form error for group page templates

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/LayoutPageTemplatesPortlet.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/LayoutPageTemplatesPortlet.java
@@ -61,6 +61,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.css-class-wrapper=portlet-layout-templates-admin",
 		"com.liferay.portlet.icon=/icons/group_pages.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
