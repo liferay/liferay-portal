5899d70f1be8139142c63fa0d788981ec25e36a9
Larissa Ribeiro <larissa.ribeiro@liferay.com>
2026-07-03 15:32:14 -0300

    LPD-93350 Show the Content Coverage Matrix in the project view

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/fragment/renderer/ContentCoverageMatrixComponentSectionFragmentRenderer.java
@@ -0,0 +1,134 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.fragment.renderer;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.cmp.site.initializer.internal.util.ActionUtil;
+import com.liferay.site.cmp.site.initializer.internal.util.ObjectEntryUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Larissa Ribeiro
+ */
+@Component(service = FragmentRenderer.class)
+public class ContentCoverageMatrixComponentSectionFragmentRenderer
+	extends BaseComponentSectionFragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "sections";
+	}
+
+	@Override
+	protected String getComponentName(HttpServletRequest httpServletRequest) {
+		return "ContentGapMatrixCard";
+	}
+
+	@Override
+	protected String getLabelKey() {
+		return "content-coverage-matrix";
+	}
+
+	@Override
+	protected String getModuleName() {
+		return "site-cmp-site-initializer";
+	}
+
+	@Override
+	protected Map<String, Object> getProps(
+		FragmentRendererContext fragmentRendererContext,
+		HttpServletRequest httpServletRequest) {
+
+		ObjectEntry objectEntry = ObjectEntryUtil.getObjectEntry(
+			httpServletRequest);
+
+		if (objectEntry == null) {
+			return null;
+		}
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.fetchObjectDefinition(
+				objectEntry.getObjectDefinitionId());
+
+		if (objectDefinition == null) {
+			return null;
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return HashMapBuilder.<String, Object>put(
+			"editProjectURL",
+			StringBundler.concat(
+				ActionUtil.getBaseEditProjectURL(
+					objectDefinition, themeDisplay),
+				objectEntry.getObjectEntryId(), "?redirect=",
+				themeDisplay.getURLCurrent())
+		).put(
+			"hasFunnelStagesOrPersonas",
+			() -> {
+				AssetVocabulary funnelStageAssetVocabulary =
+					_assetVocabularyLocalService.
+						fetchAssetVocabularyByExternalReferenceCode(
+							"L_CMP_FUNNEL_STAGE",
+							themeDisplay.getSiteGroupId());
+				AssetVocabulary personasAssetVocabulary =
+					_assetVocabularyLocalService.
+						fetchAssetVocabularyByExternalReferenceCode(
+							"L_CMP_PERSONAS", themeDisplay.getSiteGroupId());
+
+				for (AssetCategory assetCategory :
+						_assetCategoryLocalService.getCategories(
+							objectEntry.getModelClassName(),
+							objectEntry.getObjectEntryId())) {
+
+					if (((funnelStageAssetVocabulary != null) &&
+						 (assetCategory.getVocabularyId() ==
+							 funnelStageAssetVocabulary.getVocabularyId())) ||
+						((personasAssetVocabulary != null) &&
+						 (assetCategory.getVocabularyId() ==
+							 personasAssetVocabulary.getVocabularyId()))) {
+
+						return true;
+					}
+				}
+
+				return false;
+			}
+		).put(
+			"projectId", objectEntry.getObjectEntryId()
+		).build();
+	}
+
+	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+}
--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -8,6 +8,7 @@ export {default as EditorToolbar} from './components/EditorToolbar';
 export {default as ProjectSelector} from './components/ProjectSelector';
 export {default as StateSelector} from './components/StateSelector';
 export {default as Tags} from './components/Tags';
+export {default as ContentGapMatrixCard} from './components/content_gap_matrix/ContentGapMatrixCard';
 export {default as Categorization} from './components/project/Categorization';
 export {default as ProjectHistory} from './components/project/ProjectHistory';
 export {default as ProjectInfoSummary} from './components/project/ProjectInfoSummary';
