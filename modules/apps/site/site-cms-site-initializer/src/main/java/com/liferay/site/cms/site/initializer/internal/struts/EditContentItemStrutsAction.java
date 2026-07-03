e446b369c9184fa16dcd4a041e9d59ce391b6c51
Pedro Leite <pedro.leite@liferay.com>
2026-07-03 10:55:42 -0300

    LPD-94024 [BE] Resolve content edit links by external reference code

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/struts/EditContentItemStrutsAction.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/struts/EditContentItemStrutsAction.java
@@ -11,14 +11,17 @@ import com.liferay.fragment.service.FragmentEntryLinkService;
 import com.liferay.info.item.InfoItemServiceRegistry;
 import com.liferay.info.search.InfoSearchClassMapperRegistry;
 import com.liferay.layout.manager.FormManager;
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryService;
 import com.liferay.portal.kernel.struts.StrutsAction;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.site.cms.site.initializer.internal.util.ActionUtil;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -43,8 +46,7 @@ public class EditContentItemStrutsAction implements StrutsAction {
 			HttpServletResponse httpServletResponse)
 		throws Exception {
 
-		ObjectEntry objectEntry = _objectEntryService.getObjectEntry(
-			ParamUtil.getLong(httpServletRequest, "objectEntryId"));
+		ObjectEntry objectEntry = _getObjectEntry(httpServletRequest);
 
 		String editURL = ActionUtil.getEditURL(
 			_formManager, _fragmentEntryLinkListenerRegistry,
@@ -79,6 +81,32 @@ public class EditContentItemStrutsAction implements StrutsAction {
 		httpServletResponse.sendRedirect(editURL);
 
 		return null;
+	}
+
+	private ObjectEntry _getObjectEntry(HttpServletRequest httpServletRequest)
+		throws Exception {
+
+		long objectEntryId = ParamUtil.getLong(
+			httpServletRequest, "objectEntryId");
+
+		if (objectEntryId > 0) {
+			return _objectEntryService.getObjectEntry(objectEntryId);
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.getObjectDefinition(
+				themeDisplay.getCompanyId(),
+				ParamUtil.getString(
+					httpServletRequest, "objectDefinitionName"));
+
+		return _objectEntryService.getObjectEntry(
+			ParamUtil.getString(httpServletRequest, "externalReferenceCode"),
+			ParamUtil.getLong(httpServletRequest, "groupId"),
+			objectDefinition.getObjectDefinitionId());
 	}
 
 	@Reference
