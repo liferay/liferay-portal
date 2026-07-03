21b3f14965edbf8e470ba66696f6e8efa3b23d57
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-07-03 14:33:41 +0200

    LPD-90201 Implement LayoutContentVersionCreator in layout-content-service

---
+++ modules/apps/layout/layout-content-service/src/main/java/com/liferay/layout/content/internal/creator/LayoutContentVersionCreatorImpl.java
@@ -0,0 +1,69 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.content.internal.creator;
+
+import com.liferay.layout.content.creator.LayoutContentVersionCreator;
+import com.liferay.layout.content.provider.LayoutContentVersionDataProvider;
+import com.liferay.layout.content.service.LayoutContentVersionLocalService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = LayoutContentVersionCreator.class)
+public class LayoutContentVersionCreatorImpl
+	implements LayoutContentVersionCreator {
+
+	@Override
+	public void createLayoutContentVersion(Layout layout) {
+		try {
+			if (!layout.isDraftLayout()) {
+				return;
+			}
+
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			if (serviceContext == null) {
+				serviceContext = new ServiceContext();
+
+				serviceContext.setCompanyId(layout.getCompanyId());
+				serviceContext.setUserId(layout.getUserId());
+			}
+
+			_layoutContentVersionLocalService.addLayoutContentVersion(
+				null, layout.getUserId(),
+				_layoutContentVersionDataProvider.getLayoutContentVersionData(
+					layout, serviceContext),
+				layout.getNameMap(), layout.getPlid(),
+				WorkflowConstants.STATUS_APPROVED);
+		}
+		catch (Exception exception) {
+			_log.error(
+				"Unable to add layout content version for plid " +
+					layout.getPlid(),
+				exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutContentVersionCreatorImpl.class);
+
+	@Reference
+	private LayoutContentVersionDataProvider _layoutContentVersionDataProvider;
+
+	@Reference
+	private LayoutContentVersionLocalService _layoutContentVersionLocalService;
+
+}
