71a4b4102d0efc50b4862b130babbe30ff9c4979
Nícolas Moura <nicolas.moura@liferay.com>
2026-07-04 12:36:18 -0300

    LPD-58498 Add the missing Meta.AD name to the configuration

--- modules/apps/portal-security/portal-security-script-management-api/src/main/java/com/liferay/portal/security/script/management/configuration/ScriptManagementConfiguration.java
+++ modules/apps/portal-security/portal-security-script-management-api/src/main/java/com/liferay/portal/security/script/management/configuration/ScriptManagementConfiguration.java
@@ -23,7 +23,11 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface ScriptManagementConfiguration {
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(
+		deflt = "false",
+		name = "allow-script-content-to-be-executed-or-included",
+		required = false
+	)
 	public boolean allowScriptContentToBeExecutedOrIncluded();
 
 }
