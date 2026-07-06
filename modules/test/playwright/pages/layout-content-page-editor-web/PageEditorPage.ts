f628a19c99e1a8e7d325652e6638f2444fee15c6
balazssk <balazs.safrany-kovalik@liferay.com>
2026-07-06 10:42:16 +0200

    LPD-97224 Select the Swap Fragment folder by its link

--- modules/test/playwright/pages/layout-content-page-editor-web/PageEditorPage.ts
+++ modules/test/playwright/pages/layout-content-page-editor-web/PageEditorPage.ts
@@ -2098,9 +2098,7 @@ export class PageEditorPage {
 			target: iframe.locator('.card', {
 				hasText: fragmentName,
 			}),
-			trigger: iframe.locator('.card', {
-				hasText: folder,
-			}),
+			trigger: iframe.getByRole('link', {name: folder}),
 		});
 
 		await clickAndExpectToBeHidden({
