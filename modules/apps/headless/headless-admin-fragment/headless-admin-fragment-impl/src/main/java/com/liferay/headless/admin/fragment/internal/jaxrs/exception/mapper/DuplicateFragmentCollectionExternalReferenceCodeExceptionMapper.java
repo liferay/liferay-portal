162ccb69e3518c1ec90700d3ff126ff0fb9ae24f
Rubén Pulido <ruben.pulido@liferay.com>
2026-07-06 19:05:43 +0200

    LPD-88489 Apply also on a duplicate fragment set or fragment external reference code

---
+++ modules/apps/headless/headless-admin-fragment/headless-admin-fragment-impl/src/main/java/com/liferay/headless/admin/fragment/internal/jaxrs/exception/mapper/DuplicateFragmentCollectionExternalReferenceCodeExceptionMapper.java
@@ -0,0 +1,54 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.fragment.internal.jaxrs.exception.mapper;
+
+import com.liferay.fragment.exception.DuplicateFragmentCollectionExternalReferenceCodeException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.Fragment)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.Fragment.DuplicateFragmentCollectionExternalReferenceCodeExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DuplicateFragmentCollectionExternalReferenceCodeExceptionMapper
+	extends BaseExceptionMapper
+		<DuplicateFragmentCollectionExternalReferenceCodeException> {
+
+	@Override
+	protected Problem getProblem(
+		DuplicateFragmentCollectionExternalReferenceCodeException
+			duplicateFragmentCollectionExternalReferenceCodeException) {
+
+		return new Problem(
+			Response.Status.CONFLICT,
+			_language.get(
+				_acceptLanguage.getPreferredLocale(),
+				"this-external-reference-code-is-already-in-use"));
+	}
+
+	@Context
+	private AcceptLanguage _acceptLanguage;
+
+	@Reference
+	private Language _language;
+
+}
