564ea586d3994e558b6d08b6d1f6cd124d0e1355
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-07-06 17:45:45 +0200

    LPD-97197 Add unit test coverage for companyId propagation in EditDispatchTriggerMVCActionCommand
    Ensures _sendMessage attaches the trigger's own companyId to the
    dispatched Message so Run Now works correctly under database
    partitioning.

---
+++ modules/apps/dispatch/dispatch-web/src/test/java/com/liferay/dispatch/web/internal/portlet/action/EditDispatchTriggerMVCActionCommandTest.java
@@ -0,0 +1,95 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dispatch.web.internal.portlet.action;
+
+import com.liferay.dispatch.constants.DispatchConstants;
+import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.dispatch.service.DispatchTriggerLocalService;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Magdalena Jedraszak
+ */
+public class EditDispatchTriggerMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void test() throws Exception {
+		long companyId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			_dispatchTrigger.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		long dispatchTriggerId = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			_dispatchTriggerLocalService.getDispatchTrigger(dispatchTriggerId)
+		).thenReturn(
+			_dispatchTrigger
+		);
+
+		ReflectionTestUtil.invoke(
+			_editDispatchTriggerMVCActionCommand, "_sendMessage",
+			new Class<?>[] {long.class}, dispatchTriggerId);
+
+		ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(
+			Message.class);
+
+		Mockito.verify(
+			_messageBus
+		).sendMessage(
+			Mockito.eq(DispatchConstants.EXECUTOR_DESTINATION_NAME),
+			messageArgumentCaptor.capture()
+		);
+
+		Message message = messageArgumentCaptor.getValue();
+
+		Assert.assertEquals(companyId, message.getLong("companyId"));
+	}
+
+	@Mock
+	private DispatchTrigger _dispatchTrigger;
+
+	@Mock
+	private DispatchTriggerLocalService _dispatchTriggerLocalService;
+
+	@InjectMocks
+	private final EditDispatchTriggerMVCActionCommand
+		_editDispatchTriggerMVCActionCommand =
+			new EditDispatchTriggerMVCActionCommand();
+
+	@Mock
+	private MessageBus _messageBus;
+
+}
