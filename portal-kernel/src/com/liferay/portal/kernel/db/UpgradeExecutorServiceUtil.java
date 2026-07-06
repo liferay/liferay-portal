4e40e6913a4313ea11fc4f4dca04280cea6065cf
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2026-07-06 12:15:54 +0200

    LPD-96132 Add a shared upgrade executor service util
    Introduce UpgradeExecutorServiceUtil as a common home for the bounded thread pools that upgrade fan-out relies on. It lazily creates two daemon, self-reaping executors through a DCLSingleton: a schema executor sized to the available processors for the few heavy schema operations, and a data executor sized to ninety percent of jdbc.default.maximumPoolSize for the many light row batches. A shutdown method destroys both.

---
+++ portal-kernel/src/com/liferay/portal/kernel/db/UpgradeExecutorServiceUtil.java
@@ -0,0 +1,71 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.db;
+
+import com.liferay.petra.concurrent.DCLSingleton;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.NamedThreadFactory;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class UpgradeExecutorServiceUtil {
+
+	public static ExecutorService getDataExecutorService() {
+		return _dataExecutorServiceDCLSingleton.getSingleton(
+			() -> _getExecutorService(
+				"UpgradeDataExecutor", getDataExecutorServicePoolSize()));
+	}
+
+	public static int getDataExecutorServicePoolSize() {
+		int jdbcDefaultMaximumPoolSize = GetterUtil.getInteger(
+			PropsUtil.get("jdbc.default.maximumPoolSize"));
+
+		return Math.max(1, (int)(0.9 * jdbcDefaultMaximumPoolSize));
+	}
+
+	public static ExecutorService getSchemaExecutorService() {
+		return _schemaExecutorServiceDCLSingleton.getSingleton(
+			() -> {
+				Runtime runtime = Runtime.getRuntime();
+
+				return _getExecutorService(
+					"UpgradeSchemaExecutor", runtime.availableProcessors());
+			});
+	}
+
+	public static void shutdown() {
+		_dataExecutorServiceDCLSingleton.destroy(
+			executorService -> executorService.shutdownNow());
+		_schemaExecutorServiceDCLSingleton.destroy(
+			executorService -> executorService.shutdownNow());
+	}
+
+	private static ExecutorService _getExecutorService(
+		String name, int maximumPoolSize) {
+
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+			maximumPoolSize, maximumPoolSize, 60, TimeUnit.SECONDS,
+			new LinkedBlockingQueue<>(),
+			new NamedThreadFactory(name, Thread.NORM_PRIORITY, null));
+
+		threadPoolExecutor.allowCoreThreadTimeOut(true);
+
+		return threadPoolExecutor;
+	}
+
+	private static final DCLSingleton<ExecutorService>
+		_dataExecutorServiceDCLSingleton = new DCLSingleton<>();
+	private static final DCLSingleton<ExecutorService>
+		_schemaExecutorServiceDCLSingleton = new DCLSingleton<>();
+
+}
