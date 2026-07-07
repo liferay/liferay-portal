e3324be5076191f08c10510f8db485e74a6ff1d5
smottal <117469730+smottal@users.noreply.github.com>
2026-07-07 18:56:54 +0200

    LPD-96681 Fix merge

--- modules/apps/headless/headless-asset-library/headless-asset-library-test/src/testIntegration/java/com/liferay/headless/asset/library/resource/v1_0/test/AssetLibraryResourceTest.java
+++ modules/apps/headless/headless-asset-library/headless-asset-library-test/src/testIntegration/java/com/liferay/headless/asset/library/resource/v1_0/test/AssetLibraryResourceTest.java
@@ -474,10 +474,7 @@ public class AssetLibraryResourceTest extends BaseAssetLibraryResourceTestCase {
 
 		int depotEntryType = DepotConstants.TYPE_ASSET_LIBRARY;
 
-		if (assetLibrary.getType() == AssetLibrary.Type.CATALOG) {
-			depotEntryType = DepotConstants.TYPE_CATALOG;
-		}
-		else if (assetLibrary.getType() == AssetLibrary.Type.DESIGN_LIBRARY) {
+		if (assetLibrary.getType() == AssetLibrary.Type.DESIGN_LIBRARY) {
 			depotEntryType = DepotConstants.TYPE_DESIGN_LIBRARY;
 		}
 		else if (assetLibrary.getType() == AssetLibrary.Type.PROJECT) {
