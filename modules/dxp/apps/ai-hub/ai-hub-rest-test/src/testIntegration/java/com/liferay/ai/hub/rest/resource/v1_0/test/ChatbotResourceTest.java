cecdfaf619936803b2e62d0dfdd6e59cbb285d96
Davyson Melo <davyson.melo@liferay.com>
2026-07-07 07:28:43 -0300

    LPD-95685 test: assert chatbot avatar is returned as an object

--- modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/ChatbotResourceTest.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/ChatbotResourceTest.java
@@ -12,9 +12,11 @@ import com.liferay.account.service.AccountEntryUserRelLocalService;
 import com.liferay.ai.hub.rest.client.dto.v1_0.Chatbot;
 import com.liferay.ai.hub.rest.client.resource.v1_0.ChatbotResource;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.test.util.DLTestUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
@@ -23,9 +25,14 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
@@ -115,6 +122,20 @@ public class ChatbotResourceTest extends BaseChatbotResourceTestCase {
 			HashMapBuilder.<String, Serializable>put(
 				"active", true
 			).put(
+				"avatar",
+				() -> {
+					FileEntry fileEntry = TempFileEntryUtil.addTempFileEntry(
+						TestPropsValues.getGroupId(),
+						TestPropsValues.getUserId(), StringUtil.randomString(),
+						TempFileEntryUtil.getTempFileName(
+							StringUtil.randomString() + ".png"),
+						FileUtil.createTempFile(
+							DLTestUtil.randomTextFileBytes()),
+						ContentTypes.IMAGE_PNG);
+
+					return fileEntry.getFileEntryId();
+				}
+			).put(
 				"externalReferenceCode", chatbotExternalReferenceCode
 			).put(
 				"r_accountToAIHubChatbots_accountEntryId",
@@ -145,6 +166,8 @@ public class ChatbotResourceTest extends BaseChatbotResourceTestCase {
 			postChatbot.getExternalReferenceCode());
 
 		Assert.assertTrue(getChatbot.getActive());
+		Assert.assertNotNull(
+			MapUtil.getString(getChatbot.getAvatar(), "fileURL"));
 		Assert.assertEquals(postChatbot.getTitle(), getChatbot.getTitle());
 	}
 
