41b0d70c7ae5ce5b18a4adfa29c056ded622f92b
Georgel Pop <georgel.pop@liferay.com>
2026-07-07 16:40:35 +0200

    LPD-95291 Test the audience external reference code

--- modules/apps/audiences/audiences-test/src/testIntegration/java/com/liferay/audiences/service/test/AudiencesEntryLocalServiceTest.java
+++ modules/apps/audiences/audiences-test/src/testIntegration/java/com/liferay/audiences/service/test/AudiencesEntryLocalServiceTest.java
@@ -8,12 +8,14 @@ package com.liferay.audiences.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.audiences.exception.AudiencesEntryJSONException;
 import com.liferay.audiences.exception.AudiencesEntryNameException;
+import com.liferay.audiences.exception.DuplicateAudiencesEntryExternalReferenceCodeException;
 import com.liferay.audiences.model.AudiencesEntry;
 import com.liferay.audiences.service.AudiencesEntryLocalService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.AssertUtils;
+import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
@@ -50,35 +52,57 @@ public class AudiencesEntryLocalServiceTest {
 	}
 
 	@Test
+	@TestInfo("LPD-95291")
 	public void testAddAudiencesEntry() throws Exception {
+		String externalReferenceCode = RandomTestUtil.randomString();
 		String name = RandomTestUtil.randomString();
 
 		AudiencesEntry audiencesEntry = _addAudiencesEntry(
+			externalReferenceCode,
 			StringBundler.concat(
 				"{\"conjunction\": \"AND\", \"rules\": [{\"attribute\": ",
 				"\"url\", \"operator\": \"eq\", \"value\": \"",
 				RandomTestUtil.randomString(), "\"}]}"),
 			name);
 
+		Assert.assertEquals(
+			externalReferenceCode, audiencesEntry.getExternalReferenceCode());
 		Assert.assertNotNull(audiencesEntry.getJSON());
 		Assert.assertEquals(name, audiencesEntry.getName());
 
 		AssertUtils.assertFailure(
 			AudiencesEntryNameException.class, null,
-			() -> _addAudiencesEntry(StringPool.BLANK));
+			() -> _addAudiencesEntry(null, StringPool.BLANK, StringPool.BLANK));
 		AssertUtils.assertFailure(
-			AudiencesEntryJSONException.class, null,
+			AudiencesEntryJSONException.class,
+			"/conjunction: INVALID is not a valid enum value",
 			() -> _addAudiencesEntry(
-				"{\"conjunction\": \"INVALID\", \"rules\": []}",
+				null, "{\"conjunction\": \"INVALID\", \"rules\": []}",
 				RandomTestUtil.randomString()));
 	}
 
+	@Test(
+		expected = DuplicateAudiencesEntryExternalReferenceCodeException.class
+	)
+	@TestInfo("LPD-95291")
+	public void testAddAudiencesEntryWithExistingExternalReferenceCode()
+		throws Exception {
+
+		AudiencesEntry audiencesEntry = _addAudiencesEntry(
+			null, StringPool.BLANK, RandomTestUtil.randomString());
+
+		_audiencesEntryLocalService.addAudiencesEntry(
+			audiencesEntry.getExternalReferenceCode(), StringPool.BLANK,
+			RandomTestUtil.randomString(), _serviceContext);
+	}
+
 	@Test
+	@TestInfo("LPD-95291")
 	public void testDeleteAudiencesEntry() throws Exception {
 		AudiencesEntry audiencesEntry =
 			_audiencesEntryLocalService.addAudiencesEntry(
-				null, RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), _serviceContext);
+				null, StringPool.BLANK, RandomTestUtil.randomString(),
+				_serviceContext);
 
 		long audiencesEntryId = audiencesEntry.getAudiencesEntryId();
 
@@ -89,16 +113,20 @@ public class AudiencesEntryLocalServiceTest {
 	}
 
 	@Test
+	@TestInfo("LPD-95291")
 	public void testUpdateAudiencesEntry() throws Exception {
 		AudiencesEntry audiencesEntry = _addAudiencesEntry(
-			RandomTestUtil.randomString());
+			null, StringPool.BLANK, RandomTestUtil.randomString());
 
+		String externalReferenceCode = RandomTestUtil.randomString();
 		String name = RandomTestUtil.randomString();
 
 		audiencesEntry = _audiencesEntryLocalService.updateAudiencesEntry(
-			audiencesEntry.getAudiencesEntryId(), audiencesEntry.getJSON(),
-			name);
+			audiencesEntry.getAudiencesEntryId(), externalReferenceCode,
+			audiencesEntry.getJSON(), name);
 
+		Assert.assertEquals(
+			externalReferenceCode, audiencesEntry.getExternalReferenceCode());
 		Assert.assertEquals(name, audiencesEntry.getName());
 
 		AudiencesEntry updatedAudiencesEntry = audiencesEntry;
@@ -107,24 +135,44 @@ public class AudiencesEntryLocalServiceTest {
 			AudiencesEntryNameException.class, null,
 			() -> _audiencesEntryLocalService.updateAudiencesEntry(
 				updatedAudiencesEntry.getAudiencesEntryId(),
+				updatedAudiencesEntry.getExternalReferenceCode(),
 				updatedAudiencesEntry.getJSON(), StringPool.BLANK));
 		AssertUtils.assertFailure(
-			AudiencesEntryJSONException.class, null,
+			AudiencesEntryJSONException.class,
+			"/conjunction: INVALID is not a valid enum value",
 			() -> _audiencesEntryLocalService.updateAudiencesEntry(
-				updatedAudiencesEntry.getAudiencesEntryId(), StringPool.BLANK,
+				updatedAudiencesEntry.getAudiencesEntryId(),
+				updatedAudiencesEntry.getExternalReferenceCode(),
+				"{\"conjunction\": \"INVALID\", \"rules\": []}",
 				updatedAudiencesEntry.getName()));
 	}
 
-	private AudiencesEntry _addAudiencesEntry(String name) throws Exception {
-		return _addAudiencesEntry(StringPool.BLANK, name);
+	@Test(
+		expected = DuplicateAudiencesEntryExternalReferenceCodeException.class
+	)
+	@TestInfo("LPD-95291")
+	public void testUpdateAudiencesEntryWithExistingExternalReferenceCode()
+		throws Exception {
+
+		AudiencesEntry audiencesEntry1 = _addAudiencesEntry(
+			null, StringPool.BLANK, RandomTestUtil.randomString());
+
+		AudiencesEntry audiencesEntry2 = _addAudiencesEntry(
+			null, StringPool.BLANK, RandomTestUtil.randomString());
+
+		_audiencesEntryLocalService.updateAudiencesEntry(
+			audiencesEntry2.getAudiencesEntryId(),
+			audiencesEntry1.getExternalReferenceCode(),
+			audiencesEntry2.getJSON(), audiencesEntry2.getName());
 	}
 
-	private AudiencesEntry _addAudiencesEntry(String json, String name)
+	private AudiencesEntry _addAudiencesEntry(
+			String externalReferenceCode, String json, String name)
 		throws Exception {
 
 		AudiencesEntry audiencesEntry =
 			_audiencesEntryLocalService.addAudiencesEntry(
-				null, json, name, _serviceContext);
+				externalReferenceCode, json, name, _serviceContext);
 
 		_audiencesEntries.add(audiencesEntry);
 
