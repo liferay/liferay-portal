12ce363c96eccace383ceffd0623a64802096569
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-07-07 10:56:18 +0200

    LPD-97205 Migrate the Message Boards editor image and link tests

--- modules/test/playwright/tests/message-boards-web/main/mbThreadFormatting.spec.ts
+++ modules/test/playwright/tests/message-boards-web/main/mbThreadFormatting.spec.ts
@@ -4,7 +4,10 @@
  */
 
 import {expect, mergeTests} from '@playwright/test';
+import {createReadStream} from 'fs';
+import path from 'path';
 
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
 import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
@@ -12,6 +15,7 @@ import {messageBoardsPagesTest} from '../../../fixtures/messageBoardsTest';
 import getRandomString from '../../../utils/getRandomString';
 
 const test = mergeTests(
+	apiHelpersTest,
 	isolatedSiteTest,
 	featureFlagsTest({'LPD-11235': {enabled: true}}),
 	loginTest(),
@@ -47,3 +51,128 @@ test(
 		).toBeVisible();
 	}
 );
+
+test(
+	'Can add an image from the Document Library to a thread body',
+	{tag: '@LPS-136911'},
+	async ({apiHelpers, messageBoardsEditThreadPage, page, site}) => {
+		const documentTitle = getRandomString().replace(/-/g, '');
+		const subject = getRandomString();
+
+		// Seed an image in the Document Library
+
+		await apiHelpers.headlessDelivery.postDocument(
+			site.id,
+			createReadStream(path.join(__dirname, 'dependencies/image1.jpeg')),
+			{
+				fileName: `${documentTitle}.jpeg`,
+				title: documentTitle,
+			}
+		);
+
+		// Add a thread with the image inserted into the body
+
+		await messageBoardsEditThreadPage.goto(site.friendlyUrlPath);
+
+		await messageBoardsEditThreadPage.subjectSelector.fill(subject);
+
+		await messageBoardsEditThreadPage.insertBodyImage(documentTitle);
+
+		await messageBoardsEditThreadPage.publishButton.click();
+
+		// The image renders in the published thread
+
+		const threadImage = page.locator('.message-content img');
+
+		await expect(threadImage).toHaveAttribute(
+			'src',
+			new RegExp(documentTitle)
+		);
+
+		await expect
+			.poll(() =>
+				threadImage.evaluate(
+					(image: HTMLImageElement) => image.naturalWidth
+				)
+			)
+			.toBeGreaterThan(0);
+	}
+);
+
+test('Can add a link to a document in a thread body', async ({
+	apiHelpers,
+	messageBoardsEditThreadPage,
+	page,
+	site,
+}) => {
+	const documentTitle = getRandomString().replace(/-/g, '');
+
+	// Seed a document to link to
+
+	await apiHelpers.headlessDelivery.postDocument(
+		site.id,
+		createReadStream(path.join(__dirname, 'dependencies/image1.jpeg')),
+		{
+			fileName: `${documentTitle}.jpeg`,
+			title: documentTitle,
+		}
+	);
+
+	// Add a thread that links to the document
+
+	await messageBoardsEditThreadPage.goto(site.friendlyUrlPath);
+
+	await messageBoardsEditThreadPage.subjectSelector.fill(getRandomString());
+
+	await messageBoardsEditThreadPage.insertBodyLinkToDocument(
+		'Link to Document',
+		documentTitle
+	);
+
+	await messageBoardsEditThreadPage.publishButton.click();
+
+	// The link renders in the published thread
+
+	await expect(
+		page
+			.locator('.message-content')
+			.getByRole('link', {name: 'Link to Document'})
+	).toHaveAttribute('href', new RegExp(documentTitle));
+});
+
+test('Can add a link to a page in a thread body', async ({
+	apiHelpers,
+	messageBoardsEditThreadPage,
+	page,
+	site,
+}) => {
+	const pageName = getRandomString().replace(/-/g, '');
+
+	// Seed a page to link to
+
+	await apiHelpers.jsonWebServicesLayout.addLayout({
+		groupId: String(site.id),
+		title: pageName,
+	});
+
+	// Add a thread that links to the page
+
+	await messageBoardsEditThreadPage.goto(site.friendlyUrlPath);
+
+	await messageBoardsEditThreadPage.subjectSelector.fill(getRandomString());
+
+	await messageBoardsEditThreadPage.insertBodyLinkToPage(
+		'Link to Page',
+		pageName
+	);
+
+	await messageBoardsEditThreadPage.publishButton.click();
+
+	// The link renders in the published thread
+
+	await expect(
+		page
+			.locator('.message-content')
+			.getByRole('link', {name: 'Link to Page'})
+	).toHaveAttribute('href', new RegExp(pageName));
+});
