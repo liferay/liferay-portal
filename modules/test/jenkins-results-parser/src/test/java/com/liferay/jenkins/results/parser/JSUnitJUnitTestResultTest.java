0cbde4ee2ea1dcfeab3ddfae2f8ec12fbe010368
Kenji Heigel <kenji.heigel@liferay.com>
2026-07-08 12:04:32 -0700

    LRCI-7525 Add unit test for JS unit test result clay task names

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/JSUnitJUnitTestResultTest.java
@@ -0,0 +1,56 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import org.json.JSONObject;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Kenji Heigel
+ */
+public class JSUnitJUnitTestResultTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testGetTestTaskName() {
+		testEquals(
+			":apps:frontend-js:frontend-js-clay-web:packageRunTest",
+			_getTestTaskName(
+				"liferay-portal.modules.apps.frontend-js.frontend-js-clay-" +
+					"web.clay.clay-button.src.__tests__.index"));
+		testEquals(
+			":apps:frontend-js:frontend-js-web:packageRunTest",
+			_getTestTaskName(
+				"liferay-portal.modules.apps.frontend-js.frontend-js-web.src." +
+					"__tests__.index"));
+		testEquals(
+			":apps:portal-search:portal-search-web:packageRunTest",
+			_getTestTaskName(
+				"liferay-portal.modules.apps.portal-search.portal-search-" +
+					"web.test.js.index"));
+	}
+
+	private String _getTestTaskName(String className) {
+		JSUnitJUnitTestResult jsUnitJUnitTestResult = new JSUnitJUnitTestResult(
+			Mockito.mock(Build.class),
+			new JSONObject(
+			).put(
+				"className", className
+			).put(
+				"duration", 0.5
+			).put(
+				"name", "renders the component"
+			).put(
+				"status", "PASSED"
+			));
+
+		return jsUnitJUnitTestResult.getTestTaskName();
+	}
+
+}
