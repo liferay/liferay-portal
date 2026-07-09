fdc1062d97f0bccb57b081b5e417868003f8d733
Brian Chan <brian.chan@liferay.com>
2026-07-09 11:32:21 +0800

    LPD-95995 SF

--- modules/apps/site-navigation/site-navigation-language-web-test/src/testIntegration/java/com/liferay/site/navigation/language/web/internal/portlet/test/SiteNavigationLanguagePortletTest.java
+++ modules/apps/site-navigation/site-navigation-language-web-test/src/testIntegration/java/com/liferay/site/navigation/language/web/internal/portlet/test/SiteNavigationLanguagePortletTest.java
@@ -29,7 +29,7 @@ public class SiteNavigationLanguagePortletTest {
 		new LiferayIntegrationTestRule();
 
 	@Test
-	public void testPortletIsInstanceable() {
+	public void testIsInstanceable() {
 		Portlet portlet = PortletLocalServiceUtil.getPortletById(
 			SiteNavigationLanguagePortletKeys.SITE_NAVIGATION_LANGUAGE);
 
