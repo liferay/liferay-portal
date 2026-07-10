ac56833571c5aa459c827367c5f1a8b1c669fa2c
Kenji Heigel <kenji.heigel@liferay.com>
2026-07-09 17:41:44 -0700

    LRCI-7525 Add unit test for the axis partition of batch test classes

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/clazz/group/BatchTestClassGroupTest.java
@@ -0,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.test.clazz.group;
+
+import com.liferay.jenkins.results.parser.test.clazz.TestClass;
+import com.liferay.jenkins.results.parser.test.clazz.TestClassFactory;
+
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Kenji Heigel
+ */
+public class BatchTestClassGroupTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testGetAxisTestClassGroups() {
+		BatchTestClassGroup batchTestClassGroup = new BatchTestClassGroup(
+			"default", BatchTestClassGroupTestUtil.getPortalTestClassJob()) {
+		};
+
+		int axisMaxSize = batchTestClassGroup.getAxisMaxSize();
+
+		for (int i = 0; i < (axisMaxSize + 2); i++) {
+			batchTestClassGroup.addTestClass(
+				TestClassFactory.newTestClass(
+					batchTestClassGroup,
+					new File("test-class-" + i)));
+		}
+
+		batchTestClassGroup.setAxisTestClassGroups();
+
+		List<TestClass> testClasses = batchTestClassGroup.getTestClasses();
+
+		List<AxisTestClassGroup> axisTestClassGroups =
+			batchTestClassGroup.getAxisTestClassGroups();
+
+		Assert.assertEquals(
+			axisTestClassGroups.toString(),
+			(int)Math.ceil((double)testClasses.size() / axisMaxSize),
+			axisTestClassGroups.size());
+
+		List<TestClass> axisTestClasses = new ArrayList<>();
+
+		for (AxisTestClassGroup axisTestClassGroup : axisTestClassGroups) {
+			axisTestClasses.addAll(axisTestClassGroup.getTestClasses());
+		}
+
+		Collections.sort(axisTestClasses);
+
+		Assert.assertEquals(testClasses, axisTestClasses);
+	}
+
+}
