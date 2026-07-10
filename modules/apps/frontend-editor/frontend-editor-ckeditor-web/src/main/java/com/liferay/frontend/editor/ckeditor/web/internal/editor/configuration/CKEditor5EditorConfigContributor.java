c382bb6402579040d638e2cd8682ee9491249253
chestofwonders <miguelarroyoes@hotmail.com>
2026-07-10 17:47:13 +0200

    LPD-95090 Check whether the installation is licensed for CKEditor 5 premium features

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditor5EditorConfigContributor.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditor5EditorConfigContributor.java
@@ -6,20 +6,32 @@
 package com.liferay.frontend.editor.ckeditor.web.internal.editor.configuration;
 
 import com.liferay.frontend.editor.ckeditor.web.internal.configuration.CKEditor5Configuration;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ReleaseInfo;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.nio.charset.StandardCharsets;
+
+import java.util.Base64;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marko Cikos
@@ -47,17 +59,21 @@ public class CKEditor5EditorConfigContributor
 			inputEditorTaglibAttributes.get("liferay-ui:input-editor:name"));
 		String placeholder = LanguageUtil.format(
 			themeDisplay.getLocale(), "start-writing-content", false);
+		String licenseKey = _ckEditor5Configuration.licenseKey();
 
 		jsonObject.put(
 			"editorType", "ckeditor5"
 		).put(
 			"itemSelectorEventName", namespace + name + "selectItem"
 		).put(
-			"licenseKey", _ckEditor5Configuration.licenseKey()
+			"licenseKey", licenseKey
 		).put(
 			"placeholder", placeholder
 		).put(
 			"preset", "advanced"
+		).put(
+			"showPasteFromOfficeEnhanced",
+			ReleaseInfo.isDXP() && _isLicenseKeyValid(licenseKey)
 		);
 	}
 
@@ -68,6 +84,54 @@ public class CKEditor5EditorConfigContributor
 			CKEditor5Configuration.class, properties);
 	}
 
+	private boolean _isLicenseKeyValid(String licenseKey) {
+		if (Validator.isNull(licenseKey)) {
+			return false;
+		}
+
+		String[] parts = licenseKey.split("\\.");
+
+		if (parts.length != 3) {
+			return false;
+		}
+
+		try {
+			String payload = parts[1];
+
+			int paddingLength = (4 - (payload.length() % 4)) % 4;
+
+			JSONObject payloadJSONObject = _jsonFactory.createJSONObject(
+				new String(
+					Base64.getUrlDecoder(
+					).decode(
+						payload.concat(StringPool.EQUAL.repeat(paddingLength))
+					),
+					StandardCharsets.UTF_8));
+
+			long expirationTime = payloadJSONObject.getLong("exp", 0);
+
+			if (expirationTime > (System.currentTimeMillis() / Time.SECOND)) {
+				return true;
+			}
+
+			return false;
+		}
+		catch (IllegalArgumentException | JSONException exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Unable to parse the CKEditor 5 license key", exception);
+			}
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CKEditor5EditorConfigContributor.class);
+
 	private volatile CKEditor5Configuration _ckEditor5Configuration;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 }
