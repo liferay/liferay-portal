9a5fe7a1dde0173258eb0736b4ed2139a93acca6
Kevin Tan <kevin.tan@liferay.com>
2026-07-10 14:54:55 -0700

    LPD-97670 site-cmp-site-initializer: Remove redundant feature flag check
    The calendar view is only registered in ProjectTasksFDSPropsTransformer when the LPD-69885 feature flag is enabled, so CalendarView never mounts without it and the inner check around dayCellContent is dead weight.

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/views/calendar_view/CalendarView.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/views/calendar_view/CalendarView.tsx
@@ -431,6 +431,56 @@ export default function CalendarView({
 					setCurrentView(view.type);
 					setTitle(view.title);
 				}}
+				dayCellContent={(arg) => {
+					const dateMarker =
+						currentView === 'dayGridWeek'
+							? getProjectDateMarker(
+									dateUtils.format(arg.date, 'yyyy-MM-dd'),
+									projectDates
+								)
+							: null;
+
+					return (
+						<>
+							{arg.dayNumberText || String(arg.date.getDate())}
+
+							<ClayButtonWithIcon
+								aria-label={Liferay.Language.get('add-task')}
+								borderless
+								className="lfr__calendar-view-add-task-button"
+								displayType="secondary"
+								onClick={() =>
+									openCreateTaskModal(
+										dateUtils.format(arg.date, 'yyyy-MM-dd')
+									)
+								}
+								rounded
+								size="xs"
+								symbol="plus"
+								title={Liferay.Language.get('add-task')}
+							/>
+
+							{dateMarker && (
+								<span
+									className={classNames(
+										'lfr__calendar-view-date-marker',
+										`lfr__calendar-view-date-marker-${dateMarker}`
+									)}
+								>
+									<ClayIcon symbol="flag-full" />
+
+									{dateMarker === 'startDate'
+										? Liferay.Language.get(
+												'project-start-date'
+											)
+										: Liferay.Language.get(
+												'project-due-date'
+											)}
+								</span>
+							)}
+						</>
+					);
+				}}
 				dayHeaderFormat={{weekday: 'long'}}
 				dayMaxEvents
 				drop={async (arg) => {
@@ -524,67 +574,6 @@ export default function CalendarView({
 				moreLinkHint={Liferay.Language.get('view-all-tasks')}
 				plugins={[dayGridPlugin, interactionPlugin]}
 				ref={calendarRef}
-				{...(Liferay.FeatureFlags['LPD-69885'] && {
-					dayCellContent: (arg) => {
-						const dateMarker =
-							currentView === 'dayGridWeek'
-								? getProjectDateMarker(
-										dateUtils.format(
-											arg.date,
-											'yyyy-MM-dd'
-										),
-										projectDates
-									)
-								: null;
-
-						return (
-							<>
-								{arg.dayNumberText ||
-									String(arg.date.getDate())}
-
-								<ClayButtonWithIcon
-									aria-label={Liferay.Language.get(
-										'add-task'
-									)}
-									borderless
-									className="lfr__calendar-view-add-task-button"
-									displayType="secondary"
-									onClick={() =>
-										openCreateTaskModal(
-											dateUtils.format(
-												arg.date,
-												'yyyy-MM-dd'
-											)
-										)
-									}
-									rounded
-									size="xs"
-									symbol="plus"
-									title={Liferay.Language.get('add-task')}
-								/>
-
-								{dateMarker && (
-									<span
-										className={classNames(
-											'lfr__calendar-view-date-marker',
-											`lfr__calendar-view-date-marker-${dateMarker}`
-										)}
-									>
-										<ClayIcon symbol="flag-full" />
-
-										{dateMarker === 'startDate'
-											? Liferay.Language.get(
-													'project-start-date'
-												)
-											: Liferay.Language.get(
-													'project-due-date'
-												)}
-									</span>
-								)}
-							</>
-						);
-					},
-				})}
 			/>
 
 			{moreLinkPopover && (
