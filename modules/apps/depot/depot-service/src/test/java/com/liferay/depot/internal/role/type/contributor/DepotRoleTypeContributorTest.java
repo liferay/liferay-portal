4b790357e09619551be37afb8d309ef94d05c58e
brbalazs <balazs.breier@liferay.com>
2026-07-14 12:58:49 +0200

    LPD-97897 Unit test

--- modules/apps/depot/depot-service/src/test/java/com/liferay/depot/internal/role/type/contributor/DepotRoleTypeContributorTest.java
+++ modules/apps/depot/depot-service/src/test/java/com/liferay/depot/internal/role/type/contributor/DepotRoleTypeContributorTest.java
@@ -8,8 +8,13 @@ package com.liferay.depot.internal.role.type.contributor;
 import com.liferay.depot.constants.DepotRolesConstants;
 import com.liferay.depot.internal.roles.admin.role.type.contributor.DepotRoleTypeContributor;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -38,6 +43,35 @@ public class DepotRoleTypeContributorTest {
 				DepotRolesConstants.DESIGN_LIBRARY_OWNER
 			},
 			_depotRoleTypeContributor.getExcludedRoleNames());
+	}
+
+	@Test
+	public void testGetName() {
+		DepotRoleTypeContributor depotRoleTypeContributor =
+			new DepotRoleTypeContributor();
+
+		try (MockedStatic<FeatureFlagManagerUtil> mockedStatic =
+				Mockito.mockStatic(FeatureFlagManagerUtil.class)) {
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				false
+			);
+
+			Assert.assertEquals(
+				"asset-library", depotRoleTypeContributor.getName());
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertEquals("depot", depotRoleTypeContributor.getName());
+		}
 	}
 
 	@Test
@@ -123,6 +157,73 @@ public class DepotRoleTypeContributorTest {
 	}
 
 	@Test
+	public void testGetTabTitle() {
+		DepotRoleTypeContributor depotRoleTypeContributor =
+			new DepotRoleTypeContributor();
+
+		_setLanguage(depotRoleTypeContributor);
+
+		try (MockedStatic<FeatureFlagManagerUtil> mockedStatic =
+				Mockito.mockStatic(FeatureFlagManagerUtil.class)) {
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				false
+			);
+
+			Assert.assertEquals(
+				"asset-library-roles",
+				depotRoleTypeContributor.getTabTitle(LocaleUtil.US));
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertEquals(
+				"depot-roles",
+				depotRoleTypeContributor.getTabTitle(LocaleUtil.US));
+		}
+	}
+
+	@Test
+	public void testGetTitle() {
+		DepotRoleTypeContributor depotRoleTypeContributor =
+			new DepotRoleTypeContributor();
+
+		_setLanguage(depotRoleTypeContributor);
+
+		try (MockedStatic<FeatureFlagManagerUtil> mockedStatic =
+				Mockito.mockStatic(FeatureFlagManagerUtil.class)) {
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				false
+			);
+
+			Assert.assertEquals(
+				"asset-library-role",
+				depotRoleTypeContributor.getTitle(LocaleUtil.US));
+
+			mockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-17564"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertEquals(
+				"depot-role", depotRoleTypeContributor.getTitle(LocaleUtil.US));
+		}
+	}
+
+	@Test
 	public void testIsAllowAssignMembers() {
 		_testIsAllowAssignMembersWithAdministrator();
 		_testIsAllowAssignMembersWithMember();
@@ -164,6 +265,21 @@ public class DepotRoleTypeContributorTest {
 		);
 
 		return role;
+	}
+
+	private void _setLanguage(
+		DepotRoleTypeContributor depotRoleTypeContributor) {
+
+		Language language = Mockito.mock(Language.class);
+
+		Mockito.when(
+			language.get(Mockito.any(Locale.class), Mockito.anyString())
+		).thenAnswer(
+			invocation -> invocation.getArgument(1)
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			depotRoleTypeContributor, "_language", language);
 	}
 
 	private void _testIsAllowAssignMembersWithAdministrator() {
