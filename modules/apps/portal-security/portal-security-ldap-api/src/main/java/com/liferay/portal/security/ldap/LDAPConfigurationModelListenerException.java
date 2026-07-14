ccc86092147aa4584a3c983fd84abcb11e269a71
Caio Farias <caio.farias@liferay.com>
2026-07-14 11:44:37 -0300

    LPD-86301 Validate FIPS LDAP configuration with localized errors

---
+++ modules/apps/portal-security/portal-security-ldap-api/src/main/java/com/liferay/portal/security/ldap/LDAPConfigurationModelListenerException.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.ldap;
+
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+
+import java.util.Dictionary;
+
+/**
+ * @author Jorge García Jiménez
+ */
+public class LDAPConfigurationModelListenerException
+	extends ConfigurationModelListenerException {
+
+	public LDAPConfigurationModelListenerException(
+		String causeMessage, Class<?> configurationClass,
+		Class<?> listenerClass, Object[] messageArguments, String messageKey,
+		Dictionary<String, Object> properties) {
+
+		super(causeMessage, configurationClass, listenerClass, properties);
+
+		_messageArguments = messageArguments;
+		_messageKey = messageKey;
+	}
+
+	public Object[] getMessageArguments() {
+		return _messageArguments;
+	}
+
+	public String getMessageKey() {
+		return _messageKey;
+	}
+
+	private final Object[] _messageArguments;
+	private final String _messageKey;
+
+}
---
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/configuration/persistence/listener/LDAPAuthConfigurationModelListener.java
@@ -0,0 +1,55 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.security.ldap.internal.configuration.persistence.listener;
+
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+import com.liferay.portal.kernel.security.fips.FIPSModeValidator;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.security.ldap.LDAPConfigurationModelListenerException;
+import com.liferay.portal.security.ldap.authenticator.configuration.LDAPAuthConfiguration;
+import com.liferay.portal.security.ldap.constants.LDAPConstants;
+
+import java.util.Dictionary;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jorge García Jiménez
+ */
+@Component(
+	property = "model.class.name=com.liferay.portal.security.ldap.authenticator.configuration.LDAPAuthConfiguration",
+	service = ConfigurationModelListener.class
+)
+public class LDAPAuthConfigurationModelListener
+	implements ConfigurationModelListener {
+
+	@Override
+	public void onBeforeSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		if (!LDAPConstants.AUTH_METHOD_PASSWORD_COMPARE.equals(
+				GetterUtil.getString(
+					properties.get(LDAPConstants.AUTH_METHOD)))) {
+
+			return;
+		}
+
+		String algorithm = GetterUtil.getString(
+			properties.get(LDAPConstants.PASSWORD_ENCRYPTION_ALGORITHM));
+
+		try {
+			FIPSModeValidator.validateAlgorithm(algorithm);
+		}
+		catch (SecurityException securityException) {
+			throw new LDAPConfigurationModelListenerException(
+				securityException.getMessage(), LDAPAuthConfiguration.class,
+				getClass(), new Object[] {algorithm},
+				"the-algorithm-x-is-not-allowed-in-fips-mode", properties);
+		}
+	}
+
+}
