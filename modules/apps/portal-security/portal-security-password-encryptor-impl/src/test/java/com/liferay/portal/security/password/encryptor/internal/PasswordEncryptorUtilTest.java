1f3ea7447b9bcfcc42e22e1e1d46f9bcfceff74c
Brian Chan <brian.chan@liferay.com>
2026-07-14 15:20:16 +0800

    LPD-94045 Case sensitive sort

--- modules/apps/portal-security/portal-security-password-encryptor-impl/src/test/java/com/liferay/portal/security/password/encryptor/internal/PasswordEncryptorUtilTest.java
+++ modules/apps/portal-security/portal-security-password-encryptor-impl/src/test/java/com/liferay/portal/security/password/encryptor/internal/PasswordEncryptorUtilTest.java
@@ -122,16 +122,16 @@ public class PasswordEncryptorUtilTest {
 
 	@Test
 	public void testEncryptFailure() throws Exception {
-		_testEncryptFailure(RandomTestUtil.randomString(), StringPool.BLANK, StringPool.BLANK);
-		_testEncryptFailure(null, null, null);
-		_testEncryptFailure(null, null, StringPool.BLANK);
-		_testEncryptFailure(null, StringPool.BLANK, null);
-		_testEncryptFailure(null, StringPool.BLANK, StringPool.BLANK);
-		_testEncryptFailure(StringPool.BLANK, null, null);
-		_testEncryptFailure(StringPool.BLANK, null, StringPool.BLANK);
-		_testEncryptFailure(StringPool.BLANK, StringPool.BLANK, null);
-		_testEncryptFailure(StringPool.BLANK, StringPool.BLANK, StringPool.BLANK);
 		_testEncryptFailure(PasswordEncryptor.TYPE_SHA, "password", "W6ph5Mm5Pz8GgiULbPgzG37mj9g=");
+		_testEncryptFailure(RandomTestUtil.randomString(), StringPool.BLANK, StringPool.BLANK);
+		_testEncryptFailure(StringPool.BLANK, StringPool.BLANK, StringPool.BLANK);
+		_testEncryptFailure(StringPool.BLANK, StringPool.BLANK, null);
+		_testEncryptFailure(StringPool.BLANK, null, StringPool.BLANK);
+		_testEncryptFailure(StringPool.BLANK, null, null);
+		_testEncryptFailure(null, StringPool.BLANK, StringPool.BLANK);
+		_testEncryptFailure(null, StringPool.BLANK, null);
+		_testEncryptFailure(null, null, StringPool.BLANK);
+		_testEncryptFailure(null, null, null);
 	}
 
 	@Test
