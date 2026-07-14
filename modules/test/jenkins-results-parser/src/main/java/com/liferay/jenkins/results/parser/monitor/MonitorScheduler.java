97dcdc6bf738936fa8ab37f853490914c3bded7d
Brittney Nguyen <brittney.nguyen@liferay.com>
2026-07-14 16:47:58 -0700

    LRCI-7816 Add the monitor scheduler

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/monitor/MonitorScheduler.java
@@ -0,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.monitor;
+
+import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Brittney Nguyen
+ */
+public class MonitorScheduler {
+
+	public MonitorScheduler(MonitorResultStore monitorResultStore) {
+		_monitorResultStore = monitorResultStore;
+	}
+
+	public List<Monitor> getDueMonitors(List<Monitor> monitors) {
+		List<Monitor> dueMonitors = new ArrayList<>();
+
+		long currentTimeMillis =
+			JenkinsResultsParserUtil.getCurrentTimeMillis();
+
+		for (Monitor monitor : monitors) {
+			if (_isDue(currentTimeMillis, monitor)) {
+				dueMonitors.add(monitor);
+			}
+		}
+
+		return dueMonitors;
+	}
+
+	private boolean _isDue(long currentTimeMillis, Monitor monitor) {
+		MonitorResult latestMonitorResult =
+			_monitorResultStore.getLatestMonitorResult(monitor.getId());
+
+		if (latestMonitorResult == null) {
+			return true;
+		}
+
+		MonitorConfig monitorConfig = monitor.getMonitorConfig();
+
+		if ((currentTimeMillis - latestMonitorResult.getTimestamp()) >=
+				(monitorConfig.getCadence() * 1000)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private final MonitorResultStore _monitorResultStore;
+
+}
