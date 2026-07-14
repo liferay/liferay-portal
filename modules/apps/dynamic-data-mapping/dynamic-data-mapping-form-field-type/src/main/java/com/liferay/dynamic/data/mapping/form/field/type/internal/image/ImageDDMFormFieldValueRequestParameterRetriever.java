f72c1a67f8aa97482071466e93c4d82be8426f4d
Kammerer <victor.kammerer@liferay.com>
2026-07-14 11:18:36 -0300

    LPD-98219 Save empty value when the image field JSON is empty

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/image/ImageDDMFormFieldValueRequestParameterRetriever.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/image/ImageDDMFormFieldValueRequestParameterRetriever.java
@@ -7,6 +7,9 @@ package com.liferay.dynamic.data.mapping.form.field.type.internal.image;
 
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldValueRequestParameterRetriever;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -33,16 +36,21 @@ public class ImageDDMFormFieldValueRequestParameterRetriever
 		String parameterValue = httpServletRequest.getParameter(
 			ddmFormFieldParameterName);
 
-		if (!Validator.isBlank(parameterValue)) {
-			parameterValue = String.valueOf(
-				getJSONObject(_log, parameterValue));
+		if (Validator.isBlank(parameterValue)) {
+			if (parameterValue != null) {
+				return parameterValue;
+			}
+
+			return defaultDDMFormFieldParameterValue;
 		}
 
-		if (parameterValue != null) {
-			return parameterValue;
+		JSONObject jsonObject = getJSONObject(_log, parameterValue);
+
+		if (JSONUtil.isEmpty(jsonObject)) {
+			return StringPool.BLANK;
 		}
 
-		return defaultDDMFormFieldParameterValue;
+		return jsonObject.toString();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
