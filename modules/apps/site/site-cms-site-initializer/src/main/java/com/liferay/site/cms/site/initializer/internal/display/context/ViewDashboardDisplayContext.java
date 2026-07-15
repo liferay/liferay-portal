f0c324747b7ac36bb659cff79efe578928115698
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-07-15 09:26:20 +0100

    LPD-98182 Provide the asset additional props to the dashboard

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewDashboardDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewDashboardDisplayContext.java
@@ -6,21 +6,29 @@
 package com.liferay.site.cms.site.initializer.internal.display.context;
 
 import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.document.library.configuration.DLConfiguration;
 import com.liferay.learn.LearnMessageUtil;
 import com.liferay.object.constants.ObjectFolderConstants;
+import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.site.cms.site.initializer.internal.util.CommentUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Map;
 
@@ -31,11 +39,16 @@ public class ViewDashboardDisplayContext {
 
 	public ViewDashboardDisplayContext(
 		AnalyticsSettingsManager analyticsSettingsManager,
-		GroupLocalService groupLocalService, RoleLocalService roleLocalService,
-		ThemeDisplay themeDisplay) {
+		DLConfiguration dlConfiguration, GroupLocalService groupLocalService,
+		HttpServletRequest httpServletRequest,
+		ObjectDefinitionService objectDefinitionService,
+		RoleLocalService roleLocalService, ThemeDisplay themeDisplay) {
 
 		_analyticsSettingsManager = analyticsSettingsManager;
+		_dlConfiguration = dlConfiguration;
 		_groupLocalService = groupLocalService;
+		_httpServletRequest = httpServletRequest;
+		_objectDefinitionService = objectDefinitionService;
 		_roleLocalService = roleLocalService;
 		_themeDisplay = themeDisplay;
 	}
@@ -67,6 +80,8 @@ public class ViewDashboardDisplayContext {
 
 	public Map<String, Object> getReactData() throws PortalException {
 		return HashMapBuilder.<String, Object>put(
+			"additionalProps", _getAdditionalProps()
+		).put(
 			"admin",
 			() -> _roleLocalService.hasUserRole(
 				_themeDisplay.getUserId(), _themeDisplay.getCompanyId(),
@@ -100,11 +115,106 @@ public class ViewDashboardDisplayContext {
 		).build();
 	}
 
+	private Map<String, Object> _getAdditionalProps() {
+		return HashMapBuilder.<String, Object>put(
+			"autocompleteURL", SectionDisplayContextUtil.getAutocompleteURL()
+		).put(
+			"breadcrumbProps",
+			HashMapBuilder.<String, Object>put(
+				"breadcrumbItems",
+				JSONUtil.putAll(
+					JSONUtil.put(
+						"active", false
+					).put(
+						"label", _getLayoutName()
+					))
+			).put(
+				"hideSpace", true
+			).build()
+		).put(
+			"candidateAssetLibraries",
+			SectionDisplayContextUtil.getDepotEntriesJSONArray(
+				_httpServletRequest, null)
+		).put(
+			"cmsGroupId",
+			() -> {
+				try {
+					Group group = _groupLocalService.getGroup(
+						_themeDisplay.getCompanyId(), GroupConstants.CMS);
+
+					return GetterUtil.getLong(group.getGroupId());
+				}
+				catch (PortalException portalException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(portalException);
+					}
+				}
+
+				return null;
+			}
+		).put(
+			"collaboratorURLs",
+			() -> SectionDisplayContextUtil.getCollaboratorURLs(
+				_themeDisplay.getCompanyId(), _objectDefinitionService,
+				new String[] {
+					ObjectFolderConstants.
+						EXTERNAL_REFERENCE_CODE_CONTENT_STRUCTURES,
+					ObjectFolderConstants.EXTERNAL_REFERENCE_CODE_FILE_TYPES
+				})
+		).put(
+			"commentsProps", CommentUtil.getCommentsProps(_httpServletRequest)
+		).put(
+			"contentViewURL",
+			SectionDisplayContextUtil.getContentViewURL(_themeDisplay)
+		).put(
+			"fileMimeTypeCssClasses",
+			() -> {
+				if (_dlConfiguration == null) {
+					return null;
+				}
+
+				return SectionDisplayContextUtil.getFileMimeTypeCssClasses(
+					_dlConfiguration);
+			}
+		).put(
+			"fileMimeTypeIcons",
+			() -> {
+				if (_dlConfiguration == null) {
+					return null;
+				}
+
+				return SectionDisplayContextUtil.getFileMimeTypeIcons(
+					_dlConfiguration);
+			}
+		).put(
+			"objectDefinitionCssClasses",
+			SectionDisplayContextUtil.getObjectDefinitionCssClasses()
+		).put(
+			"objectDefinitionIcons",
+			SectionDisplayContextUtil.getObjectDefinitionIcons()
+		).put(
+			"redirect", _themeDisplay.getURLCurrent()
+		).build();
+	}
+
+	private String _getLayoutName() {
+		Layout layout = _themeDisplay.getLayout();
+
+		if (layout == null) {
+			return null;
+		}
+
+		return layout.getName(_themeDisplay.getLocale(), true);
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ViewDashboardDisplayContext.class);
 
 	private final AnalyticsSettingsManager _analyticsSettingsManager;
+	private final DLConfiguration _dlConfiguration;
 	private final GroupLocalService _groupLocalService;
+	private final HttpServletRequest _httpServletRequest;
+	private final ObjectDefinitionService _objectDefinitionService;
 	private final RoleLocalService _roleLocalService;
 	private final ThemeDisplay _themeDisplay;
 
--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewDashboardJSPSectionFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ViewDashboardJSPSectionFragmentRenderer.java
@@ -6,7 +6,10 @@
 package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
 
 import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.document.library.configuration.DLConfiguration;
 import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.object.service.ObjectDefinitionService;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -14,13 +17,20 @@ import com.liferay.site.cms.site.initializer.internal.display.context.ViewDashbo
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adriano Interaminense
  */
-@Component(service = FragmentRenderer.class)
+@Component(
+	configurationPid = "com.liferay.document.library.configuration.DLConfiguration",
+	service = FragmentRenderer.class
+)
 public class ViewDashboardJSPSectionFragmentRenderer
 	extends BaseJSPSectionFragmentRenderer<ViewDashboardDisplayContext> {
 
@@ -29,12 +39,20 @@ public class ViewDashboardJSPSectionFragmentRenderer
 		return "dashboard";
 	}
 
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_dlConfiguration = ConfigurableUtil.createConfigurable(
+			DLConfiguration.class, properties);
+	}
+
 	@Override
 	protected ViewDashboardDisplayContext getDisplayContext(
 		HttpServletRequest httpServletRequest) {
 
 		return new ViewDashboardDisplayContext(
-			_analyticsSettingsManager, groupLocalService, _roleLocalService,
+			_analyticsSettingsManager, _dlConfiguration, groupLocalService,
+			httpServletRequest, _objectDefinitionService, _roleLocalService,
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY));
 	}
@@ -46,6 +64,11 @@ public class ViewDashboardJSPSectionFragmentRenderer
 
 	@Reference
 	private AnalyticsSettingsManager _analyticsSettingsManager;
+
+	private volatile DLConfiguration _dlConfiguration;
+
+	@Reference
+	private ObjectDefinitionService _objectDefinitionService;
 
 	@Reference
 	private RoleLocalService _roleLocalService;
