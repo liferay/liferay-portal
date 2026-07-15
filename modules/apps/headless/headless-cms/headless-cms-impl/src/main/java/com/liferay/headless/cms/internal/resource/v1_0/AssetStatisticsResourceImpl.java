de72b8ec051bd5c0d25fee9ec5222076db912b62
Mikel Lorza <mikel.lorza@liferay.com>
2026-07-15 13:07:39 +0200

    LPD-97865 Scope asset-statistics by assetLibraryId (group ID)

--- modules/apps/headless/headless-cms/headless-cms-impl/src/main/java/com/liferay/headless/cms/internal/resource/v1_0/AssetStatisticsResourceImpl.java
+++ modules/apps/headless/headless-cms/headless-cms-impl/src/main/java/com/liferay/headless/cms/internal/resource/v1_0/AssetStatisticsResourceImpl.java
@@ -42,19 +42,18 @@ public class AssetStatisticsResourceImpl
 	extends BaseAssetStatisticsResourceImpl {
 
 	@Override
-	public AssetStatistics getAssetStatistics() throws Exception {
+	public AssetStatistics getAssetStatistics(Long assetLibraryId)
+		throws Exception {
+
 		if (!FeatureFlagManagerUtil.isEnabled(
 				contextCompany.getCompanyId(), "LPD-17564")) {
 
 			throw new UnsupportedOperationException();
 		}
 
-		List<Long> depotEntryGroupIds =
-			_depotEntryService.getDepotEntryGroupIds(
-				contextCompany.getCompanyId(), contextUser.getUserId(),
-				DepotConstants.TYPE_SPACE);
+		Long[] groupIds = _getGroupIds(assetLibraryId);
 
-		if (depotEntryGroupIds.isEmpty()) {
+		if (ArrayUtil.isEmpty(groupIds)) {
 			return _toAssetStatistics();
 		}
 
@@ -73,10 +72,14 @@ public class AssetStatisticsResourceImpl
 		}
 
 		Date date = new Date();
-		Long[] groupIds = depotEntryGroupIds.toArray(new Long[0]);
 
 		return new AssetStatistics() {
 			{
+				setApprovedCount(
+					() -> _getCount(
+						groupIds, objectDefinitionIds,
+						ObjectEntryTable.INSTANCE.status.eq(
+							WorkflowConstants.STATUS_APPROVED)));
 				setExpiredCount(
 					() -> _getCount(
 						groupIds, objectDefinitionIds,
@@ -98,15 +101,44 @@ public class AssetStatisticsResourceImpl
 						groupIds, objectDefinitionIds,
 						ObjectEntryTable.INSTANCE.status.eq(
 							WorkflowConstants.STATUS_DRAFT)));
+				setPendingCount(
+					() -> _getCount(
+						groupIds, objectDefinitionIds,
+						ObjectEntryTable.INSTANCE.status.eq(
+							WorkflowConstants.STATUS_PENDING)));
 				setReviewDateOverdueCount(
 					() -> _getCount(
 						groupIds, objectDefinitionIds,
-						ObjectEntryTable.INSTANCE.reviewDate.lt(date)));
+						ObjectEntryTable.INSTANCE.reviewDate.lt(
+							date
+						).and(
+							ObjectEntryTable.INSTANCE.status.in(
+								CMSWorkflowConstants.STATUSES)
+						)));
+				setScheduledCount(
+					() -> _getCount(
+						groupIds, objectDefinitionIds,
+						ObjectEntryTable.INSTANCE.status.eq(
+							WorkflowConstants.STATUS_SCHEDULED)));
 				setTotalCount(
 					() -> _getCount(
 						groupIds, objectDefinitionIds,
 						ObjectEntryTable.INSTANCE.status.in(
 							CMSWorkflowConstants.STATUSES)));
+				setUpcomingReviewCount(
+					() -> _getCount(
+						groupIds, objectDefinitionIds,
+						ObjectEntryTable.INSTANCE.reviewDate.gt(
+							date
+						).and(
+							ObjectEntryTable.INSTANCE.reviewDate.lte(
+								new Date(
+									date.getTime() +
+										(Time.DAY * _UPCOMING_REVIEW_DAYS)))
+						).and(
+							ObjectEntryTable.INSTANCE.status.in(
+								CMSWorkflowConstants.STATUSES)
+						)));
 			}
 		};
 	}
@@ -128,19 +160,42 @@ public class AssetStatisticsResourceImpl
 		}
 	}
 
+	private Long[] _getGroupIds(Long assetLibraryId) {
+		List<Long> depotEntryGroupIds =
+			_depotEntryService.getDepotEntryGroupIds(
+				contextCompany.getCompanyId(), contextUser.getUserId(),
+				DepotConstants.TYPE_SPACE);
+
+		if (assetLibraryId == null) {
+			return depotEntryGroupIds.toArray(new Long[0]);
+		}
+
+		if (!depotEntryGroupIds.contains(assetLibraryId)) {
+			return new Long[0];
+		}
+
+		return new Long[] {assetLibraryId};
+	}
+
 	private AssetStatistics _toAssetStatistics() {
 		return new AssetStatistics() {
 			{
+				setApprovedCount(() -> 0L);
 				setExpiredCount(() -> 0L);
 				setExpiringSoonCount(() -> 0L);
 				setInDraftCount(() -> 0L);
+				setPendingCount(() -> 0L);
 				setReviewDateOverdueCount(() -> 0L);
+				setScheduledCount(() -> 0L);
 				setTotalCount(() -> 0L);
+				setUpcomingReviewCount(() -> 0L);
 			}
 		};
 	}
 
 	private static final int _EXPIRING_SOON_DAYS = 7;
+
+	private static final int _UPCOMING_REVIEW_DAYS = 7;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		AssetStatisticsResourceImpl.class);
