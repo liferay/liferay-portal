8528581d0330c4212bb1c35831f47ddd65953391
Carlos Correa <carlos.correa@liferay.com>
2026-07-15 13:56:20 +0200

    LPD-96103 Sort

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineImportTaskExecutorImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineImportTaskExecutorImpl.java
@@ -212,6 +212,16 @@ public class BatchEngineImportTaskExecutorImpl
 	protected void activate(
 		BundleContext bundleContext, Map<String, Object> properties) {
 
+		_batchEngineImportTaskExceptionHandlers =
+			ServiceTrackerListFactory.open(
+				bundleContext, BatchEngineImportTaskExceptionHandler.class);
+		_importTaskPostActions = ServiceTrackerListFactory.open(
+			bundleContext, ImportTaskPostAction.class);
+		_importTaskPreActions = ServiceTrackerListFactory.open(
+			bundleContext, ImportTaskPreAction.class);
+		_itemReaderPostActions = ServiceTrackerListFactory.open(
+			bundleContext, ItemReaderPostAction.class);
+
 		ServiceReferenceMapper<String, BatchEngineContentProcessor>
 			propertyServiceReferenceMapper =
 				new PropertyServiceReferenceMapper<>("field.name");
@@ -227,16 +237,6 @@ public class BatchEngineImportTaskExecutorImpl
 						serviceReference, emitter);
 				}
 			});
-
-		_batchEngineImportTaskExceptionHandlers =
-			ServiceTrackerListFactory.open(
-				bundleContext, BatchEngineImportTaskExceptionHandler.class);
-		_importTaskPostActions = ServiceTrackerListFactory.open(
-			bundleContext, ImportTaskPostAction.class);
-		_importTaskPreActions = ServiceTrackerListFactory.open(
-			bundleContext, ImportTaskPreAction.class);
-		_itemReaderPostActions = ServiceTrackerListFactory.open(
-			bundleContext, ItemReaderPostAction.class);
 	}
 
 	protected <T> void addBatchEngineImportTaskError(
@@ -282,11 +282,11 @@ public class BatchEngineImportTaskExecutorImpl
 
 	@Deactivate
 	protected void deactivate() {
-		_serviceTrackerMap.close();
 		_batchEngineImportTaskExceptionHandlers.close();
 		_importTaskPostActions.close();
 		_importTaskPreActions.close();
 		_itemReaderPostActions.close();
+		_serviceTrackerMap.close();
 	}
 
 	private <T> BatchEngineImportTask _commitItems(
