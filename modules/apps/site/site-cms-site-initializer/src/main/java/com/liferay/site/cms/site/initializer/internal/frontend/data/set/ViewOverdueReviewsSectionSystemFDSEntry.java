c634178c01b57e1bb25ac2cd2609d7e81433fd87
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-07-16 17:23:02 +0200

    LPD-98462 Add the Overdue Reviews section

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/ViewOverdueReviewsSectionSystemFDSEntry.java
@@ -0,0 +1,112 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set;
+
+import com.liferay.frontend.data.set.SystemFDSEntry;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+import com.liferay.site.cms.site.initializer.internal.display.context.SectionDisplayContextUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Veronica Gonzalez
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMSSiteInitializerFDSNames.OVERDUE_REVIEWS_SECTION,
+	service = SystemFDSEntry.class
+)
+public class ViewOverdueReviewsSectionSystemFDSEntry implements SystemFDSEntry {
+
+	@Override
+	public String getAdditionalAPIURLParameters(
+		HttpServletRequest httpServletRequest) {
+
+		String nowISO = Instant.now(
+		).truncatedTo(
+			ChronoUnit.SECONDS
+		).toString();
+
+		String filterString = "dateReview lt " + nowISO;
+
+		long groupId = GetterUtil.getLong(
+			httpServletRequest.getParameter("groupId"));
+
+		if (groupId > 0) {
+			filterString = StringBundler.concat(
+				filterString, " and groupIds/any(g:g eq ", groupId, ")");
+		}
+		else {
+			filterString = SectionDisplayContextUtil.appendGroupIds(
+				filterString, httpServletRequest);
+		}
+
+		filterString = SectionDisplayContextUtil.appendStatus(filterString);
+
+		return StringBundler.concat(
+			"emptySearch=true&filter=", filterString,
+			"&nestedFields=embedded,systemProperties.objectDefinitionBrief",
+			"&sort=dateReview:asc");
+	}
+
+	@Override
+	public int getDefaultItemsPerPage() {
+		return 20;
+	}
+
+	@Override
+	public String getDescription() {
+		return "CMS Overdue Reviews Section";
+	}
+
+	@Override
+	public boolean getHideManagementBarInEmptyState() {
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return CMSSiteInitializerFDSNames.OVERDUE_REVIEWS_SECTION;
+	}
+
+	@Override
+	public String getPropsTransformer() {
+		return "{OverdueReviewsFDSPropsTransformer} from " +
+			"site-cms-site-initializer";
+	}
+
+	@Override
+	public String getRESTApplication() {
+		return "/search/v1.0";
+	}
+
+	@Override
+	public String getRESTEndpoint() {
+		return "/v1.0/search";
+	}
+
+	@Override
+	public String getRESTSchema() {
+		return "SearchResult";
+	}
+
+	@Override
+	public String getSymbol() {
+		return "date";
+	}
+
+	@Override
+	public String getTitle() {
+		return "Overdue Reviews Section";
+	}
+
+}
