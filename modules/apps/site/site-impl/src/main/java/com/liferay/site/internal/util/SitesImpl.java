ed624f7d07dd5bcc0764cb0827fafc659be99ad7
Carlos Correa <carlos.correa@liferay.com>
2026-07-17 12:48:25 +0200

    LPD-98554 Add also a valdation in the backend layer

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
@@ -25,6 +25,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.change.tracking.CTTransactionException;
 import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.lock.Lock;
 import com.liferay.portal.kernel.lock.LockManagerUtil;
 import com.liferay.portal.kernel.log.Log;
@@ -571,6 +572,13 @@ public class SitesImpl implements Sites {
 	public void mergeLayoutSetPrototypeLayouts(
 			LayoutSetPrototype layoutSetPrototype, long userId)
 		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				layoutSetPrototype.getCompanyId(), "LPD-82107")) {
+
+			throw new UnsupportedOperationException(
+				"The site template merge is not available");
+		}
 
 		if (ExportImportThreadLocal.isExportInProcess() ||
 			ExportImportThreadLocal.isImportInProcess() ||
