71dafeb6df6e78a9dddd6eb4bc3d7ea8879118dc
Jorge García Jiménez <jorge.garcia@liferay.com>
2026-07-17 13:37:44 +0200

    LPD-98318 Blacklist SHA-1 and rsa-sha1 for SAML signatures in FIPS mode
    When FIPS mode is enabled, the SAML signing and validation configurations now reject SHA-1 and MD5 based signature, digest, and MAC algorithms. The activation combines each configuration's existing blacklist with the operator-configured blacklist and the FIPS-disallowed algorithms in a single step.

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/bootstrap/SecurityConfigurationBootstrap.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/bootstrap/SecurityConfigurationBootstrap.java
@@ -5,6 +5,7 @@
 
 package com.liferay.saml.opensaml.integration.internal.bootstrap;
 
+import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.saml.opensaml.integration.internal.util.ConfigurationServiceBootstrapUtil;
 
 import java.util.Collection;
@@ -21,6 +22,8 @@ import org.opensaml.xmlsec.impl.BasicDecryptionConfiguration;
 import org.opensaml.xmlsec.impl.BasicEncryptionConfiguration;
 import org.opensaml.xmlsec.impl.BasicSignatureSigningConfiguration;
 import org.opensaml.xmlsec.impl.BasicSignatureValidationConfiguration;
+import org.opensaml.xmlsec.impl.BasicWhitelistBlacklistConfiguration;
+import org.opensaml.xmlsec.signature.support.SignatureConstants;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -28,6 +31,7 @@ import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Carlos Sierra Andrés
+ * @author Jorge García Jiménez
  */
 @Component(service = {})
 public class SecurityConfigurationBootstrap {
@@ -49,32 +53,29 @@ public class SecurityConfigurationBootstrap {
 				DefaultSecurityConfigurationBootstrap.
 					buildDefaultSignatureValidationConfiguration();
 
+		String[] blacklistedAlgorithms = new String[0];
+
 		Object blacklistedAlgorithmsObject = properties.get(
 			"blacklisted.algorithms");
 
 		if (blacklistedAlgorithmsObject instanceof String[]) {
-			basicDecryptionConfiguration.setBlacklistedAlgorithms(
-				_combine(
-					basicDecryptionConfiguration.getBlacklistedAlgorithms(),
-					(String[])blacklistedAlgorithmsObject));
-
-			basicEncryptionConfiguration.setBlacklistedAlgorithms(
-				_combine(
-					basicEncryptionConfiguration.getBlacklistedAlgorithms(),
-					(String[])blacklistedAlgorithmsObject));
-
-			basicSignatureSigningConfiguration.setBlacklistedAlgorithms(
-				_combine(
-					basicSignatureSigningConfiguration.
-						getBlacklistedAlgorithms(),
-					(String[])blacklistedAlgorithmsObject));
-
-			basicSignatureValidationConfiguration.setBlacklistedAlgorithms(
-				_combine(
-					basicSignatureValidationConfiguration.
-						getBlacklistedAlgorithms(),
-					(String[])blacklistedAlgorithmsObject));
+			blacklistedAlgorithms = (String[])blacklistedAlgorithmsObject;
 		}
+
+		String[] fipsDisallowedAlgorithms = new String[0];
+
+		if (PropsValues.FIPS_ENABLED) {
+			fipsDisallowedAlgorithms = _FIPS_DISALLOWED_ALGORITHMS;
+		}
+
+		_blacklist(basicDecryptionConfiguration, blacklistedAlgorithms);
+		_blacklist(basicEncryptionConfiguration, blacklistedAlgorithms);
+		_blacklist(
+			basicSignatureSigningConfiguration, blacklistedAlgorithms,
+			fipsDisallowedAlgorithms);
+		_blacklist(
+			basicSignatureValidationConfiguration, blacklistedAlgorithms,
+			fipsDisallowedAlgorithms);
 
 		ConfigurationServiceBootstrapUtil.register(
 			DecryptionConfiguration.class, basicDecryptionConfiguration);
@@ -88,14 +89,31 @@ public class SecurityConfigurationBootstrap {
 			basicSignatureValidationConfiguration);
 	}
 
-	private Collection<String> _combine(
-		Collection<String> collection, String... strings) {
+	private void _blacklist(
+		BasicWhitelistBlacklistConfiguration
+			basicWhitelistBlacklistConfiguration,
+		String[]... stringsArrays) {
 
-		Collection<String> combinedCollection = new HashSet<>(collection);
+		Collection<String> blacklistedAlgorithms = new HashSet<>(
+			basicWhitelistBlacklistConfiguration.getBlacklistedAlgorithms());
 
-		Collections.addAll(combinedCollection, strings);
+		for (String[] strings : stringsArrays) {
+			Collections.addAll(blacklistedAlgorithms, strings);
+		}
 
-		return combinedCollection;
+		basicWhitelistBlacklistConfiguration.setBlacklistedAlgorithms(
+			blacklistedAlgorithms);
 	}
+
+	private static final String[] _FIPS_DISALLOWED_ALGORITHMS = {
+		SignatureConstants.ALGO_ID_DIGEST_NOT_RECOMMENDED_MD5,
+		SignatureConstants.ALGO_ID_DIGEST_SHA1,
+		SignatureConstants.ALGO_ID_MAC_HMAC_NOT_RECOMMENDED_MD5,
+		SignatureConstants.ALGO_ID_MAC_HMAC_SHA1,
+		SignatureConstants.ALGO_ID_SIGNATURE_DSA_SHA1,
+		SignatureConstants.ALGO_ID_SIGNATURE_ECDSA_SHA1,
+		SignatureConstants.ALGO_ID_SIGNATURE_NOT_RECOMMENDED_RSA_MD5,
+		SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA1
+	};
 
 }
