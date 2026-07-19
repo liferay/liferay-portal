b819743d3caec25a1b30e86c4478d4a78aa311d6
shuyangzhou <shuyang.zhou@liferay.com>
2026-07-19 11:08:33 -0700

    LPD-98680 Inject and call persistence directly in announcements services

--- portal-impl/src/com/liferay/portlet/announcements/service/impl/AnnouncementsEntryServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/announcements/service/impl/AnnouncementsEntryServiceImpl.java
@@ -16,12 +16,12 @@ import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.service.RoleLocalService;
-import com.liferay.portal.kernel.service.TeamLocalService;
 import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.service.permission.OrganizationPermissionUtil;
 import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
 import com.liferay.portal.kernel.service.permission.RolePermissionUtil;
+import com.liferay.portal.kernel.service.persistence.RolePersistence;
+import com.liferay.portal.kernel.service.persistence.TeamPersistence;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.service.permission.UserGroupPermissionUtil;
@@ -79,10 +79,11 @@ public class AnnouncementsEntryServiceImpl
 			}
 
 			if (className.equals(Role.class.getName())) {
-				Role role = _roleLocalService.getRole(classPK);
+				Role role = _rolePersistence.findByPrimaryKey(classPK);
 
 				if (role.isTeam()) {
-					Team team = _teamLocalService.getTeam(role.getClassPK());
+					Team team = _teamPersistence.findByPrimaryKey(
+						role.getClassPK());
 
 					if (!GroupPermissionUtil.contains(
 							permissionChecker, team.getGroupId(),
@@ -132,8 +133,8 @@ public class AnnouncementsEntryServiceImpl
 
 	@Override
 	public AnnouncementsEntry getEntry(long entryId) throws PortalException {
-		AnnouncementsEntry entry = announcementsEntryLocalService.getEntry(
-			entryId);
+		AnnouncementsEntry entry =
+			announcementsEntryPersistence.findByPrimaryKey(entryId);
 
 		AnnouncementsEntryPermission.check(
 			getPermissionChecker(), entry, ActionKeys.VIEW);
@@ -155,10 +156,10 @@ public class AnnouncementsEntryServiceImpl
 			priority);
 	}
 
-	@BeanReference(type = RoleLocalService.class)
-	private RoleLocalService _roleLocalService;
+	@BeanReference(type = RolePersistence.class)
+	private RolePersistence _rolePersistence;
 
-	@BeanReference(type = TeamLocalService.class)
-	private TeamLocalService _teamLocalService;
+	@BeanReference(type = TeamPersistence.class)
+	private TeamPersistence _teamPersistence;
 
 }
