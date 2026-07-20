19025ef8ac6b265ab4011b6007718b94d13225ca
shuyangzhou <shuyang.zhou@liferay.com>
2026-07-20 12:09:29 -0700

    LPD-99062 Add a test for JavaServiceGetFetchCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
@@ -981,6 +981,18 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testServiceGetFetch() throws Exception {
+		test(
+			"GetFetchLocalServiceImpl.testjava",
+			StringBundler.concat(
+				"The \"getDirectFetch\" method returns a nullable fetch ",
+				"result, which its \"get\" name promises will never be null; ",
+				"return a throwing find (raising a NoSuch*Exception) or ",
+				"rename the method to \"fetch\""),
+			17);
+	}
+
+	@Test
 	public void testServiceProxyFactoryNewServiceTrackedInstance()
 		throws Exception {
 
