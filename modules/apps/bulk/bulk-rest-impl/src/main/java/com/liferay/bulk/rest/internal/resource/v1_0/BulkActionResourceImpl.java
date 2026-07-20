fcf0abac8028116101515ac5112a8f814e6e7860
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-07-20 12:39:59 +0200

    LPD-98462 Run the UpdateReviewDate bulk selection action

--- modules/apps/bulk/bulk-rest-impl/src/main/java/com/liferay/bulk/rest/internal/resource/v1_0/BulkActionResourceImpl.java
+++ modules/apps/bulk/bulk-rest-impl/src/main/java/com/liferay/bulk/rest/internal/resource/v1_0/BulkActionResourceImpl.java
@@ -22,6 +22,7 @@ import com.liferay.bulk.rest.dto.v1_0.PermissionObjectBulkSelectionAction;
 import com.liferay.bulk.rest.dto.v1_0.SelectionScope;
 import com.liferay.bulk.rest.dto.v1_0.StatusObjectBulkSelectionAction;
 import com.liferay.bulk.rest.dto.v1_0.UpdateObjectValuesBulkSelectionAction;
+import com.liferay.bulk.rest.dto.v1_0.UpdateReviewDateObjectBulkSelectionAction;
 import com.liferay.bulk.rest.internal.odata.entity.v1_0.BulkActionEntityModel;
 import com.liferay.bulk.rest.internal.selection.v1_0.BulkActionBulkSelectionFactory;
 import com.liferay.bulk.rest.resource.v1_0.BulkActionResource;
@@ -523,6 +524,12 @@ public class BulkActionResourceImpl extends BaseBulkActionResourceImpl {
 
 			return _updateObjectValuesBulkSelectionAction;
 		}
+		else if (BulkAction.Type.
+					UPDATE_REVIEW_DATE_OBJECT_BULK_SELECTION_ACTION.equals(
+						type)) {
+
+			return _updateReviewDateObjectBulkSelectionAction;
+		}
 
 		throw new UnsupportedOperationException();
 	}
@@ -730,6 +737,18 @@ public class BulkActionResourceImpl extends BaseBulkActionResourceImpl {
 
 			return hashMapWrapper.put(
 				"values", (Serializable)updateValuesBulkAction.getValues()
+			).build();
+		}
+		else if (BulkAction.Type.
+					UPDATE_REVIEW_DATE_OBJECT_BULK_SELECTION_ACTION.equals(
+						type)) {
+
+			UpdateReviewDateObjectBulkSelectionAction
+				updateReviewDateBulkAction =
+					(UpdateReviewDateObjectBulkSelectionAction)bulkAction;
+
+			return hashMapWrapper.put(
+				"reviewDate", updateReviewDateBulkAction.getReviewDate()
 			).build();
 		}
 
@@ -1238,5 +1257,9 @@ public class BulkActionResourceImpl extends BaseBulkActionResourceImpl {
 
 	@Reference(target = "(bulk.selection.action.key=update.object.values)")
 	private BulkSelectionAction<Object> _updateObjectValuesBulkSelectionAction;
+
+	@Reference(target = "(bulk.selection.action.key=update.review.date.object)")
+	private BulkSelectionAction<Object>
+		_updateReviewDateObjectBulkSelectionAction;
 
 }
