44f274dc67d3d37a4010dab9f2105f21f871dcba
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-07-20 16:48:08 +0200

    LPD-98539 SF Auto

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/exception/FragmentCompositionGroupIdException.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/exception/FragmentCompositionGroupIdException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.fragment.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -18,7 +19,9 @@ public class FragmentCompositionGroupIdException extends PortalException {
 		super(msg);
 	}
 
-	public FragmentCompositionGroupIdException(String msg, Throwable throwable) {
+	public FragmentCompositionGroupIdException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
