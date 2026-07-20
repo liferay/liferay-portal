315046f58fb0e4dc9a2d68d929ea63fbc05417d2
Matyas Wollner <matyas.wollner@liferay.com>
2026-07-20 19:13:59 +0200

    LPD-97485 unit tests

--- modules/apps/object/object-web/src/test/java/com/liferay/object/web/internal/info/item/handler/ObjectEntryInfoItemExceptionRequestHandlerTest.java
+++ modules/apps/object/object-web/src/test/java/com/liferay/object/web/internal/info/item/handler/ObjectEntryInfoItemExceptionRequestHandlerTest.java
@@ -8,7 +8,15 @@ package com.liferay.object.web.internal.info.item.handler;
 import com.liferay.asset.kernel.exception.AssetCategoryException;
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.info.exception.InfoFormValidationException;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
+import com.liferay.portal.kernel.exception.GroupFriendlyURLException;
+import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.test.TestInfo;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.Assert;
@@ -29,10 +37,45 @@ public class ObjectEntryInfoItemExceptionRequestHandlerTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
-	@TestInfo("LPD-96532")
+	@TestInfo({"LPD-96532", "LPD-97485"})
 	public void testHandleInfoFormException() throws Exception {
 		_testHandleInfoFormExceptionWhenAssetCategoryExceptionTypeIsAtLeastOneCategory();
 		_testHandleInfoFormExceptionWhenAssetCategoryExceptionTypeIsTooManyCategories();
+		_testHandleInfoFormExceptionWhenDuplicateExternalReferenceCode();
+		_testHandleInfoFormExceptionWhenDuplicateFriendlyURL();
+	}
+
+	private InfoItemFormProvider<?> _mockInfoItemFormProvider()
+		throws Exception {
+
+		InfoItemFormProvider<?> infoItemFormProvider = Mockito.mock(
+			InfoItemFormProvider.class);
+		InfoForm infoForm = Mockito.mock(InfoForm.class);
+
+		InfoField<?> infoField = Mockito.mock(InfoField.class);
+
+		Mockito.when(
+			infoField.getUniqueId()
+		).thenReturn(
+			RandomTestUtil.randomString()
+		);
+
+		Mockito.doReturn(
+			infoField
+		).when(
+			infoForm
+		).getInfoField(
+			Mockito.anyString()
+		);
+
+		Mockito.when(
+			infoItemFormProvider.getInfoForm(
+				Mockito.anyString(), Mockito.anyLong())
+		).thenReturn(
+			infoForm
+		);
+
+		return infoItemFormProvider;
 	}
 
 	private void _testHandleInfoFormExceptionWhenAssetCategoryExceptionTypeIsAtLeastOneCategory()
@@ -79,6 +122,34 @@ public class ObjectEntryInfoItemExceptionRequestHandlerTest {
 				assetVocabulary,
 				infoFormValidationException.getAssetVocabulary());
 		}
+	}
+
+	private void _testHandleInfoFormExceptionWhenDuplicateExternalReferenceCode()
+		throws Exception {
+
+		Assert.assertThrows(
+			InfoFormValidationException.DuplicateExternalReferenceCode.class,
+			() ->
+				ObjectEntryInfoItemExceptionRequestHandler.
+					handleInfoFormException(
+						new DuplicateExternalReferenceCodeException(), 0,
+						_mockInfoItemFormProvider(),
+						Mockito.mock(ObjectDefinition.class)));
+	}
+
+	private void _testHandleInfoFormExceptionWhenDuplicateFriendlyURL()
+		throws Exception {
+
+		Assert.assertThrows(
+			InfoFormValidationException.DuplicateFriendlyURL.class,
+			() ->
+				ObjectEntryInfoItemExceptionRequestHandler.
+					handleInfoFormException(
+						new ModelListenerException(
+							new GroupFriendlyURLException(
+								GroupFriendlyURLException.DUPLICATE)),
+						0, _mockInfoItemFormProvider(),
+						Mockito.mock(ObjectDefinition.class)));
 	}
 
 }
