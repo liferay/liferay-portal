4107d2788fd8e883f8c5b373985645b22b3d242a
Marcio Fonseca <marcio.fonseca@liferay.com>
2026-07-20 17:42:06 -0300

    LPD-98636 create AssignIndividualsDataToPropertiesStep in ConnectMarketoCampaign

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/settings/pages/data-source/ConnectMarketoCampaign.tsx
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/settings/pages/data-source/ConnectMarketoCampaign.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import WizardPage, {Step} from 'settings/components/base-page/WizardPage';
+import {Alert} from 'shared/types';
+import {AssignIndividualsDataToPropertiesStep} from 'settings/components/salesforce/steps/AssignIndividualsDataToChannelsStep';
 import {ConnectMarketoCampaignStep} from 'settings/components/marketo/steps/ConnectMarketoCampaignStep';
-import {SyncMarketoCampaignDataStep} from 'settings/components/marketo/steps/SyncMarketoCampaignDataStep';
 import {sub} from 'shared/util/lang';
+import {SyncMarketoCampaignDataStep} from 'settings/components/marketo/steps/SyncMarketoCampaignDataStep';
+import {updateMarketoCampaign} from 'shared/api/data-source';
 
 const steps: Step[] = [
 	{
@@ -23,6 +26,49 @@ const steps: Step[] = [
 		title: sub(Liferay.Language.get('sync-x-data'), [
 			Liferay.Language.get('marketo'),
 		]) as string,
+	},
+
+	{
+		content: (props: any) => (
+			<AssignIndividualsDataToPropertiesStep
+				{...props}
+				onSubmit={(dataSource) => {
+					const contactsConfiguration = dataSource.provider?.get(
+						'contactsConfiguration'
+					);
+
+					const individualsEnabled =
+						contactsConfiguration?.get('enableAllLeads');
+
+					if (individualsEnabled) {
+						props.addAlert({
+							alertType: Alert.Types.Success,
+							message: Liferay.Language.get(
+								'the-data-source-setup-is-now-complete,-and-you-will-begin-to-see-data-as-activities-occur-on-your-sites'
+							),
+						});
+					}
+					else {
+						props.addAlert({
+							alertType: Alert.Types.Success,
+							message: Liferay.Language.get(
+								'the-data-source-setup-has-finished'
+							),
+						});
+					}
+				}}
+				updateDataSourceFn={
+					updateMarketoCampaign as (params: {
+						[key: string]: any;
+					}) => Promise<any>
+				}
+			/>
+		),
+
+		description: Liferay.Language.get(
+			'properties-let-you-consolidate-data-from-individuals,-accounts,-campaigns,-sites,-and-commerce-channels-in-one-place.-an-individuals-data-is-available-in-every-property-they-are-assigned-to'
+		),
+		title: Liferay.Language.get('assign-individuals-data-to-properties'),
 	},
 ];
 
