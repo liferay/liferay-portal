7c30f247f71ef87cabd2f4e0e6754abda20f0bb6
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-07-21 16:08:15 -0300

    LPD-98752 Initialize the CMP site on the first request after CMS

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/instance/lifecycle/AddDefaultLayoutInitialRequestPortalInstanceLifecycleListener.java
@@ -0,0 +1,61 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.instance.lifecycle;
+
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.instance.lifecycle.InitialRequestPortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.license.util.App;
+import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.site.cmp.site.initializer.internal.util.SiteInitializerUtil;
+import com.liferay.site.initializer.SiteInitializer;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carolina Barbosa
+ */
+@Component(service = PortalInstanceLifecycleListener.class)
+public class AddDefaultLayoutInitialRequestPortalInstanceLifecycleListener
+	extends InitialRequestPortalInstanceLifecycleListener {
+
+	@Activate
+	@Override
+	protected void activate(BundleContext bundleContext) {
+		super.activate(bundleContext);
+	}
+
+	@Override
+	protected void doPortalInstanceRegistered(long companyId) throws Exception {
+		if (!LicenseManagerUtil.isAppEnabled(App.CMP)) {
+			return;
+		}
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setProductionModeWithSafeCloseable()) {
+
+			com.liferay.site.cms.site.initializer.util.SiteInitializerUtil.
+				initialize(companyId, _cmsSiteInitializer);
+
+			SiteInitializerUtil.initialize(companyId, _cmpSiteInitializer);
+		}
+	}
+
+	@Reference(
+		target = "(site.initializer.key=com.liferay.site.initializer.cmp)"
+	)
+	private SiteInitializer _cmpSiteInitializer;
+
+	@Reference(
+		target = "(site.initializer.key=com.liferay.site.initializer.cms)"
+	)
+	private SiteInitializer _cmsSiteInitializer;
+
+}
