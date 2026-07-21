83241b7fa54d2f39f3aea3a98c8e58ebc8ae3f7e
Alessio Rendina <alessio.rendina@liferay.com>
2026-07-21 12:44:06 +0200

    LPD-93751 Test the pricing class status upgrade process

---
+++ modules/apps/commerce/commerce-pricing-test/src/testIntegration/java/com/liferay/commerce/pricing/internal/upgrade/v2_4_0/test/CommercePricingClassUpgradeProcessTest.java
@@ -0,0 +1,97 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.pricing.internal.upgrade.v2_4_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.pricing.model.CommercePricingClass;
+import com.liferay.commerce.pricing.service.CommercePricingClassLocalService;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@RunWith(Arquillian.class)
+public class CommercePricingClassUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpgradeCommercePricingClassStatus() throws Exception {
+		CommercePricingClass commercePricingClass =
+			_commercePricingClassLocalService.addCommercePricingClass(
+				TestPropsValues.getUserId(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(),
+				ServiceContextTestUtil.getServiceContext());
+
+		try (Connection connection = DataAccess.getConnection();
+
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				"update CommercePricingClass set status = ? where " +
+					"commercePricingClassId = ?")) {
+
+			preparedStatement.setInt(1, WorkflowConstants.STATUS_DENIED);
+			preparedStatement.setLong(
+				2, commercePricingClass.getCommercePricingClassId());
+
+			preparedStatement.executeUpdate();
+		}
+
+		_runUpgrade();
+
+		commercePricingClass =
+			_commercePricingClassLocalService.getCommercePricingClass(
+				commercePricingClass.getCommercePricingClassId());
+
+		Assert.assertEquals(
+			WorkflowConstants.STATUS_APPROVED,
+			commercePricingClass.getStatus());
+	}
+
+	private void _runUpgrade() throws Exception {
+		UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator, _CLASS_NAME);
+
+		upgradeProcess.upgrade();
+
+		EntityCacheUtil.clearCache();
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.commerce.pricing.internal.upgrade.v2_4_0." +
+			"CommercePricingClassUpgradeProcess";
+
+	@Inject
+	private CommercePricingClassLocalService _commercePricingClassLocalService;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.commerce.pricing.internal.upgrade.registry.CommercePricingServiceUpgradeStepRegistrator))"
+	)
+	private UpgradeStepRegistrator _upgradeStepRegistrator;
+
+}
