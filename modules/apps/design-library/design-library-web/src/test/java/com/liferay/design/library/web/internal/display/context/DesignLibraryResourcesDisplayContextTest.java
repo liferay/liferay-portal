49926e7528f9e36b8362b08dd2aa6d4a1403da03
Gabriel Prates <gabsprates@gmail.com>
2026-07-22 16:00:31 +0200

    LPD-98939 Set up the request before building the display context
    The display context constructor reads the theme display from the request eagerly, so the request attribute must be stubbed before the context is built or the fragment and style book tests get a null theme display.

--- modules/apps/design-library/design-library-web/src/test/java/com/liferay/design/library/web/internal/display/context/DesignLibraryResourcesDisplayContextTest.java
+++ modules/apps/design-library/design-library-web/src/test/java/com/liferay/design/library/web/internal/display/context/DesignLibraryResourcesDisplayContextTest.java
@@ -69,8 +69,8 @@ public class DesignLibraryResourcesDisplayContextTest {
 
 	@Before
 	public void setUp() {
-		_setUpDesignLibraryResourcesDisplayContext();
 		_setUpJSONFactoryUtil();
+		_setUpDesignLibraryResourcesDisplayContext();
 	}
 
 	@After
