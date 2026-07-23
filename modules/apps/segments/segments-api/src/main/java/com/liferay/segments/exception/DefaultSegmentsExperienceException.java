fea0b65e24cad44fe5ab2bc35004f5c684b07d4c
Alberto Chaparro <alberto.chaparro@liferay.com>
2026-07-23 13:54:24 +0200

    LPD-95007 buildService

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/exception/DefaultSegmentsExperienceException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+package com.liferay.segments.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class DefaultSegmentsExperienceException extends PortalException {
+
+	public DefaultSegmentsExperienceException() {
+	}
+
+	public DefaultSegmentsExperienceException(String msg) {
+		super(msg);
+	}
+
+	public DefaultSegmentsExperienceException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public DefaultSegmentsExperienceException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
