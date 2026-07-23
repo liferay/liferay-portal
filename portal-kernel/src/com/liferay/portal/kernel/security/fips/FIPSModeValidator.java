c37ae7a3c272de9b2d5be11d1ce60be55331e072
Lucas Miranda <lucas.miranda@liferay.com>
2026-07-23 12:04:46 -0300

    LPD-93272 Add on-demand FIPS self-test

--- portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSModeValidator.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSModeValidator.java
@@ -34,6 +34,31 @@ import java.util.regex.Pattern;
  */
 public class FIPSModeValidator {
 
+	public static FIPSHealthCheckResult runSelfTests() {
+		if (!PropsValues.FIPS_ENABLED) {
+			return FIPSHealthCheckResult.notApplicable();
+		}
+
+		synchronized (_selfTestLock) {
+			try {
+				String providerName = _fipsSelfTestExecutor.execute();
+
+				return FIPSHealthCheckResult.healthy(providerName);
+			}
+			catch (FIPSSelfTestException fipsSelfTestException) {
+				return FIPSHealthCheckResult.failed(
+					fipsSelfTestException.getProviderName(),
+					fipsSelfTestException.getFailedTest(),
+					fipsSelfTestException.getFipsState(),
+					fipsSelfTestException.getProviderMessage());
+			}
+			catch (Exception exception) {
+				return FIPSHealthCheckResult.failed(
+					null, "self-test-execution", null, exception.getMessage());
+			}
+		}
+	}
+
 	public static void validate() {
 		Provider[] providers = Security.getProviders();
 
@@ -270,7 +295,10 @@ public class FIPSModeValidator {
 			List.of(
 				"BCJSSE", "JdkLDAP", "JdkSASL", "SUN", "SunJCE", "SunJGSS",
 				"SunSASL", "XMLDSig"));
+	private static final FIPSSelfTestExecutor _fipsSelfTestExecutor =
+		new ReflectionFIPSSelfTestExecutor();
 	private static final Pattern _pbkdf2Pattern = Pattern.compile(
 		"^[^/]*(?:/([0-9]+))?/([0-9]+)$");
+	private static final Object _selfTestLock = new Object();
 
 }
