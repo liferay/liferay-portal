be2ae047b37143160a3af4d20cf15aae4e7a78cd
Guilherme Camacho <guilherme.camacho@liferay.com>
2026-07-23 14:54:10 -0300

    LPD-99049 Stop asserting the internal tag in the add task test
    The add task Struts action no longer stamps the internal L_CMP_TASK tag,
    so drop the assertion that expected it.

--- modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/struts/test/AddTaskStrutsActionTest.java
+++ modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/struts/test/AddTaskStrutsActionTest.java
@@ -6,7 +6,6 @@
 package com.liferay.site.cmp.site.initializer.internal.struts.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
@@ -125,22 +124,10 @@ public class AddTaskStrutsActionTest {
 			MapUtil.getLong(
 				cmpTaskObjectEntry.getValues(),
 				"r_cmpProjectToCMPTasks_c_cmpProjectId"));
-
-		String[] tagNames = _assetTagLocalService.getTagNames(
-			_cmpTaskObjectDefinition.getClassName(),
-			cmpTaskObjectEntry.getObjectEntryId());
-
-		Assert.assertTrue(
-			StringUtil.startsWith(
-				tagNames[0],
-				_cmpTaskObjectDefinition.getExternalReferenceCode()));
 	}
 
 	@Inject(filter = "path=/cms/add_task")
 	private StrutsAction _addTaskStrutsAction;
-
-	@Inject
-	private AssetTagLocalService _assetTagLocalService;
 
 	private ObjectEntry _cmpProjectObjectEntry;
 	private ObjectDefinition _cmpTaskObjectDefinition;
