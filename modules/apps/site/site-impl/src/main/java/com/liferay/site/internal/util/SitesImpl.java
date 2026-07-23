7ef4c7da107a853f0c5d3aa26561a5bf89084072
Jaime Leon <jaime.leon@liferay.com>
2026-07-23 09:53:04 +0200

    LPD-87027 Look up the CT settings configuration helper lazily

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/util/SitesImpl.java
@@ -43,6 +43,7 @@ import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
@@ -587,7 +588,11 @@ public class SitesImpl implements Sites {
 					"import, or staging process is in progress");
 		}
 
-		if (_ctSettingsConfigurationHelper.isEnabled(
+		CTSettingsConfigurationHelper ctSettingsConfigurationHelper =
+			_ctSettingsConfigurationHelperSnapshot.get();
+
+		if ((ctSettingsConfigurationHelper != null) &&
+			ctSettingsConfigurationHelper.isEnabled(
 				layoutSetPrototype.getCompanyId())) {
 
 			throw new IllegalStateException(
@@ -1161,11 +1166,12 @@ public class SitesImpl implements Sites {
 
 	private static final Log _log = LogFactoryUtil.getLog(SitesImpl.class);
 
-	@Reference
-	private AssetEntryLocalService _assetEntryLocalService;
+	private static final Snapshot<CTSettingsConfigurationHelper>
+		_ctSettingsConfigurationHelperSnapshot = new Snapshot<>(
+			SitesImpl.class, CTSettingsConfigurationHelper.class);
 
 	@Reference
-	private CTSettingsConfigurationHelper _ctSettingsConfigurationHelper;
+	private AssetEntryLocalService _assetEntryLocalService;
 
 	@Reference
 	private ExportImportConfigurationLocalService
