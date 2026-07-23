d53181222d0ea766d860af906029bdbaf0432375
Pedro Leite <pedro.leite@liferay.com>
2026-07-23 09:44:23 -0300

    LPD-99046 Allow filtering search results by cmpProjectObjectEntryIds

--- modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/odata/entity/v1_0/SearchResultEntityModel.java
+++ modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/odata/entity/v1_0/SearchResultEntityModel.java
@@ -39,6 +39,10 @@ public class SearchResultEntityModel implements EntityModel {
 					"cmpPersonaCategoryIds",
 					locale -> Field.ASSET_INTERNAL_CATEGORY_IDS)),
 			new CollectionEntityField(
+				new IntegerEntityField(
+					"cmpProjectObjectEntryIds",
+					locale -> "cmpProjectObjectEntryIds")),
+			new CollectionEntityField(
 				new IntegerEntityField("groupIds", locale -> Field.GROUP_ID)),
 			new CollectionEntityField(
 				new IntegerEntityField(
