980fb4d1850cc80cc0dc227cd86402c63ef9a5f5
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-07-23 10:39:10 +0100

    LPD-99212 Use the connect sites empty state in the asset performance tab

--- modules/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/cms/CheckPermissions.tsx
+++ modules/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/cms/CheckPermissions.tsx
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayButton from '@clayui/button';
 import ClayLink from '@clayui/link';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import React from 'react';
@@ -11,6 +10,7 @@ import React from 'react';
 import useFetch from '../../hooks/useFetch';
 import {buildQueryString} from '../../utils/buildQueryString';
 import EmptyState from '../EmptyState';
+import ConnectSites from './ConnectSites';
 import ConnectToAnalyticsCloud, {
 	buildAnalyticsCloudConfigURL,
 } from './ConnectToAnalyticsCloud';
@@ -32,28 +32,8 @@ const EmptyStates: React.FC<IEmptyStateProps> = ({
 	siteSyncedToAnalyticsCloud,
 }) => {
 	if (!connectedToSpace) {
-		if (admin) {
-			return (
-				<EmptyState
-					description={Liferay.Language.get(
-						'connect-sites-to-this-space'
-					)}
-					title={Liferay.Language.get('no-sites-are-connected-yet')}
-				>
-					<ClayButton onClick={onConnectSites} size="sm">
-						{Liferay.Language.get('connect')}
-					</ClayButton>
-				</EmptyState>
-			);
-		}
-
 		return (
-			<EmptyState
-				description={Liferay.Language.get(
-					'please-contact-an-administrator-to-sync-sites-to-this-space'
-				)}
-				title={Liferay.Language.get('no-sites-are-connected-yet')}
-			/>
+			<ConnectSites onConnectSites={admin ? onConnectSites : undefined} />
 		);
 	}
 
