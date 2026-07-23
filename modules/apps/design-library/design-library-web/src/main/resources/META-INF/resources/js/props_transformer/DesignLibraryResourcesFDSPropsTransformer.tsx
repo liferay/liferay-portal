61055003270bc463c958ec4ef3ad9a4b180da096
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-07-23 23:56:21 +0100

    LPD-99234 Use the fragment set icon and pink color in the resources dataset

--- modules/apps/design-library/design-library-web/src/main/resources/META-INF/resources/js/props_transformer/DesignLibraryResourcesFDSPropsTransformer.tsx
+++ modules/apps/design-library/design-library-web/src/main/resources/META-INF/resources/js/props_transformer/DesignLibraryResourcesFDSPropsTransformer.tsx
@@ -209,7 +209,9 @@ export default function DesignLibraryResourcesFDSPropsTransformer(
 										? 'design-library-fds-sticker-fragment-set'
 										: 'design-library-fds-sticker-stylebook'
 								}
-								symbol="book"
+								symbol={getSymbol(
+									rendererProps?.itemData?.entryClassName
+								)}
 							/>
 						);
 					},
@@ -291,4 +293,12 @@ export default function DesignLibraryResourcesFDSPropsTransformer(
 			},
 		],
 	};
+}
+
+function getSymbol(entryClassName?: string): string {
+	if (entryClassName === FRAGMENT_COLLECTION_ENTRY_CLASS_NAME) {
+		return 'squares';
+	}
+
+	return 'book';
 }
