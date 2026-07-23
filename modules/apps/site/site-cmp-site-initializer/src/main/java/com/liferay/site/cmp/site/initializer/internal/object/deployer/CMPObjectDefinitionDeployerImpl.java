49c3b5065eb29d3db0e7b871ae74ef5dd81e3a0b
Pedro Leite <pedro.leite@liferay.com>
2026-07-23 09:44:08 -0300

    LPD-99046 Denormalize linked CMP project IDs onto CMS asset documents

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/object/deployer/CMPObjectDefinitionDeployerImpl.java
@@ -0,0 +1,83 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.object.deployer;
+
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.deployer.ObjectDefinitionDeployer;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.rest.filter.factory.FilterFactory;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryFolderLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.site.cmp.site.initializer.internal.search.spi.model.index.contributor.CMPObjectEntryModelDocumentContributor;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pedro Leite
+ */
+@Component(service = ObjectDefinitionDeployer.class)
+public class CMPObjectDefinitionDeployerImpl
+	implements ObjectDefinitionDeployer {
+
+	@Override
+	public List<ServiceRegistration<?>> deploy(
+		ObjectDefinition objectDefinition) {
+
+		if (!objectDefinition.isCMS()) {
+			return Collections.emptyList();
+		}
+
+		return ListUtil.fromArray(
+			_bundleContext.registerService(
+				(Class<ModelDocumentContributor<?>>)
+					(Class<?>)ModelDocumentContributor.class,
+				new CMPObjectEntryModelDocumentContributor(
+					_filterFactory, _groupLocalService,
+					_objectDefinitionLocalService,
+					_objectEntryFolderLocalService, _objectEntryLocalService),
+				HashMapDictionaryBuilder.<String, Object>put(
+					"indexer.class.name", objectDefinition.getClassName()
+				).build()));
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	private BundleContext _bundleContext;
+
+	@Reference(
+		target = "(filter.factory.key=" + ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT + ")"
+	)
+	private FilterFactory<Predicate> _filterFactory;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryFolderLocalService _objectEntryFolderLocalService;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+}
---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/search/spi/model/index/contributor/CMPObjectEntryModelDocumentContributor.java
@@ -0,0 +1,156 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.search.spi.model.index.contributor;
+
+import com.liferay.object.constants.ObjectEntryFolderConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectEntryFolder;
+import com.liferay.object.rest.filter.factory.FilterFactory;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryFolderLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.List;
+
+/**
+ * @author Pedro Leite
+ */
+public class CMPObjectEntryModelDocumentContributor
+	implements ModelDocumentContributor<ObjectEntry> {
+
+	public CMPObjectEntryModelDocumentContributor(
+		FilterFactory<Predicate> filterFactory,
+		GroupLocalService groupLocalService,
+		ObjectDefinitionLocalService objectDefinitionLocalService,
+		ObjectEntryFolderLocalService objectEntryFolderLocalService,
+		ObjectEntryLocalService objectEntryLocalService) {
+
+		_filterFactory = filterFactory;
+		_groupLocalService = groupLocalService;
+		_objectDefinitionLocalService = objectDefinitionLocalService;
+		_objectEntryFolderLocalService = objectEntryFolderLocalService;
+		_objectEntryLocalService = objectEntryLocalService;
+	}
+
+	@Override
+	public void contribute(Document document, ObjectEntry objectEntry) {
+		try {
+			_contribute(document, objectEntry);
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+	}
+
+	private void _contribute(Document document, ObjectEntry objectEntry)
+		throws PortalException {
+
+		Group group = _groupLocalService.fetchGroup(objectEntry.getGroupId());
+
+		if (group == null) {
+			return;
+		}
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_CMP_PROJECT_LINK", objectEntry.getCompanyId());
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectEntryFolder rootObjectEntryFolder = _getRootObjectEntryFolder(
+			_objectEntryFolderLocalService.fetchObjectEntryFolder(
+				objectEntry.getObjectEntryFolderId()));
+
+		if (rootObjectEntryFolder == null) {
+			return;
+		}
+
+		String externalReferenceCode =
+			rootObjectEntryFolder.getExternalReferenceCode();
+
+		if (!StringUtil.equals(
+				externalReferenceCode,
+				ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_CONTENTS) &&
+			!StringUtil.equals(
+				externalReferenceCode,
+				ObjectEntryFolderConstants.EXTERNAL_REFERENCE_CODE_FILES)) {
+
+			return;
+		}
+
+		List<Long> objectEntryIds = _objectEntryLocalService.getPrimaryKeys(
+			new Long[0], objectEntry.getCompanyId(), 0,
+			objectDefinition.getObjectDefinitionId(),
+			_filterFactory.create(
+				StringBundler.concat(
+					"classExternalReferenceCode eq '",
+					objectEntry.getExternalReferenceCode(),
+					"' and className eq '", objectEntry.getModelClassName(),
+					"' and groupExternalReferenceCode eq '",
+					group.getExternalReferenceCode(), "'"),
+				objectDefinition),
+			false, null, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		if (objectEntryIds.isEmpty()) {
+			return;
+		}
+
+		document.addKeyword(
+			"cmpProjectObjectEntryIds",
+			TransformUtil.transformToLongArray(
+				objectEntryIds,
+				objectEntryId -> MapUtil.getLong(
+					_objectEntryLocalService.getValues(objectEntryId),
+					"r_cmpProjectToCMPProjectLinks_c_cmpProjectId")));
+	}
+
+	private ObjectEntryFolder _getRootObjectEntryFolder(
+		ObjectEntryFolder objectEntryFolder) {
+
+		if (objectEntryFolder == null) {
+			return null;
+		}
+
+		String[] parts = StringUtil.split(
+			objectEntryFolder.getTreePath(), CharPool.SLASH);
+
+		if (parts.length <= 2) {
+			return objectEntryFolder;
+		}
+
+		return _objectEntryFolderLocalService.fetchObjectEntryFolder(
+			GetterUtil.getLong(parts[1]));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CMPObjectEntryModelDocumentContributor.class);
+
+	private final FilterFactory<Predicate> _filterFactory;
+	private final GroupLocalService _groupLocalService;
+	private final ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private final ObjectEntryFolderLocalService _objectEntryFolderLocalService;
+	private final ObjectEntryLocalService _objectEntryLocalService;
+
+}
