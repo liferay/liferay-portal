7497c9b9becd5b3c568923e8385d502e76a5c518
Pedro Leite <pedro.leite@liferay.com>
2026-07-24 11:27:55 -0300

    LPD-99411 [BE] Create new ProjectMembersSummarySection displayContext and fragmentRenderer classes

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/fragment/renderer/ViewProjectMembersSummaryJSPSectionFragmentRenderer.java
@@ -0,0 +1,77 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.fragment.renderer;
+
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.site.cmp.site.initializer.internal.display.context.ViewProjectMembersSummarySectionDisplayContext;
+import com.liferay.site.cmp.site.initializer.internal.util.ObjectEntryUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pedro Leite
+ */
+@Component(service = FragmentRenderer.class)
+public class ViewProjectMembersSummaryJSPSectionFragmentRenderer
+	extends BaseJSPSectionFragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "sections";
+	}
+
+	@Override
+	protected Object getDisplayContext(HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		ObjectEntry objectEntry = ObjectEntryUtil.getObjectEntry(
+			httpServletRequest);
+
+		if (objectEntry == null) {
+			return null;
+		}
+
+		return new ViewProjectMembersSummarySectionDisplayContext(
+			objectEntry.getGroupId(), _groupLocalService,
+			_groupModelResourcePermission, httpServletRequest, language,
+			_userGroupLocalService, _userLocalService);
+	}
+
+	@Override
+	protected String getJSPPath() {
+		return "/view_project_members_summary.jsp";
+	}
+
+	@Override
+	protected String getLabelKey() {
+		return "project-members-summary";
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.kernel.model.Group)"
+	)
+	private ModelResourcePermission<Group> _groupModelResourcePermission;
+
+	@Reference
+	private UserGroupLocalService _userGroupLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
