a58b7ea06ee852b5f4b52364c29f4a89a8d141e5
Mario Leandro <mario.leandro@liferay.com>
2026-07-24 16:33:03 -0300

    LPD-99371 Add server support for the revamped Design Libraries home

---
+++ modules/apps/design-library/design-library-web/src/main/java/com/liferay/design/library/web/internal/display/context/DesignLibraryConnectedSitesDisplayContext.java
@@ -0,0 +1,81 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.design.library.web.internal.display.context;
+
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.module.service.Snapshot;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
+/**
+ * @author Mario Leandro
+ */
+public class DesignLibraryConnectedSitesDisplayContext
+	extends BaseDesignLibraryDisplayContext {
+
+	public DesignLibraryConnectedSitesDisplayContext(
+		DepotEntry depotEntry, HttpServletRequest httpServletRequest) {
+
+		super(depotEntry, httpServletRequest);
+	}
+
+	public String getConnectedSitesAPIURL() throws PortalException {
+		return getAssetLibraryURL(
+			getGroup(), "/connected-sites?page=1&pageSize=10");
+	}
+
+	public Map<String, Object> getConnectedSitesEmptyState() {
+		return buildEmptyState(
+			"connect-sites-to-this-design-library",
+			"no-sites-are-connected-yet");
+	}
+
+	public Map<String, Object> getConnectedSitesFDSAdditionalProps()
+		throws PortalException {
+
+		Group group = getGroup();
+
+		return HashMapBuilder.<String, Object>put(
+			"externalReferenceCode", group.getExternalReferenceCode()
+		).put(
+			"hasConnectSitesPermission", hasUpdatePermission(group)
+		).build();
+	}
+
+	public Map<String, Object> getConnectedSitesSectionHeaderProps()
+		throws PortalException {
+
+		return HashMapBuilder.<String, Object>putAll(
+			getConnectedSitesFDSAdditionalProps()
+		).put(
+			"count", _getConnectedSitesCount()
+		).build();
+	}
+
+	private int _getConnectedSitesCount() {
+		DepotEntryGroupRelLocalService depotEntryGroupRelLocalService =
+			_depotEntryGroupRelLocalServiceSnapshot.get();
+
+		if (depotEntryGroupRelLocalService == null) {
+			return 0;
+		}
+
+		return depotEntryGroupRelLocalService.getDepotEntryGroupRelsCount(
+			depotEntry);
+	}
+
+	private static final Snapshot<DepotEntryGroupRelLocalService>
+		_depotEntryGroupRelLocalServiceSnapshot = new Snapshot<>(
+			DesignLibraryConnectedSitesDisplayContext.class,
+			DepotEntryGroupRelLocalService.class);
+
+}
