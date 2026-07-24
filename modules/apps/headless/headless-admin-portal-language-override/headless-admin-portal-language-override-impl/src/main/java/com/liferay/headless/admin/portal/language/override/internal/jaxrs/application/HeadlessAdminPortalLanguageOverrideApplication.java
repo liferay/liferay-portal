e0da0e6770b400dbf8036f42f48a074522b2cbab
Thiago Buarque <thiago.buarque@liferay.com>
2026-07-24 14:37:59 -0400

    LPD-97678 REST Builder

---
+++ modules/apps/headless/headless-admin-portal-language-override/headless-admin-portal-language-override-impl/src/main/java/com/liferay/headless/admin/portal/language/override/internal/jaxrs/application/HeadlessAdminPortalLanguageOverrideApplication.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.portal.language.override.internal.jaxrs.application;
+
+import jakarta.annotation.Generated;
+
+import jakarta.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Thiago Buarque
+ * @generated
+ */
+@Component(
+	property = {
+		"liferay.jackson=false",
+		"osgi.jaxrs.application.base=/headless-admin-portal-language-override",
+		"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.Vulcan)",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.Portal.Language.Override"
+	},
+	service = Application.class
+)
+@Generated("")
+public class HeadlessAdminPortalLanguageOverrideApplication
+	extends Application {
+}
+// LIFERAY-REST-BUILDER-HASH:303968337
