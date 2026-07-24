1a9c35c6d0359783b517077a9a9d541b4ced0f62
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-07-24 12:28:43 +0200

    LPD-99381 Use es_ES instead of es-ES for the space language settings

--- modules/test/playwright/tests/site-cms-site-initializer/main/contentEditor.spec.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/main/contentEditor.spec.ts
@@ -189,8 +189,8 @@ test(
 		await apiHelpers.headlessAssetLibrary.createAssetLibrary({
 			name: spaceName,
 			settings: {
-				availableLanguageIds: ['es-ES'],
-				defaultLanguageId: 'es-ES',
+				availableLanguageIds: ['es_ES'],
+				defaultLanguageId: 'es_ES',
 				useCustomLanguages: true,
 			},
 			type: 'Space',
