847b32541710a303d63556fa54f9c382c8d6cc00
Pedro Leite <pedro.leite@liferay.com>
2026-07-24 11:45:52 -0300

    LPD-99411 [BE] Move to CMP as it is not used in CMS anymore

--- modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/util/CMPDepotEntryGroupUtil.java
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/util/CMPDepotEntryGroupUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.cms.site.initializer.util;
+package com.liferay.site.cmp.site.initializer.internal.util;
 
 import com.liferay.depot.constants.DepotConstants;
 import com.liferay.depot.service.DepotEntryLocalServiceUtil;
@@ -22,7 +22,7 @@ import java.util.Set;
 /**
  * @author Pedro Leite
  */
-public class CMSDepotEntryGroupUtil {
+public class CMPDepotEntryGroupUtil {
 
 	public static String getFilterString() {
 		Set<Long> depotEntryUserGroupIds = new HashSet<>();
