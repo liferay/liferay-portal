44c00f184e8f051816c1e69547abfd9dde7cd509
Pedro Leite <pedro.leite@liferay.com>
2026-07-24 15:16:18 -0300

    LPD-99411 [BE] Create fdsview classes

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/frontend/data/set/view/table/ProjectMembersUserGroupsSummarySectionTableFDSView.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.frontend.data.set.view.table;
+
+import com.liferay.frontend.data.set.view.FDSView;
+import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
+import com.liferay.frontend.data.set.view.table.FDSTableSchema;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
+import com.liferay.site.cmp.site.initializer.internal.constants.CMPSiteInitializerFDSNames;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pedro Leite
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMPSiteInitializerFDSNames.CMP_PROJECT_MEMBERS_USER_GROUPS_SUMMARY,
+	service = FDSView.class
+)
+public class ProjectMembersUserGroupsSummarySectionTableFDSView
+	extends BaseTableFDSView {
+
+	@Override
+	public FDSTableSchema getFDSTableSchema(Locale locale) {
+		FDSTableSchemaBuilder fdsTableSchemaBuilder =
+			_fdsTableSchemaBuilderFactory.create();
+
+		return fdsTableSchemaBuilder.add(
+			"name", "members",
+			fdsTableSchemaField -> fdsTableSchemaField.setContentRenderer(
+				"userGroupTableCellRenderer")
+		).build();
+	}
+
+	@Reference
+	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
+
+}
