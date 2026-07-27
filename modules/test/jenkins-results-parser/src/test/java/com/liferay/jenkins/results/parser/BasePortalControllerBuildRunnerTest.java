e2be2a37f0c3d2d6063791092fc9336cf94d8872
Kenji Heigel <kenji.heigel@liferay.com>
2026-07-27 11:28:49 -0700

    LRCI-7844 Test the concurrent-unique-SHA controller flag

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/BasePortalControllerBuildRunnerTest.java
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
+
+/**
+ * @author Kenji Heigel
+ */
+public class BasePortalControllerBuildRunnerTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testRun() {
+		_testRun("false");
+		_testRun("true");
+	}
+
+	private void _testRun(String allowConcurrentBuildsUniqueSHAString) {
+		Environment environment = Mockito.mock(Environment.class);
+
+		Environment.setInstance(environment);
+
+		Mockito.when(
+			environment.doGet("ALLOW_CONCURRENT_BUILDS_UNIQUE_SHA")
+		).thenReturn(
+			allowConcurrentBuildsUniqueSHAString
+		);
+
+		BasePortalControllerBuildRunner<?> basePortalControllerBuildRunner =
+			Mockito.mock(BasePortalControllerBuildRunner.class);
+
+		Mockito.doReturn(
+			false
+		).when(
+			basePortalControllerBuildRunner
+		).previousBuildHasCurrentSHA();
+
+		Mockito.doCallRealMethod(
+		).when(
+			basePortalControllerBuildRunner
+		).allowConcurrentBuildsUniqueSHA();
+
+		Mockito.doCallRealMethod(
+		).when(
+			basePortalControllerBuildRunner
+		).run();
+
+		basePortalControllerBuildRunner.run();
+
+		VerificationMode verificationMode = Mockito.times(1);
+
+		if (allowConcurrentBuildsUniqueSHAString.equals("true")) {
+			verificationMode = Mockito.never();
+		}
+
+		Mockito.verify(
+			basePortalControllerBuildRunner, verificationMode
+		).allowConcurrentBuilds();
+
+		Mockito.verify(
+			basePortalControllerBuildRunner
+		).invokeBuild();
+	}
+
+}
