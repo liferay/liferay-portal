8157d68a4c439e8cc00e520a471f66861d385f58
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-27 11:23:56 -0500

    LCD-52685 add liferayenvironment type info

---
+++ cloud/operator/resources/api/licensing/v1alpha1/groupversion_info.go
@@ -0,0 +1,19 @@
+// +groupName=licensing.liferay.com
+// +kubebuilder:object:generate=true
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	SchemeBuilder = &scheme.Builder{
+		GroupVersion: schema.GroupVersion{
+			Group:   "licensing.liferay.com",
+			Version: "v1alpha1",
+		},
+	}
+
+	AddToScheme = SchemeBuilder.AddToScheme
+)
---
+++ cloud/operator/resources/api/licensing/v1alpha1/liferayenvironment_types.go
@@ -0,0 +1,89 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func init() {
+	SchemeBuilder.Register(&LiferayEnvironment{}, &LiferayEnvironmentList{})
+}
+
+type LicenseStatus struct {
+	// +optional
+	Checksum string `json:"checksum,omitempty"`
+
+	// +optional
+	LastVerified *metav1.Time `json:"lastVerified,omitempty"`
+
+	// +optional
+	MaxClusterNodes int32 `json:"maxClusterNodes,omitempty"`
+
+	// +optional
+	ValidUntil *metav1.Time `json:"validUntil,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:shortName=lenv
+// +kubebuilder:subresource:status
+type LiferayEnvironment struct {
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta   `json:",inline"`
+
+	Spec   LiferayEnvironmentSpec   `json:"spec,omitempty"`
+	Status LiferayEnvironmentStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type LiferayEnvironmentList struct {
+	metav1.ListMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta `json:",inline"`
+
+	Items []LiferayEnvironment `json:"items"`
+}
+
+type LiferayEnvironmentSpec struct {
+	// +kubebuilder:validation:Required
+	ActivationCodeSecretRef SecretKeyRef `json:"activationCodeSecretRef"`
+
+	// +optional
+	DxpVersion string `json:"dxpVersion,omitempty"`
+
+	// +optional
+	EnvironmentName string `json:"environmentName,omitempty"`
+
+	// +kubebuilder:validation:Required
+	WorkloadRef WorkloadRef `json:"workloadRef"`
+}
+
+type LiferayEnvironmentStatus struct {
+	// +optional
+	ActivatedAt *metav1.Time `json:"activatedAt,omitempty"`
+
+	// +listMapKey=type
+	// +listType=map
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// +optional
+	EnvironmentId string `json:"environmentId,omitempty"`
+
+	// +optional
+	License LicenseStatus `json:"license,omitempty"`
+
+	// +kubebuilder:validation:Enum=Degraded;Pending;Ready
+	// +optional
+	Phase string `json:"phase,omitempty"`
+}
+
+type SecretKeyRef struct {
+	// +kubebuilder:validation:Required
+	Key string `json:"key"`
+
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+}
+
+type WorkloadRef struct {
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+}
