637bba6a2a38d3cb73d0638f52e9a897ed19a746
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-07-27 18:23:08 +0200

    LPD-96543 Run the contacts demo off the portal startup path
    The demo ran inline in portalInstanceRegistered, which executes while the portal is still booting, before Tomcat opens its HTTP connector. Creating the demo data source makes the Asah backend call back into Faro before responding, so the demo, the portal startup, and the backend response deadlocked until the engine client read timeout expired and the demo aborted without data. The demo now defers to the first HTTP request, like the welcome site initializer, and runs on its own thread so the callback request is served normally.

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-contacts-demo/src/main/java/com/liferay/osb/faro/contacts/demo/internal/ContactsDemo.java
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-contacts-demo/src/main/java/com/liferay/osb/faro/contacts/demo/internal/ContactsDemo.java
@@ -8,15 +8,19 @@ package com.liferay.osb.faro.contacts.demo.internal;
 import com.liferay.osb.faro.model.FaroProject;
 import com.liferay.osb.faro.service.FaroProjectLocalService;
 import com.liferay.osb.faro.util.FaroPropsValues;
-import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.InitialRequestPortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.auth.CompanyInheritableThreadLocalCallable;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
+import java.util.concurrent.FutureTask;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -24,10 +28,17 @@ import org.osgi.service.component.annotations.Reference;
  * @author Shinn Lok
  */
 @Component(service = PortalInstanceLifecycleListener.class)
-public class ContactsDemo extends BasePortalInstanceLifecycleListener {
+public class ContactsDemo
+	extends InitialRequestPortalInstanceLifecycleListener {
+
+	@Activate
+	@Override
+	protected void activate(BundleContext bundleContext) {
+		super.activate(bundleContext);
+	}
 
 	@Override
-	public void portalInstanceRegistered(Company company) throws Exception {
+	protected void doPortalInstanceRegistered(long companyId) {
 		if (Validator.isBlank(FaroPropsValues.FARO_DEMO_CREATOR_METHOD) ||
 			StringUtil.equals(
 				FaroPropsValues.FARO_DEMO_CREATOR_METHOD, "none")) {
@@ -39,22 +50,36 @@ public class ContactsDemo extends BasePortalInstanceLifecycleListener {
 			return;
 		}
 
-		try {
-			FaroProject faroProject =
-				_faroProjectLocalService.createFaroProject(0);
+		new Thread(
+			new FutureTask<>(
+				new CompanyInheritableThreadLocalCallable<>(
+					() -> {
+						try {
+							FaroProject faroProject =
+								_faroProjectLocalService.createFaroProject(0);
 
-			faroProject.setWeDeployKey(
-				FaroPropsValues.FARO_DEFAULT_WE_DEPLOY_KEY);
-		}
-		catch (Exception exception) {
-			_log.error(exception);
-		}
+							faroProject.setWeDeployKey(
+								FaroPropsValues.FARO_DEFAULT_WE_DEPLOY_KEY);
+						}
+						catch (Exception exception) {
+							_log.error(exception);
+						}
 
-		_naniteDemoCreatorService.createDemo();
+						try {
+							_naniteDemoCreatorService.createDemo();
 
-		if (_log.isInfoEnabled()) {
-			_log.info("Completed demo data creation");
-		}
+							if (_log.isInfoEnabled()) {
+								_log.info("Completed demo data creation");
+							}
+						}
+						catch (Exception exception) {
+							_log.error(exception);
+						}
+
+						return null;
+					})),
+			"Faro Contacts Demo"
+		).start();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(ContactsDemo.class);
