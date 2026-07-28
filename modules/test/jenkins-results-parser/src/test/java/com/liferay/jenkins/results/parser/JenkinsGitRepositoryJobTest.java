a0d08e6a655d0dbb248faf113739371d44f9bca2
Kenji Heigel <kenji.heigel@liferay.com>
2026-07-27 17:04:22 -0700

    LRCI-7954 Add unit tests for diff driven PR tester job selection

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/JenkinsGitRepositoryJobTest.java
@@ -0,0 +1,217 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Kenji Heigel
+ */
+public class JenkinsGitRepositoryJobTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_gitRepositoryDir = temporaryFolder.newFolder();
+		_gitWorkingDirectory = Mockito.mock(GitWorkingDirectory.class);
+
+		JenkinsResultsParserUtil.setBuildProperties(_getBuildProperties());
+
+		mockEnvironment(
+			Collections.singletonMap(
+				"GITHUB_UPSTREAM_BRANCH_NAME", RandomTestUtil.randomString()));
+
+		File upstreamJarFile = temporaryFolder.newFile();
+
+		_writeJarFile(upstreamJarFile, null);
+
+		Mockito.doAnswer(
+			invocation -> {
+				String command = invocation.getArgument(3);
+
+				Files.copy(
+					upstreamJarFile.toPath(),
+					Paths.get(command.substring(command.indexOf(" > ") + 3)),
+					StandardCopyOption.REPLACE_EXISTING);
+
+				return new GitUtil.ExecutionResult(0, "", "");
+			}
+		).when(
+			_gitWorkingDirectory
+		).executeBashCommands(
+			Mockito.anyInt(), Mockito.anyLong(), Mockito.anyLong(),
+			Mockito.anyString()
+		);
+	}
+
+	@Test
+	public void testGetInvokedJobNames() throws Exception {
+		_testGetInvokedJobNames(
+			Arrays.asList(
+				"root-cause-analysis-tool",
+				"test-portal-acceptance-pullrequest(master)/dummy",
+				"test-portal-source-format"),
+			null, "relevant", "commands/build-common.xml",
+			"template/jobs/root-cause-analysis-tool-1/config.xml");
+		_testGetInvokedJobNames(
+			Arrays.asList("test-portal-acceptance-pullrequest(master)"), null,
+			"default", "commands/build-common.xml");
+		_testGetInvokedJobNames(
+			Arrays.asList("test-portal-release"),
+			"com/liferay/jenkins/results/parser/PortalRelease$1.class",
+			"relevant", _JAR_FILE_NAME);
+		_testGetInvokedJobNames(
+			Arrays.asList("test-portal-source-format"), "META-INF/MANIFEST.MF",
+			"relevant", _JAR_FILE_NAME);
+	}
+
+	@Rule
+	public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	private Properties _getBuildProperties() {
+		Properties buildProperties = new Properties();
+
+		buildProperties.setProperty(
+			"jenkins.pull.request.job.portal.test.suite" +
+				"[test-portal-acceptance-pullrequest(*)]",
+			"dummy");
+		buildProperties.setProperty(
+			"jenkins.pull.request.jobs[build-common-rule][relevant]",
+			"test-portal-source-format," +
+				"test-portal-acceptance-pullrequest(master)");
+		buildProperties.setProperty(
+			"jenkins.pull.request.jobs[default]",
+			"test-portal-acceptance-pullrequest(master)");
+		buildProperties.setProperty(
+			"jenkins.pull.request.jobs[portal-release-rule][relevant]",
+			"test-portal-release");
+		buildProperties.setProperty(
+			"jenkins.pull.request.jobs[relevant]", "test-portal-source-format");
+		buildProperties.setProperty(
+			"jenkins.pull.request.jobs" +
+				"[root-cause-analysis-tool-rule][relevant]",
+			"root-cause-analysis-tool");
+		buildProperties.setProperty(
+			"modified.files.includes[build-common-rule][relevant]",
+			"commands/build-common.xml,lib/jenkins/*");
+		buildProperties.setProperty(
+			"modified.files.includes[root-cause-analysis-tool-rule][relevant]",
+			"template/jobs/root-cause-analysis-tool*/**");
+		buildProperties.setProperty(
+			"modified.jar.entries.includes[portal-release-rule][relevant]",
+			"com/liferay/jenkins/results/parser/PortalRelease.class");
+		buildProperties.setProperty(
+			"modified.jar.entries.includes" +
+				"[root-cause-analysis-tool-rule][relevant]",
+			"META-INF/**");
+
+		return buildProperties;
+	}
+
+	private void _testGetInvokedJobNames(
+			List<String> expectedInvokedJobNames, String modifiedJarEntryName,
+			String testSuiteName, String... modifiedFileNames)
+		throws Exception {
+
+		_writeJarFile(
+			new File(_gitRepositoryDir, _JAR_FILE_NAME), modifiedJarEntryName);
+
+		List<File> modifiedFiles = new ArrayList<>();
+
+		for (String modifiedFileName : modifiedFileNames) {
+			modifiedFiles.add(new File(_gitRepositoryDir, modifiedFileName));
+		}
+
+		Mockito.doReturn(
+			modifiedFiles
+		).when(
+			_gitWorkingDirectory
+		).getModifiedFilesList();
+
+		JenkinsGitRepositoryJob jenkinsGitRepositoryJob = Mockito.mock(
+			JenkinsGitRepositoryJob.class);
+
+		ReflectionTestUtil.setFieldValue(
+			jenkinsGitRepositoryJob, "gitRepositoryDir", _gitRepositoryDir);
+		ReflectionTestUtil.setFieldValue(
+			jenkinsGitRepositoryJob, "gitWorkingDirectory",
+			_gitWorkingDirectory);
+
+		Mockito.doCallRealMethod(
+		).when(
+			jenkinsGitRepositoryJob
+		).getInvokedJobNames();
+
+		Mockito.doReturn(
+			testSuiteName
+		).when(
+			jenkinsGitRepositoryJob
+		).getTestSuiteName();
+
+		testEquals(
+			expectedInvokedJobNames,
+			jenkinsGitRepositoryJob.getInvokedJobNames());
+	}
+
+	private void _writeJarFile(File file, String modifiedEntryName)
+		throws Exception {
+
+		File parentFile = file.getParentFile();
+
+		parentFile.mkdirs();
+
+		try (ZipOutputStream zipOutputStream = new ZipOutputStream(
+				new FileOutputStream(file))) {
+
+			for (String entryName : _entryNames) {
+				zipOutputStream.putNextEntry(new ZipEntry(entryName));
+
+				if (entryName.equals(modifiedEntryName)) {
+					zipOutputStream.write("modified".getBytes());
+				}
+				else {
+					zipOutputStream.write(entryName.getBytes());
+				}
+
+				zipOutputStream.closeEntry();
+			}
+		}
+	}
+
+	private static final String _JAR_FILE_NAME =
+		"lib/jenkins/com.liferay.jenkins.results.parser.jar";
+
+	private static final List<String> _entryNames = Arrays.asList(
+		"META-INF/MANIFEST.MF",
+		"com/liferay/jenkins/results/parser/PortalRelease$1.class");
+
+	private File _gitRepositoryDir;
+	private GitWorkingDirectory _gitWorkingDirectory;
+
+}
