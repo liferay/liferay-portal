af528fb3e3844feffdb175f175c5735ad0b78ee7
Jaime Leon <jaime.leon@liferay.com>
2026-07-28 16:48:15 +0200

    LPD-99695 Cover Web Content Display reference import into a staged site
    Migrates the ImportWCDReferenceAfterActivateStaging Poshi test to an integration test on LayoutPortletExportImportTest, asserting a Web Content Display portlet reference to a web content survives export and import into a group with local staging enabled.

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutPortletExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutPortletExportImportTest.java
@@ -7,8 +7,18 @@ package com.liferay.exportimport.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactoryUtil;
+import com.liferay.exportimport.kernel.configuration.constants.ExportImportConfigurationConstants;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalServiceUtil;
+import com.liferay.exportimport.kernel.service.ExportImportServiceUtil;
+import com.liferay.exportimport.kernel.service.StagingLocalServiceUtil;
 import com.liferay.exportimport.test.util.lar.BaseExportImportTestCase;
+import com.liferay.journal.constants.JournalContentPortletKeys;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -17,22 +27,31 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.documentlibrary.constants.DLConstants;
 
 import jakarta.portlet.PortletPreferences;
+
+import java.io.Serializable;
+
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -174,6 +193,68 @@ public class LayoutPortletExportImportTest extends BaseExportImportTestCase {
 			portletPreferences.getValue("entryColumns", StringPool.BLANK));
 	}
 
+	@Test
+	@TestInfo("LPS-94738")
+	public void testExportImportWebContentDisplayReferenceIntoStagedGroup()
+		throws Exception {
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			group.getGroupId(), 0);
+
+		String portletId = LayoutTestUtil.addPortletToLayout(
+			layout, JournalContentPortletKeys.JOURNAL_CONTENT,
+			HashMapBuilder.put(
+				"articleId", new String[] {journalArticle.getArticleId()}
+			).put(
+				"groupId", new String[] {String.valueOf(group.getGroupId())}
+			).build());
+
+		exportLayouts(
+			new long[] {layout.getLayoutId()}, getExportParameterMap());
+
+		User user = TestPropsValues.getUser();
+
+		StagingLocalServiceUtil.enableLocalStaging(
+			user.getUserId(), importedGroup, false, false,
+			new ServiceContext());
+
+		Group stagingGroup = importedGroup.getStagingGroup();
+
+		Map<String, Serializable> importLayoutSettingsMap =
+			ExportImportConfigurationSettingsMapFactoryUtil.
+				buildImportLayoutSettingsMap(
+					user, stagingGroup.getGroupId(), false, null,
+					getImportParameterMap());
+
+		ExportImportConfiguration exportImportConfiguration =
+			ExportImportConfigurationLocalServiceUtil.
+				addExportImportConfiguration(
+					user.getUserId(), stagingGroup.getGroupId(),
+					StringPool.BLANK, StringPool.BLANK,
+					ExportImportConfigurationConstants.TYPE_IMPORT_LAYOUT,
+					importLayoutSettingsMap, WorkflowConstants.STATUS_DRAFT,
+					new ServiceContext());
+
+		ExportImportServiceUtil.importLayouts(
+			exportImportConfiguration, larFile);
+
+		Layout importedLayout = _layoutLocalService.fetchLayoutByUuidAndGroupId(
+			layout.getUuid(), stagingGroup.getGroupId(), false);
+
+		PortletPreferences portletPreferences =
+			LayoutTestUtil.getPortletPreferences(importedLayout, portletId);
+
+		Assert.assertEquals(
+			journalArticle.getArticleId(),
+			portletPreferences.getValue("articleId", StringPool.BLANK));
+
+		Assert.assertNotNull(
+			_journalArticleLocalService.fetchArticle(
+				GetterUtil.getLong(
+					portletPreferences.getValue("groupId", StringPool.BLANK)),
+				journalArticle.getArticleId()));
+	}
+
 	private void _addResources(long companyId, long groupId, String name)
 		throws Exception {
 
@@ -201,6 +282,9 @@ public class LayoutPortletExportImportTest extends BaseExportImportTestCase {
 
 	@Inject
 	private GroupLocalService _groupLocalService;
+
+	@Inject
+	private JournalArticleLocalService _journalArticleLocalService;
 
 	@Inject
 	private LayoutLocalService _layoutLocalService;
