7fdd43be4a6e8f8efa08d102383aa74bd215a558
Brian Chan <brian.chan@liferay.com>
2026-07-28 17:36:42 +0200

    LCD-52685 SF

--- cloud/operator/go_build.sh
+++ cloud/operator/go_build.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-cd resources/ && \
-	go mod download && \
-	go mod verify && \
-	go generate ./... && \
-	CGO_ENABLED=0 go build -a -ldflags="-s -w" -o manager main.go
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+function main {
+	cd resources
+
+	go mod download
+
+	go mod verify
+
+	go generate ./...
+
+	CGO_ENABLED=0 go build -a --ldflags="-s -w" -o manager main.go
+}
+
+main "${@}"
