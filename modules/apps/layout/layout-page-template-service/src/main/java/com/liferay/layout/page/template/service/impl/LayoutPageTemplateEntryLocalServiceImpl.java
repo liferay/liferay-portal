fb10372c7414d192c6ae4ff761b6ff525344f003
Victor Galan <victor.galan@liferay.com>
2026-07-29 12:42:52 +0200

    LPD-99704 Link a display page template to its real DDM structure
    The link was created unconditionally with the class type ID as the structure ID, which only holds for web content. A display page template for an object has a class type ID of zero, so it stored a link to structure zero, and one for a document type stored a link to a file entry type ID that matches no structure at all. Resolving the structure through the provider stores a usable link when there is one and stores nothing when there is not.

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
@@ -6,6 +6,7 @@
 package com.liferay.layout.page.template.service.impl;
 
 import com.liferay.asset.kernel.NoSuchClassTypeException;
+import com.liferay.dynamic.data.mapping.info.item.provider.DDMStructureInfoItemFormVariationsProvider;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLinkLocalService;
 import com.liferay.info.item.InfoItemFormVariation;
 import com.liferay.info.item.InfoItemServiceRegistry;
@@ -282,11 +283,15 @@ public class LayoutPageTemplateEntryLocalServiceImpl
 
 		// Dynamic data mapping structure link
 
-		_ddmStructureLinkLocalService.addStructureLink(
-			_classNameLocalService.getClassNameId(
-				LayoutPageTemplateEntry.class),
-			layoutPageTemplateEntry.getLayoutPageTemplateEntryId(),
-			classTypeId);
+		long ddmStructureId = _getDDMStructureId(classNameId, classTypeId);
+
+		if (ddmStructureId > 0) {
+			_ddmStructureLinkLocalService.addStructureLink(
+				_classNameLocalService.getClassNameId(
+					LayoutPageTemplateEntry.class),
+				layoutPageTemplateEntry.getLayoutPageTemplateEntryId(),
+				ddmStructureId);
+		}
 
 		return layoutPageTemplateEntry;
 	}
@@ -1169,6 +1174,27 @@ public class LayoutPageTemplateEntryLocalServiceImpl
 		}
 
 		return colorSchemeId;
+	}
+
+	private long _getDDMStructureId(long classNameId, long classTypeId) {
+		InfoItemFormVariationsProvider<?> infoItemFormVariationsProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemFormVariationsProvider.class,
+				_portal.fetchClassName(classNameId));
+
+		if (!(infoItemFormVariationsProvider instanceof
+				DDMStructureInfoItemFormVariationsProvider)) {
+
+			return 0;
+		}
+
+		DDMStructureInfoItemFormVariationsProvider<?>
+			ddmStructureInfoItemFormVariationsProvider =
+				(DDMStructureInfoItemFormVariationsProvider<?>)
+					infoItemFormVariationsProvider;
+
+		return ddmStructureInfoItemFormVariationsProvider.getDDMStructureId(
+			String.valueOf(classTypeId));
 	}
 
 	private void _validate(
