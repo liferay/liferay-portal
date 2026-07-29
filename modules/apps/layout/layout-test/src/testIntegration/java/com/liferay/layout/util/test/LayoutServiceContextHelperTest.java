0c58a5325ff86dfaaf12ef51ad757ab23fedba2f
Victor Galan <victor.galan@liferay.com>
2026-07-29 09:54:21 +0200

    LPD-99386 Simplify the mock request contract test

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/test/LayoutServiceContextHelperTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/test/LayoutServiceContextHelperTest.java
@@ -16,23 +16,21 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.servlet.HttpMethods;
 import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -51,34 +49,27 @@ public class LayoutServiceContextHelperTest {
 			new LiferayIntegrationTestRule(),
 			PermissionCheckerMethodTestRule.INSTANCE);
 
-	@Before
-	public void setUp() throws Exception {
-		_group = GroupTestUtil.addGroup();
-
-		_layout = LayoutTestUtil.addTypeContentLayout(_group);
-
-		_serviceContext = ServiceContextTestUtil.getServiceContext(
-			_group.getGroupId());
-
-		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		ServiceContextThreadLocal.popServiceContext();
-	}
-
 	@Test
 	@TestInfo({"LPD-79722", "LPD-99386"})
 	public void testGetServiceContextAutoCloseable() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		Layout layout = LayoutTestUtil.addTypeContentLayout(group);
+
 		try (AutoCloseable autoCloseable =
 				_layoutServiceContextHelper.getServiceContextAutoCloseable(
-					_layout)) {
+					layout)) {
 
 			ServiceContext serviceContext =
 				ServiceContextThreadLocal.getServiceContext();
 
 			HttpServletRequest httpServletRequest = serviceContext.getRequest();
+
+			Assert.assertNotNull(httpServletRequest.getContextPath());
+
+			Cookie[] cookies = httpServletRequest.getCookies();
+
+			Assert.assertEquals(Arrays.toString(cookies), 0, cookies.length);
 
 			Assert.assertNotNull(httpServletRequest.getLocale());
 
@@ -89,29 +80,21 @@ public class LayoutServiceContextHelperTest {
 
 			Assert.assertEquals(
 				HttpMethods.GET, httpServletRequest.getMethod());
-			Assert.assertEquals(
-				StringPool.SLASH, httpServletRequest.getRequestURI());
-			Assert.assertEquals("http", httpServletRequest.getScheme());
-			Assert.assertNotNull(httpServletRequest.getContextPath());
-			Assert.assertNotNull(httpServletRequest.getServletContext());
-			Assert.assertNotNull(httpServletRequest.getSession());
-			Assert.assertEquals(0, httpServletRequest.getCookies().length);
 
 			Map<String, String[]> parameterMap =
 				httpServletRequest.getParameterMap();
 
 			Assert.assertTrue(parameterMap.isEmpty());
+
+			Assert.assertEquals(
+				StringPool.SLASH, httpServletRequest.getRequestURI());
+			Assert.assertEquals("http", httpServletRequest.getScheme());
+			Assert.assertNotNull(httpServletRequest.getServletContext());
+			Assert.assertNotNull(httpServletRequest.getSession());
 		}
 	}
 
-	@DeleteAfterTestRun
-	private Group _group;
-
-	private Layout _layout;
-
 	@Inject
 	private LayoutServiceContextHelper _layoutServiceContextHelper;
-
-	private ServiceContext _serviceContext;
 
 }
