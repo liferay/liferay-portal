521ec5006136c0b72f4484ac0df0d6136fb6ee66
Jaime Leon <jaime.leon@liferay.com>
2026-07-29 11:24:48 +0200

    LPD-99382 Remove the unused table field from ImportReportPage

--- modules/test/playwright/tests/export-import-web/revamp/pages/ImportReportPage.ts
+++ modules/test/playwright/tests/export-import-web/revamp/pages/ImportReportPage.ts
@@ -3,24 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page} from '@playwright/test';
+import {Page} from '@playwright/test';
 
 export class ImportReportPage {
 	readonly page: Page;
-	readonly table: {
-		bodyRows: Locator;
-		container: Locator;
-	};
 
 	constructor(page: Page) {
 		this.page = page;
-
-		const tableContainer = page.locator('.fds table');
-
-		this.table = {
-			bodyRows: tableContainer.locator('tbody tr'),
-			container: tableContainer,
-		};
 	}
 
 	async goToEntryDetails(externalReferenceCode: string) {
