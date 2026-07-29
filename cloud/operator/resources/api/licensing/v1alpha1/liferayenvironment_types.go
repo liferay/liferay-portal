d47b0edeb8fc8464644391599ef79d914461a0ca
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-29 10:17:15 -0500

    LCD-52771 add extra print columns

--- cloud/operator/resources/api/licensing/v1alpha1/liferayenvironment_types.go
+++ cloud/operator/resources/api/licensing/v1alpha1/liferayenvironment_types.go
@@ -23,6 +23,11 @@ type LicenseStatus struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:JSONPath=`.status.phase`,name="Phase",type=string
+// +kubebuilder:printcolumn:JSONPath=`.status.conditions[?(@.type=="Activated")].status`,name="Activated",type=string
+// +kubebuilder:printcolumn:JSONPath=`.metadata.creationTimestamp`,name="Age",type=date
+// +kubebuilder:printcolumn:JSONPath=`.status.environmentId`,name="Environment-ID",priority=1,type=string
+// +kubebuilder:printcolumn:JSONPath=`.status.activatedAt`,name="Activated-At",priority=1,type=date
 // +kubebuilder:resource:shortName=lenv
 // +kubebuilder:subresource:status
 type LiferayEnvironment struct {
