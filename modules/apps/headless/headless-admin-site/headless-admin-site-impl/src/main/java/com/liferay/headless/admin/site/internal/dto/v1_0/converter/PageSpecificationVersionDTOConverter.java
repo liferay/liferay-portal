3cc724a3c483e912f55c6ac43f2d99e994bd3837
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-07-29 13:09:21 +0100

    LPD-99956 Return the creator of a page specification version

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationVersionDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationVersionDTOConverter.java
@@ -6,13 +6,19 @@
 package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
 import com.liferay.headless.admin.site.dto.v1_0.PageSpecificationVersion;
+import com.liferay.headless.admin.user.dto.v1_0.Creator;
 import com.liferay.layout.content.model.LayoutContentVersion;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -33,6 +39,31 @@ public class PageSpecificationVersionDTOConverter
 
 		return new PageSpecificationVersion() {
 			{
+				setCreator(
+					() -> {
+						User user = _userLocalService.fetchUser(
+							layoutContentVersion.getUserId());
+
+						if (user == null) {
+							return null;
+						}
+
+						return new Creator() {
+							{
+								setExternalReferenceCode(
+									user::getExternalReferenceCode);
+								setImage(
+									() -> user.getPortraitURL(
+										new ThemeDisplay() {
+											{
+												setPathImage(
+													_portal.getPathImage());
+											}
+										}));
+								setName(user::getFullName);
+							}
+						};
+					});
 				setDateCreated(layoutContentVersion::getCreateDate);
 				setDateModified(layoutContentVersion::getModifiedDate);
 				setExternalReferenceCode(
@@ -55,5 +86,11 @@ public class PageSpecificationVersionDTOConverter
 			}
 		};
 	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
