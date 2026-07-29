2340849a907afa432155862c00ee1d4db8fa3f95
Larissa Ribeiro <larissa.ribeiro@liferay.com>
2026-07-29 16:11:17 -0300

    LPD-100022 Hide the CMS Project filter when CMP is disabled
    The Project selection filter was served to the Contents, Files, and All asset lists regardless of the CMP feature flag, so the lists offered a filter over Project object entries that do not exist when CMP is off.

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/CMPProjectSelectionFDSFilter.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/CMPProjectSelectionFDSFilter.java
@@ -11,6 +11,7 @@ import com.liferay.frontend.data.set.filter.FDSFilter;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
 
@@ -80,6 +81,12 @@ public class CMPProjectSelectionFDSFilter extends BaseSelectionFDSFilter {
 	@Override
 	public boolean isAutocompleteEnabled() {
 		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return FeatureFlagManagerUtil.isEnabled(
+			CompanyThreadLocal.getCompanyId(), "LPD-58677");
 	}
 
 	@Reference
