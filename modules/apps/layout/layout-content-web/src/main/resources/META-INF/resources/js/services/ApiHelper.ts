b11a2adb5e6f88bf35b254e992daff0ba53adb9c
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-07-29 13:09:52 +0100

    LPD-99956 Add the API helper

---
+++ modules/apps/layout/layout-content-web/src/main/resources/META-INF/resources/js/services/ApiHelper.ts
@@ -0,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {fetch} from 'frontend-js-web';
+
+const UNEXPECTED_ERROR_MESSAGE = Liferay.Language.get(
+	'an-unexpected-error-occurred'
+);
+
+export type RequestResult<T> =
+	| {
+			data: null;
+			error: string;
+	  }
+	| {
+			data: T;
+			error: null;
+	  };
+
+async function handleRequest<T>(
+	fetcher: () => Promise<Response>
+): Promise<RequestResult<T>> {
+	try {
+		const response = await fetcher();
+
+		if (!response.ok) {
+			const {message, title} = await response.json();
+
+			return {
+				data: null,
+				error: title ?? message ?? UNEXPECTED_ERROR_MESSAGE,
+			};
+		}
+
+		return {
+			data: await response.json(),
+			error: null,
+		};
+	}
+	catch (error) {
+		return {
+			data: null,
+			error: (error as Error).message || UNEXPECTED_ERROR_MESSAGE,
+		};
+	}
+}
+
+async function get<T>(url: string, signal?: AbortSignal) {
+	return handleRequest<T>(() =>
+		fetch(url, {
+			headers: new Headers({
+				'Accept': 'application/json',
+				'Accept-Language': Liferay.ThemeDisplay.getBCP47LanguageId(),
+				'Content-Type': 'application/json',
+			}),
+			method: 'GET',
+			signal,
+		})
+	);
+}
+
+export default {get};
