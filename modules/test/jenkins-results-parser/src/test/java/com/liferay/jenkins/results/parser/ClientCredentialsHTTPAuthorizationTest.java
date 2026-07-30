052c0093db5d038012e9f83595021d4c21ba2691
Peter Yoo <peter.yoo@liferay.com>
2026-07-30 11:25:13 -0700

    LRCI-7951 Reset the Jenkins master caches in tearDown

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/ClientCredentialsHTTPAuthorizationTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/ClientCredentialsHTTPAuthorizationTest.java
@@ -14,6 +14,7 @@ import java.util.Properties;
 
 import org.json.JSONObject;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,14 +29,20 @@ public class ClientCredentialsHTTPAuthorizationTest
 
 	@Before
 	public void setUpBuildProperties() {
-		JenkinsMasterTestUtil.resetCaches();
-
 		Properties buildProperties = new Properties();
 
 		buildProperties.setProperty(
 			"jenkins.local.url[test-1-1]", "http://test-1-1");
 
 		JenkinsResultsParserUtil.setBuildProperties(buildProperties);
+	}
+
+	@After
+	@Override
+	public void tearDown() {
+		super.tearDown();
+
+		JenkinsMasterTestUtil.resetCaches();
 	}
 
 	@Test
