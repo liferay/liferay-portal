03e977aa200bf98718e08b5e41bfb0e2a3aa57df
Marcio Fonseca <marcio.fonseca@liferay.com>
2026-07-30 16:37:36 -0300

    LPD-100228 Add a configured status label to the sync entity lists

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/settings/components/marketo/MarketoCampaignEntities.tsx
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/settings/components/marketo/MarketoCampaignEntities.tsx
@@ -1,6 +1,7 @@
 import ClayIcon from '@clayui/icon';
 import ClayList from '@clayui/list';
 import ClaySticker from '@clayui/sticker';
+import Label from '@clayui/label';
 import React from 'react';
 import {ClayCheckbox} from '@clayui/form';
 import {sub} from 'shared/util/lang';
@@ -19,50 +20,74 @@ const MarketoCampaignEntities: React.FC<IMarketoCampaignEntitiesProps> = ({
 	enabledIndividuals,
 	individualsSyncedCount,
 	onIndividualsChange,
-}) => (
-	<div className="pt-1">
-		<ClayList className="mb-0">
-			<ClayList.Item flex>
-				<ClayList.ItemField>
-					<ClayCheckbox
-						checked={enabledIndividuals}
-						disabled={disabled}
-						onChange={onIndividualsChange}
-					/>
-				</ClayList.ItemField>
+}) => {
+	const configured =
+		typeof individualsSyncedCount === 'number' &&
+		individualsSyncedCount > 0;
 
-				<ClayList.ItemField>
-					<ClaySticker displayType="unstyled">
-						<ClayIcon className="text-secondary" symbol="users" />
-					</ClaySticker>
-				</ClayList.ItemField>
+	return (
+		<div className="pt-1">
+			<ClayList className="mb-0">
+				<ClayList.Item flex>
+					<ClayList.ItemField>
+						<ClayCheckbox
+							checked={enabledIndividuals}
+							disabled={disabled}
+							onChange={onIndividualsChange}
+						/>
+					</ClayList.ItemField>
 
-				<ClayList.ItemField
-					className="d-flex justify-content-center"
-					expand
-				>
-					<ClayList.ItemTitle>
-						{Liferay.Language.get('individuals')}
-					</ClayList.ItemTitle>
+					<ClayList.ItemField>
+						<ClaySticker displayType="unstyled">
+							<ClayIcon
+								className="text-secondary"
+								symbol="users"
+							/>
+						</ClaySticker>
+					</ClayList.ItemField>
 
-					<ClayList.ItemText>
-						{Liferay.Language.get(
-							'represents-fields-from-the-leads-and-companies-table-within-marketo'
-						)}
-					</ClayList.ItemText>
+					<ClayList.ItemField
+						className="d-flex justify-content-center"
+						expand
+					>
+						<ClayList.ItemTitle>
+							{Liferay.Language.get('individuals')}
+						</ClayList.ItemTitle>
 
-					{individualsSyncedCount !== undefined &&
-						individualsSyncedCount >= 0 && (
-							<ClayList.ItemText>
-								{sub(Liferay.Language.get('x-items-synced'), [
-									toLocale(individualsSyncedCount),
-								])}
-							</ClayList.ItemText>
-						)}
-				</ClayList.ItemField>
-			</ClayList.Item>
-		</ClayList>
-	</div>
-);
+						<ClayList.ItemText>
+							{Liferay.Language.get(
+								'represents-fields-from-the-leads-and-companies-table-within-marketo'
+							)}
+						</ClayList.ItemText>
+
+						{individualsSyncedCount !== undefined &&
+							individualsSyncedCount >= 0 && (
+								<ClayList.ItemText>
+									{sub(
+										Liferay.Language.get('x-items-synced'),
+										[toLocale(individualsSyncedCount)]
+									)}
+								</ClayList.ItemText>
+							)}
+					</ClayList.ItemField>
+
+					<ClayList.ItemField className="justify-content-center">
+						<Label
+							displayType={configured ? 'success' : 'secondary'}
+						>
+							{configured
+								? Liferay.Language.get(
+										'configured'
+									).toUpperCase()
+								: Liferay.Language.get(
+										'unconfigured'
+									).toUpperCase()}
+						</Label>
+					</ClayList.ItemField>
+				</ClayList.Item>
+			</ClayList>
+		</div>
+	);
+};
 
 export default MarketoCampaignEntities;
