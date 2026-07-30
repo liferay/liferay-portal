7188c6af9678f6dae3fdddddff1f1d3e2e185979
Brian I. Kim <il.kim@liferay.com>
2026-07-30 01:34:20 -0700

    LPD-97582 Integration Test

--- modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/internal/request/test/DSRequestManagerTest.java
+++ modules/apps/digital-signature/digital-signature-test/src/testIntegration/java/com/liferay/digital/signature/internal/request/test/DSRequestManagerTest.java
@@ -28,10 +28,13 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
@@ -40,6 +43,7 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.io.Serializable;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -144,6 +148,57 @@ public class DSRequestManagerTest {
 		Assert.assertTrue(
 			emailAddresses.toString(),
 			emailAddresses.contains("mei.lin@liferay.com"));
+	}
+
+	@Test
+	public void testGetRequestStatuses() throws Exception {
+		long companyId = TestPropsValues.getCompanyId();
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_DS_REQUEST", companyId);
+
+		Assert.assertNotNull(
+			"The L_DS_REQUEST object definition must exist when the feature " +
+				"flag is enabled",
+			objectDefinition);
+
+		long fileEntryId = RandomTestUtil.randomInt();
+
+		_objectEntryLocalService.addObjectEntry(
+			0, TestPropsValues.getUserId(),
+			objectDefinition.getObjectDefinitionId(), 0,
+			LocaleUtil.toLanguageId(LocaleUtil.getSiteDefault()),
+			HashMapBuilder.<String, Serializable>put(
+				"fileEntryId", fileEntryId
+			).put(
+				"providerKey", "docusign"
+			).put(
+				"providerRequestId", "test-" + fileEntryId
+			).put(
+				"requestStatus", "completed"
+			).build(),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		Map<Long, String> requestStatuses =
+			_dsRequestManager.getRequestStatusesByFileEntryId(
+				companyId, Collections.singletonList(fileEntryId));
+
+		Assert.assertEquals("completed", requestStatuses.get(fileEntryId));
+	}
+
+	@Test
+	public void testGetRequestStatusesReturnsEmptyForMissingRequest()
+		throws Exception {
+
+		Map<Long, String> requestStatuses =
+			_dsRequestManager.getRequestStatusesByFileEntryId(
+				TestPropsValues.getCompanyId(),
+				Collections.singletonList(RandomTestUtil.randomLong()));
+
+		Assert.assertTrue(requestStatuses.isEmpty());
 	}
 
 	private DSEnvelope _createDSEnvelope(long fileEntryId) {
