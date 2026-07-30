51fcc38033f27d4fc36380b6ebd7807b3035b558
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-30 10:35:40 -0500

    LCD-52771 sort args

--- cloud/operator/resources/internal/provisioning/http_client.go
+++ cloud/operator/resources/internal/provisioning/http_client.go
@@ -38,10 +38,10 @@ func (httpClient *HTTPClient) Activate(
 	}
 
 	response, error := httpClient.post(
-		context, fmt.Sprintf(
+		context, token, fmt.Sprintf(
 			"%s/o/provisioning-rest/v1.0/cloud/environment/%s/activation",
 			httpClient.BaseURL, activationRequest.EnvironmentID,
-		), token,
+		),
 	)
 
 	if error != nil {
@@ -84,8 +84,8 @@ func encodeSegment(bytes []byte) string {
 
 func (httpClient *HTTPClient) post(
 	context context.Context,
-	url string,
 	token string,
+	url string,
 ) (*http.Response, error) {
 	request, error := http.NewRequestWithContext(
 		context, http.MethodPost, url, bytes.NewReader([]byte(token)),
