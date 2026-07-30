liferay/liferay-portal

commit 3f5ada53ad7776d18667e20d2a11c5742e726bca

LPS-165697 Adapt service and local service to accept NotificationContext

modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationTemplateLocalServiceImpl.java
@@ -17,8 +17,12 @@ package com.liferay.notification.service.impl;
 import com.liferay.notification.constants.NotificationConstants;
 import com.liferay.notification.context.NotificationContext;
 import com.liferay.notification.model.NotificationQueueEntry;
+import com.liferay.notification.model.NotificationRecipient;
+import com.liferay.notification.model.NotificationRecipientSetting;
 import com.liferay.notification.model.NotificationTemplate;
 import com.liferay.notification.model.NotificationTemplateAttachment;
+import com.liferay.notification.service.NotificationRecipientLocalService;
+import com.liferay.notification.service.NotificationRecipientSettingLocalService;
 import com.liferay.notification.service.NotificationTemplateAttachmentLocalService;
 import com.liferay.notification.service.base.NotificationTemplateLocalServiceBaseImpl;
 import com.liferay.notification.service.persistence.NotificationQueueEntryPersistence;
@@ -29,22 +33,15 @@ import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.Validator;
-
-import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
@@ -64,46 +61,31 @@ public class NotificationTemplateLocalServiceImpl
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public NotificationTemplate addNotificationTemplate(
-			long userId, long objectDefinitionId, String bcc,
-			Map<Locale, String> bodyMap, String cc, String description,
-			String from, Map<Locale, String> fromNameMap, String name,
-			String recipientType, Map<Locale, String> subjectMap,
-			Map<Locale, String> toMap, String type,
-			List<Long> attachmentObjectFieldIds)
+			NotificationContext notificationContext)
 		throws PortalException {
 
-		if (Validator.isNull(type)) {
-			type = NotificationConstants.TYPE_EMAIL;
-		}
+		_validate(notificationContext);
 
-		_validate(
-			objectDefinitionId, from, name, type, attachmentObjectFieldIds);
+		NotificationType notificationType =
+			_notificationTypeServiceTracker.getNotificationType(
+				notificationContext.getType());
+
+		notificationType.validateNotificationTemplate(notificationContext);
 
 		NotificationTemplate notificationTemplate =
-			notificationTemplatePersistence.create(
-				counterLocalService.increment());
+			notificationTemplatePersistence.update(
+				notificationContext.getNotificationTemplate());
 
-		User user = _userLocalService.getUser(userId);
+		_notificationRecipientLocalService.updateNotificationRecipient(
+			notificationContext.getNotificationRecipient());
 
-		notificationTemplate.setCompanyId(user.getCompanyId());
-		notificationTemplate.setUserId(user.getUserId());
-		notificationTemplate.setUserName(user.getFullName());
+		for (NotificationRecipientSetting notificationRecipientSetting :
+				notificationContext.getNotificationRecipientSettings()) {
 
-		notificationTemplate.setObjectDefinitionId(objectDefinitionId);
-		notificationTemplate.setBcc(bcc);
-		notificationTemplate.setBodyMap(bodyMap);
-		notificationTemplate.setCc(cc);
-		notificationTemplate.setDescription(description);
-		notificationTemplate.setFrom(from);
-		notificationTemplate.setFromNameMap(fromNameMap);
-		notificationTemplate.setName(name);
-		notificationTemplate.setRecipientType(recipientType);
-		notificationTemplate.setSubjectMap(subjectMap);
-		notificationTemplate.setToMap(toMap);
-		notificationTemplate.setType(type);
-
-		notificationTemplate = notificationTemplatePersistence.update(
-			notificationTemplate);
+			_notificationRecipientSettingLocalService.
+				updateNotificationRecipientSetting(
+					notificationRecipientSetting);
+		}
 
 		_resourceLocalService.addResources(
 			notificationTemplate.getCompanyId(), 0,
@@ -112,7 +94,9 @@ public class NotificationTemplateLocalServiceImpl
 			notificationTemplate.getNotificationTemplateId(), false, true,
 			true);
 
-		for (long attachmentObjectFieldId : attachmentObjectFieldIds) {
+		for (long attachmentObjectFieldId :
+				notificationContext.getAttachmentObjectFieldIds()) {
+
 			_notificationTemplateAttachmentLocalService.
 				addNotificationTemplateAttachment(
 					notificationTemplate.getCompanyId(),
@@ -149,6 +133,20 @@ public class NotificationTemplateLocalServiceImpl
 		_resourceLocalService.deleteResource(
 			notificationTemplate, ResourceConstants.SCOPE_INDIVIDUAL);
 
+		NotificationRecipient notificationRecipient =
+			notificationTemplate.getNotificationRecipient();
+
+		_notificationRecipientLocalService.deleteNotificationRecipient(
+			notificationRecipient);
+
+		for (NotificationRecipientSetting notificationRecipientSetting :
+				notificationRecipient.getNotificationRecipientSettings()) {
+
+			_notificationRecipientSettingLocalService.
+				deleteNotificationRecipientSetting(
+					notificationRecipientSetting);
+		}
+
 		List<NotificationQueueEntry> notificationQueueEntries =
 			_notificationQueueEntryPersistence.findByNotificationTemplateId(
 				notificationTemplate.getNotificationTemplateId());
@@ -180,35 +178,35 @@ public class NotificationTemplateLocalServiceImpl
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public NotificationTemplate updateNotificationTemplate(
-			long notificationTemplateId, long objectDefinitionId, String bcc,
-			Map<Locale, String> bodyMap, String cc, String description,
-			String from, Map<Locale, String> fromNameMap, String name,
-			String recipientType, Map<Locale, String> subjectMap,
-			Map<Locale, String> toMap, String type,
-			List<Long> attachmentObjectFieldIds)
+			NotificationContext notificationContext)
 		throws PortalException {
 
-		_validate(
-			objectDefinitionId, from, name, type, attachmentObjectFieldIds);
+		_validate(notificationContext);
 
 		NotificationTemplate notificationTemplate =
-			notificationTemplatePersistence.findByPrimaryKey(
-				notificationTemplateId);
+			notificationTemplatePersistence.update(
+				notificationContext.getNotificationTemplate());
 
-		notificationTemplate.setObjectDefinitionId(objectDefinitionId);
-		notificationTemplate.setBcc(bcc);
-		notificationTemplate.setBodyMap(bodyMap);
-		notificationTemplate.setCc(cc);
-		notificationTemplate.setDescription(description);
-		notificationTemplate.setFrom(from);
-		notificationTemplate.setFromNameMap(fromNameMap);
-		notificationTemplate.setName(name);
-		notificationTemplate.setRecipientType(recipientType);
-		notificationTemplate.setSubjectMap(subjectMap);
-		notificationTemplate.setToMap(toMap);
+		NotificationRecipient notificationRecipient =
+			_notificationRecipientLocalService.updateNotificationRecipient(
+				notificationContext.getNotificationRecipient());
 
-		notificationTemplate = notificationTemplatePersistence.update(
-			notificationTemplate);
+		for (NotificationRecipientSetting notificationRecipientSetting :
+				notificationRecipient.getNotificationRecipientSettings()) {
+
+			_notificationRecipientSettingLocalService.
+				deleteNotificationRecipientSetting(
+					notificationRecipientSetting.
+						getNotificationRecipientSettingId());
+		}
+
+		for (NotificationRecipientSetting notificationRecipientSetting :
+				notificationContext.getNotificationRecipientSettings()) {
+
+			_notificationRecipientSettingLocalService.
+				updateNotificationRecipientSetting(
+					notificationRecipientSetting);
+		}
 
 		List<Long> oldAttachmentObjectFieldIds = new ArrayList<>();
 
@@ -218,7 +216,7 @@ public class NotificationTemplateLocalServiceImpl
 						notificationTemplate.getNotificationTemplateId())) {
 
 			if (ListUtil.exists(
-					attachmentObjectFieldIds,
+					notificationContext.getAttachmentObjectFieldIds(),
 					attachmentObjectFieldId -> Objects.equals(
 						attachmentObjectFieldId,
 						notificationTemplateAttachment.getObjectFieldId()))) {
@@ -235,7 +233,8 @@ public class NotificationTemplateLocalServiceImpl
 
 		for (long attachmentObjectFieldId :
 				ListUtil.remove(
-					attachmentObjectFieldIds, oldAttachmentObjectFieldIds)) {
+					notificationContext.getAttachmentObjectFieldIds(),
+					oldAttachmentObjectFieldIds)) {
 
 			_notificationTemplateAttachmentLocalService.
 				addNotificationTemplateAttachment(
@@ -247,28 +246,12 @@ public class NotificationTemplateLocalServiceImpl
 		return notificationTemplate;
 	}
 
-	private void _validate(
-			long objectDefinitionId, String from, String name, String type,
-			List<Long> attachmentObjectFieldIds)
+	private void _validate(NotificationContext notificationContext)
 		throws PortalException {
 
 		NotificationType notificationType =
-			_notificationTypeServiceTracker.getNotificationType(type);
-
-		if (notificationType == null) {
-			return;
-		}
-
-		NotificationContext notificationContext = new NotificationContext();
-
-		notificationContext.setAttachmentObjectFieldIds(
-			attachmentObjectFieldIds);
-		notificationContext.setAttributes(
-			HashMapBuilder.<String, Serializable>put(
-				"from", from
-			).build());
-		notificationContext.setObjectDefinitionId(objectDefinitionId);
-		notificationContext.setNotificationTemplateName(name);
+			_notificationTypeServiceTracker.getNotificationType(
+				notificationContext.getType());
 
 		notificationType.validateNotificationTemplate(notificationContext);
 	}
@@ -276,6 +259,14 @@ public class NotificationTemplateLocalServiceImpl
 	@Reference
 	private NotificationQueueEntryPersistence
 		_notificationQueueEntryPersistence;
+
+	@Reference
+	private NotificationRecipientLocalService
+		_notificationRecipientLocalService;
+
+	@Reference
+	private NotificationRecipientSettingLocalService
+		_notificationRecipientSettingLocalService;
 
 	@Reference
 	private NotificationTemplateAttachmentLocalService

modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationTemplateServiceImpl.java
@@ -18,15 +18,12 @@ import com.liferay.notification.constants.NotificationActionKeys;
 import com.liferay.notification.constants.NotificationConstants;
 import com.liferay.notification.model.NotificationTemplate;
 import com.liferay.notification.service.base.NotificationTemplateServiceBaseImpl;
+import com.liferay.notification.type.NotificationContext;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
-
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -47,12 +44,7 @@ public class NotificationTemplateServiceImpl
 
 	@Override
 	public NotificationTemplate addNotificationTemplate(
-			long userId, long objectDefinitionId, String bcc,
-			Map<Locale, String> bodyMap, String cc, String description,
-			String from, Map<Locale, String> fromNameMap, String name,
-			String recipientType, Map<Locale, String> subjectMap,
-			Map<Locale, String> toMap, String type,
-			List<Long> attachmentObjectFieldIds)
+			NotificationContext notificationContext)
 		throws PortalException {
 
 		_portletResourcePermission.check(
@@ -60,9 +52,7 @@ public class NotificationTemplateServiceImpl
 			NotificationActionKeys.ADD_NOTIFICATION_TEMPLATE);
 
 		return notificationTemplateLocalService.addNotificationTemplate(
-			userId, objectDefinitionId, bcc, bodyMap, cc, description, from,
-			fromNameMap, name, recipientType, subjectMap, toMap, type,
-			attachmentObjectFieldIds);
+			notificationContext);
 	}
 
 	@Override
@@ -105,21 +95,19 @@ public class NotificationTemplateServiceImpl
 
 	@Override
 	public NotificationTemplate updateNotificationTemplate(
-			long notificationTemplateId, long objectDefinitionId, String bcc,
-			Map<Locale, String> bodyMap, String cc, String description,
-			String from, Map<Locale, String> fromNameMap, String name,
-			String recipientType, Map<Locale, String> subjectMap,
-			Map<Locale, String> toMap, String type,
-			List<Long> attachmentObjectFieldIds)
+			NotificationContext notificationContext)
 		throws PortalException {
 
+		NotificationTemplate notificationTemplate =
+			notificationContext.getNotificationTemplate();
+
 		_notificationTemplateModelResourcePermission.check(
-			getPermissionChecker(), notificationTemplateId, ActionKeys.UPDATE);
+			getPermissionChecker(),
+			notificationTemplate.getNotificationTemplateId(),
+			ActionKeys.UPDATE);
 
 		return notificationTemplateLocalService.updateNotificationTemplate(
-			notificationTemplateId, objectDefinitionId, bcc, bodyMap, cc,
-			description, from, fromNameMap, name, recipientType, subjectMap,
-			toMap, type, attachmentObjectFieldIds);
+			notificationContext);
 	}
 
 	@Reference(

commit 1d0a59ec1911be20be71f0817f7e524e3656d398

LPD-100095 Skip deleting a missing notification recipient
deleteNotificationTemplate and deleteNotificationQueueEntry dereferenced the recipient without a null check, so deleting a template or queue entry whose recipient row was already gone threw. That aborted any operation cascading into it, most visibly company deletion through the notification CompanyModelListener, which then left the company registered in PortalInstancePool after DataGuard fell back to a raw delete. Fetch the recipient nullably and guard its deletion and setting cleanup on a non-null recipient so deleting such a template or queue entry is idempotent.

Root cause: born broken in 3f5ada53ad7776d18667e20d2a11c5742e726bca (LPS-165697), which added the unguarded recipient deletion while the recipient getter was already a nullable fetch.

modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationQueueEntryLocalServiceImpl.java
@@ -188,17 +188,19 @@ public class NotificationQueueEntryLocalServiceImpl
 				notificationQueueEntry.getNotificationQueueEntryId());
 
 		NotificationRecipient notificationRecipient =
-			notificationQueueEntry.getNotificationRecipient();
+			notificationQueueEntry.fetchNotificationRecipient();
 
-		_notificationRecipientLocalService.deleteNotificationRecipient(
-			notificationRecipient);
+		if (notificationRecipient != null) {
+			_notificationRecipientLocalService.deleteNotificationRecipient(
+				notificationRecipient);
 
-		for (NotificationRecipientSetting notificationRecipientSetting :
-				notificationRecipient.getNotificationRecipientSettings()) {
+			for (NotificationRecipientSetting notificationRecipientSetting :
+					notificationRecipient.getNotificationRecipientSettings()) {
 
-			_notificationRecipientSettingLocalService.
-				deleteNotificationRecipientSetting(
-					notificationRecipientSetting);
+				_notificationRecipientSettingLocalService.
+					deleteNotificationRecipientSetting(
+						notificationRecipientSetting);
+			}
 		}
 
 		Repository repository = _getRepository(notificationQueueEntry);

modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationTemplateLocalServiceImpl.java
@@ -311,17 +311,19 @@ public class NotificationTemplateLocalServiceImpl
 			notificationTemplate, ResourceConstants.SCOPE_INDIVIDUAL);
 
 		NotificationRecipient notificationRecipient =
-			notificationTemplate.getNotificationRecipient();
+			notificationTemplate.fetchNotificationRecipient();
 
-		_notificationRecipientLocalService.deleteNotificationRecipient(
-			notificationRecipient);
+		if (notificationRecipient != null) {
+			_notificationRecipientLocalService.deleteNotificationRecipient(
+				notificationRecipient);
 
-		for (NotificationRecipientSetting notificationRecipientSetting :
-				notificationRecipient.getNotificationRecipientSettings()) {
+			for (NotificationRecipientSetting notificationRecipientSetting :
+					notificationRecipient.getNotificationRecipientSettings()) {
 
-			_notificationRecipientSettingLocalService.
-				deleteNotificationRecipientSetting(
-					notificationRecipientSetting);
+				_notificationRecipientSettingLocalService.
+					deleteNotificationRecipientSetting(
+						notificationRecipientSetting);
+			}
 		}
 
 		List<NotificationQueueEntry> notificationQueueEntries =

modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/service/test/NotificationTemplateLocalServiceTest.java
@@ -16,6 +16,7 @@ import com.liferay.notification.exception.NotificationTemplateExternalReferenceC
 import com.liferay.notification.model.NotificationRecipient;
 import com.liferay.notification.model.NotificationRecipientSetting;
 import com.liferay.notification.model.NotificationTemplate;
+import com.liferay.notification.service.NotificationRecipientLocalService;
 import com.liferay.notification.service.NotificationRecipientSettingLocalService;
 import com.liferay.notification.service.NotificationTemplateLocalService;
 import com.liferay.notification.test.util.NotificationTemplateUtil;
@@ -184,6 +185,32 @@ public class NotificationTemplateLocalServiceTest {
 			notificationTemplate);
 	}
 
+	@FeatureFlag("LPD-17564")
+	@Test
+	public void testDeleteNotificationTemplateWithMissingNotificationRecipient()
+		throws Exception {
+
+		User user = TestPropsValues.getUser();
+
+		NotificationTemplate notificationTemplate =
+			_notificationTemplateLocalService.addNotificationTemplate(
+				_externalReferenceCode, user.getUserId(),
+				NotificationConstants.TYPE_EMAIL);
+
+		NotificationRecipient notificationRecipient =
+			notificationTemplate.fetchNotificationRecipient();
+
+		Assert.assertNotNull(notificationRecipient);
+
+		_notificationRecipientLocalService.deleteNotificationRecipient(
+			notificationRecipient);
+
+		Assert.assertNull(notificationTemplate.fetchNotificationRecipient());
+
+		_notificationTemplateLocalService.deleteNotificationTemplate(
+			notificationTemplate);
+	}
+
 	private void _assertNotificationRecipientSetting(
 			String name, long notificationRecipientId)
 		throws Exception {
@@ -200,6 +227,10 @@ public class NotificationTemplateLocalServiceTest {
 	}
 
 	private String _externalReferenceCode;
+
+	@Inject
+	private NotificationRecipientLocalService
+		_notificationRecipientLocalService;
 
 	@Inject
 	private NotificationRecipientSettingLocalService