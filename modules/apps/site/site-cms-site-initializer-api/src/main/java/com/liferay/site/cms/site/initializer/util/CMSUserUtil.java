5cb8505ea156cf923d0ae5eb572e60a9f549df3d
Pedro Leite <pedro.leite@liferay.com>
2026-07-30 14:18:54 -0300

    LPD-99447 [BE] Delegate CMSUserUtil to the provider

--- modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/CMSUserUtil.java
+++ modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/CMSUserUtil.java
@@ -5,29 +5,15 @@
 
 package com.liferay.site.cms.site.initializer.util;
 
-import com.liferay.depot.constants.DepotConstants;
-import com.liferay.depot.service.DepotEntryLocalServiceUtil;
-import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.model.UserGroup;
-import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
-import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserGroupLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
+import com.liferay.site.cms.site.initializer.users.provider.CMSUsersProvider;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Pedro Leite
@@ -39,55 +25,21 @@ public class CMSUserUtil {
 	}
 
 	public static Set<User> getUsers(String keywords, int start, int end) {
-		List<Long> depotEntryGroupIds = TransformUtil.transform(
-			DepotEntryLocalServiceUtil.getDepotEntryGroupIds(
-				CompanyThreadLocal.getCompanyId(), DepotConstants.TYPE_SPACE),
-			groupId -> {
-				PermissionChecker permissionChecker =
-					PermissionThreadLocal.getPermissionChecker();
+		CMSUsersProvider cmsUsersProvider = _serviceTracker.getService();
 
-				if (permissionChecker.isGroupAdmin(groupId) ||
-					GroupLocalServiceUtil.hasUserGroup(
-						PrincipalThreadLocal.getUserId(), groupId)) {
-
-					return groupId;
-				}
-
-				return null;
-			});
-
-		if (ListUtil.isEmpty(depotEntryGroupIds)) {
-			return Collections.emptySet();
-		}
-
-		Set<User> users = new TreeSet<>(
-			Comparator.comparing(User::getFullName));
-
-		users.addAll(
-			UserLocalServiceUtil.searchBySocial(
-				CompanyThreadLocal.getCompanyId(),
-				ArrayUtil.toLongArray(depotEntryGroupIds), null, keywords,
-				start, end));
-
-		Set<Long> depotEntryUserGroupIds = new HashSet<>();
-
-		for (long depotEntryGroupId : depotEntryGroupIds) {
-			depotEntryUserGroupIds.addAll(
-				TransformUtil.transform(
-					UserGroupLocalServiceUtil.getGroupUserGroups(
-						depotEntryGroupId),
-					UserGroup::getUserGroupId));
-		}
-
-		if (SetUtil.isNotEmpty(depotEntryUserGroupIds)) {
-			users.addAll(
-				UserLocalServiceUtil.searchBySocial(
-					CompanyThreadLocal.getCompanyId(), null,
-					ArrayUtil.toLongArray(depotEntryUserGroupIds), keywords,
-					start, end));
-		}
-
-		return users;
+		return new LinkedHashSet<>(
+			cmsUsersProvider.getUsers(keywords, start, end));
 	}
+
+	private static final ServiceTracker<CMSUsersProvider, CMSUsersProvider>
+		_serviceTracker =
+			new ServiceTracker<CMSUsersProvider, CMSUsersProvider>(
+				SystemBundleUtil.getBundleContext(), CMSUsersProvider.class,
+				null) {
+
+				{
+					open();
+				}
+			};
 
 }
