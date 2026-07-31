69a195fa04ed70aa42ddc6694f1268cb7de94346
Pedro Leite <pedro.leite@liferay.com>
2026-07-31 14:07:43 -0300

    LPD-97891 [BE] Update existing test

--- modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/model/listener/test/ObjectEntryModelListenerTest.java
+++ modules/apps/site/site-cmp-site-initializer-test/src/testIntegration/java/com/liferay/site/cmp/site/initializer/internal/model/listener/test/ObjectEntryModelListenerTest.java
@@ -103,7 +103,8 @@ public class ObjectEntryModelListenerTest {
 			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 		_assertResourceActions(
 			cmpProjectObjectEntry, DepotRolesConstants.PROJECT_MEMBER,
-			ActionKeys.ADD_DISCUSSION, ActionKeys.VIEW);
+			ActionKeys.ADD_DISCUSSION, ActionKeys.VIEW,
+			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 
 		ObjectEntry cmpProjectLinkObjectEntry =
 			CMPTestUtil.addCMPProjectLinkObjectEntry(cmpProjectObjectEntry);
@@ -133,7 +134,8 @@ public class ObjectEntryModelListenerTest {
 			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 		_assertResourceActions(
 			cmpTaskObjectEntry, DepotRolesConstants.PROJECT_CONTRIBUTOR,
-			ActionKeys.ADD_DISCUSSION, ActionKeys.UPDATE, ActionKeys.VIEW);
+			ActionKeys.ADD_DISCUSSION, ActionKeys.UPDATE, ActionKeys.VIEW,
+			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 		_assertResourceActions(
 			cmpTaskObjectEntry, DepotRolesConstants.PROJECT_MANAGER,
 			ActionKeys.ADD_DISCUSSION, ActionKeys.DELETE,
@@ -143,7 +145,8 @@ public class ObjectEntryModelListenerTest {
 			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 		_assertResourceActions(
 			cmpTaskObjectEntry, DepotRolesConstants.PROJECT_MEMBER,
-			ActionKeys.ADD_DISCUSSION, ActionKeys.VIEW);
+			ActionKeys.ADD_DISCUSSION, ActionKeys.VIEW,
+			ObjectActionKeys.OBJECT_ENTRY_HISTORY);
 
 		ObjectEntry cmpTaskLinkObjectEntry =
 			CMPTestUtil.addCMPTaskLinkObjectEntry(cmpTaskObjectEntry);
