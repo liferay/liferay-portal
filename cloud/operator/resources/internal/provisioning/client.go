5ac278e428d21d20b616f9a26d7d1fca85a1a2a7
Gregory Amerson <gregory.amerson@liferay.com>
2026-07-31 08:05:27 -0500

    LCD-52772 add entitlements api

--- cloud/operator/resources/internal/provisioning/client.go
+++ cloud/operator/resources/internal/provisioning/client.go
@@ -13,8 +13,25 @@ type ActivationRequest struct {
 	PublicKey       string
 }
 
+type App struct {
+	LpkgDownloadLink string
+	Name             string
+}
+
 type Client interface {
 	Activate(activationRequest ActivationRequest, context context.Context, privateKey *rsa.PrivateKey) error
+	Entitlements(context context.Context, entitlementsRequest EntitlementsRequest, privateKey *rsa.PrivateKey) (*Entitlements, error)
+}
+
+type Entitlements struct {
+	Apps            []App
+	LicenseXML      []byte
+	MaxClusterNodes int32
+}
+
+type EntitlementsRequest struct {
+	DxpVersion    string
+	EnvironmentID string
 }
 
 var ErrorActivationRejected = errors.New("provisioning: activation rejected")
