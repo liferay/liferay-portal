7aaa0c336a0e3d0fb74a2fcf8800e0c2a3ffba0d
Carlos Correa <carlos.correa@liferay.com>
2026-07-31 08:37:46 +0200

    LPD-99438 Sort

--- modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineImportTaskExecutorTest.java
+++ modules/apps/batch-engine/batch-engine-test/src/testIntegration/java/com/liferay/batch/engine/internal/test/BatchEngineImportTaskExecutorTest.java
@@ -877,8 +877,8 @@ public class BatchEngineImportTaskExecutorTest
 				).build());
 
 		try {
-			_assertSkippedCallbackURL(testApplication, "localhost");
 			_assertFailedCallbackURL(testApplication, "255.255.255.255");
+			_assertSkippedCallbackURL(testApplication, "localhost");
 
 			try (CompanyConfigurationTemporarySwapper
 					companyConfigurationTemporarySwapper =
@@ -902,8 +902,8 @@ public class BatchEngineImportTaskExecutorTest
 								new String[] {"localhost"}
 							).build())) {
 
-				_assertSkippedCallbackURL(testApplication, "localhost");
 				_assertSkippedCallbackURL(testApplication, "255.255.255.255");
+				_assertSkippedCallbackURL(testApplication, "localhost");
 			}
 
 			try (CompanyConfigurationTemporarySwapper
