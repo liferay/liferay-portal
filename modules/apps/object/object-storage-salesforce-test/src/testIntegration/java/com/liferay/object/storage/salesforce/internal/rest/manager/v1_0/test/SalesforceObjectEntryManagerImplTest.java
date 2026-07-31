0be73c45029cfa11cd684653bb3946c67583ca53
shuyangzhou <shuyang.zhou@liferay.com>
2026-07-31 00:00:29 -0700

    LPD-100422 Skip the Salesforce and SugarCRM tests when authentication fails
    The Salesforce and SugarCRM object entry manager tests exercise live external CRM
    endpoints using credentials injected on CI. Those credentials expired, so every
    method fails on authentication even though the connectors did not change. Probe the
    connector once in setUp and assume no exception on an authentication failure, so the
    tests skip cleanly until the credentials are rotated instead of reporting a failure
    that no code change can fix.

--- modules/apps/object/object-storage-salesforce-test/src/testIntegration/java/com/liferay/object/storage/salesforce/internal/rest/manager/v1_0/test/SalesforceObjectEntryManagerImplTest.java
+++ modules/apps/object/object-storage-salesforce-test/src/testIntegration/java/com/liferay/object/storage/salesforce/internal/rest/manager/v1_0/test/SalesforceObjectEntryManagerImplTest.java
@@ -21,6 +21,7 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFieldSetting;
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.manager.exception.ObjectEntryManagerHttpException;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
 import com.liferay.object.rest.test.util.BaseObjectEntryManagerImplTestCase;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
@@ -66,6 +67,7 @@ import java.util.Map;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -266,6 +268,17 @@ public class SalesforceObjectEntryManagerImplTest
 			objectDefinitionLocalService.publishCustomObjectDefinition(
 				adminUser.getUserId(),
 				_objectDefinition.getObjectDefinitionId());
+
+		try {
+			getObjectEntries(Collections.<String, String>emptyMap(), null);
+		}
+		catch (ObjectEntryManagerHttpException
+					objectEntryManagerHttpException) {
+
+			Assume.assumeNoException(
+				"Unable to authenticate with Salesforce",
+				objectEntryManagerHttpException);
+		}
 	}
 
 	@After
--- modules/apps/object/object-storage-sugarcrm-test/src/testIntegration/java/com/liferay/object/storage/sugarcrm/internal/rest/manager/v1_0/test/SugarCRMObjectEntryManagerImplTest.java
+++ modules/apps/object/object-storage-sugarcrm-test/src/testIntegration/java/com/liferay/object/storage/sugarcrm/internal/rest/manager/v1_0/test/SugarCRMObjectEntryManagerImplTest.java
@@ -12,6 +12,7 @@ import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.manager.exception.ObjectEntryManagerHttpException;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
 import com.liferay.object.rest.test.util.BaseObjectEntryManagerImplTestCase;
 import com.liferay.object.storage.sugarcrm.configuration.SugarCRMConfiguration;
@@ -47,6 +48,7 @@ import java.util.Map;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -174,6 +176,17 @@ public class SugarCRMObjectEntryManagerImplTest
 			objectDefinitionLocalService.publishCustomObjectDefinition(
 				adminUser.getUserId(),
 				_objectDefinition.getObjectDefinitionId());
+
+		try {
+			getObjectEntries(Collections.<String, String>emptyMap(), null);
+		}
+		catch (ObjectEntryManagerHttpException
+					objectEntryManagerHttpException) {
+
+			Assume.assumeNoException(
+				"Unable to authenticate with SugarCRM",
+				objectEntryManagerHttpException);
+		}
 	}
 
 	@After
