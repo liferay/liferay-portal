e85c2871efa73d9087493689b8b8237762b05a39
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-08-03 09:54:31 +0200

    LPD-100486 Provision the Design Library roles in the test fixture
    Since LPD-98812, addDepotEntry resolves the owner role from the depot subtype, and LPD-99354 gated the Design Library variant behind the LPD-57283 feature flag. The class-level FeatureFlag annotation does not provision those roles, because FeatureFlagTestRule only flips a PropsUtil value and installs a MockFeatureFlagManager, so it runs neither DepotRolesPortalInstanceLifecycleListener at portal instance registration nor DesignLibraryFeatureFlagListener at runtime. The flag therefore read as enabled while the roles did not exist, and every test failed in setUp with NoSuchRoleException. LPD-99530 fixed the same defect in the style book and master layout fixtures; this one was missed.

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/notifications/test/DepotEntryUserNotificationTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/internal/notifications/test/DepotEntryUserNotificationTest.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.FeatureFlagTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
@@ -73,6 +74,9 @@ public class DepotEntryUserNotificationTest {
 
 	@Before
 	public void setUp() throws Exception {
+		FeatureFlagTestUtil.invokeFeatureFlagListeners(
+			TestPropsValues.getCompanyId(), true, "LPD-57283");
+
 		_depotEntry1 = _depotEntryLocalService.addDepotEntry(
 			HashMapBuilder.put(
 				LocaleUtil.getDefault(), StringUtil.randomString()
