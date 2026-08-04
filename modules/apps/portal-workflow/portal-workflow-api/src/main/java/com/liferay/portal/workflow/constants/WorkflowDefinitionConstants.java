903bfe513d089f5a1e1b84939596c04cc181371e
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-08-03 23:56:56 -0300

    LPD-99498 refactor: remove AI Hub constants, decrease the couple between the AI Hub and portal workflow
    # breaking
    
    ## What modules/apps/portal-workflow/portal-workflow-api/src/main/java/com/liferay/portal/workflow/constants/WorkflowDefinitionConstants.java
    
    Remove AI Hub agent workflow definition constants
    
    ## Why
    
    AI Hub should be decoupled from Liferay DXP core
    
    ----

--- modules/apps/portal-workflow/portal-workflow-api/src/main/java/com/liferay/portal/workflow/constants/WorkflowDefinitionConstants.java
+++ modules/apps/portal-workflow/portal-workflow-api/src/main/java/com/liferay/portal/workflow/constants/WorkflowDefinitionConstants.java
@@ -17,58 +17,13 @@ public class WorkflowDefinitionConstants {
 	public static final String EXTERNAL_REFERENCE_CODE_SINGLE_APPROVER =
 		"L_SINGLE_APPROVER";
 
-	public static final String NAME_CHANGE_TONE = "Change Tone";
-
-	public static final String NAME_CONTENT_GAP_ANALYSIS =
-		"Content Gap Analysis";
-
-	public static final String NAME_FIND_MATCHING_ASSETS =
-		"Find Matching Assets";
-
-	public static final String NAME_FIX_SPELLING_AND_GRAMMAR =
-		"Fix Spelling and Grammar";
-
-	public static final String NAME_GENERATE_CONTENT = "Generate Content";
-
-	public static final String NAME_GENERATE_FIELD_VALUE =
-		"Generate Field Value";
-
-	public static final String NAME_GENERATE_IMAGE = "Generate Image";
-
-	public static final String NAME_IMPROVE_WRITING = "Improve Writing";
-
-	public static final String NAME_LIFERAY_SEARCH = "Liferay Search";
-
-	public static final String NAME_MAKE_LONGER = "Make Longer";
-
-	public static final String NAME_MAKE_SHORTER = "Make Shorter";
-
 	public static final String NAME_MESSAGE_BOARDS_USER_STATS_MODERATION =
 		"Message Boards User Stats Moderation";
 
-	public static final String NAME_PAGE_BUILDER = "Page Builder";
-
-	public static final String NAME_SEO_STUDIO_DESCRIPTION_GENERATOR =
-		"SEO Studio Description Generator";
-
-	public static final String NAME_SEO_STUDIO_TITLE_GENERATOR =
-		"SEO Studio Title Generator";
-
 	public static final String NAME_SINGLE_APPROVER = "Single Approver";
-
-	public static final String NAME_TRANSLATE_CONTENT = "Translate Content";
 
 	public static final String SCOPE_AI = "ai";
 
 	public static final String SCOPE_ALL = "all";
-
-	public static final String[] SYSTEM_WORKFLOW_DEFINITION_NAMES = {
-		NAME_CHANGE_TONE, NAME_CONTENT_GAP_ANALYSIS, NAME_GENERATE_FIELD_VALUE,
-		NAME_FIND_MATCHING_ASSETS, NAME_FIX_SPELLING_AND_GRAMMAR,
-		NAME_GENERATE_CONTENT, NAME_GENERATE_IMAGE, NAME_IMPROVE_WRITING,
-		NAME_LIFERAY_SEARCH, NAME_MAKE_LONGER, NAME_MAKE_SHORTER,
-		NAME_PAGE_BUILDER, NAME_SEO_STUDIO_DESCRIPTION_GENERATOR,
-		NAME_SEO_STUDIO_TITLE_GENERATOR, NAME_TRANSLATE_CONTENT
-	};
 
 }
