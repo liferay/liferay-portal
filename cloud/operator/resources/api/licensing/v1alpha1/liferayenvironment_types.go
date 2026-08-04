7b0d05d28a7c745b4128a2c9ba5b6d2d2f31dd93
Gregory Amerson <gregory.amerson@liferay.com>
2026-08-04 12:00:13 -0500

    LCD-52857 add LicenseValid and ValidUtil columns to LiferayEnvironment

--- cloud/operator/resources/api/licensing/v1alpha1/liferayenvironment_types.go
+++ cloud/operator/resources/api/licensing/v1alpha1/liferayenvironment_types.go
@@ -26,6 +26,8 @@ type LicenseStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:printcolumn:JSONPath=`.status.phase`,name="Phase",type=string
 // +kubebuilder:printcolumn:JSONPath=`.status.conditions[?(@.type=="Activated")].status`,name="Activated",type=string
+// +kubebuilder:printcolumn:JSONPath=`.status.conditions[?(@.type=="LicenseValid")].status`,name="License",type=string
+// +kubebuilder:printcolumn:JSONPath=`.status.license.validUntil`,name="Valid-Until",type=string
 // +kubebuilder:printcolumn:JSONPath=`.metadata.creationTimestamp`,name="Age",type=date
 // +kubebuilder:printcolumn:JSONPath=`.status.environmentId`,name="Environment-ID",priority=1,type=string
 // +kubebuilder:printcolumn:JSONPath=`.status.activatedAt`,name="Activated-At",priority=1,type=date
