970967198ab11d56f1ebb38dbec7d92446c9d87b
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2026-08-04 13:58:57 +0200

    LPD-98854 Fix the User Roles commerce health check
    The User role is a system role that the portal creates with resource permissions already attached, so the guard added in LPD-89370 never let the forced permission reload run for it. Clicking Fix Issue on the User Roles health check silently did nothing and the portlet reported an unexpected error, leaving the return object definitions and list type definitions unreachable to regular users.

--- modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/internal/helper/test/CommerceRoleHelperTest.java
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/internal/helper/test/CommerceRoleHelperTest.java
@@ -8,6 +8,9 @@ package com.liferay.commerce.internal.helper.test;
 import com.liferay.account.constants.AccountRoleConstants;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.helper.CommerceRoleHelper;
+import com.liferay.list.type.model.ListTypeDefinition;
+import com.liferay.list.type.service.ListTypeDefinitionLocalService;
+import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
@@ -16,11 +19,15 @@ import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
@@ -127,10 +134,65 @@ public class CommerceRoleHelperTest {
 			resourcePermissions.toString(), size, resourcePermissions.size());
 	}
 
+	@FeatureFlag("LPD-10562")
+	@Test
+	public void testCheckCommerceUserRoles() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				TestPropsValues.getGroupId(), TestPropsValues.getUserId());
+
+		ListTypeDefinition listTypeDefinition =
+			_listTypeDefinitionLocalService.
+				fetchListTypeDefinitionByExternalReferenceCode(
+					_RETURN_REASONS_EXTERNAL_REFERENCE_CODE,
+					TestPropsValues.getCompanyId());
+
+		if (listTypeDefinition == null) {
+			listTypeDefinition =
+				_listTypeDefinitionLocalService.addListTypeDefinition(
+					_RETURN_REASONS_EXTERNAL_REFERENCE_CODE,
+					TestPropsValues.getUserId(),
+					Collections.singletonMap(
+						LocaleUtil.getSiteDefault(),
+						RandomTestUtil.randomString()),
+					false, Collections.emptyList(), serviceContext);
+		}
+
+		Role role = _roleLocalService.getRole(
+			TestPropsValues.getCompanyId(), RoleConstants.USER);
+
+		_resourcePermissionLocalService.setResourcePermissions(
+			TestPropsValues.getCompanyId(),
+			listTypeDefinition.getModelClassName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			String.valueOf(listTypeDefinition.getListTypeDefinitionId()),
+			role.getRoleId(), new String[0]);
+
+		_commerceRoleHelper.checkCommerceUserRoles(serviceContext);
+
+		Assert.assertFalse(
+			_commerceRoleHelper.hasCommerceUserPermissions(
+				TestPropsValues.getCompanyId()));
+
+		serviceContext.setAttribute("forceReloadPermissions", Boolean.TRUE);
+
+		_commerceRoleHelper.checkCommerceUserRoles(serviceContext);
+
+		Assert.assertTrue(
+			_commerceRoleHelper.hasCommerceUserPermissions(
+				TestPropsValues.getCompanyId()));
+	}
+
+	private static final String _RETURN_REASONS_EXTERNAL_REFERENCE_CODE =
+		"L_COMMERCE_RETURN_REASONS";
+
 	@Inject(
 		filter = "component.name=com.liferay.commerce.internal.helper.CommerceRoleHelperImpl"
 	)
 	private CommerceRoleHelper _commerceRoleHelper;
+
+	@Inject
+	private ListTypeDefinitionLocalService _listTypeDefinitionLocalService;
 
 	@Inject
 	private ResourcePermissionLocalService _resourcePermissionLocalService;
