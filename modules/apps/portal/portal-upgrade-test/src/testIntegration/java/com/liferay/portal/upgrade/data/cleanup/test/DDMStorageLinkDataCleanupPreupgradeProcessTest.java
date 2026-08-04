977b87036aa924d678cbd8051e72cda9a9aca37a
Ankita Malik <ankita.malik@liferay.com>
2026-08-04 14:50:49 +0530

    LPD-99741 Add test to ensure journal article default values survive the preupgrade data cleanup

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/DDMStorageLinkDataCleanupPreupgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/DDMStorageLinkDataCleanupPreupgradeProcessTest.java
@@ -6,20 +6,32 @@
 package com.liferay.portal.upgrade.data.cleanup.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMFieldLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.data.cleanup.util.OrphanReferencesDataCleanupUtil;
 import com.liferay.portal.test.log.LogCapture;
 import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upgrade.data.cleanup.DDMStorageLinkDataCleanupPreupgradeProcess;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import java.util.List;
 
@@ -113,5 +125,109 @@ public class DDMStorageLinkDataCleanupPreupgradeProcessTest
 				"delete from DDMFieldAttribute where storageId = " + contentId);
 		}
 	}
+
+	@Test
+	public void testUpgradeKeepsDDMStructureDefaultValues() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		JournalArticle journalArticle = null;
+
+		try {
+			journalArticle = JournalTestUtil.addArticleDefaultValues(
+				TestPropsValues.getUserId(), group.getGroupId(),
+				RandomTestUtil.randomString());
+
+			DDMStructure ddmStructure = journalArticle.getDDMStructure();
+
+			Assert.assertNotNull(
+				_ddmFieldLocalService.getDDMFormValues(
+					ddmStructure.getDDMForm(), journalArticle.getId()));
+
+			upgrade();
+
+			Assert.assertNotNull(
+				_ddmFieldLocalService.getDDMFormValues(
+					ddmStructure.getDDMForm(), journalArticle.getId()));
+		}
+		finally {
+			if (journalArticle != null) {
+				_ddmTemplateLocalService.deleteTemplate(
+					journalArticle.getDDMTemplate());
+			}
+
+			_groupLocalService.deleteGroup(group);
+		}
+	}
+
+	@Test
+	public void testUpgradeWithJournalArticleDDMFields() throws Exception {
+		long id = RandomTestUtil.nextLong();
+
+		runSQL(
+			StringBundler.concat(
+				"insert into JournalArticle (mvccVersion, ctCollectionId, ",
+				"id_) values (0, 0, ", id, ")"));
+		runSQL(
+			StringBundler.concat(
+				"insert into DDMField (mvccVersion, ctCollectionId, fieldId, ",
+				"storageId) values (0, 0, ", RandomTestUtil.nextLong(), ", ",
+				id, ")"));
+		runSQL(
+			StringBundler.concat(
+				"insert into DDMFieldAttribute (mvccVersion, ctCollectionId, ",
+				"fieldAttributeId, storageId) values (0, 0, ",
+				RandomTestUtil.nextLong(), ", ", id, ")"));
+
+		try (Connection connection = DataAccess.getConnection();
+			LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				OrphanReferencesDataCleanupUtil.class.getName(),
+				LoggerTestUtil.INFO)) {
+
+			upgrade();
+
+			List<String> messages = logCapture.getMessages();
+
+			Assert.assertEquals(messages.toString(), 0, messages.size());
+
+			Assert.assertEquals(
+				1, _getCount(connection, "storageId", "DDMField", id));
+			Assert.assertEquals(
+				1, _getCount(connection, "storageId", "DDMFieldAttribute", id));
+		}
+		finally {
+			runSQL("delete from JournalArticle where id_ = " + id);
+			runSQL("delete from DDMField where storageId = " + id);
+			runSQL("delete from DDMFieldAttribute where storageId = " + id);
+		}
+	}
+
+	private long _getCount(
+			Connection connection, String columnName, String tableName,
+			long value)
+		throws Exception {
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				StringBundler.concat(
+					"select count(*) as count from ", tableName, " where ",
+					columnName, " = ?"))) {
+
+			preparedStatement.setLong(1, value);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				resultSet.next();
+
+				return resultSet.getLong("count");
+			}
+		}
+	}
+
+	@Inject
+	private DDMFieldLocalService _ddmFieldLocalService;
+
+	@Inject
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
 
 }
