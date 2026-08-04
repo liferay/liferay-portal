605e70e27ce90063370a4749bf06e8ca0a972da9
Gregory Amerson <gregory.amerson@liferay.com>
2026-08-04 11:52:00 -0500

    LCD-52857 add license.go unit test

---
+++ cloud/operator/resources/internal/license/license_test.go
@@ -0,0 +1,83 @@
+package license
+
+import (
+	"testing"
+)
+
+const licenseXML1 = `<?xml version="1.0"?>
+<licenses>
+    <license>
+        <description>Digital Sales Room Cloud Native Environment</description>
+        <expiration-date>Friday, March 2, 2029 12:00:00 AM GMT</expiration-date>
+        <license-type>enterprise</license-type>
+        <license-version>3</license-version>
+        <owner>Fry Some Animal</owner>
+        <product-name>Digital Sales Room</product-name>
+        <product-id>23bd2e2b-c4aa-45d7-86c9-ba0e2f890bf5</product-id>
+        <product-version>1</product-version>
+        <start-date>Wednesday, July 1, 2026 12:00:00 AM GMT</start-date>
+    </license>
+    <license>
+        <account-name>Fry Some Animal</account-name>
+        <description>Cloud Native</description>
+        <expiration-date>Friday, March 2, 2029 12:00:00 AM GMT</expiration-date>
+        <instance-size>Sizing 4</instance-size>
+        <license-name>DXP Non-Production (Virtual Cluster)</license-name>
+        <license-type>virtual-cluster</license-type>
+        <license-version>6</license-version>
+        <max-cluster-nodes>1</max-cluster-nodes>
+        <owner>Fry Some Animal</owner>
+        <product-name>DXP Production</product-name>
+        <product-version>2026.Q3</product-version>
+        <start-date>Wednesday, July 1, 2026 12:00:00 AM GMT</start-date>
+    </license>
+</licenses>`
+
+const licenseXML2 = `<?xml version="1.0"?>
+<licenses>
+    <license>
+		<expiration-date>not a real date</expiration-date>
+		<license-type>enterprise</license-type>
+    </license>
+    <license>
+		<expiration-date>Sunday, January 5, 2031 12:00:00 AM GMT</expiration-date>
+		<license-type>virtual-cluster</license-type>
+    </license>
+</licenses>`
+
+const licenseXML3 = `<?xml version="1.0"?>
+<licenses>
+    <license>
+		<license-type>enterprise</license-type>
+    </license>
+</licenses>`
+
+func TestExpirationDateReturnsVirtualClusterDate(t *testing.T) {
+	expirationDate, error := ExpirationDate([]byte(licenseXML1))
+
+	if error != nil {
+		t.Fatalf("unexpected error: %v", error)
+	}
+
+	if formatted := expirationDate.Format("2006-01-02"); formatted != "2029-03-02" {
+		t.Errorf("expected 2029-03-02, got %s", formatted)
+	}
+}
+
+func TestExpirationDateSkipsNonVirtualClusterLicenses(t *testing.T) {
+	expirationDate, error := ExpirationDate([]byte(licenseXML2))
+
+	if error != nil {
+		t.Fatalf("unexpected error: %v", error)
+	}
+
+	if formatted := expirationDate.Format("2006-01-02"); formatted != "2031-01-05" {
+		t.Errorf("expected 2031-01-05, got %s", formatted)
+	}
+}
+
+func TestExpirationDateWhenNoVirtualClusterLicenseReturnsError(t *testing.T) {
+	if _, error := ExpirationDate([]byte(licenseXML3)); error == nil {
+		t.Fatal("expected an error, got nil")
+	}
+}
