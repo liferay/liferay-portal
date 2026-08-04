144c6e8c7b34118e3ede3c01fcdfe9cebcf179ec
Lucas Miranda <lucas.miranda@liferay.com>
2026-08-04 16:17:27 -0300

    LPD-93272 Implement FIPS health verification endpoint

--- modules/apps/portal-security/portal-security-fips-rest-impl/src/main/java/com/liferay/portal/security/fips/rest/internal/resource/v1_0/FIPSHealthVerificationResourceImpl.java
+++ modules/apps/portal-security/portal-security-fips-rest-impl/src/main/java/com/liferay/portal/security/fips/rest/internal/resource/v1_0/FIPSHealthVerificationResourceImpl.java
@@ -5,7 +5,17 @@
 
 package com.liferay.portal.security.fips.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.security.fips.FIPSApplicationState;
+import com.liferay.portal.kernel.security.fips.FIPSApplicationStateMachineUtil;
+import com.liferay.portal.kernel.security.fips.FIPSModeValidator;
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.security.fips.rest.dto.v1_0.FIPSHealthVerification;
 import com.liferay.portal.security.fips.rest.resource.v1_0.FIPSHealthVerificationResource;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
@@ -20,4 +30,48 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class FIPSHealthVerificationResourceImpl
 	extends BaseFIPSHealthVerificationResourceImpl {
+
+	@Override
+	public FIPSHealthVerification postFIPSHealthVerification()
+		throws Exception {
+
+		if (!PropsValues.FIPS_ENABLED) {
+			throw new WebApplicationException(
+				Response.status(
+					Response.Status.NOT_FOUND
+				).build());
+		}
+
+		FIPSHealthVerification fipsHealthVerification =
+			new FIPSHealthVerification();
+
+		fipsHealthVerification.setDateVerified(() -> new Date());
+
+		try {
+			FIPSApplicationStateMachineUtil.selfTest(
+				FIPSModeValidator::validate);
+		}
+		catch (Exception exception) {
+			fipsHealthVerification.setErrorMessage(exception::getMessage);
+		}
+
+		FIPSApplicationState fipsApplicationState =
+			FIPSApplicationStateMachineUtil.getFIPSApplicationState();
+
+		fipsHealthVerification.setStatus(
+			() -> FIPSHealthVerification.Status.create(
+				fipsApplicationState.name()));
+
+		if (fipsApplicationState != FIPSApplicationState.OPERATIONAL) {
+			throw new WebApplicationException(
+				Response.status(
+					Response.Status.SERVICE_UNAVAILABLE
+				).entity(
+					fipsHealthVerification
+				).build());
+		}
+
+		return fipsHealthVerification;
+	}
+
 }
