7c19a11a7a144a9d4f989a8f3a4f861e41bb2a11
brbalazs <balazs.breier@liferay.com>
2026-08-05 19:36:25 +0200

    LPD-101011 Unit tests

--- modules/apps/commerce/commerce-subscription-web/src/test/java/com/liferay/commerce/subscription/web/internal/portlet/action/EditCommerceSubscriptionContentMVCActionCommandTest.java
+++ modules/apps/commerce/commerce-subscription-web/src/test/java/com/liferay/commerce/subscription/web/internal/portlet/action/EditCommerceSubscriptionContentMVCActionCommandTest.java
@@ -5,11 +5,17 @@
 
 package com.liferay.commerce.subscription.web.internal.portlet.action;
 
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.model.CommerceSubscriptionEntry;
 import com.liferay.commerce.service.CommerceSubscriptionEntryLocalService;
 import com.liferay.commerce.subscription.CommerceSubscriptionEntryActionHelper;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.test.portlet.MockActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockActionResponse;
@@ -78,41 +84,27 @@ public class EditCommerceSubscriptionContentMVCActionCommandTest {
 
 	@Test
 	public void testDoProcessAction() throws Exception {
-		CommerceSubscriptionEntry commerceSubscriptionEntry = Mockito.mock(
-			CommerceSubscriptionEntry.class);
+		long commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+		long companyId = RandomTestUtil.randomLong();
+		long userId = RandomTestUtil.randomLong();
+
+		_setUpCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId, companyId, userId);
+
+		ThemeDisplay themeDisplay = _getThemeDisplay(companyId, userId);
 
 		Mockito.when(
-			commerceSubscriptionEntry.getCompanyId()
+			themeDisplay.isSignedIn()
 		).thenReturn(
-			RandomTestUtil.randomLong()
+			false
 		);
 
-		Mockito.when(
-			_commerceSubscriptionEntryLocalService.getCommerceSubscriptionEntry(
-				Mockito.anyLong())
-		).thenReturn(
-			commerceSubscriptionEntry
-		);
-
-		MockActionRequest mockActionRequest = new MockActionRequest();
-
-		ThemeDisplay themeDisplay = Mockito.mock(ThemeDisplay.class);
-
-		Mockito.when(
-			themeDisplay.getCompanyId()
-		).thenReturn(
-			RandomTestUtil.randomLong()
-		);
-
-		mockActionRequest.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
-
-		mockActionRequest.setParameter(
-			Constants.CMD, RandomTestUtil.randomString());
-		mockActionRequest.setParameter(
-			"commerceSubscriptionEntryId",
-			String.valueOf(RandomTestUtil.randomLong()));
+		MockActionRequest mockActionRequest = _getMockActionRequest(
+			"suspend", themeDisplay);
 
 		MockActionResponse mockActionResponse = new MockActionResponse();
+
+		SessionErrors.clear(mockActionRequest);
 
 		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
 			mockActionRequest, mockActionResponse);
@@ -122,7 +114,274 @@ public class EditCommerceSubscriptionContentMVCActionCommandTest {
 				mockActionRequest, PrincipalException.class.getName()));
 		Assert.assertEquals(
 			"/error.jsp", mockActionResponse.getRenderParameter("mvcPath"));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.never()
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
+
+		commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+
+		_setUpCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId, companyId,
+			RandomTestUtil.randomLong());
+
+		mockActionRequest = _getMockActionRequest(
+			"suspend",
+			_getThemeDisplay(companyId + 1, RandomTestUtil.randomLong()));
+
+		mockActionResponse = new MockActionResponse();
+
+		SessionErrors.clear(mockActionRequest);
+
+		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
+			mockActionRequest, mockActionResponse);
+
+		Assert.assertTrue(
+			SessionErrors.contains(
+				mockActionRequest, PrincipalException.class.getName()));
+		Assert.assertEquals(
+			"/error.jsp", mockActionResponse.getRenderParameter("mvcPath"));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.never()
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
+
+		commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+		userId = RandomTestUtil.randomLong();
+
+		_setUpCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId, companyId, userId);
+
+		mockActionRequest = _getMockActionRequest(
+			"suspend", _getThemeDisplay(companyId, userId + 1));
+
+		mockActionResponse = new MockActionResponse();
+
+		SessionErrors.clear(mockActionRequest);
+
+		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
+			mockActionRequest, mockActionResponse);
+
+		Assert.assertTrue(
+			SessionErrors.contains(
+				mockActionRequest, PrincipalException.class.getName()));
+		Assert.assertEquals(
+			"/error.jsp", mockActionResponse.getRenderParameter("mvcPath"));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.never()
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
+
+		commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+		userId = RandomTestUtil.randomLong();
+
+		_setUpCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId, companyId, userId);
+
+		mockActionRequest = _getMockActionRequest(
+			"suspend", _getThemeDisplay(companyId, userId));
+
+		SessionErrors.clear(mockActionRequest);
+
+		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
+			mockActionRequest, new MockActionResponse());
+
+		Assert.assertFalse(
+			SessionErrors.contains(
+				mockActionRequest, PrincipalException.class.getName()));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.times(1)
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
+
+		commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+		userId = RandomTestUtil.randomLong();
+
+		_setUpCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId, companyId, userId);
+
+		Mockito.when(
+			_portletResourcePermission.contains(
+				Mockito.any(PermissionChecker.class), Mockito.anyLong(),
+				Mockito.anyString())
+		).thenReturn(
+			true
+		);
+
+		mockActionRequest = _getMockActionRequest(
+			"suspend", _getThemeDisplay(companyId, userId + 1));
+
+		SessionErrors.clear(mockActionRequest);
+
+		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
+			mockActionRequest, new MockActionResponse());
+
+		Assert.assertFalse(
+			SessionErrors.contains(
+				mockActionRequest, PrincipalException.class.getName()));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.times(1)
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
+
+		commerceSubscriptionEntryId = RandomTestUtil.randomLong();
+		userId = RandomTestUtil.randomLong();
+
+		CommerceSubscriptionEntry commerceSubscriptionEntry =
+			_setUpCommerceSubscriptionEntry(
+				commerceSubscriptionEntryId, companyId, userId);
+
+		long commerceOrderId = RandomTestUtil.randomLong();
+
+		CommerceOrderItem commerceOrderItem = Mockito.mock(
+			CommerceOrderItem.class);
+
+		Mockito.when(
+			commerceOrderItem.getCommerceOrderId()
+		).thenReturn(
+			commerceOrderId
+		);
+
+		Mockito.when(
+			commerceSubscriptionEntry.fetchCommerceOrderItem()
+		).thenReturn(
+			commerceOrderItem
+		);
+
+		Mockito.when(
+			_commerceOrderModelResourcePermission.contains(
+				Mockito.any(PermissionChecker.class),
+				Mockito.eq(commerceOrderId), Mockito.eq(ActionKeys.UPDATE))
+		).thenReturn(
+			true
+		);
+
+		mockActionRequest = _getMockActionRequest(
+			"suspend", _getThemeDisplay(companyId, userId + 1));
+
+		SessionErrors.clear(mockActionRequest);
+
+		_editCommerceSubscriptionContentMVCActionCommand.doProcessAction(
+			mockActionRequest, new MockActionResponse());
+
+		Assert.assertFalse(
+			SessionErrors.contains(
+				mockActionRequest, PrincipalException.class.getName()));
+
+		Mockito.verify(
+			_commerceSubscriptionEntryActionHelper, Mockito.times(1)
+		).suspendCommerceSubscriptionEntry(
+			commerceSubscriptionEntryId
+		);
 	}
+
+	private MockActionRequest _getMockActionRequest(
+		String cmd, ThemeDisplay themeDisplay) {
+
+		MockActionRequest mockActionRequest = new MockActionRequest();
+
+		mockActionRequest.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+
+		mockActionRequest.setParameter(Constants.CMD, cmd);
+		mockActionRequest.setParameter(
+			"commerceSubscriptionEntryId",
+			String.valueOf(RandomTestUtil.randomLong()));
+
+		return mockActionRequest;
+	}
+
+	private ThemeDisplay _getThemeDisplay(long companyId, long userId) {
+		ThemeDisplay themeDisplay = Mockito.mock(ThemeDisplay.class);
+
+		Mockito.when(
+			themeDisplay.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		Mockito.when(
+			themeDisplay.getPermissionChecker()
+		).thenReturn(
+			Mockito.mock(PermissionChecker.class)
+		);
+
+		Mockito.when(
+			themeDisplay.getUserId()
+		).thenReturn(
+			userId
+		);
+
+		Mockito.when(
+			themeDisplay.isSignedIn()
+		).thenReturn(
+			true
+		);
+
+		return themeDisplay;
+	}
+
+	private CommerceSubscriptionEntry _setUpCommerceSubscriptionEntry(
+			long commerceSubscriptionEntryId, long companyId, long userId)
+		throws Exception {
+
+		CommerceSubscriptionEntry commerceSubscriptionEntry = Mockito.mock(
+			CommerceSubscriptionEntry.class);
+
+		Mockito.when(
+			commerceSubscriptionEntry.getCommerceSubscriptionEntryId()
+		).thenReturn(
+			commerceSubscriptionEntryId
+		);
+
+		Mockito.when(
+			commerceSubscriptionEntry.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		Mockito.when(
+			commerceSubscriptionEntry.getGroupId()
+		).thenReturn(
+			RandomTestUtil.randomLong()
+		);
+
+		Mockito.when(
+			commerceSubscriptionEntry.getUserId()
+		).thenReturn(
+			userId
+		);
+
+		Mockito.when(
+			_commerceSubscriptionEntryLocalService.getCommerceSubscriptionEntry(
+				Mockito.anyLong())
+		).thenReturn(
+			commerceSubscriptionEntry
+		);
+
+		Mockito.when(
+			_portletResourcePermission.contains(
+				Mockito.any(PermissionChecker.class), Mockito.anyLong(),
+				Mockito.anyString())
+		).thenReturn(
+			false
+		);
+
+		return commerceSubscriptionEntry;
+	}
+
+	@Mock
+	private ModelResourcePermission<CommerceOrder>
+		_commerceOrderModelResourcePermission;
 
 	@Mock
 	private CommerceSubscriptionEntryActionHelper
@@ -136,5 +395,8 @@ public class EditCommerceSubscriptionContentMVCActionCommandTest {
 	private final EditCommerceSubscriptionContentMVCActionCommand
 		_editCommerceSubscriptionContentMVCActionCommand =
 			new EditCommerceSubscriptionContentMVCActionCommand();
+
+	@Mock
+	private PortletResourcePermission _portletResourcePermission;
 
 }
