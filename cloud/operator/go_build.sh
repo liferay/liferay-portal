e7d5db4365a08e35469dc5d574ccc77beb819e2f
Gregory Amerson <gregory.amerson@liferay.com>
2026-08-06 10:36:47 -0500

    LCD-52890 add source format for generated CRD inline go_build.sh

--- cloud/operator/go_build.sh
+++ cloud/operator/go_build.sh
@@ -11,11 +11,18 @@ function main {
 
 	cd resources
 
+	if [[ ${mode} == "generate" ]]
+	then
+		_generate
+
+		return
+	fi
+
 	if [[ ${mode} == "fast" ]]
 	then
-		go generate ./...
+		_generate
 
-		mkdir -p build
+		mkdir --parents build
 
 		CGO_ENABLED=0 GOOS=linux go build -o build/manager .
 
@@ -26,9 +33,63 @@ function main {
 
 	go mod verify
 
-	go generate ./...
+	_generate
 
 	CGO_ENABLED=0 go build -a --ldflags="-s -w" -o manager main.go
 }
+
+function _format_generated {
+	if ! command -v java > /dev/null
+	then
+		echo "java is required to format the generated CRD"
+
+		exit 1
+	fi
+
+	local version
+
+	version=$(curl \
+		--fail \
+		--silent \
+		"${_SOURCE_FORMATTER_URL}/maven-metadata.xml" | \
+		grep --extended-regexp --only-matching '<release>[^<]+' | \
+		sed --regexp-extended 's,<release>,,')
+
+	local dist_dir="${HOME}/.liferay/source-formatter/source-formatter-${version}"
+
+	if [ ! -x "${dist_dir}/bin/source-formatter" ]
+	then
+		local temp_dir
+
+		temp_dir=$(mktemp --directory)
+
+		curl \
+			--fail \
+			--location \
+			--output "${temp_dir}/source-formatter.zip" \
+			--silent \
+			"${_SOURCE_FORMATTER_URL}/${version}/com.liferay.source.formatter-${version}.zip"
+
+		mkdir --parents "${HOME}/.liferay/source-formatter"
+
+		unzip -o -q "${temp_dir}/source-formatter.zip" -d "${HOME}/.liferay/source-formatter"
+
+		rm --recursive --force "${temp_dir}"
+	fi
+
+	"${dist_dir}/bin/source-formatter" \
+		source.auto.fix=true \
+		source.files="${_CRD_FILE}"
+}
+
+function _generate {
+	go generate ./...
+
+	_format_generated
+}
+
+_CRD_FILE="$(cd .. && pwd)/helm/dxp-operator/crds/licensing.liferay.com_liferayenvironments.yaml"
+
+_SOURCE_FORMATTER_URL="https://repository-cdn.liferay.com/nexus/content/groups/public/com/liferay/com.liferay.source.formatter"
 
 main "${@}"
