74b621da1b3dce94fc38553f146bf0dd1c31b927
Fábio Alves <fabio.alves@liferay.com>
2026-08-07 08:21:00 -0300

    LPD-98918 Move CMPLicenseUtil to CMS API module

--- modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/CMPLicenseUtil.java
+++ modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/CMPLicenseUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.cms.site.initializer.internal.util;
+package com.liferay.site.cms.site.initializer.util;
 
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
