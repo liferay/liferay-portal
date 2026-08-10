05beed961fdd8bdc2eced48db2ca7048d44c4628
Alan Huang <alan.huang@liferay.com>
2026-08-11 02:04:24 +0800

    LPD-97979 Add JDKDynamicAgentLoadingCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JDKDynamicAgentLoadingCheck.java
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.check;
+
+/**
+ * @author Alan Huang
+ */
+public class JDKDynamicAgentLoadingCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		int x = -1;
+
+		while (true) {
+			x = content.indexOf("-XX:+EnableDynamicAgentLoading", x + 1);
+
+			if (x == -1) {
+				return content;
+			}
+
+			addMessage(
+				fileName,
+				"Do not use \"-XX:+EnableDynamicAgentLoading\", see https:" +
+					"//openjdk.org/jeps/451",
+				getLineNumber(content, x));
+		}
+	}
+
+}
