f98f28dee3bbbe448b8c8896ac5ce7a2d0ab18b0
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-08-10 15:58:56 +0100

    LPD-101379 Remove the unused collection applied filters fragment key

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/config/constants/collectionAppliedFiltersFragmentKey.js
+++
@@ -1,7 +0,0 @@
-/**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
- * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
- */
-
-export const COLLECTION_APPLIED_FILTERS_FRAGMENT_ENTRY_KEY =
-	'com.liferay.fragment.renderer.collection.filter.internal.CollectionAppliedFiltersFragmentRenderer';
