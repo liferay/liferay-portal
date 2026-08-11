0b6184ad546e6f47f5c0ee132150f8c34176ff05
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-08-11 15:08:23 +0200

    LPD-102076 Count the contents that reference an expired asset

--- modules/apps/headless/headless-cms/headless-cms-impl/src/main/java/com/liferay/headless/cms/internal/resource/v1_0/AssetStatisticsResourceImpl.java
+++ modules/apps/headless/headless-cms/headless-cms-impl/src/main/java/com/liferay/headless/cms/internal/resource/v1_0/AssetStatisticsResourceImpl.java
@@ -15,16 +15,28 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntryTable;
 import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.expression.Predicate;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.QueriesUtil;
+import com.liferay.portal.search.query.TermsQuery;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
 import com.liferay.portal.vulcan.util.GroupUtil;
 import com.liferay.site.cms.site.initializer.constants.CMSWorkflowConstants;
+import com.liferay.site.cms.site.initializer.util.CMSOutboundLinksUtil;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -75,6 +87,8 @@ public class AssetStatisticsResourceImpl
 						groupIds, objectDefinitionIds,
 						ObjectEntryTable.INSTANCE.status.eq(
 							WorkflowConstants.STATUS_APPROVED)));
+				setBrokenLinksCount(
+					() -> _getBrokenLinksCount(groupIds, objectDefinitionIds));
 				setExpiredCount(
 					() -> _getCount(
 						groupIds, objectDefinitionIds,
@@ -138,6 +152,87 @@ public class AssetStatisticsResourceImpl
 		};
 	}
 
+	private BooleanQuery _createOutboundLinksBooleanQuery(
+		List<String> outboundLinkTokens) {
+
+		BooleanQuery booleanQuery = QueriesUtil.booleanQuery();
+
+		for (int i = 0; i < outboundLinkTokens.size(); i += _MAX_TERMS_COUNT) {
+			List<String> values = outboundLinkTokens.subList(
+				i, Math.min(i + _MAX_TERMS_COUNT, outboundLinkTokens.size()));
+
+			booleanQuery.addShouldQueryClauses(
+				_createTermsQuery(
+					CMSOutboundLinksUtil.FIELD_NAME,
+					values.toArray(new String[0])));
+		}
+
+		booleanQuery.setMinimumShouldMatch(1);
+
+		return booleanQuery;
+	}
+
+	private TermsQuery _createTermsQuery(String fieldName, String... values) {
+		TermsQuery termsQuery = QueriesUtil.terms(fieldName);
+
+		termsQuery.addValues(values);
+
+		return termsQuery;
+	}
+
+	private long _getBrokenLinksCount(
+		Long[] groupIds, Long[] objectDefinitionIds) {
+
+		try {
+			if (!FeatureFlagManagerUtil.isEnabled(
+					contextCompany.getCompanyId(), "LPD-82226")) {
+
+				return 0;
+			}
+
+			List<String> expiredAssetTokens = _getExpiredAssetTokens(
+				objectDefinitionIds);
+
+			if (expiredAssetTokens.isEmpty()) {
+				return 0;
+			}
+
+			BooleanQuery booleanQuery = QueriesUtil.booleanQuery();
+
+			booleanQuery.addFilterQueryClauses(
+				_createOutboundLinksBooleanQuery(expiredAssetTokens),
+				_createTermsQuery("cms_section", "contents", "files"),
+				_createTermsQuery(
+					Field.STATUS,
+					ArrayUtil.toStringArray(CMSWorkflowConstants.STATUSES)),
+				QueriesUtil.term("rootDescendantNode", false));
+
+			SearchResponse searchResponse = _searcher.search(
+				_searchRequestBuilderFactory.builder(
+				).companyId(
+					contextCompany.getCompanyId()
+				).emptySearchEnabled(
+					true
+				).groupIds(
+					ArrayUtil.toArray(groupIds)
+				).query(
+					booleanQuery
+				).withSearchContext(
+					searchContext -> searchContext.setAttribute(
+						Field.STATUS, WorkflowConstants.STATUS_ANY)
+				).build());
+
+			return searchResponse.getCount();
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			return 0;
+		}
+	}
+
 	private long _getCount(
 		Long[] groupIds, Long[] objectDefinitionIds, Predicate predicate) {
 
@@ -153,6 +248,39 @@ public class AssetStatisticsResourceImpl
 
 			return 0;
 		}
+	}
+
+	private List<String> _getExpiredAssetTokens(Long[] objectDefinitionIds) {
+		List<String> expiredAssetTokens = new ArrayList<>();
+
+		List<Object[]> results = _objectEntryLocalService.dslQuery(
+			DSLQueryFactoryUtil.select(
+				ObjectEntryTable.INSTANCE.externalReferenceCode,
+				ObjectEntryTable.INSTANCE.objectEntryId
+			).from(
+				ObjectEntryTable.INSTANCE
+			).where(
+				ObjectEntryTable.INSTANCE.companyId.eq(
+					contextCompany.getCompanyId()
+				).and(
+					ObjectEntryTable.INSTANCE.objectDefinitionId.in(
+						objectDefinitionIds)
+				).and(
+					ObjectEntryTable.INSTANCE.status.eq(
+						WorkflowConstants.STATUS_EXPIRED)
+				)
+			));
+
+		for (Object[] objects : results) {
+			expiredAssetTokens.add(
+				CMSOutboundLinksUtil.getObjectEntryExternalReferenceCodeToken(
+					GetterUtil.getString(objects[0])));
+			expiredAssetTokens.add(
+				CMSOutboundLinksUtil.getObjectEntryIdToken(
+					GetterUtil.getLong(objects[1])));
+		}
+
+		return expiredAssetTokens;
 	}
 
 	private Long[] _getGroupIds(Long assetLibraryId) {
@@ -180,6 +308,7 @@ public class AssetStatisticsResourceImpl
 		return new AssetStatistics() {
 			{
 				setApprovedCount(() -> 0L);
+				setBrokenLinksCount(() -> 0L);
 				setExpiredCount(() -> 0L);
 				setExpiringSoonCount(() -> 0L);
 				setInDraftCount(() -> 0L);
@@ -193,6 +322,8 @@ public class AssetStatisticsResourceImpl
 	}
 
 	private static final int _EXPIRING_SOON_DAYS = 7;
+
+	private static final int _MAX_TERMS_COUNT = 65536;
 
 	private static final int _UPCOMING_REVIEW_DAYS = 7;
 
@@ -210,5 +341,11 @@ public class AssetStatisticsResourceImpl
 
 	@Reference
 	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
 
 }
