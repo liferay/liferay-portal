f4f7d33b5091b86b8bb7ce55387f7e5a45c6b01f
Daniel Raposo <daniel.raposo@liferay.com>
2026-08-11 11:25:12 +0200

    LPD-101270 Resolve the export dates as a range at every run
    A relaunch clones the stored configuration, so the dynamic export date selections must also be stored as a range that is resolved when the process runs instead of dates frozen at request time.

--- modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
+++ modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
@@ -18,7 +18,6 @@ import com.liferay.exportimport.rest.dto.v1_0.ExportProcessRequest;
 import com.liferay.exportimport.rest.dto.v1_0.ProcessProgress;
 import com.liferay.exportimport.rest.dto.v1_0.Status;
 import com.liferay.exportimport.rest.internal.util.BackgroundTaskUtil;
-import com.liferay.exportimport.rest.internal.util.DateRangeUtil;
 import com.liferay.exportimport.rest.internal.util.GroupUtil;
 import com.liferay.exportimport.rest.internal.util.ParameterMapUtil;
 import com.liferay.exportimport.rest.internal.util.PermissionUtil;
@@ -42,7 +41,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.util.DateRange;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -378,8 +376,13 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 		PermissionUtil.checkExportPermission(
 			contextCompany.getCompanyId(), groupId);
 
-		Map<String, String[]> parameterMap = ParameterMapUtil.toParameterMap(
-			exportProcessRequest, false);
+		Map<String, String[]> parameterMap =
+			ParameterMapUtil.putDateRangeParameters(
+				exportProcessRequest.getDateRangeTypeAsString(),
+				exportProcessRequest.getStartDate(),
+				exportProcessRequest.getEndDate(),
+				ParameterMapUtil.toParameterMap(exportProcessRequest, false),
+				contextUser);
 
 		boolean privateLayout = parameterMap.containsKey(
 			PreviewPortletDataHandlerUtil.PRIVATE_PAGES_CONTROL_NAME);
@@ -419,8 +422,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 					parameterMap, contextAcceptLanguage.getPreferredLocale(),
 					contextUser.getTimeZone());
 
-		_putDateRange(exportProcessRequest, settingsMap);
-
 		ExportImportConfiguration exportImportConfiguration =
 			_exportImportConfigurationLocalService.
 				addDraftExportImportConfiguration(
@@ -458,8 +459,13 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 			fileName += ".lar";
 		}
 
-		Map<String, String[]> parameterMap = ParameterMapUtil.toParameterMap(
-			exportProcessRequest, true);
+		Map<String, String[]> parameterMap =
+			ParameterMapUtil.putDateRangeParameters(
+				exportProcessRequest.getDateRangeTypeAsString(),
+				exportProcessRequest.getStartDate(),
+				exportProcessRequest.getEndDate(),
+				ParameterMapUtil.toParameterMap(exportProcessRequest, true),
+				contextUser);
 
 		Map<String, Serializable> settingsMap =
 			_exportImportConfigurationSettingsMapFactory.
@@ -467,8 +473,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 					contextUser.getUserId(), plid, groupId, portletId,
 					parameterMap, contextAcceptLanguage.getPreferredLocale(),
 					contextUser.getTimeZone(), fileName);
-
-		_putDateRange(exportProcessRequest, settingsMap);
 
 		settingsMap.put("name", exportProcessRequest.getName());
 
@@ -485,22 +489,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 
 		return _toExportProcess(
 			_backgroundTaskLocalService.getBackgroundTask(backgroundTaskId));
-	}
-
-	private void _putDateRange(
-		ExportProcessRequest exportProcessRequest,
-		Map<String, Serializable> settingsMap) {
-
-		DateRange dateRange = DateRangeUtil.toDateRange(
-			exportProcessRequest.getStartDate(),
-			exportProcessRequest.getEndDate());
-
-		if (dateRange == null) {
-			return;
-		}
-
-		settingsMap.put("endDate", dateRange.getEndDate());
-		settingsMap.put("startDate", dateRange.getStartDate());
 	}
 
 	private void _setSorts(DynamicQuery dynamicQuery, Sort[] sorts) {
