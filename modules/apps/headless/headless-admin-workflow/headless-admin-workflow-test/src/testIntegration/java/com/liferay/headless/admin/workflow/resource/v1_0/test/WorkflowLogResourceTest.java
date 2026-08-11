41b1e1949bbd84d0f6240db31f0aea32ce9cc9ee
Kammerer <victor.kammerer@liferay.com>
2026-08-11 12:53:38 -0300

    LPD-98711 Cover workflow log authorization

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowLogResourceTest.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowLogResourceTest.java
@@ -6,28 +6,40 @@
 package com.liferay.headless.admin.workflow.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.workflow.client.dto.v1_0.ObjectReviewed;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowDefinition;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowInstance;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowLog;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowTask;
 import com.liferay.headless.admin.workflow.client.pagination.Page;
 import com.liferay.headless.admin.workflow.client.pagination.Pagination;
+import com.liferay.headless.admin.workflow.client.problem.Problem;
+import com.liferay.headless.admin.workflow.client.resource.v1_0.WorkflowLogResource;
 import com.liferay.headless.admin.workflow.resource.v1_0.test.util.ObjectReviewedTestUtil;
 import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowDefinitionTestUtil;
 import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowInstanceTestUtil;
 import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowTaskTestUtil;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.WorkflowInstanceLink;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.service.WorkflowInstanceLinkLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 
 import java.util.Arrays;
@@ -67,12 +79,27 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 		_siteContentReviewerRole = _roleLocalService.getRole(
 			testGroup.getCompanyId(), RoleConstants.SITE_CONTENT_REVIEWER);
 
+		_objectReviewed = ObjectReviewedTestUtil.addObjectReviewed();
+
 		_workflowInstance = WorkflowInstanceTestUtil.addWorkflowInstance(
-			testGroup.getGroupId(), ObjectReviewedTestUtil.addObjectReviewed(),
-			_workflowDefinition);
+			testGroup.getGroupId(), _objectReviewed, _workflowDefinition);
 
 		_workflowTask = WorkflowTaskTestUtil.getWorkflowTask(
 			_workflowInstance.getId());
+
+		String password = RandomTestUtil.randomString();
+
+		_user = UserTestUtil.addUser(testCompany, password);
+
+		_userWorkflowLogResource = WorkflowLogResource.builder(
+		).authentication(
+			_user.getEmailAddress(), password
+		).endpoint(
+			testCompany.getVirtualHostname(),
+			PortalUtil.getPortalServerPort(false), "http"
+		).locale(
+			LocaleUtil.getDefault()
+		).build();
 	}
 
 	@Override
@@ -106,6 +133,8 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 			(List<WorkflowLog>)page.getItems());
 
 		assertValid(page);
+
+		_testGetWorkflowInstanceWorkflowLogsPageWithoutPermission();
 	}
 
 	@Override
@@ -155,6 +184,16 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 
 	@Override
 	@Test
+	public void testGetWorkflowLog() throws Exception {
+		super.testGetWorkflowLog();
+
+		_testGetWorkflowLogWithNonexistentWorkflowLogId();
+		_testGetWorkflowLogWithoutPermission();
+		_testGetWorkflowLogWithPermission();
+	}
+
+	@Override
+	@Test
 	public void testGetWorkflowTaskWorkflowLogsPage() throws Exception {
 		Page<WorkflowLog> page =
 			workflowLogResource.getWorkflowTaskWorkflowLogsPage(
@@ -171,6 +210,8 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 			(List<WorkflowLog>)page.getItems());
 
 		assertValid(page);
+
+		_testGetWorkflowTaskWorkflowLogsPageWithoutPermission();
 	}
 
 	@Override
@@ -244,6 +285,68 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 		return testGetWorkflowLog_addWorkflowLog();
 	}
 
+	private void _assertNotFound(UnsafeRunnable<Exception> unsafeRunnable) {
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.portal.vulcan.internal.jaxrs.exception.mapper." +
+					"WebApplicationExceptionMapper",
+				LoggerTestUtil.WARN)) {
+
+			Problem.ProblemException problemException = Assert.assertThrows(
+				Problem.ProblemException.class, unsafeRunnable::run);
+
+			Problem problem = problemException.getProblem();
+
+			Assert.assertEquals("NOT_FOUND", problem.getStatus());
+		}
+	}
+
+	private void _testGetWorkflowInstanceWorkflowLogsPageWithoutPermission() {
+		_assertNotFound(
+			() -> _userWorkflowLogResource.getWorkflowInstanceWorkflowLogsPage(
+				_workflowInstance.getId(),
+				new String[] {WorkflowLog.Type.TASK_ASSIGN.getValue()},
+				Pagination.of(1, 2)));
+	}
+
+	private void _testGetWorkflowLogWithNonexistentWorkflowLogId() {
+		_assertNotFound(
+			() -> workflowLogResource.getWorkflowLog(
+				RandomTestUtil.randomLong()));
+	}
+
+	private void _testGetWorkflowLogWithoutPermission() throws Exception {
+		WorkflowLog workflowLog = testGetWorkflowLog_addWorkflowLog();
+
+		_assertNotFound(
+			() -> _userWorkflowLogResource.getWorkflowLog(workflowLog.getId()));
+	}
+
+	private void _testGetWorkflowLogWithPermission() throws Exception {
+		WorkflowInstanceLink workflowInstanceLink =
+			_workflowInstanceLinkLocalService.addWorkflowInstanceLink(
+				_user.getUserId(), testCompany.getCompanyId(),
+				testGroup.getGroupId(), ObjectReviewed.class.getName(),
+				_objectReviewed.getId(), _workflowInstance.getId());
+
+		WorkflowLog workflowLog = testGetWorkflowLog_addWorkflowLog();
+
+		WorkflowLog userWorkflowLog = _userWorkflowLogResource.getWorkflowLog(
+			workflowLog.getId());
+
+		Assert.assertEquals(workflowLog.getId(), userWorkflowLog.getId());
+
+		_workflowInstanceLinkLocalService.deleteWorkflowInstanceLink(
+			workflowInstanceLink);
+	}
+
+	private void _testGetWorkflowTaskWorkflowLogsPageWithoutPermission() {
+		_assertNotFound(
+			() -> _userWorkflowLogResource.getWorkflowTaskWorkflowLogsPage(
+				_workflowTask.getId(),
+				new String[] {WorkflowLog.Type.TASK_ASSIGN.getValue()},
+				Pagination.of(1, 2)));
+	}
+
 	private WorkflowLog _toAssignToHimselfWorkflowLog() throws Exception {
 		return new WorkflowLog() {
 			{
@@ -286,6 +389,8 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 	@Inject
 	private Language _language;
 
+	private ObjectReviewed _objectReviewed;
+
 	@Inject
 	private Portal _portal;
 
@@ -293,7 +398,13 @@ public class WorkflowLogResourceTest extends BaseWorkflowLogResourceTestCase {
 	private RoleLocalService _roleLocalService;
 
 	private Role _siteContentReviewerRole;
+	private User _user;
+	private WorkflowLogResource _userWorkflowLogResource;
 	private WorkflowInstance _workflowInstance;
+
+	@Inject
+	private WorkflowInstanceLinkLocalService _workflowInstanceLinkLocalService;
+
 	private WorkflowTask _workflowTask;
 
 	@Inject
