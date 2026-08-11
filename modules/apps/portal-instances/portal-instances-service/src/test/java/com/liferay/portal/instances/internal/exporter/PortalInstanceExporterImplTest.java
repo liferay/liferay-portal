95c618bee55f198ef4e172d74ad94a231cf5aac6
Jorge Avalos <jorge.avalos@liferay.com>
2026-08-11 11:07:39 -0700

    LPD-92619 Cover the portal instance export

---
+++ modules/apps/portal-instances/portal-instances-service/src/test/java/com/liferay/portal/instances/internal/exporter/PortalInstanceExporterImplTest.java
@@ -0,0 +1,366 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.instances.internal.exporter;
+
+import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.db.partition.util.DBPartitionUtil;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.sql.SQLException;
+
+import java.util.Collections;
+import java.util.Dictionary;
+
+import org.apache.felix.cm.file.ConfigurationHandler;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @author Jorge Avalos
+ */
+public class PortalInstanceExporterImplTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		_dbPartitionUtilMockedStatic = Mockito.mockStatic(
+			DBPartitionUtil.class);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			Collections.emptyMap()
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_portalInstanceExporterImpl, "_companyService", _companyService);
+		ReflectionTestUtil.setFieldValue(
+			_portalInstanceExporterImpl, "_groupLocalService",
+			_groupLocalService);
+	}
+
+	@After
+	public void tearDown() {
+		_dbPartitionUtilMockedStatic.close();
+	}
+
+	@Test
+	public void testExportPortalInstance() throws Exception {
+		String exportedPartitionName =
+			_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		Assert.assertEquals(
+			DBPartitionUtil.DATABASE_EXPORTED_PARTITION_SCHEMA_NAME_PREFIX +
+				_COMPANY_ID_1,
+			exportedPartitionName);
+
+		Mockito.verify(
+			_companyService
+		).exportCompany(
+			_COMPANY_ID_1
+		);
+	}
+
+	@Test
+	public void testExportPortalInstanceForCompanyScope() throws Exception {
+		String dictionaryString = _getDictionaryString(
+			ExtendedObjectClassDefinition.Scope.COMPANY, _COMPANY_ID_1);
+
+		_setUpConfigurations(dictionaryString);
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				_COMPANY_ID_1, _CONFIGURATION_ID, dictionaryString));
+	}
+
+	@Test
+	public void testExportPortalInstanceForEnabledDatabasePartitioning()
+		throws Exception {
+
+		try (SafeCloseable safeCloseable =
+				PropsValuesTestUtil.swapWithSafeCloseable(
+					"DATABASE_PARTITION_ENABLED", true)) {
+
+			_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+		}
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.getConfigurations(Mockito.anyLong()),
+			Mockito.never());
+	}
+
+	@Test
+	public void testExportPortalInstanceForFailedCompanyExport()
+		throws Exception {
+
+		_setUpConfigurations(
+			_getDictionaryString(
+				ExtendedObjectClassDefinition.Scope.COMPANY, _COMPANY_ID_1));
+
+		Mockito.when(
+			_companyService.exportCompany(_COMPANY_ID_1)
+		).thenThrow(
+			new PrincipalException.MustBeOmniadmin(RandomTestUtil.randomLong())
+		);
+
+		Assert.assertThrows(
+			PrincipalException.MustBeOmniadmin.class,
+			() -> _portalInstanceExporterImpl.exportPortalInstance(
+				_COMPANY_ID_1));
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForFailedConfigurationExport()
+		throws Exception {
+
+		String dictionaryString = _getDictionaryString(
+			ExtendedObjectClassDefinition.Scope.COMPANY, _COMPANY_ID_1);
+
+		_setUpConfigurations(dictionaryString);
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.exportConfiguration(
+				_COMPANY_ID_1, _CONFIGURATION_ID, dictionaryString)
+		).thenThrow(
+			new SQLException()
+		);
+
+		Assert.assertThrows(
+			SQLException.class,
+			() -> _portalInstanceExporterImpl.exportPortalInstance(
+				_COMPANY_ID_1));
+	}
+
+	@Test
+	public void testExportPortalInstanceForGroupScope() throws Exception {
+		_setUpGroup(_COMPANY_ID_1, _GROUP_ID);
+
+		String dictionaryString = _getDictionaryString(
+			ExtendedObjectClassDefinition.Scope.GROUP, _GROUP_ID);
+
+		_setUpConfigurations(dictionaryString);
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				_COMPANY_ID_1, _CONFIGURATION_ID, dictionaryString));
+	}
+
+	@Test
+	public void testExportPortalInstanceForMissingGroup() throws Exception {
+		Mockito.when(
+			_groupLocalService.fetchGroup(_GROUP_ID)
+		).thenReturn(
+			null
+		);
+
+		_setUpConfigurations(
+			_getDictionaryString(
+				ExtendedObjectClassDefinition.Scope.GROUP, _GROUP_ID));
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForNullDictionaryString()
+		throws Exception {
+
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			Collections.<String, String>singletonMap(_CONFIGURATION_ID, null)
+		);
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForOtherCompanyGroupScope()
+		throws Exception {
+
+		_setUpGroup(_COMPANY_ID_2, _GROUP_ID);
+		_setUpConfigurations(
+			_getDictionaryString(
+				ExtendedObjectClassDefinition.Scope.GROUP, _GROUP_ID));
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForOtherCompanyGroupWithCompanyScope()
+		throws Exception {
+
+		_setUpGroup(_COMPANY_ID_2, _GROUP_ID);
+		_setUpConfigurations(
+			_getDictionaryString(
+				HashMapDictionaryBuilder.<String, Object>put(
+					ExtendedObjectClassDefinition.Scope.COMPANY.
+						getPropertyKey(),
+					_COMPANY_ID_1
+				).put(
+					ExtendedObjectClassDefinition.Scope.GROUP.getPropertyKey(),
+					_GROUP_ID
+				).build()));
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForOtherCompanyScope()
+		throws Exception {
+
+		_setUpConfigurations(
+			_getDictionaryString(
+				ExtendedObjectClassDefinition.Scope.COMPANY, _COMPANY_ID_2));
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	@Test
+	public void testExportPortalInstanceForPortletInstanceScope()
+		throws Exception {
+
+		String dictionaryString = _getDictionaryString(
+			ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE,
+			RandomTestUtil.randomString());
+
+		_setUpConfigurations(dictionaryString);
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				_COMPANY_ID_1, _CONFIGURATION_ID, dictionaryString));
+	}
+
+	@Test
+	public void testExportPortalInstanceForSystemScope() throws Exception {
+		_setUpConfigurations(
+			_getDictionaryString(
+				RandomTestUtil.randomString(), RandomTestUtil.randomString()));
+
+		_portalInstanceExporterImpl.exportPortalInstance(_COMPANY_ID_1);
+
+		_assertNoConfigurationExported();
+	}
+
+	private void _assertNoConfigurationExported() {
+		_dbPartitionUtilMockedStatic.verify(
+			() -> DBPartitionUtil.exportConfiguration(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyString()),
+			Mockito.never());
+	}
+
+	private String _getDictionaryString(Dictionary<String, Object> dictionary)
+		throws Exception {
+
+		UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
+			new UnsyncByteArrayOutputStream();
+
+		ConfigurationHandler.write(unsyncByteArrayOutputStream, dictionary);
+
+		return new String(
+			unsyncByteArrayOutputStream.toByteArray(), StringPool.UTF8);
+	}
+
+	private String _getDictionaryString(
+			ExtendedObjectClassDefinition.Scope scope, Object scopePK)
+		throws Exception {
+
+		return _getDictionaryString(scope.getPropertyKey(), scopePK);
+	}
+
+	private String _getDictionaryString(String propertyKey, Object value)
+		throws Exception {
+
+		return _getDictionaryString(
+			HashMapDictionaryBuilder.<String, Object>put(
+				propertyKey, value
+			).build());
+	}
+
+	private void _setUpConfigurations(String dictionaryString) {
+		_dbPartitionUtilMockedStatic.when(
+			() -> DBPartitionUtil.getConfigurations(CompanyConstants.SYSTEM)
+		).thenReturn(
+			HashMapBuilder.put(
+				_CONFIGURATION_ID, dictionaryString
+			).build()
+		);
+	}
+
+	private void _setUpGroup(long companyId, long groupId) {
+		Group group = Mockito.mock(Group.class);
+
+		Mockito.when(
+			group.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		Mockito.when(
+			_groupLocalService.fetchGroup(groupId)
+		).thenReturn(
+			group
+		);
+	}
+
+	private static final long _COMPANY_ID_1 = RandomTestUtil.randomLong();
+
+	private static final long _COMPANY_ID_2 = RandomTestUtil.randomLong();
+
+	private static final String _CONFIGURATION_ID =
+		RandomTestUtil.randomString();
+
+	private static final long _GROUP_ID = RandomTestUtil.randomLong();
+
+	private final CompanyService _companyService = Mockito.mock(
+		CompanyService.class);
+	private MockedStatic<DBPartitionUtil> _dbPartitionUtilMockedStatic;
+	private final GroupLocalService _groupLocalService = Mockito.mock(
+		GroupLocalService.class);
+	private final PortalInstanceExporterImpl _portalInstanceExporterImpl =
+		new PortalInstanceExporterImpl();
+
+}
---
+++ modules/apps/portal-instances/portal-instances-web/src/test/java/com/liferay/portal/instances/web/internal/portlet/action/ExportInstanceMVCActionCommandTest.java
@@ -0,0 +1,243 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.instances.web.internal.portlet.action;
+
+import com.liferay.portal.instances.exporter.PortalInstanceExporter;
+import com.liferay.portal.kernel.exception.RequiredCompanyException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockActionRequest;
+import com.liferay.portal.kernel.test.portlet.MockActionResponse;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import jakarta.portlet.ActionRequest;
+import jakarta.portlet.ActionResponse;
+import jakarta.portlet.PortletRequest;
+
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @author Jorge Avalos
+ */
+public class ExportInstanceMVCActionCommandTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		Mockito.when(
+			_language.format(
+				Mockito.nullable(Locale.class), Mockito.anyString(),
+				Mockito.<Object>any())
+		).thenAnswer(
+			invocationOnMock ->
+				invocationOnMock.getArgument(1) + ":" +
+					invocationOnMock.getArgument(2)
+		);
+
+		_featureFlagManagerUtilMockedStatic = Mockito.mockStatic(
+			FeatureFlagManagerUtil.class);
+
+		_featureFlagManagerUtilMockedStatic.when(
+			() -> FeatureFlagManagerUtil.isEnabled(
+				Mockito.anyLong(), Mockito.eq("LPD-11342"))
+		).thenReturn(
+			true
+		);
+
+		_htmlUtilMockedStatic = Mockito.mockStatic(HtmlUtil.class);
+
+		_htmlUtilMockedStatic.when(
+			() -> HtmlUtil.escape(Mockito.anyString())
+		).thenAnswer(
+			invocationOnMock -> invocationOnMock.getArgument(0)
+		);
+
+		_jsonPortletResponseUtilMockedStatic = Mockito.mockStatic(
+			JSONPortletResponseUtil.class);
+
+		_jsonPortletResponseUtilMockedStatic.when(
+			() -> JSONPortletResponseUtil.writeJSON(
+				Mockito.any(ActionRequest.class),
+				Mockito.any(ActionResponse.class),
+				Mockito.any(JSONObject.class))
+		).then(
+			invocationOnMock -> {
+				_jsonObject = invocationOnMock.getArgument(2);
+
+				return null;
+			}
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_exportInstanceMVCActionCommand, "_language", _language);
+		ReflectionTestUtil.setFieldValue(
+			_exportInstanceMVCActionCommand, "_portalInstanceExporter",
+			_portalInstanceExporter);
+	}
+
+	@After
+	public void tearDown() {
+		_featureFlagManagerUtilMockedStatic.close();
+		_htmlUtilMockedStatic.close();
+		_jsonPortletResponseUtilMockedStatic.close();
+	}
+
+	@Test
+	public void testErrorForEscapedExceptionMessage() throws Exception {
+		_assertError(
+			new RequiredCompanyException(_MESSAGE),
+			"export-failed-with-message-x:" + _MESSAGE);
+
+		_htmlUtilMockedStatic.verify(() -> HtmlUtil.escape(_MESSAGE));
+	}
+
+	@Test
+	public void testErrorForMustBeOmniadminException() throws Exception {
+		PrincipalException.MustBeOmniadmin mustBeOmniadminException =
+			new PrincipalException.MustBeOmniadmin(_permissionChecker);
+
+		_assertError(
+			mustBeOmniadminException,
+			"export-failed-with-message-x:" +
+				GetterUtil.getString(mustBeOmniadminException.getMessage()));
+	}
+
+	@Test
+	public void testErrorForNullExceptionMessage() throws Exception {
+		_assertError(
+			new RequiredCompanyException(), "export-failed-with-message-x:");
+	}
+
+	@Test
+	public void testSchemaNameOnSuccess() throws Exception {
+		Mockito.when(
+			_portalInstanceExporter.exportPortalInstance(_COMPANY_ID)
+		).thenReturn(
+			_EXPORTED_PARTITION_NAME
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(_COMPANY_ID), new MockActionResponse());
+
+		Assert.assertEquals(
+			"the-instance-was-exported-to-the-schema-x:" +
+				_EXPORTED_PARTITION_NAME,
+			_jsonObject.getString("successMessage"));
+
+		Assert.assertFalse(_jsonObject.has("error"));
+
+		Mockito.verify(
+			_portalInstanceExporter
+		).exportPortalInstance(
+			_COMPANY_ID
+		);
+
+		_jsonPortletResponseUtilMockedStatic.verify(
+			() -> JSONPortletResponseUtil.writeJSON(
+				Mockito.any(ActionRequest.class),
+				Mockito.any(ActionResponse.class), Mockito.eq(_jsonObject)));
+	}
+
+	@Test
+	public void testUnsupportedOperationExceptionForDisabledFeatureFlag() {
+		_featureFlagManagerUtilMockedStatic.when(
+			() -> FeatureFlagManagerUtil.isEnabled(
+				Mockito.anyLong(), Mockito.eq("LPD-11342"))
+		).thenReturn(
+			false
+		);
+
+		Assert.assertThrows(
+			UnsupportedOperationException.class,
+			() -> _exportInstanceMVCActionCommand.doProcessAction(
+				_getMockActionRequest(_COMPANY_ID), new MockActionResponse()));
+
+		Mockito.verifyNoInteractions(_portalInstanceExporter);
+	}
+
+	private void _assertError(Exception exception, String expectedError)
+		throws Exception {
+
+		Mockito.when(
+			_portalInstanceExporter.exportPortalInstance(_COMPANY_ID)
+		).thenThrow(
+			exception
+		);
+
+		_exportInstanceMVCActionCommand.doProcessAction(
+			_getMockActionRequest(_COMPANY_ID), new MockActionResponse());
+
+		Assert.assertEquals(expectedError, _jsonObject.getString("error"));
+		Assert.assertFalse(_jsonObject.has("successMessage"));
+
+		_jsonPortletResponseUtilMockedStatic.verify(
+			() -> JSONPortletResponseUtil.writeJSON(
+				Mockito.any(ActionRequest.class),
+				Mockito.any(ActionResponse.class), Mockito.eq(_jsonObject)));
+	}
+
+	private MockActionRequest _getMockActionRequest(long companyId) {
+		MockActionRequest mockActionRequest = new MockActionRequest();
+
+		mockActionRequest.addParameter("companyId", String.valueOf(companyId));
+		mockActionRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, Mockito.mock(ThemeDisplay.class));
+
+		return mockActionRequest;
+	}
+
+	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
+
+	private static final String _EXPORTED_PARTITION_NAME =
+		RandomTestUtil.randomString();
+
+	private static final String _MESSAGE = RandomTestUtil.randomString();
+
+	private final ExportInstanceMVCActionCommand
+		_exportInstanceMVCActionCommand = new ExportInstanceMVCActionCommand() {
+
+			@Override
+			protected void hideDefaultSuccessMessage(
+				PortletRequest portletRequest) {
+			}
+
+		};
+
+	private MockedStatic<FeatureFlagManagerUtil>
+		_featureFlagManagerUtilMockedStatic;
+	private MockedStatic<HtmlUtil> _htmlUtilMockedStatic;
+	private JSONObject _jsonObject;
+	private MockedStatic<JSONPortletResponseUtil>
+		_jsonPortletResponseUtilMockedStatic;
+	private final Language _language = Mockito.mock(Language.class);
+	private final PermissionChecker _permissionChecker = Mockito.mock(
+		PermissionChecker.class);
+	private final PortalInstanceExporter _portalInstanceExporter = Mockito.mock(
+		PortalInstanceExporter.class);
+
+}
