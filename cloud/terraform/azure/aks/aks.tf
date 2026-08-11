d2935ef9b5ab9cd43b394acc78f439f2b07a7e14
Timothy Bell <timothy.bell@liferay.com>
2026-08-11 10:26:15 -0500

    LCD-52903 SF

--- cloud/terraform/azure/aks/aks.tf
+++ cloud/terraform/azure/aks/aks.tf
@@ -30,13 +30,13 @@ resource "azurerm_kubernetes_cluster" "main" {
 		only_critical_addons_enabled=true
 		os_disk_type="Ephemeral"
 		temporary_name_for_rotation="systemtmp"
-		vm_size=var.machine_type
-		vnet_subnet_id=azurerm_subnet.main.id
 		upgrade_settings {
 			drain_timeout_in_minutes=0
 			max_surge="10%"
 			node_soak_duration_in_minutes=0
 		}
+		vm_size=var.machine_type
+		vnet_subnet_id=azurerm_subnet.main.id
 	}
 	depends_on=[
 		azurerm_role_assignment.cluster_network_contributor,
