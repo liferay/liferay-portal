71169b17a3a9874fdbdeeb0a6afd52ca6ad0f475
Alan Huang <alan.huang@liferay.com>
2026-08-13 00:19:15 +0800

    LPD-97976 Improve regex

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaVirtualThreadsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaVirtualThreadsCheck.java
@@ -5,7 +5,11 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.tools.ToolsUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Alan Huang
@@ -17,30 +21,29 @@ public class JavaVirtualThreadsCheck extends BaseFileCheck {
 		return true;
 	}
 
+	@Override
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
 		_checkMethodCall(
-			fileName, content, "Executors.newVirtualThreadPerTaskExecutor");
-		_checkMethodCall(fileName, content, "Thread.ofVirtual");
-		_checkMethodCall(fileName, content, "Thread.startVirtualThread");
+			fileName, content, "Executors", "newVirtualThreadPerTaskExecutor");
+		_checkMethodCall(fileName, content, "Thread", "ofVirtual");
+		_checkMethodCall(fileName, content, "Thread", "startVirtualThread");
 
 		return content;
 	}
 
 	private void _checkMethodCall(
-		String fileName, String content, String methodCall) {
+		String fileName, String content, String className, String methodName) {
 
-		int x = -1;
+		Pattern pattern = Pattern.compile(
+			StringBundler.concat(
+				"\\b", className, "\\.\\s*", methodName, "\\b\\("));
 
-		while (true) {
-			x = content.indexOf(methodCall + "(", x + 1);
+		Matcher matcher = pattern.matcher(content);
 
-			if (x == -1) {
-				return;
-			}
-
-			if (ToolsUtil.isInsideQuotes(content, x)) {
+		while (matcher.find()) {
+			if (ToolsUtil.isInsideQuotes(content, matcher.start())) {
 				continue;
 			}
 
@@ -49,7 +52,7 @@ public class JavaVirtualThreadsCheck extends BaseFileCheck {
 				"Do not use virtual threads (e.g., Executors.newVirtualThread" +
 					"PerTaskExecutor(), Thread.ofVirtual(), Thread.start" +
 						"VirtualThread())",
-				getLineNumber(content, x));
+				getLineNumber(content, matcher.start()));
 		}
 	}
 
