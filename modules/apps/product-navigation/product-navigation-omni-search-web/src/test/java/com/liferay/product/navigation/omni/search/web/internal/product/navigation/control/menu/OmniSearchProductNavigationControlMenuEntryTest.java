0b8572f6358a6d38f474ec8ae6de41e9b0d0c084
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2026-08-12 11:22:26 +0200

    LPD-101797 Add unit tests for OmniSearchProductNavigationControlMenuEntry.isShow

---
+++ modules/apps/product-navigation/product-navigation-omni-search-web/src/test/java/com/liferay/product/navigation/omni/search/web/internal/product/navigation/control/menu/OmniSearchProductNavigationControlMenuEntryTest.java
@@ -0,0 +1,136 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.product.navigation.omni.search.web.internal.product.navigation.control.menu;
+
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Ambrín Chaudhary
+ */
+public class OmniSearchProductNavigationControlMenuEntryTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testIsShowWhenFeatureFlagLPD78171IsDisabled() throws Exception {
+		try (MockedStatic<FeatureFlagManagerUtil>
+				featureFlagManagerUtilMockedStatic = Mockito.mockStatic(
+					FeatureFlagManagerUtil.class)) {
+
+			featureFlagManagerUtilMockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-78171"))
+			).thenReturn(
+				false
+			);
+
+			Assert.assertFalse(
+				_omniSearchProductNavigationControlMenuEntry.isShow(
+					_getMockHttpServletRequest(Constants.VIEW, false)));
+		}
+	}
+
+	@Test
+	public void testIsShowWhenLayoutModeIsEdit() throws Exception {
+		try (MockedStatic<FeatureFlagManagerUtil>
+				featureFlagManagerUtilMockedStatic = Mockito.mockStatic(
+					FeatureFlagManagerUtil.class)) {
+
+			featureFlagManagerUtilMockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-78171"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertFalse(
+				_omniSearchProductNavigationControlMenuEntry.isShow(
+					_getMockHttpServletRequest(Constants.EDIT, true)));
+		}
+	}
+
+	@Test
+	public void testIsShowWhenUserIsNotSignedIn() throws Exception {
+		try (MockedStatic<FeatureFlagManagerUtil>
+				featureFlagManagerUtilMockedStatic = Mockito.mockStatic(
+					FeatureFlagManagerUtil.class)) {
+
+			featureFlagManagerUtilMockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-78171"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertFalse(
+				_omniSearchProductNavigationControlMenuEntry.isShow(
+					_getMockHttpServletRequest(Constants.VIEW, false)));
+		}
+	}
+
+	@Test
+	public void testIsShowWhenUserIsSignedIn() throws Exception {
+		try (MockedStatic<FeatureFlagManagerUtil>
+				featureFlagManagerUtilMockedStatic = Mockito.mockStatic(
+					FeatureFlagManagerUtil.class)) {
+
+			featureFlagManagerUtilMockedStatic.when(
+				() -> FeatureFlagManagerUtil.isEnabled(
+					Mockito.anyLong(), Mockito.eq("LPD-78171"))
+			).thenReturn(
+				true
+			);
+
+			Assert.assertTrue(
+				_omniSearchProductNavigationControlMenuEntry.isShow(
+					_getMockHttpServletRequest(Constants.VIEW, true)));
+		}
+	}
+
+	private MockHttpServletRequest _getMockHttpServletRequest(
+		String layoutMode, boolean signedIn) {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		ThemeDisplay themeDisplay = Mockito.mock(ThemeDisplay.class);
+
+		Mockito.when(
+			themeDisplay.isSignedIn()
+		).thenReturn(
+			signedIn
+		);
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		mockHttpServletRequest.setParameter("p_l_mode", layoutMode);
+
+		return mockHttpServletRequest;
+	}
+
+	private final OmniSearchProductNavigationControlMenuEntry
+		_omniSearchProductNavigationControlMenuEntry =
+			new OmniSearchProductNavigationControlMenuEntry();
+
+}
