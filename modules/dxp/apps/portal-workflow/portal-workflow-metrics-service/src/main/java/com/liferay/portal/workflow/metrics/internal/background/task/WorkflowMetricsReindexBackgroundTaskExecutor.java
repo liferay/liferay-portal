5de125faf815c3c99fdddbeb3ce3b914460c505d
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-08-12 12:45:10 -0300

    LPD-75968 portal-workflow-metrics-service: Remove obsolete index lifecycle block from WorkflowMetricsReindexBackgroundTaskExecutor
    https://liferay.atlassian.net/browse/LPD-75968

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/background/task/WorkflowMetricsReindexBackgroundTaskExecutor.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/background/task/WorkflowMetricsReindexBackgroundTaskExecutor.java
@@ -20,12 +20,8 @@ import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.security.auth.CompanyInheritableThreadLocalCallable;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.search.capabilities.SearchCapabilities;
-import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
-import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.workflow.metrics.internal.background.task.constants.WorkflowMetricsReindexBackgroundTaskConstants;
 import com.liferay.portal.workflow.metrics.internal.petra.executor.WorkflowMetricsPortalExecutor;
-import com.liferay.portal.workflow.metrics.internal.search.index.WorkflowMetricsIndex;
 import com.liferay.portal.workflow.metrics.search.background.task.WorkflowMetricsReindexStatusMessageSender;
 import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexer;
 import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexerRegistry;
@@ -74,15 +70,6 @@ public class WorkflowMetricsReindexBackgroundTaskExecutor
 
 		_workflowMetricsReindexStatusMessageSender.sendStatusMessage(
 			0, indexEntityNames.length, StringPool.BLANK);
-
-		for (String indexEntityName : indexEntityNames) {
-			WorkflowMetricsIndex workflowMetricsIndex =
-				WorkflowMetricsIndex.toWorkflowMetricsIndex(indexEntityName);
-
-			workflowMetricsIndex.deleteAllDocuments(
-				_searchCapabilities, _searchEngineAdapter, _indexNameBuilder,
-				backgroundTask.getCompanyId());
-		}
 
 		List<NoticeableFuture<?>> noticeableFutures = new ArrayList<>();
 
@@ -182,15 +169,6 @@ public class WorkflowMetricsReindexBackgroundTaskExecutor
 	@Reference
 	private BackgroundTaskStatusMessageSender
 		_backgroundTaskStatusMessageSender;
-
-	@Reference
-	private IndexNameBuilder _indexNameBuilder;
-
-	@Reference
-	private SearchCapabilities _searchCapabilities;
-
-	@Reference
-	private SearchEngineAdapter _searchEngineAdapter;
 
 	@Reference
 	private WorkflowMetricsPortalExecutor _workflowMetricsPortalExecutor;
