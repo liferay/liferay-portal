59e7c57d8b769ef1ff58dc3e79fc5bab8f5dc855
Jerry Adriano <ajerry550@gmail.com>
2026-08-13 13:37:35 -0300

    Revert "LPD-92565 Allow the classic-2 theme's package.json to use the theme build script"
    This reverts commit 92390d0036ab917ee18b2b7a9deb20786a43269a.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONPackageJSONCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSONPackageJSONCheck.java
@@ -72,7 +72,6 @@ public class JSONPackageJSONCheck extends BaseFileCheck {
 			absolutePath.endsWith("commerce-theme-speedwell/package.json") ||
 			absolutePath.endsWith("frontend-theme-admin/package.json") ||
 			absolutePath.endsWith("frontend-theme-ai-hub/package.json") ||
-			absolutePath.endsWith("frontend-theme-classic-2/package.json") ||
 			absolutePath.endsWith("frontend-theme-classic/package.json") ||
 			absolutePath.endsWith("frontend-theme-cms/package.json") ||
 			absolutePath.endsWith("frontend-theme-dialect/package.json") ||
