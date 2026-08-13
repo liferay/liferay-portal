2d502c522c309389c26a6ae7ae4980563ad1a73b
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2026-08-13 15:50:57 +0200

    LPD-101870 Add a test utility to delete the models a test adds
    Brian's review of the class name cleanup asked for a utility. Seven data cleanup preupgrade tests read a table before the test and delete whatever appeared in it afterwards, so the same idiom is spelled out once per test and per model. Each method here reads the table and returns a SafeCloseable that deletes the delta on close, which is the same contract as a database savepoint and the reason the methods are named after one.

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/data/cleanup/test/util/DataCleanupTestUtil.java
@@ -0,0 +1,75 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.data.cleanup.test.util;
+
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.ResourceAction;
+import com.liferay.portal.kernel.model.SystemEvent;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.ResourceActionLocalServiceUtil;
+import com.liferay.portal.kernel.service.SystemEventLocalServiceUtil;
+
+import java.util.List;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class DataCleanupTestUtil {
+
+	public static SafeCloseable setClassNamesSavepointWithSafeCloseable() {
+		List<ClassName> classNames = ClassNameLocalServiceUtil.getClassNames(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		return () -> {
+			for (ClassName className :
+					ClassNameLocalServiceUtil.getClassNames(
+						QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
+
+				if (!classNames.contains(className)) {
+					ClassNameLocalServiceUtil.deleteClassName(className);
+				}
+			}
+		};
+	}
+
+	public static SafeCloseable setResourceActionsSavepointWithSafeCloseable() {
+		List<ResourceAction> resourceActions =
+			ResourceActionLocalServiceUtil.getResourceActions(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		return () -> {
+			for (ResourceAction resourceAction :
+					ResourceActionLocalServiceUtil.getResourceActions(
+						QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
+
+				if (!resourceActions.contains(resourceAction)) {
+					ResourceActionLocalServiceUtil.deleteResourceAction(
+						resourceAction);
+				}
+			}
+		};
+	}
+
+	public static SafeCloseable setSystemEventsSavepointWithSafeCloseable() {
+		List<SystemEvent> systemEvents =
+			SystemEventLocalServiceUtil.getSystemEvents(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		return () -> {
+			for (SystemEvent systemEvent :
+					SystemEventLocalServiceUtil.getSystemEvents(
+						QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
+
+				if (!systemEvents.contains(systemEvent)) {
+					SystemEventLocalServiceUtil.deleteSystemEvent(systemEvent);
+				}
+			}
+		};
+	}
+
+}
