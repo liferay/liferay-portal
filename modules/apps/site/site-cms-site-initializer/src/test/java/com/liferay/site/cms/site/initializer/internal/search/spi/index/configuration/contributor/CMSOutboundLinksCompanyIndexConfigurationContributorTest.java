ff9c0615729b2df94b80482c69ebb34a0d7dc24a
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-08-13 09:16:44 +0200

    LPD-102076 Test the outbound links index mapping

---
+++ modules/apps/site/site-cms-site-initializer/src/test/java/com/liferay/site/cms/site/initializer/internal/search/spi/index/configuration/contributor/CMSOutboundLinksCompanyIndexConfigurationContributorTest.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.search.spi.index.configuration.contributor;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.spi.index.configuration.contributor.helper.MappingsHelper;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.site.cms.site.initializer.util.CMSOutboundLinksUtil;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class CMSOutboundLinksCompanyIndexConfigurationContributorTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testContributeMappings() {
+		CMSOutboundLinksCompanyIndexConfigurationContributor
+			cmsOutboundLinksCompanyIndexConfigurationContributor =
+				new CMSOutboundLinksCompanyIndexConfigurationContributor();
+
+		MappingsHelper mappingsHelper = Mockito.mock(MappingsHelper.class);
+
+		cmsOutboundLinksCompanyIndexConfigurationContributor.contributeMappings(
+			RandomTestUtil.randomLong(), mappingsHelper);
+
+		ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(
+			String.class);
+
+		Mockito.verify(
+			mappingsHelper
+		).putMappings(
+			argumentCaptor.capture()
+		);
+
+		Assert.assertEquals(
+			StringBundler.concat(
+				"{\"properties\":{\"", CMSOutboundLinksUtil.FIELD_NAME,
+				"\":{\"type\":\"keyword\"}}}"),
+			StringUtil.removeChars(
+				argumentCaptor.getValue(), CharPool.NEW_LINE, CharPool.RETURN,
+				CharPool.SPACE, CharPool.TAB));
+	}
+
+}
