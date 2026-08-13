599b5014ffff19476f70b7af5d41995fe389d34b
Timothy Bell <timothy.bell@liferay.com>
2026-08-13 12:12:34 -0500

    LCD-53003 Add cloud-agnostic api

--- cloud/terraform/azure/platform/argocd.tf
+++ cloud/terraform/azure/platform/argocd.tf
@@ -2,6 +2,6 @@ module "argocd" {
 	argocd_admin_login_enabled=var.argocd_admin_login_enabled
 	argocd_external_access_config=var.argocd_external_access_config
 	argocd_helm_chart_version=var.argocd_helm_chart_version
-	infrastructure_api_group="azure.liferay.com"
+	infrastructure_api_group="liferay.com"
 	source="../../modules/argocd"
 }
