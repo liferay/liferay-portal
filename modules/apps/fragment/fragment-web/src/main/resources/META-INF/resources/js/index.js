886ba2355e7e4419bbc79cb8c9904e5e3bacc4b7
Thiago Buarque <thiago.buarque@liferay.com>
2026-08-13 08:24:08 -0300

    LPD-98891 Register fragment sets as a design library resource type

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+export {default as AddFragmentDesignLibraryModalContent} from './AddFragmentDesignLibraryModalContent';
 export {default as ContributedFragmentEntryDropdownPropsTransformer} from './ContributedFragmentEntryDropdownPropsTransformer';
 export {default as FormFragmentsConfiguration} from './FormFragmentsConfiguration';
 export {default as FragmentCollectionDropdownPropsTransformer} from './FragmentCollectionDropdownPropsTransformer';
