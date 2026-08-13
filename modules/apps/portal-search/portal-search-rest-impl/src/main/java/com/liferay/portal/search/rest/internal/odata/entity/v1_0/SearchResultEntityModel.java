c8ed0a19d645d4b3ecbc0fb72f8690a005dd3a5e
Thiago Buarque <thiago.buarque@liferay.com>
2026-08-13 08:38:25 -0300

    LPD-98892 Expose type as a filterable field in the search entity model

--- modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/odata/entity/v1_0/SearchResultEntityModel.java
+++ modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/odata/entity/v1_0/SearchResultEntityModel.java
@@ -125,7 +125,8 @@ public class SearchResultEntityModel implements EntityModel {
 							LocaleUtil.toLanguageId(locale)));
 
 					return sortableFieldName.concat(".keyword_lowercase");
-				}));
+				}),
+			new StringEntityField("type", locale -> "type"));
 	}
 
 	@Override
