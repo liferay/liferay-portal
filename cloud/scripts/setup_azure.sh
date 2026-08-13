e0811489e082eed7caf8b2473069a6afe3f4c07d
Timothy Bell <timothy.bell@liferay.com>
2026-08-13 09:14:54 -0500

    LCD-52741 Add an underscore prefix to the sourced-only script

--- cloud/scripts/setup_azure.sh
+++ cloud/scripts/setup_azure.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-source "$(dirname "${BASH_SOURCE[0]}")/azure_common.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/_azure_common.sh"
 
 function main {
 	if [ ${#} -eq 0 ]
