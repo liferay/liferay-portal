aff551d349b793fb1f7b233eea0d36caba881c8e
Timothy Bell <timothy.bell@liferay.com>
2026-08-14 12:17:26 -0500

    LCD-53016 SF

--- cloud/terraform/azure/platform/outputs.tf
+++ cloud/terraform/azure/platform/outputs.tf
@@ -1,8 +1,8 @@
-output "deployment_context" {
-	value=local.deployment_context
-}
 output "cluster_secret_store_provider" {
 	value=local.cluster_secret_store_provider
+}
+output "deployment_context" {
+	value=local.deployment_context
 }
 output "external_secrets_client_id" {
 	value=azurerm_user_assigned_identity.external_secrets.client_id
