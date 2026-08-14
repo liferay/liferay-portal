4c51d90f2b23cbe0608f9f5e2932d0be20c36a57
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-08-14 09:59:58 +0200

    LPD-102498 Drop the unused commerce field metadata

--- modules/apps/analytics/analytics-settings-rest-api/src/main/java/com/liferay/analytics/settings/rest/constants/FieldOrderConstants.java
+++ modules/apps/analytics/analytics-settings-rest-api/src/main/java/com/liferay/analytics/settings/rest/constants/FieldOrderConstants.java
@@ -10,37 +10,11 @@ package com.liferay.analytics.settings.rest.constants;
  */
 public class FieldOrderConstants {
 
-	public static final String[] FIELD_ORDER_EXAMPLES = {
-		"AB-34098-789-N", "30130", "30130", "2017-07-21", "USD",
-		"key1=value1, key2=value2, ...", "AB-34098-789-N", "30130",
-		"2017-08-21", "2017-07-21", "[item, item,...]", "0", "AB-34098-789-N",
-		"30130", "paypal", "0", "0", "113", "12345"
-	};
-
-	public static final String[] FIELD_ORDER_ITEM_EXAMPLES = {
-		"12345", "2017-07-21", "key1=value1, key2=value2, ...",
-		"AB-34098-789-N", "200", "30130", "2017-07-21", "Hand Saw", "", "30128",
-		"30128", "2", "12341234", "True", "pc", "101", "12345"
-	};
-
 	public static final String[] FIELD_ORDER_ITEM_NAMES = {
 		"cpDefinitionId", "createDate", "customFields", "externalReferenceCode",
 		"finalPrice", "id", "modifiedDate", "name", "options", "orderId",
 		"parentOrderItemId", "quantity", "sku", "subscription", "unitOfMeasure",
 		"unitPrice", "userId"
-	};
-
-	public static final String[] FIELD_ORDER_ITEM_REQUIRED_NAMES = {
-		"cpDefinitionId", "createDate", "customFields", "externalReferenceCode",
-		"finalPrice", "id", "modifiedDate", "name", "options", "orderId",
-		"parentOrderItemId", "quantity", "sku", "subscription", "unitOfMeasure",
-		"unitPrice", "userId"
-	};
-
-	public static final String[] FIELD_ORDER_ITEM_TYPES = {
-		"Integer", "String", "Object", "String", "Number", "Integer", "String",
-		"Object", "String", "Integer", "Integer", "Integer", "String",
-		"Boolean", "String", "Number", "Integer"
 	};
 
 	public static final String[] FIELD_ORDER_NAMES = {
@@ -49,20 +23,6 @@ public class FieldOrderConstants {
 		"modifiedDate", "orderDate", "orderItems", "orderStatus",
 		"orderTypeExternalReferenceCode", "orderTypeId", "paymentMethod",
 		"paymentStatus", "status", "total", "userId"
-	};
-
-	public static final String[] FIELD_ORDER_REQUIRED_NAMES = {
-		"accountExternalReferenceCode", "accountId", "channelId", "createDate",
-		"currencyCode", "customFields", "externalReferenceCode", "id",
-		"modifiedDate", "orderDate", "orderItems", "orderStatus",
-		"orderTypeExternalReferenceCode", "orderTypeId", "paymentMethod",
-		"paymentStatus", "status", "total", "userId"
-	};
-
-	public static final String[] FIELD_ORDER_TYPES = {
-		"String", "Integer", "Integer", "String", "String", "Object", "String",
-		"Integer", "String", "String", "Array", "Integer", "String", "Integer",
-		"String", "Integer", "Integer", "BigDecimal", "Integer"
 	};
 
 }
--- modules/apps/analytics/analytics-settings-rest-api/src/main/java/com/liferay/analytics/settings/rest/constants/FieldProductConstants.java
+++ modules/apps/analytics/analytics-settings-rest-api/src/main/java/com/liferay/analytics/settings/rest/constants/FieldProductConstants.java
@@ -10,52 +10,12 @@ package com.liferay.analytics.settings.rest.constants;
  */
 public class FieldProductConstants {
 
-	public static final String[] FIELD_CATEGORY_EXAMPLES = {
-		"AB-34098-789-N", "30130", "Some Category", "12345",
-		"Default Vocabulary"
-	};
-
 	public static final String[] FIELD_CATEGORY_NAMES = {
 		"externalReferenceCode", "id", "name", "siteId", "vocabulary"
 	};
 
-	public static final String[] FIELD_CATEGORY_REQUIRED_NAMES = {
-		"externalReferenceCode", "id", "name", "siteId", "vocabulary"
-	};
-
-	public static final String[] FIELD_CATEGORY_TYPES = {
-		"String", "Integer", "String", "Integer", "String"
-	};
-
-	public static final String[] FIELD_PRODUCT_CHANNEL_EXAMPLES = {
-		"EUR", "EU-1234", "12345", "European Mobile Channel", "site"
-	};
-
 	public static final String[] FIELD_PRODUCT_CHANNEL_NAMES = {
 		"currencyCode", "externalReferenceCode", "id", "name", "type"
-	};
-
-	public static final String[] FIELD_PRODUCT_CHANNEL_REQUIRED_NAMES = {
-		"currencyCode", "externalReferenceCode", "id", "name", "type"
-	};
-
-	public static final String[] FIELD_PRODUCT_CHANNEL_TYPES = {
-		"String", "String", "Integer", "String", "String"
-	};
-
-	public static final String[] FIELD_PRODUCT_EXAMPLES = {
-		"30054", "12345, 12346, ...", "13.5", "2017-07-21",
-		"key1=value1,key2=value2,...",
-		"Professional hand stainless steel saw for wood. Made to last and " +
-			"saw forever. Made of best steel",
-		"false", "2017-07-21", "2017-08-21", "2018-08-21", "AB-34098-789-N",
-		"AB-34098-789-N", "30130", "OptionA", "AB-34098-789-N",
-		"Meta description EN", "Meta keyword EN", "Meta title EN", "2017-08-21",
-		"Hand Saw", "12345, 12346, ...", "optiona", "12345, 12346, ...",
-		"30000", "ProductOption, ProductOption, ...",
-		"ProductSpecification, ProductSpecification, ...", "simple", "true",
-		"true", "tt12345", "tt12345, tt34556", "quantity", "0", "false",
-		"tag1, tag2, ...", "product-url-us, ...", "10pz", "Values EN"
 	};
 
 	public static final String[] FIELD_PRODUCT_NAMES = {
@@ -68,27 +28,6 @@ public class FieldProductConstants {
 		"productSpecifications", "productType", "published", "purchasable",
 		"sku", "skus", "specificationKey", "status", "subscriptionEnabled",
 		"tags", "urls", "value", "values"
-	};
-
-	public static final String[] FIELD_PRODUCT_REQUIRED_NAMES = {
-		"catalogId", "categoryIds", "cost", "createDate", "customFields",
-		"description", "discontinued", "displayDate", "expirationDate",
-		"expirationDate", "externalReferenceCode", "gtin", "id", "key",
-		"manufacturerPartNumber", "metaDescription", "metaKeyword", "metaTitle",
-		"modifiedDate", "name", "optionCategoryId", "optionKey",
-		"productChannelIds", "productId", "productOptions",
-		"productSpecifications", "productType", "published", "purchasable",
-		"sku", "skus", "specificationKey", "status", "subscriptionEnabled",
-		"tags", "urls", "value", "values"
-	};
-
-	public static final String[] FIELD_PRODUCT_TYPES = {
-		"Integer", "Array", "Number", "String", "Object", "Object", "Boolean",
-		"String", "String", "String", "String", "String", "Integer", "String",
-		"String", "Object", "Object", "Object", "String", "Object", "Integer",
-		"String", "Array", "Integer", "Array", "Array", "String", "Boolean",
-		"Boolean", "String", "Array", "String", "Integer", "boolean", "Array",
-		"Object", "String", "Object"
 	};
 
 }
