043b607caa73000166d1f074cb16551a1e8b1e44
chestofwonders <miguelarroyoes@hotmail.com>
2026-08-14 11:27:59 +0200

    LPD-102224 Reject unregistered custom attributes in audience criteria
    An audience author only needs MANAGE_AUDIENCES_ENTRIES to persist a
    custom: attribute pointing at an arbitrary URL, which the client later
    imports and executes for every visitor.

--- modules/apps/audiences/audiences-service/src/main/java/com/liferay/audiences/service/impl/AudiencesEntryLocalServiceImpl.java
+++ modules/apps/audiences/audiences-service/src/main/java/com/liferay/audiences/service/impl/AudiencesEntryLocalServiceImpl.java
@@ -5,22 +5,33 @@
 
 package com.liferay.audiences.service.impl;
 
+import com.liferay.audiences.criteria.AudiencesCriteria;
+import com.liferay.audiences.criteria.AudiencesCriteriaProvider;
+import com.liferay.audiences.criteria.AudiencesCriteriaType;
+import com.liferay.audiences.exception.AudiencesEntryAttributeException;
 import com.liferay.audiences.exception.AudiencesEntryJSONException;
 import com.liferay.audiences.exception.AudiencesEntryNameException;
 import com.liferay.audiences.model.AudiencesEntry;
 import com.liferay.audiences.service.base.AudiencesEntryLocalServiceBaseImpl;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
 import com.liferay.portal.json.validator.JSONValidator;
 import com.liferay.portal.json.validator.JSONValidatorException;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -40,14 +51,14 @@ public class AudiencesEntryLocalServiceImpl
 			String externalReferenceCode, long userId, String json, String name)
 		throws PortalException {
 
-		_validate(json, name);
+		User user = _userLocalService.getUser(userId);
+
+		_validate(user.getCompanyId(), json, name);
 
 		AudiencesEntry audiencesEntry = audiencesEntryPersistence.create(
 			counterLocalService.increment());
 
 		audiencesEntry.setExternalReferenceCode(externalReferenceCode);
-
-		User user = _userLocalService.getUser(userId);
 
 		audiencesEntry.setCompanyId(user.getCompanyId());
 		audiencesEntry.setUserId(user.getUserId());
@@ -108,10 +119,10 @@ public class AudiencesEntryLocalServiceImpl
 			String json, String name)
 		throws PortalException {
 
-		_validate(json, name);
-
 		AudiencesEntry audiencesEntry =
 			audiencesEntryPersistence.findByPrimaryKey(audiencesEntryId);
+
+		_validate(audiencesEntry.getCompanyId(), json, name);
 
 		audiencesEntry.setExternalReferenceCode(externalReferenceCode);
 
@@ -126,7 +137,9 @@ public class AudiencesEntryLocalServiceImpl
 		return audiencesEntryPersistence.update(audiencesEntry);
 	}
 
-	private void _validate(String json, String name) throws PortalException {
+	private void _validate(long companyId, String json, String name)
+		throws PortalException {
+
 		try {
 			_criteriaJSONValidator.validate(json);
 		}
@@ -138,6 +151,59 @@ public class AudiencesEntryLocalServiceImpl
 		if (Validator.isNull(name)) {
 			throw new AudiencesEntryNameException();
 		}
+
+		_validateAttributes(companyId, json);
+	}
+
+	private void _validateAttributes(
+			JSONObject jsonObject, Set<String> validAttributes)
+		throws PortalException {
+
+		JSONArray rulesJSONArray = jsonObject.getJSONArray("rules");
+
+		if (rulesJSONArray == null) {
+			String attribute = jsonObject.getString("attribute");
+
+			if (attribute.startsWith("custom:") &&
+				!validAttributes.contains(attribute)) {
+
+				throw new AudiencesEntryAttributeException(
+					StringBundler.concat(
+						"Attribute \"", attribute,
+						"\" is not a valid custom attribute"));
+			}
+
+			return;
+		}
+
+		for (int i = 0; i < rulesJSONArray.length(); i++) {
+			_validateAttributes(
+				rulesJSONArray.getJSONObject(i), validAttributes);
+		}
+	}
+
+	private void _validateAttributes(long companyId, String json)
+		throws PortalException {
+
+		if (Validator.isNull(json)) {
+			return;
+		}
+
+		Set<String> validAttributes = new HashSet<>();
+
+		for (AudiencesCriteriaType audiencesCriteriaType :
+				_audiencesCriteriaProvider.getAudiencesCriteriaTypes(
+					companyId, LocaleUtil.getSiteDefault())) {
+
+			for (AudiencesCriteria audiencesCriteria :
+					audiencesCriteriaType.getAudiencesCriterias()) {
+
+				validAttributes.add(audiencesCriteria.getKey());
+			}
+		}
+
+		_validateAttributes(
+			_jsonFactory.createJSONObject(json), validAttributes);
 	}
 
 	private static final JSONValidator _criteriaJSONValidator =
@@ -146,7 +212,13 @@ public class AudiencesEntryLocalServiceImpl
 				"dependencies/audiences-criteria-json-schema.json"));
 
 	@Reference
+	private AudiencesCriteriaProvider _audiencesCriteriaProvider;
+
+	@Reference
 	private CustomSQL _customSQL;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private UserLocalService _userLocalService;
