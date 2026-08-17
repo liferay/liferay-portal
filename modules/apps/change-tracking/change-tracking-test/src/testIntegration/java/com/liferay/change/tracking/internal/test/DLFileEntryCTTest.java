166e02c71daaa2d502437952aad6c5ff50efe83f
Kiana Suetani <kiana.suetani@liferay.com>
2026-08-17 12:21:20 -0700

    LPD-103182 Enable automaticFriendlyURLConflictResolutionEnabled around conflict resolution assertions in DLFileEntryCTTest

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/DLFileEntryCTTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/DLFileEntryCTTest.java
@@ -6,6 +6,7 @@
 package com.liferay.change.tracking.internal.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.configuration.CTSettingsConfiguration;
 import com.liferay.change.tracking.conflict.ConflictInfo;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.model.CTEntry;
@@ -25,6 +26,7 @@ import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.constants.FriendlyURLResolverConstants;
@@ -39,6 +41,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
@@ -99,15 +102,26 @@ public class DLFileEntryCTTest {
 
 		_addFileEntry(fileName, title);
 
-		Map<Long, List<ConflictInfo>> conflictInfosMap =
-			_ctCollectionLocalService.checkConflicts(_ctCollection);
+		try (CompanyConfigurationTemporarySwapper
+				companyConfigurationTemporarySwapper =
+					new CompanyConfigurationTemporarySwapper(
+						TestPropsValues.getCompanyId(),
+						CTSettingsConfiguration.class.getName(),
+						HashMapDictionaryBuilder.<String, Object>put(
+							"automaticFriendlyURLConflictResolutionEnabled",
+							true
+						).build())) {
 
-		List<ConflictInfo> conflictInfos = conflictInfosMap.get(
-			_classNameLocalService.getClassNameId(
-				FriendlyURLEntryLocalization.class));
+			Map<Long, List<ConflictInfo>> conflictInfosMap =
+				_ctCollectionLocalService.checkConflicts(_ctCollection);
 
-		for (ConflictInfo conflictInfo : conflictInfos) {
-			Assert.assertTrue(conflictInfo.isResolved());
+			List<ConflictInfo> conflictInfos = conflictInfosMap.get(
+				_classNameLocalService.getClassNameId(
+					FriendlyURLEntryLocalization.class));
+
+			for (ConflictInfo conflictInfo : conflictInfos) {
+				Assert.assertTrue(conflictInfo.isResolved());
+			}
 		}
 	}
 
@@ -245,15 +259,16 @@ public class DLFileEntryCTTest {
 					RandomTestUtil.randomLong(), urlTitle, serviceContext);
 		}
 
-		CTCollection ctCollection2 = _ctCollectionLocalService.addCTCollection(
-			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
-			0, "LPD-87156-pub2", null);
+		CTCollection conflictCTCollection =
+			_ctCollectionLocalService.addCTCollection(
+				null, TestPropsValues.getCompanyId(),
+				TestPropsValues.getUserId(), 0, "LPD-87156-pub2", null);
 
 		FriendlyURLEntry friendlyURLEntry2;
 
 		try (SafeCloseable safeCloseable =
 				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
-					ctCollection2.getCtCollectionId())) {
+					conflictCTCollection.getCtCollectionId())) {
 
 			friendlyURLEntry2 =
 				_friendlyURLEntryLocalService.addFriendlyURLEntry(
@@ -261,23 +276,37 @@ public class DLFileEntryCTTest {
 					RandomTestUtil.randomLong(), urlTitle, serviceContext);
 		}
 
-		_ctProcessLocalService.addCTProcess(
-			_ctCollection.getUserId(), _ctCollection.getCtCollectionId());
+		try (CompanyConfigurationTemporarySwapper
+				companyConfigurationTemporarySwapper =
+					new CompanyConfigurationTemporarySwapper(
+						TestPropsValues.getCompanyId(),
+						CTSettingsConfiguration.class.getName(),
+						HashMapDictionaryBuilder.<String, Object>put(
+							"automaticFriendlyURLConflictResolutionEnabled",
+							true
+						).build())) {
 
-		_ctProcessLocalService.addCTProcess(
-			ctCollection2.getUserId(), ctCollection2.getCtCollectionId());
+			_ctProcessLocalService.addCTProcess(
+				_ctCollection.getUserId(), _ctCollection.getCtCollectionId());
 
-		friendlyURLEntry1 = _friendlyURLEntryLocalService.fetchFriendlyURLEntry(
-			friendlyURLEntry1.getFriendlyURLEntryId());
-		friendlyURLEntry2 = _friendlyURLEntryLocalService.fetchFriendlyURLEntry(
-			friendlyURLEntry2.getFriendlyURLEntryId());
+			_ctProcessLocalService.addCTProcess(
+				conflictCTCollection.getUserId(),
+				conflictCTCollection.getCtCollectionId());
 
-		Assert.assertEquals(
-			urlTitle, friendlyURLEntry1.getUrlTitle(languageId));
-		Assert.assertEquals(
-			urlTitle + "-1", friendlyURLEntry2.getUrlTitle(languageId));
+			friendlyURLEntry1 =
+				_friendlyURLEntryLocalService.fetchFriendlyURLEntry(
+					friendlyURLEntry1.getFriendlyURLEntryId());
+			friendlyURLEntry2 =
+				_friendlyURLEntryLocalService.fetchFriendlyURLEntry(
+					friendlyURLEntry2.getFriendlyURLEntryId());
 
-		_ctCollectionLocalService.deleteCTCollection(ctCollection2);
+			Assert.assertEquals(
+				urlTitle, friendlyURLEntry1.getUrlTitle(languageId));
+			Assert.assertEquals(
+				urlTitle + "-1", friendlyURLEntry2.getUrlTitle(languageId));
+		}
+
+		_ctCollectionLocalService.deleteCTCollection(conflictCTCollection);
 	}
 
 	@Test
