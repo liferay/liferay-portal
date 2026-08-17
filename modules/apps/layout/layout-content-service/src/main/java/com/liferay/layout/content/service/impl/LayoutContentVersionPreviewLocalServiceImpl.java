fd300891719f04b426a0af5c400b84cf7ab07f23
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-08-17 13:38:39 +0200

    LPD-90030 Implement LayoutContentVersionPreviewLocalService

--- modules/apps/layout/layout-content-service/src/main/java/com/liferay/layout/content/service/impl/LayoutContentVersionPreviewLocalServiceImpl.java
+++ modules/apps/layout/layout-content-service/src/main/java/com/liferay/layout/content/service/impl/LayoutContentVersionPreviewLocalServiceImpl.java
@@ -5,10 +5,19 @@
 
 package com.liferay.layout.content.service.impl;
 
+import com.liferay.layout.content.model.LayoutContentVersion;
+import com.liferay.layout.content.model.LayoutContentVersionPreview;
 import com.liferay.layout.content.service.base.LayoutContentVersionPreviewLocalServiceBaseImpl;
+import com.liferay.layout.content.service.persistence.LayoutContentVersionPersistence;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -19,4 +28,71 @@ import org.osgi.service.component.annotations.Component;
 )
 public class LayoutContentVersionPreviewLocalServiceImpl
 	extends LayoutContentVersionPreviewLocalServiceBaseImpl {
+
+	@Override
+	public LayoutContentVersionPreview addLayoutContentVersionPreview(
+			long userId, long layoutContentVersionId, String html,
+			String languageId, String segmentsExperienceERC)
+		throws PortalException {
+
+		LayoutContentVersion layoutContentVersion = _checkFeatureFlagEnabled(
+			layoutContentVersionId);
+
+		LayoutContentVersionPreview layoutContentVersionPreview =
+			layoutContentVersionPreviewPersistence.create(
+				counterLocalService.increment(
+					LayoutContentVersionPreview.class.getName()));
+
+		layoutContentVersionPreview.setGroupId(
+			layoutContentVersion.getGroupId());
+		layoutContentVersionPreview.setCompanyId(
+			layoutContentVersion.getCompanyId());
+		layoutContentVersionPreview.setUserId(userId);
+
+		User user = _userLocalService.getUser(userId);
+
+		layoutContentVersionPreview.setUserName(user.getFullName());
+
+		layoutContentVersionPreview.setLayoutContentVersionId(
+			layoutContentVersionId);
+		layoutContentVersionPreview.setHtml(html);
+		layoutContentVersionPreview.setLanguageId(languageId);
+		layoutContentVersionPreview.setSegmentsExperienceERC(
+			segmentsExperienceERC);
+
+		return layoutContentVersionPreviewPersistence.update(
+			layoutContentVersionPreview);
+	}
+
+	@Override
+	public void deleteLayoutContentVersionPreviews(
+		long layoutContentVersionId) {
+
+		layoutContentVersionPreviewPersistence.removeByLayoutContentVersionId(
+			layoutContentVersionId);
+	}
+
+	@Override
+	public LayoutContentVersionPreview fetchLayoutContentVersionPreview(
+		long layoutContentVersionId, String languageId,
+		String segmentsExperienceERC) {
+
+		return layoutContentVersionPreviewPersistence.fetchByLCVI_L_SEERC(
+			layoutContentVersionId, languageId, segmentsExperienceERC);
+	}
+
+	@Override
+	public List<LayoutContentVersionPreview> getLayoutContentVersionPreviews(
+		long layoutContentVersionId) {
+
+		return layoutContentVersionPreviewPersistence.
+			findByLayoutContentVersionId(layoutContentVersionId);
+	}
+
+	@Reference
+	private LayoutContentVersionPersistence _layoutContentVersionPersistence;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
