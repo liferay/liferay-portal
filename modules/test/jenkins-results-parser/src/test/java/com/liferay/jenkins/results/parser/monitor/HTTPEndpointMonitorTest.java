af0eb1397f8fd459b9bee94c77eb2a54b058ceba
Brittney Nguyen <brittney.nguyen@liferay.com>
2026-08-16 22:24:19 -0700

    LRCI-7966 Add HTTP endpoint monitor tests

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/monitor/HTTPEndpointMonitorTest.java
@@ -0,0 +1,290 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.monitor;
+
+import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
+import com.liferay.jenkins.results.parser.RandomTestUtil;
+import com.liferay.jenkins.results.parser.UrlReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.net.SocketTimeoutException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Brittney Nguyen
+ */
+public class HTTPEndpointMonitorTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testExecuteLatencyMaximum() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderOutput(
+			_MILLIS_LATENCY, RandomTestUtil.randomString(), _URL, urlReader);
+
+		Properties monitorProperties = _newMonitorProperties();
+
+		monitorProperties.setProperty(
+			"monitor[a].threshold[latency.maximum.millis]", "1");
+
+		MonitorResult monitorResult = _execute(monitorProperties);
+
+		testEquals(MonitorResult.Status.CRITICAL, monitorResult.getStatus());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		testEquals(
+			JenkinsResultsParserUtil.combine(
+				"Endpoint ", _URL, " responded in ",
+				metrics.get("latency.millis"),
+				" ms, exceeding its maximum latency of 1 ms"),
+			monitorResult.getMessage());
+	}
+
+	@Test
+	public void testExecuteLatencyMaximumWithinBound() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderOutput(RandomTestUtil.randomString(), _URL, urlReader);
+
+		Properties monitorProperties = _newMonitorProperties();
+
+		monitorProperties.setProperty(
+			"monitor[a].threshold[latency.maximum.millis]", "60000");
+
+		MonitorResult monitorResult = _execute(monitorProperties);
+
+		testEquals(MonitorResult.Status.OK, monitorResult.getStatus());
+	}
+
+	@Test
+	public void testExecuteLatencyMaximumWithoutThreshold() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderOutput(
+			_MILLIS_LATENCY, RandomTestUtil.randomString(), _URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.OK, monitorResult.getStatus());
+	}
+
+	@Test
+	public void testExecuteMissingFailureMessage() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderException(new IOException(), _URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.CRITICAL, monitorResult.getStatus());
+		testEquals(
+			JenkinsResultsParserUtil.combine(
+				"Unable to read ", _URL, ": java.io.IOException"),
+			monitorResult.getMessage());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		Assert.assertTrue(metrics.isEmpty());
+	}
+
+	@Test
+	public void testExecuteNotFound() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderException(new FileNotFoundException(_URL), _URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.CRITICAL, monitorResult.getStatus());
+		testEquals(
+			JenkinsResultsParserUtil.combine(
+				"Endpoint ", _URL, " was not found"),
+			monitorResult.getMessage());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		Assert.assertTrue(metrics.isEmpty());
+	}
+
+	@Test
+	public void testExecuteOK() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderOutput(RandomTestUtil.randomString(), _URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.OK, monitorResult.getStatus());
+		testEquals(
+			JenkinsResultsParserUtil.combine("Endpoint ", _URL, " is OK"),
+			monitorResult.getMessage());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		Assert.assertNotNull(metrics.get("latency.millis"));
+
+		verifyUrlReaderRead(false, 54000, urlReader);
+	}
+
+	@Test
+	public void testExecuteResponseCode() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderException(
+			new IOException(
+				"Server returned HTTP response code: 503 for URL: " + _URL),
+			_URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.CRITICAL, monitorResult.getStatus());
+		testEquals(
+			JenkinsResultsParserUtil.combine(
+				"Endpoint ", _URL, " returned the response code 503"),
+			monitorResult.getMessage());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		Assert.assertTrue(metrics.isEmpty());
+	}
+
+	@Test
+	public void testExecuteTimeout() throws Exception {
+		UrlReader urlReader = mockUrlReader();
+
+		setUrlReaderException(
+			new SocketTimeoutException("Read timed out"), _URL, urlReader);
+
+		MonitorResult monitorResult = _execute(_newMonitorProperties());
+
+		testEquals(MonitorResult.Status.CRITICAL, monitorResult.getStatus());
+		testEquals(
+			JenkinsResultsParserUtil.combine(
+				"Unable to read ", _URL, ": Read timed out"),
+			monitorResult.getMessage());
+
+		Map<String, String> metrics = monitorResult.getMetrics();
+
+		Assert.assertTrue(metrics.isEmpty());
+	}
+
+	@Test
+	public void testHTTPEndpointMonitor() {
+		_testHTTPEndpointMonitorInvalidProperty(
+			"monitor[a].parameter[url]",
+			"file:///" + RandomTestUtil.randomString());
+		_testHTTPEndpointMonitorInvalidProperty(
+			"monitor[a].parameter[url]", RandomTestUtil.randomString());
+		_testHTTPEndpointMonitorInvalidProperty(
+			"monitor[a].threshold[latency.maximum.millis]", "-1");
+		_testHTTPEndpointMonitorInvalidProperty(
+			"monitor[a].threshold[latency.maximum.millis]", "not-a-number");
+
+		_testHTTPEndpointMonitorMissingProperty("monitor[a].parameter[url]");
+	}
+
+	@Test
+	public void testHTTPEndpointMonitorUserInfo() {
+		_testHTTPEndpointMonitorUserInfo("");
+		_testHTTPEndpointMonitorUserInfo("//");
+		_testHTTPEndpointMonitorUserInfo("htps://");
+		_testHTTPEndpointMonitorUserInfo("https://");
+	}
+
+	private MonitorResult _execute(Properties monitorProperties) {
+		HTTPEndpointMonitor httpEndpointMonitor = _newHTTPEndpointMonitor(
+			monitorProperties);
+
+		return httpEndpointMonitor.execute();
+	}
+
+	private HTTPEndpointMonitor _newHTTPEndpointMonitor(
+		Properties monitorProperties) {
+
+		List<MonitorConfig> monitorConfigs =
+			MonitorConfigLoader.getMonitorConfigs(monitorProperties);
+
+		return new HTTPEndpointMonitor(monitorConfigs.get(0));
+	}
+
+	private Properties _newMonitorProperties() {
+		Properties monitorProperties = new Properties();
+
+		monitorProperties.setProperty("monitor[a].parameter[url]", _URL);
+		monitorProperties.setProperty("monitor[a].type", "http-endpoint");
+
+		return monitorProperties;
+	}
+
+	private String _testHTTPEndpointMonitorExpectedIllegalArgumentException(
+		Properties monitorProperties) {
+
+		try {
+			_newHTTPEndpointMonitor(monitorProperties);
+
+			Assert.fail("Expected IllegalArgumentException");
+		}
+		catch (IllegalArgumentException illegalArgumentException) {
+			return illegalArgumentException.getMessage();
+		}
+
+		return null;
+	}
+
+	private void _testHTTPEndpointMonitorInvalidProperty(
+		String name, String value) {
+
+		Properties monitorProperties = _newMonitorProperties();
+
+		monitorProperties.setProperty(name, value);
+
+		_testHTTPEndpointMonitorExpectedIllegalArgumentException(
+			monitorProperties);
+	}
+
+	private void _testHTTPEndpointMonitorMissingProperty(String name) {
+		Properties monitorProperties = _newMonitorProperties();
+
+		monitorProperties.remove(name);
+
+		_testHTTPEndpointMonitorExpectedIllegalArgumentException(
+			monitorProperties);
+	}
+
+	private void _testHTTPEndpointMonitorUserInfo(String urlPrefix) {
+		Properties monitorProperties = _newMonitorProperties();
+
+		monitorProperties.setProperty(
+			"monitor[a].parameter[url]",
+			JenkinsResultsParserUtil.combine(
+				urlPrefix, RandomTestUtil.randomString(), ":", _PASSWORD, "@",
+				RandomTestUtil.randomString()));
+
+		String message =
+			_testHTTPEndpointMonitorExpectedIllegalArgumentException(
+				monitorProperties);
+
+		Assert.assertFalse(message.contains(_PASSWORD));
+		Assert.assertTrue(message.contains("[REDACTED]"));
+	}
+
+	private static final long _MILLIS_LATENCY = 50;
+
+	private static final String _PASSWORD = "correct-horse-battery-staple";
+
+	private static final String _URL = "https://repository.liferay.com";
+
+}
