8f5ca84feb62fdfa8ed76b6cab7543bbfb0eadcd
Larissa Ribeiro <larissa.ribeiro@liferay.com>
2026-08-17 14:13:09 -0300

    LPD-102015 [BE] Serve the project user groups from the CMS provider
    Applies the same guards the user accounts endpoint uses: the LPD-58677 feature flag, and an ASSIGN_MEMBERS check on the group of the project the caller names. The candidate set comes from the provider rather than from the project, so a group must belong to a Space the caller can reach before it can be added to a project.

--- modules/apps/headless/headless-cmp/headless-cmp-impl/src/main/java/com/liferay/headless/cmp/internal/resource/v1_0/UserGroupResourceImpl.java
+++ modules/apps/headless/headless-cmp/headless-cmp-impl/src/main/java/com/liferay/headless/cmp/internal/resource/v1_0/UserGroupResourceImpl.java
@@ -5,17 +5,87 @@
 
 package com.liferay.headless.cmp.internal.resource.v1_0;
 
+import com.liferay.headless.cmp.dto.v1_0.UserGroup;
 import com.liferay.headless.cmp.resource.v1_0.UserGroupResource;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryService;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.site.cms.site.initializer.user.groups.provider.CMSUserGroupsProvider;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
- * @author Carolina Barbosa
+ * @author Larissa Ribeiro
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/user-group.properties",
 	scope = ServiceScope.PROTOTYPE, service = UserGroupResource.class
 )
 public class UserGroupResourceImpl extends BaseUserGroupResourceImpl {
+
+	@Override
+	public Page<UserGroup> getProjectUserGroupsPage(
+			Long projectId, String search, Pagination pagination)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				contextCompany.getCompanyId(), "LPD-58677")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		ObjectEntry objectEntry = _objectEntryService.getObjectEntry(projectId);
+
+		_groupModelResourcePermission.check(
+			PermissionThreadLocal.getPermissionChecker(),
+			objectEntry.getGroupId(), ActionKeys.ASSIGN_MEMBERS);
+
+		return Page.of(
+			transform(
+				_cmsUserGroupsProvider.getUserGroups(
+					search, pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toUserGroup),
+			pagination, _cmsUserGroupsProvider.getUserGroupsCount(search));
+	}
+
+	private UserGroup _toUserGroup(
+		com.liferay.portal.kernel.model.UserGroup serviceBuilderUserGroup) {
+
+		return new UserGroup() {
+			{
+				setExternalReferenceCode(
+					serviceBuilderUserGroup::getExternalReferenceCode);
+				setId(serviceBuilderUserGroup::getUserGroupId);
+				setName(serviceBuilderUserGroup::getName);
+				setUsersCount(
+					() -> (long)_userLocalService.getUserGroupUsersCount(
+						serviceBuilderUserGroup.getUserGroupId()));
+			}
+		};
+	}
+
+	@Reference
+	private CMSUserGroupsProvider _cmsUserGroupsProvider;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.kernel.model.Group)"
+	)
+	private ModelResourcePermission<Group> _groupModelResourcePermission;
+
+	@Reference
+	private ObjectEntryService _objectEntryService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
