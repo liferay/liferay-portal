e249ce355bd37c475b26658d504ec8dc4d993472
Selena Aungst <selena.aungst@liferay.com>
2026-08-17 16:48:33 -0700

    LPD-102470 portal-workflow-kaleo-test: Assert that the Kaleo definition ID is indexed.
    https://liferay.atlassian.net/browse/LPD-102470

--- modules/apps/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/index/contributor/test/KaleoTaskInstanceTokenModelDocumentContributorTest.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/index/contributor/test/KaleoTaskInstanceTokenModelDocumentContributorTest.java
@@ -8,13 +8,18 @@ package com.liferay.portal.workflow.kaleo.internal.search.spi.model.index.contri
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentImpl;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.workflow.constants.WorkflowDefinitionConstants;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
 import com.liferay.portal.workflow.kaleo.model.KaleoInstance;
 import com.liferay.portal.workflow.kaleo.model.KaleoInstanceToken;
 import com.liferay.portal.workflow.kaleo.model.KaleoTaskInstanceToken;
+import com.liferay.portal.workflow.kaleo.service.KaleoInstanceLocalService;
 import com.liferay.portal.workflow.kaleo.service.test.BaseKaleoLocalServiceTestCase;
 
 import java.io.Serializable;
@@ -36,18 +41,37 @@ public class KaleoTaskInstanceTokenModelDocumentContributorTest
 	public void testContributeWhenWorkflowHandlerIsNotRegistered()
 		throws Exception {
 
-		KaleoInstance kaleoInstance = addKaleoInstance();
+		KaleoDefinition kaleoDefinition = addKaleoDefinition(
+			WorkflowDefinitionConstants.SCOPE_ALL);
+
+		KaleoDefinitionVersion kaleoDefinitionVersion =
+			getLatestKaleoDefinitionVersion(kaleoDefinition);
+
+		String className = RandomTestUtil.randomString();
+
+		KaleoInstance kaleoInstance =
+			_kaleoInstanceLocalService.addKaleoInstance(
+				kaleoDefinition.getKaleoDefinitionId(),
+				kaleoDefinitionVersion.getKaleoDefinitionVersionId(),
+				kaleoDefinition.getName(), 1,
+				HashMapBuilder.<String, Serializable>put(
+					WorkflowConstants.CONTEXT_ENTRY_CLASS_NAME, className
+				).put(
+					WorkflowConstants.CONTEXT_ENTRY_CLASS_PK, "1"
+				).build(),
+				serviceContext);
 
 		KaleoInstanceToken kaleoInstanceToken = addKaleoInstanceToken(
 			kaleoInstance);
 
+		String taskName = RandomTestUtil.randomString();
+
 		KaleoTaskInstanceToken kaleoTaskInstanceToken =
 			kaleoTaskInstanceTokenLocalService.addKaleoTaskInstanceToken(
-				kaleoInstanceToken.getKaleoInstanceTokenId(), 1, _TASK_NAME,
+				kaleoInstanceToken.getKaleoInstanceTokenId(), 1, taskName,
 				Collections.emptyList(), null,
 				HashMapBuilder.<String, Serializable>put(
-					WorkflowConstants.CONTEXT_ENTRY_CLASS_NAME,
-					_UNREGISTERED_CLASS_NAME
+					WorkflowConstants.CONTEXT_ENTRY_CLASS_NAME, className
 				).put(
 					WorkflowConstants.CONTEXT_ENTRY_CLASS_PK, "1"
 				).build(),
@@ -57,15 +81,15 @@ public class KaleoTaskInstanceTokenModelDocumentContributorTest
 
 		_modelDocumentContributor.contribute(document, kaleoTaskInstanceToken);
 
+		Assert.assertEquals(className, document.get("className"));
 		Assert.assertEquals(
-			_UNREGISTERED_CLASS_NAME, document.get("className"));
-		Assert.assertEquals(_TASK_NAME, document.get("taskName"));
+			String.valueOf(kaleoDefinition.getKaleoDefinitionId()),
+			document.get("kaleoDefinitionId"));
+		Assert.assertEquals(taskName, document.get("taskName"));
 	}
 
-	private static final String _TASK_NAME = "task";
-
-	private static final String _UNREGISTERED_CLASS_NAME =
-		"com.liferay.portal.workflow.kaleo.test.NoWorkflowHandlerModel";
+	@Inject
+	private KaleoInstanceLocalService _kaleoInstanceLocalService;
 
 	@Inject(
 		filter = "component.name=com.liferay.portal.workflow.kaleo.internal.search.spi.model.index.contributor.KaleoTaskInstanceTokenModelDocumentContributor"
