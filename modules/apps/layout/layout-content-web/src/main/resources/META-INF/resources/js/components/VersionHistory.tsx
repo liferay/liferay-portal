bd5552738a5c0e3552e25b66dc4537c0cc0e8e54
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-08-17 15:30:07 +0100

    LPD-102760 Confirm the restore only when there is a draft

--- modules/apps/layout/layout-content-web/src/main/resources/META-INF/resources/js/components/VersionHistory.tsx
+++ modules/apps/layout/layout-content-web/src/main/resources/META-INF/resources/js/components/VersionHistory.tsx
@@ -120,18 +120,20 @@ export default function VersionHistory({config}: Props) {
 			return;
 		}
 
-		const confirmed = await openConfirmModal({
-			buttonLabel: Liferay.Language.get('restore'),
-			center: true,
-			status: 'warning',
-			text: Liferay.Language.get(
-				'you-are-about-to-restore-an-older-version-of-the-page.-all-your-unsaved-changes-will-be-lost'
-			),
-			title: Liferay.Language.get('restore-version'),
-		});
+		if (config.layout.status === 'draft') {
+			const confirmed = await openConfirmModal({
+				buttonLabel: Liferay.Language.get('restore'),
+				center: true,
+				status: 'warning',
+				text: Liferay.Language.get(
+					'you-are-about-to-restore-an-older-version-of-the-page.-all-your-unsaved-changes-will-be-lost'
+				),
+				title: Liferay.Language.get('restore-version'),
+			});
 
-		if (!confirmed) {
-			return;
+			if (!confirmed) {
+				return;
+			}
 		}
 
 		const {error} = await PageVersionService.restorePageVersion(
