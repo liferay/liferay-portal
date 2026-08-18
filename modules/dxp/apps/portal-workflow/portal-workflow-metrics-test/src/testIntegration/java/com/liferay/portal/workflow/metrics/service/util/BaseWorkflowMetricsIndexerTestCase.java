a8959a6bce3884d33c72187d4ae2f2e0ee95649b
Joshua Cords <joshua.cords@liferay.com>
2026-08-18 14:23:54 -0700

    LPD-75968 portal-workflow-metrics-test: adjust Base Test Case to expose behavior when reindexing from scratch
    https://liferay.atlassian.net/browse/LPD-75968

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/service/util/BaseWorkflowMetricsIndexerTestCase.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/service/util/BaseWorkflowMetricsIndexerTestCase.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.engine.adapter.index.DeleteIndexRequest;
+import com.liferay.portal.search.index.IndexNameBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.workflow.kaleo.definition.Node;
 import com.liferay.portal.workflow.kaleo.definition.Task;
@@ -37,6 +39,7 @@ import com.liferay.portal.workflow.kaleo.service.KaleoInstanceTokenLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoNodeLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoTaskInstanceTokenLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoTaskLocalService;
+import com.liferay.portal.workflow.metrics.search.index.constants.WorkflowMetricsIndexNameConstants;
 import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexer;
 import com.liferay.portal.workflow.metrics.search.index.reindexer.WorkflowMetricsReindexerRegistry;
 
@@ -473,6 +476,19 @@ public abstract class BaseWorkflowMetricsIndexerTestCase
 	}
 
 	private void _reindexMetricIndexes(long companyId) throws Exception {
+		String indexNamePrefix = _indexNameBuilder.getIndexName(companyId);
+
+		searchEngineAdapter.execute(
+			new DeleteIndexRequest(
+				indexNamePrefix +
+					WorkflowMetricsIndexNameConstants.SUFFIX_INSTANCE,
+				indexNamePrefix + WorkflowMetricsIndexNameConstants.SUFFIX_NODE,
+				indexNamePrefix +
+					WorkflowMetricsIndexNameConstants.SUFFIX_PROCESS,
+				indexNamePrefix + WorkflowMetricsIndexNameConstants.SUFFIX_TASK,
+				indexNamePrefix +
+					WorkflowMetricsIndexNameConstants.SUFFIX_TRANSITION));
+
 		_reindex(companyId, "instance");
 		_reindex(companyId, "node");
 		_reindex(companyId, "process");
@@ -481,6 +497,16 @@ public abstract class BaseWorkflowMetricsIndexerTestCase
 	}
 
 	private void _reindexSLAIndexes(long companyId) throws Exception {
+		String indexNamePrefix = _indexNameBuilder.getIndexName(companyId);
+
+		searchEngineAdapter.execute(
+			new DeleteIndexRequest(
+				indexNamePrefix +
+					WorkflowMetricsIndexNameConstants.
+						SUFFIX_SLA_INSTANCE_RESULT,
+				indexNamePrefix +
+					WorkflowMetricsIndexNameConstants.SUFFIX_SLA_TASK_RESULT));
+
 		_reindex(companyId, "sla-instance-result");
 		_reindex(companyId, "sla-task-result");
 	}
@@ -489,6 +515,9 @@ public abstract class BaseWorkflowMetricsIndexerTestCase
 
 	@Inject
 	private BlogsEntryLocalService _blogsEntryLocalService;
+
+	@Inject
+	private IndexNameBuilder _indexNameBuilder;
 
 	private KaleoDefinition _kaleoDefinition;
 
