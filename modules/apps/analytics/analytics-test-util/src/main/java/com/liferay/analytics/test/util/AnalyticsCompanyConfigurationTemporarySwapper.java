81b92f3c89be77bb866358623e7823a58e4928f3
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2026-08-18 12:31:47 +0200

    LPD-102164 Add a swapper for the Analytics Cloud configuration
    Every test that reaches Analytics Cloud opens the same fifteen line company configuration swapper, filled with the same three random values, and there are more than thirty of them across the analytics modules. A swapper of its own carries that configuration, and a test names only what it cares about: the company, and where it matters the data source id and whether every group is enabled.

---
+++ modules/apps/analytics/analytics-test-util/src/main/java/com/liferay/analytics/test/util/AnalyticsCompanyConfigurationTemporarySwapper.java
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.analytics.test.util;
+
+import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
+import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class AnalyticsCompanyConfigurationTemporarySwapper
+	extends CompanyConfigurationTemporarySwapper {
+
+	public AnalyticsCompanyConfigurationTemporarySwapper(long companyId)
+		throws Exception {
+
+		this(companyId, RandomTestUtil.randomString());
+	}
+
+	public AnalyticsCompanyConfigurationTemporarySwapper(
+			long companyId, String dataSourceId)
+		throws Exception {
+
+		this(companyId, dataSourceId, true);
+	}
+
+	public AnalyticsCompanyConfigurationTemporarySwapper(
+			long companyId, String dataSourceId, boolean enableAllGroupIds)
+		throws Exception {
+
+		super(
+			companyId, AnalyticsConfiguration.class.getName(),
+			HashMapDictionaryBuilder.<String, Object>put(
+				"liferayAnalyticsDataSourceId", dataSourceId
+			).put(
+				"liferayAnalyticsEnableAllGroupIds", enableAllGroupIds
+			).put(
+				"liferayAnalyticsFaroBackendSecuritySignature",
+				RandomTestUtil.randomString()
+			).put(
+				"liferayAnalyticsFaroBackendURL",
+				"http://" + RandomTestUtil.randomString()
+			).build());
+	}
+
+}
