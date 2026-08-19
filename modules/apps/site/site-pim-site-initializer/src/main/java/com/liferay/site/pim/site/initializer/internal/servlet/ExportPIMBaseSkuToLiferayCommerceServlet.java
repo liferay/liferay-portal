e2e4b8f8dba4fde56348ce53339d2157f3ee7c37
smottal <117469730+smottal@users.noreply.github.com>
2026-08-19 18:14:47 +0200

    LPD-101844 Manage variants in export

--- modules/apps/site/site-pim-site-initializer/src/main/java/com/liferay/site/pim/site/initializer/internal/servlet/ExportPIMBaseSkuToLiferayCommerceServlet.java
+++ modules/apps/site/site-pim-site-initializer/src/main/java/com/liferay/site/pim/site/initializer/internal/servlet/ExportPIMBaseSkuToLiferayCommerceServlet.java
@@ -8,10 +8,14 @@ package com.liferay.site.pim.site.initializer.internal.servlet;
 import com.liferay.depot.constants.DepotConstants;
 import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.rest.filter.factory.FilterFactory;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -24,9 +28,12 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.site.pim.site.initializer.constants.PIMObjectDefinitionConstants;
+import com.liferay.site.pim.site.initializer.internal.link.VariantPIMLinkType;
 
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
@@ -37,6 +44,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -83,15 +94,35 @@ public class ExportPIMBaseSkuToLiferayCommerceServlet extends HttpServlet {
 					_depotEntryLocalService.getDepotEntries(
 						companyId, DepotConstants.TYPE_SPACE)) {
 
+				Map<String, String> clusterKeys = _getVariantPIMLinkClusterKeys(
+					companyId, depotEntry.getGroupId());
+
+				Map<String, List<ObjectEntry>> objectEntriesMap =
+					new LinkedHashMap<>();
+
 				for (ObjectEntry objectEntry :
 						_objectEntryLocalService.getObjectEntries(
 							depotEntry.getGroupId(),
 							objectDefinition.getObjectDefinitionId(),
 							QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
 
-					jsonArray.put(
-						_toJSONObject(
-							_objectEntryLocalService.getValues(objectEntry)));
+					String externalReferenceCode =
+						objectEntry.getExternalReferenceCode();
+
+					List<ObjectEntry> objectEntries =
+						objectEntriesMap.computeIfAbsent(
+							GetterUtil.getString(
+								clusterKeys.get(externalReferenceCode),
+								externalReferenceCode),
+							key -> new ArrayList<>());
+
+					objectEntries.add(objectEntry);
+				}
+
+				for (List<ObjectEntry> objectEntries :
+						objectEntriesMap.values()) {
+
+					jsonArray.put(_toJSONObject(objectEntries));
 				}
 			}
 
@@ -109,6 +140,46 @@ public class ExportPIMBaseSkuToLiferayCommerceServlet extends HttpServlet {
 		}
 	}
 
+	private Map<String, String> _getVariantPIMLinkClusterKeys(
+			long companyId, long groupId)
+		throws PortalException {
+
+		Map<String, String> clusterKeys = new HashMap<>();
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					PIMObjectDefinitionConstants.EXTERNAL_REFERENCE_CODE_LINK,
+					companyId);
+
+		if (objectDefinition == null) {
+			return clusterKeys;
+		}
+
+		for (Map<String, Serializable> values :
+				_objectEntryLocalService.getValuesList(
+					groupId, companyId, 0,
+					objectDefinition.getObjectDefinitionId(),
+					_filterFactory.create(
+						StringBundler.concat(
+							"type eq '", VariantPIMLinkType.TYPE, "'"),
+						objectDefinition),
+					null, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null)) {
+
+			String clusterKey = MapUtil.getString(values, "clusterKey");
+
+			if (Validator.isNull(clusterKey)) {
+				continue;
+			}
+
+			clusterKeys.put(
+				MapUtil.getString(values, "sourceClassExternalReferenceCode"),
+				clusterKey);
+		}
+
+		return clusterKeys;
+	}
+
 	private void _sendError(HttpServletResponse httpServletResponse)
 		throws IOException {
 
@@ -123,8 +194,28 @@ public class ExportPIMBaseSkuToLiferayCommerceServlet extends HttpServlet {
 			).toString());
 	}
 
-	private JSONObject _toJSONObject(Map<String, Serializable> values) {
-		String code = MapUtil.getString(values, "code");
+	private JSONObject _toJSONObject(List<ObjectEntry> objectEntries)
+		throws PortalException {
+
+		JSONArray skusJSONArray = _jsonFactory.createJSONArray();
+
+		for (ObjectEntry objectEntry : objectEntries) {
+			skusJSONArray.put(
+				JSONUtil.put(
+					"published", true
+				).put(
+					"purchasable", true
+				).put(
+					"sku",
+					MapUtil.getString(
+						_objectEntryLocalService.getValues(objectEntry), "code")
+				));
+		}
+
+		ObjectEntry objectEntry = objectEntries.get(0);
+
+		Map<String, Serializable> values = _objectEntryLocalService.getValues(
+			objectEntry);
 
 		return JSONUtil.put(
 			"catalogId", "[$MASTER_CATALOG_ID$]"
@@ -132,21 +223,13 @@ public class ExportPIMBaseSkuToLiferayCommerceServlet extends HttpServlet {
 			"description",
 			JSONUtil.put("en_US", MapUtil.getString(values, "description"))
 		).put(
-			"externalReferenceCode", code
+			"externalReferenceCode", objectEntry.getExternalReferenceCode()
 		).put(
 			"name", JSONUtil.put("en_US", MapUtil.getString(values, "name"))
 		).put(
 			"productType", "simple"
 		).put(
-			"skus",
-			JSONUtil.put(
-				JSONUtil.put(
-					"published", true
-				).put(
-					"purchasable", true
-				).put(
-					"sku", code
-				))
+			"skus", skusJSONArray
 		);
 	}
 
@@ -155,6 +238,11 @@ public class ExportPIMBaseSkuToLiferayCommerceServlet extends HttpServlet {
 
 	@Reference
 	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Reference(
+		target = "(filter.factory.key=" + ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT + ")"
+	)
+	private FilterFactory<Predicate> _filterFactory;
 
 	@Reference
 	private JSONFactory _jsonFactory;
