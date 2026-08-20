f29744a5f18e2f8e408ac52b871b1b8023cfd31f
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-08-20 12:21:41 +0200

    LPD-103027 Open the broken links section from the dashboard card

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/governance/components/AttentionRequired.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/dashboard/governance/components/AttentionRequired.tsx
@@ -63,6 +63,7 @@ const ATTENTION_CARDS: AttentionCard[] = [
 ];
 
 const SECTION_PATHS: Partial<Record<keyof AssetStatistics, string>> = {
+	brokenLinksCount: 'broken-links',
 	expiredCount: 'expired-assets',
 	pendingCount: 'pending-workflows',
 	reviewDateOverdueCount: 'overdue-reviews',
