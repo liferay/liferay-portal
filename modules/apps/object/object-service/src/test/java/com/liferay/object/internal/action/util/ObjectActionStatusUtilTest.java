2ad285420564554d5f0293e585ba26738cb1fea1
shuyangzhou <shuyang.zhou@liferay.com>
2026-08-20 06:39:11 -0700

    LPD-103119 Test a failed status write ends in the log, not the caller
    Run the helper without a surrounding transaction, where the callback runs immediately, with a service whose updateStatus throws. The helper must swallow the failure into its own error log entry instead of rethrowing it at the caller.

---
+++ modules/apps/object/object-service/src/test/java/com/liferay/object/internal/action/util/ObjectActionStatusUtilTest.java
@@ -0,0 +1,73 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.action.util;
+
+import com.liferay.object.constants.ObjectActionConstants;
+import com.liferay.object.model.ObjectAction;
+import com.liferay.object.service.ObjectActionLocalServiceWrapper;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ObjectActionStatusUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testUpdateStatusAfterCommitLogsFailedWrite() throws Exception {
+
+		// Without a surrounding transaction the callback runs immediately, so
+		// a failing status write must end in the log here instead of reaching
+		// the caller
+
+		long objectActionId = RandomTestUtil.randomLong();
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				ObjectActionStatusUtil.class.getName(), LoggerTestUtil.ERROR)) {
+
+			ObjectActionStatusUtil.updateStatusAfterCommit(
+				new ObjectActionLocalServiceWrapper() {
+
+					@Override
+					public ObjectAction updateStatus(
+						long curObjectActionId, int status) {
+
+						throw new SystemException();
+					}
+
+				},
+				objectActionId, ObjectActionConstants.STATUS_FAILED);
+
+			List<LogEntry> logEntries = logCapture.getLogEntries();
+
+			Assert.assertEquals(logEntries.toString(), 1, logEntries.size());
+
+			LogEntry logEntry = logEntries.get(0);
+
+			Assert.assertEquals(
+				"Unable to update the status of object action " +
+					objectActionId,
+				logEntry.getMessage());
+		}
+	}
+
+}
