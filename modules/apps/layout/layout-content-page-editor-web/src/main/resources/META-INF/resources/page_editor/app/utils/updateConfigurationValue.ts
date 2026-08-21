dd08e9224a178d687d5e2d333f024b73d71d1949
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-08-21 12:52:53 +0100

    LPD-103044 Migrate updateConfigurationValue to TypeScript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/updateConfigurationValue.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/updateConfigurationValue.ts
@@ -3,9 +3,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {
+	ConfigurationValue,
+	FragmentEntryLink,
+} from '../actions/addFragmentEntryLinks';
 import {FREEMARKER_FRAGMENT_ENTRY_PROCESSOR} from '../config/constants/freemarkerFragmentEntryProcessor';
 import {config} from '../config/index';
+import {Dispatch} from '../contexts/StoreContext';
 import updateFragmentConfiguration from '../thunks/updateFragmentConfiguration';
+
+type FragmentConfiguration = {
+	fieldSets?: Array<{
+		fields: Array<{localizable?: boolean; name: string}>;
+	}>;
+};
 
 export default function updateConfigurationValue({
 	configuration,
@@ -14,6 +25,13 @@ export default function updateConfigurationValue({
 	languageId,
 	name,
 	value,
+}: {
+	configuration?: FragmentConfiguration;
+	dispatch: Dispatch;
+	fragmentEntryLink: FragmentEntryLink;
+	languageId: Liferay.Language.Locale;
+	name: string;
+	value: ConfigurationValue;
 }) {
 	const configurationValues =
 		fragmentEntryLink.editableValues?.[
@@ -45,7 +63,6 @@ export default function updateConfigurationValue({
 		updateFragmentConfiguration({
 			configurationValues: nextConfigurationValues,
 			fragmentEntryLink,
-			languageId,
 		})
 	);
 }
