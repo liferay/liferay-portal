dbab217bc8fc1f6822698869b6843c53e507222c
Gabriel Lima <gabriel.lima@liferay.com>
2026-08-21 12:22:06 -0300

    LPD-103373 Skip selecting an already selected attribute option
    Wait for the option to become visible before deciding whether to click
    it or dismiss it, instead of assuming it always needs a click. When
    the option is already selected, clicking it again would just toggle
    it back off.

--- modules/test/playwright/tests/client-extension-web/js/pages/EditJSClientExtensionsPage.ts
+++ modules/test/playwright/tests/client-extension-web/js/pages/EditJSClientExtensionsPage.ts
@@ -1,11 +1,12 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 import {Locator, Page} from '@playwright/test';
 
 import {clickAndExpectToBeHidden} from '../../../../utils/clickAndExpectToBeHidden';
+import {clickAndExpectToBeVisible} from '../../../../utils/clickAndExpectToBeVisible';
 import {EditClientExtensionsPage} from '../../pages/EditClientExtensionsPage';
 
 export class EditJSClientExtensionsPage extends EditClientExtensionsPage {
@@ -22,41 +23,53 @@ export class EditJSClientExtensionsPage extends EditClientExtensionsPage {
 	}
 
 	async addScriptAttribute(name: string, type: string, value: string) {
-		const addAttributeGroupButton = this.page.getByRole('button', {
-			name: 'Add Attribute Group',
-		});
+		const {nameField, typeField, valueField} =
+			await this.addAttributeGroup();
 
-		await addAttributeGroupButton.click();
+		await nameField.fill(name);
 
-		await this.page
-			.locator(
-				`#_${this.portletName}_name_${this._currentAttributeIndex}`
-			)
-			.fill(name);
-
-		await this.page
-			.locator(
-				`#_${this.portletName}_type_${this._currentAttributeIndex}`
-			)
-			.click();
-
-		const option = this.page.getByRole('option', {name: type});
-
-		await clickAndExpectToBeHidden({target: option, trigger: option});
-
-		const attributeValueField = this.page.locator(
-			`#_${this.portletName}_value_${this._currentAttributeIndex}`
-		);
+		await this.selectOption(type, typeField);
 
 		if (type === 'boolean') {
-			await attributeValueField.click();
-
-			await this.page.getByRole('option', {name: value}).click();
+			await this.selectOption(value, valueField);
 		}
 		else {
-			await attributeValueField.fill(value);
+			await valueField.fill(value);
 		}
+	}
 
-		this._currentAttributeIndex++;
+	private async addAttributeGroup() {
+		await this.page
+			.getByRole('button', {
+				name: 'Add Attribute Group',
+			})
+			.click();
+
+		const index = this._currentAttributeIndex++;
+
+		const nameField = this.page.locator(
+			`#_${this.portletName}_name_${index}`
+		);
+		const typeField = this.page.locator(
+			`#_${this.portletName}_type_${index}`
+		);
+		const valueField = this.page.locator(
+			`#_${this.portletName}_value_${index}`
+		);
+
+		return {nameField, typeField, valueField};
+	}
+
+	private async selectOption(name: string, trigger: Locator) {
+		const target = this.page.getByRole('option', {name});
+
+		await clickAndExpectToBeVisible({target, trigger});
+
+		if ((await target.getAttribute('aria-selected')) === 'true') {
+			await clickAndExpectToBeHidden({target, trigger});
+		}
+		else {
+			await clickAndExpectToBeHidden({target, trigger: target});
+		}
 	}
 }
