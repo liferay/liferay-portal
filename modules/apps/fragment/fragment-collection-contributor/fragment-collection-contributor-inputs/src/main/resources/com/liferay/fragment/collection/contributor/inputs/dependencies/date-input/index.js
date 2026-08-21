985b9c5e1ae686d562baa983aa343b1a0505cd32
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-08-21 14:59:19 +0100

    LPD-103380 Render the Clay date picker in the date fragments

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/date-input/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/date-input/index.js
@@ -1,62 +1,29 @@
+const container = document.getElementById(
+	`${fragmentElementId}-date-input-picker`
+);
+
+// `input.errorMessage` reaches this script empty, so read the rendered error.
+
 const error = document.getElementById(`${fragmentElementId}-date-input-error`);
-const inputElement = document.getElementById(`${fragmentElementId}-date-input`);
 
-if (inputElement) {
-	if (input.attributes?.readOnly) {
-		inputElement.addEventListener('keydown', (event) => {
-			if (event.code === 'Space') {
-				event.preventDefault();
-			}
+if (container) {
+	import('@liferay/fragment-impl/api').then(({renderDateInput}) => {
+		renderDateInput(container, {
+			availableLanguageIds: input.attributes.availableLanguageIds,
+			defaultLanguageId: input.attributes.defaultLanguageId,
+			disabled:
+				layoutMode === 'edit' || input.attributes.disabled === true,
+			focus: Boolean(error),
+			localizable: input.localizable,
+			name: input.name,
+			namespace: fragmentElementId,
+			readOnly: input.readOnly,
+			readOnlyLabelId: `${fragmentElementId}-date-read-only`,
+			required: input.required,
+			time: false,
+			unlocalizedFieldsState: input.attributes.unlocalizedFieldsState,
+			value: input.value,
+			valueI18n: input.valueI18n,
 		});
-	}
-
-	if (layoutMode === 'edit') {
-		inputElement.setAttribute('disabled', true);
-	}
-	else {
-		const defaultLanguageId = input.attributes.defaultLanguageId;
-
-		import('@liferay/fragment-impl/api').then(
-			({
-				focusInput,
-				registerLocalizedInput,
-				registerUnlocalizedInput,
-			}) => {
-				if (error) {
-					focusInput(inputElement);
-				}
-
-				if (input.localizable) {
-					const {onChange} = registerLocalizedInput({
-						availableLanguageIds:
-							input.attributes.availableLanguageIds,
-						defaultLanguageId,
-						initialValues: input.valueI18n,
-						inputElement,
-						inputName: input.name,
-						localizationInputsContainer: inputElement.parentNode,
-						namespace: fragmentElementId,
-					});
-
-					inputElement.addEventListener('change', (event) => {
-						onChange(event.target.value);
-					});
-				}
-				else {
-					registerUnlocalizedInput({
-						defaultLanguageId,
-						inputElement,
-						readOnlyInputLabel: document.getElementById(
-							`${fragmentElementId}-date-read-only`
-						),
-						unlocalizedFieldsState:
-							input.attributes.unlocalizedFieldsState,
-						unlocalizedMessageContainer: document.getElementById(
-							`${fragmentElementId}-unlocalized-info`
-						),
-					});
-				}
-			}
-		);
-	}
+	});
 }
