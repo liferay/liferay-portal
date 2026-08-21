22fdb197f7a3c1662f5cd11550f2bb93a4b1df51
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-08-21 17:40:56 +0200

    LPD-103230 Gate the broken link edit action by the update permission

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/action/ViewBrokenLinksSectionFDSItemsActions.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/action/ViewBrokenLinksSectionFDSItemsActions.java
@@ -53,6 +53,8 @@ public class ViewBrokenLinksSectionFDSItemsActions implements FDSItemsActions {
 				LanguageUtil.get(httpServletRequest, "edit")
 			).setMethod(
 				"get"
+			).setPermissionKey(
+				"update"
 			).build(
 				"edit"
 			));
