fa330e2f63f4c6c34687008c54cd3bf2bb179adf
Akhash R <akhash.ramprakash@liferay.com>
2026-08-21 19:22:01 +0530

    LPD-103010 Cover the publication cleanup passes

--- modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/capabilities/test/TemporaryFileEntriesCapabilityTest.java
+++ modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/capabilities/test/TemporaryFileEntriesCapabilityTest.java
@@ -6,9 +6,13 @@
 package com.liferay.repository.capabilities.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.User;
@@ -21,15 +25,18 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.io.InputStream;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import org.junit.Assert;
@@ -58,41 +65,107 @@ public class TemporaryFileEntriesCapabilityTest {
 
 	@Test
 	public void testDeleteExpiredTemporaryFileEntries() throws Exception {
-		FileEntry fileEntry = TempFileEntryUtil.addTempFileEntry(
-			_group.getGroupId(), _user.getUserId(),
-			RandomTestUtil.randomString(), "image.jpg", getInputStream(),
-			ContentTypes.IMAGE_JPEG);
+		_addExpiredTempFileEntry(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomString() + ".jpg");
 
-		DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.getDLFileEntry(
-			fileEntry.getFileEntryId());
-
-		dlFileEntry.setCreateDate(
-			new Date(System.currentTimeMillis() - Time.WEEK));
-
-		DLFileEntryLocalServiceUtil.updateDLFileEntry(dlFileEntry);
-
-		Repository repository = RepositoryLocalServiceUtil.fetchRepository(
-			_group.getGroupId(), TempFileEntryUtil.class.getName(),
-			TempFileEntryUtil.class.getName());
-
-		LocalRepository localRepository =
-			RepositoryProviderUtil.getLocalRepository(
-				repository.getRepositoryId());
+		LocalRepository localRepository = _getLocalRepository();
 
 		int foldersAndFileEntriesAndFileShortcutsCount =
 			getFoldersAndFileEntriesAndFileShortcutsCount(localRepository);
 
 		Assert.assertTrue(foldersAndFileEntriesAndFileShortcutsCount > 0);
 
-		TemporaryFileEntriesCapability temporaryFileEntriesCapability =
-			localRepository.getCapability(TemporaryFileEntriesCapability.class);
-
-		temporaryFileEntriesCapability.setTemporaryFileEntriesTimeout(Time.DAY);
-
-		temporaryFileEntriesCapability.deleteExpiredTemporaryFileEntries();
+		_deleteExpiredTemporaryFileEntries(localRepository);
 
 		Assert.assertEquals(
 			0, getFoldersAndFileEntriesAndFileShortcutsCount(localRepository));
+	}
+
+	@Test
+	public void testDeleteExpiredTemporaryFileEntriesInPublication()
+		throws Exception {
+
+		String folderName = RandomTestUtil.randomString();
+
+		_addExpiredTempFileEntry(
+			folderName, RandomTestUtil.randomString() + ".jpg");
+
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), _user.getUserId(), 0,
+			RandomTestUtil.randomString(), null);
+
+		FileEntry fileEntry = null;
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			fileEntry = _addExpiredTempFileEntry(
+				folderName, RandomTestUtil.randomString() + ".jpg");
+		}
+
+		LocalRepository localRepository = _getLocalRepository();
+
+		_deleteExpiredTemporaryFileEntries(localRepository);
+
+		Assert.assertEquals(
+			0, getFoldersAndFileEntriesAndFileShortcutsCount(localRepository));
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			Assert.assertNull(
+				DLFileEntryLocalServiceUtil.fetchDLFileEntry(
+					fileEntry.getFileEntryId()));
+			Assert.assertEquals(
+				0,
+				getFoldersAndFileEntriesAndFileShortcutsCount(localRepository));
+		}
+	}
+
+	@Test
+	public void testDeleteExpiredTemporaryFileEntriesWhenPublicationFileEntryIsNotExpired()
+		throws Exception {
+
+		String folderName = RandomTestUtil.randomString();
+
+		_addExpiredTempFileEntry(
+			folderName, RandomTestUtil.randomString() + ".jpg");
+
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), _user.getUserId(), 0,
+			RandomTestUtil.randomString(), null);
+
+		String fileName = RandomTestUtil.randomString() + ".jpg";
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			TempFileEntryUtil.addTempFileEntry(
+				_group.getGroupId(), _user.getUserId(), folderName, fileName,
+				getInputStream(), ContentTypes.IMAGE_JPEG);
+		}
+
+		_deleteExpiredTemporaryFileEntries(_getLocalRepository());
+
+		String[] tempFileNames = TempFileEntryUtil.getTempFileNames(
+			_group.getGroupId(), _user.getUserId(), folderName);
+
+		Assert.assertEquals(
+			Arrays.toString(tempFileNames), 0, tempFileNames.length);
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			FileEntry fileEntry = TempFileEntryUtil.getTempFileEntry(
+				_group.getGroupId(), _user.getUserId(), folderName, fileName);
+
+			Assert.assertEquals(fileName, fileEntry.getFileName());
+		}
 	}
 
 	protected int getFoldersAndFileEntriesAndFileShortcutsCount(
@@ -120,6 +193,52 @@ public class TemporaryFileEntriesCapabilityTest {
 		return classLoader.getResourceAsStream(
 			"com/liferay/portal/util/dependencies/test.jpg");
 	}
+
+	private FileEntry _addExpiredTempFileEntry(
+			String folderName, String fileName)
+		throws Exception {
+
+		FileEntry fileEntry = TempFileEntryUtil.addTempFileEntry(
+			_group.getGroupId(), _user.getUserId(), folderName, fileName,
+			getInputStream(), ContentTypes.IMAGE_JPEG);
+
+		DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.getDLFileEntry(
+			fileEntry.getFileEntryId());
+
+		dlFileEntry.setCreateDate(
+			new Date(System.currentTimeMillis() - Time.WEEK));
+
+		DLFileEntryLocalServiceUtil.updateDLFileEntry(dlFileEntry);
+
+		return fileEntry;
+	}
+
+	private void _deleteExpiredTemporaryFileEntries(
+			LocalRepository localRepository)
+		throws Exception {
+
+		TemporaryFileEntriesCapability temporaryFileEntriesCapability =
+			localRepository.getCapability(TemporaryFileEntriesCapability.class);
+
+		temporaryFileEntriesCapability.setTemporaryFileEntriesTimeout(Time.DAY);
+
+		temporaryFileEntriesCapability.deleteExpiredTemporaryFileEntries();
+	}
+
+	private LocalRepository _getLocalRepository() throws Exception {
+		Repository repository = RepositoryLocalServiceUtil.fetchRepository(
+			_group.getGroupId(), TempFileEntryUtil.class.getName(),
+			TempFileEntryUtil.class.getName());
+
+		return RepositoryProviderUtil.getLocalRepository(
+			repository.getRepositoryId());
+	}
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection;
+
+	@Inject
+	private CTCollectionLocalService _ctCollectionLocalService;
 
 	@DeleteAfterTestRun
 	private Group _group;
