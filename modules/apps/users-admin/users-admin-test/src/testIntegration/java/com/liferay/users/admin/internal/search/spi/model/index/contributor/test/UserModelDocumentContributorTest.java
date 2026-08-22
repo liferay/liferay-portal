22b10d9432d36cf5441bf1abdc720e89e16be65c
Jeff Wu <jeffwu0724@gmail.com>
2026-08-22 09:29:52 -0700

    LPD-102989 Assert the address fields the contributors index
    Contribute a document outside reindex mode and another inside it, then assert the same country and region values on both. The path outside reindex mode reads each Address entity, so it is the control for the bulk query the next commit changes.
    
    Each pass gets its own document because the user contributor catches Exception: a contribution that fails writes nothing, and a reused document would keep the control's values and hide it.

---
+++ modules/apps/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/internal/search/spi/model/index/contributor/test/UserModelDocumentContributorTest.java
@@ -0,0 +1,125 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.users.admin.internal.search.spi.model.index.contributor.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.model.ListType;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.Region;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.DocumentImpl;
+import com.liferay.portal.kernel.search.ReindexCacheThreadLocal;
+import com.liferay.portal.kernel.service.AddressLocalService;
+import com.liferay.portal.kernel.service.CountryService;
+import com.liferay.portal.kernel.service.ListTypeService;
+import com.liferay.portal.kernel.service.RegionService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jiefeng Wu
+ */
+@RunWith(Arquillian.class)
+public class UserModelDocumentContributorTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testContributeAddressInReindexMode() throws Exception {
+		_user = UserTestUtil.addUser();
+
+		Country country = _countryService.getCountryByName(
+			_user.getCompanyId(), "canada");
+
+		Region region = _regionService.getRegion(country.getCountryId(), "AB");
+
+		List<ListType> listTypes = _listTypeService.getListTypes(
+			_user.getCompanyId(), ListTypeConstants.CONTACT_ADDRESS);
+
+		ListType listType = listTypes.get(0);
+
+		_addressLocalService.addAddress(
+			null, _user.getUserId(), Contact.class.getName(),
+			_user.getContactId(), country.getCountryId(),
+			listType.getListTypeId(), region.getRegionId(),
+			RandomTestUtil.randomString(), null, false, null, false,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), null, RandomTestUtil.randomString(),
+			null, new ServiceContext());
+
+		Document document = new DocumentImpl();
+
+		_userModelDocumentContributor.contribute(document, _user);
+
+		_assertAddressFields(document, region);
+
+		Document reindexModeDocument = new DocumentImpl();
+
+		try (SafeCloseable safeCloseable =
+				ReindexCacheThreadLocal.openReindexMode()) {
+
+			_userModelDocumentContributor.contribute(
+				reindexModeDocument, _user);
+		}
+
+		_assertAddressFields(reindexModeDocument, region);
+	}
+
+	private void _assertAddressFields(Document document, Region region) {
+		Assert.assertTrue(
+			ArrayUtil.contains(document.getValues("country"), "canada"));
+		Assert.assertArrayEquals(
+			new String[] {StringUtil.toLowerCase(region.getName())},
+			document.getValues("region"));
+	}
+
+	@Inject
+	private AddressLocalService _addressLocalService;
+
+	@Inject
+	private CountryService _countryService;
+
+	@Inject
+	private ListTypeService _listTypeService;
+
+	@Inject
+	private RegionService _regionService;
+
+	@DeleteAfterTestRun
+	private User _user;
+
+	@Inject(
+		filter = "component.name=com.liferay.users.admin.internal.search.spi.model.index.contributor.UserModelDocumentContributor"
+	)
+	private ModelDocumentContributor<User> _userModelDocumentContributor;
+
+}
