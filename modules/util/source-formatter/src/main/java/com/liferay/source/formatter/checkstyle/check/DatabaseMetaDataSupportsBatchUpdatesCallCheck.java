2463ca805c9e8704b1ddc4ca6294485d199ccfd6
Alan Huang <alan.huang@liferay.com>
2026-08-25 01:49:51 +0800

    LPD-103443 Use checkstyle

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/DatabaseMetaDataSupportsBatchUpdatesCallCheck.java
@@ -0,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.checkstyle.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class DatabaseMetaDataSupportsBatchUpdatesCallCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.METHOD_CALL};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		String absolutePath = getAbsolutePath();
+
+		if (absolutePath.endsWith("/AutoBatchPreparedStatementUtil.java")) {
+			return;
+		}
+
+		DetailAST dotDetailAST = detailAST.findFirstToken(TokenTypes.DOT);
+
+		if (dotDetailAST == null) {
+			return;
+		}
+
+		List<String> names = getNames(dotDetailAST, false);
+
+		if ((names.size() != 2) ||
+			!StringUtil.equals(names.get(1), "supportsBatchUpdates")) {
+
+			return;
+		}
+
+		String variableTypeName = getVariableTypeName(
+			detailAST, names.get(0), false);
+
+		if (variableTypeName.equals("DatabaseMetaData")) {
+			log(detailAST, _MSG_AVOID_METHOD_CALL);
+		}
+	}
+
+	private static final String _MSG_AVOID_METHOD_CALL = "method.call.avoid";
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/ServletResponseUtilSendFileCallCheck.java
@@ -0,0 +1,54 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.checkstyle.check;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class ServletResponseUtilSendFileCallCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.METHOD_CALL};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		String absolutePath = getAbsolutePath();
+
+		if (!absolutePath.endsWith("ResourceCommand.java")) {
+			return;
+		}
+
+		DetailAST dotDetailAST = detailAST.findFirstToken(TokenTypes.DOT);
+
+		if (dotDetailAST == null) {
+			return;
+		}
+
+		List<String> names = getNames(dotDetailAST, false);
+
+		if (names.size() != 2) {
+			return;
+		}
+
+		if (StringUtil.equals(names.get(0), "ServletResponseUtil") &&
+			StringUtil.equals(names.get(1), "sendFile")) {
+
+			log(detailAST, _MSG_AVOID_METHOD_CALL);
+		}
+	}
+
+	private static final String _MSG_AVOID_METHOD_CALL = "method.call.avoid";
+
+}
