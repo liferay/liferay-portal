25110248b720a62a6d4a028e6573dd5d566a50b2
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-08-24 11:14:28 +0200

    LPD-101811 Resolve compared version values server-side and diff every field

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/servlet/CompareObjectEntryVersionsCMSServlet.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/servlet/CompareObjectEntryVersionsCMSServlet.java
@@ -6,6 +6,10 @@
 package com.liferay.site.cms.site.initializer.internal.servlet;
 
 import com.liferay.diff.DiffHtml;
+import com.liferay.object.model.ObjectEntryVersion;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.service.ObjectEntryService;
+import com.liferay.object.service.ObjectEntryVersionLocalService;
 import com.liferay.petra.io.StreamUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONException;
@@ -23,6 +27,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.StringReader;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -64,16 +73,55 @@ public class CompareObjectEntryVersionsCMSServlet extends BaseCMSServlet {
 		}
 
 		try {
-			String diffHtml = _diffHtml.diff(
-				new StringReader(jsonObject.getString("source")),
-				new StringReader(jsonObject.getString("target")));
+			long objectEntryId = jsonObject.getLong("objectEntryId");
+
+			_objectEntryService.getObjectEntry(objectEntryId);
+
+			String languageId = jsonObject.getString("languageId");
+
+			Map<String, String> sourceFieldValues = _getFieldValues(
+				languageId, objectEntryId, jsonObject.getInt("sourceVersion"));
+			Map<String, String> targetFieldValues = _getFieldValues(
+				languageId, objectEntryId, jsonObject.getInt("targetVersion"));
+
+			JSONObject sourceDiffsJSONObject = _jsonFactory.createJSONObject();
+			JSONObject targetDiffsJSONObject = _jsonFactory.createJSONObject();
+
+			Set<String> fieldNames = new TreeSet<>(sourceFieldValues.keySet());
+
+			fieldNames.addAll(targetFieldValues.keySet());
+
+			for (String fieldName : fieldNames) {
+				String source = sourceFieldValues.getOrDefault(
+					fieldName, StringPool.BLANK);
+				String target = targetFieldValues.getOrDefault(
+					fieldName, StringPool.BLANK);
+
+				if (source.equals(target)) {
+					continue;
+				}
+
+				sourceDiffsJSONObject.put(
+					fieldName,
+					_diffHtml.diff(
+						new StringReader(target), new StringReader(source)));
+				targetDiffsJSONObject.put(
+					fieldName,
+					_diffHtml.diff(
+						new StringReader(source), new StringReader(target)));
+			}
 
 			httpServletResponse.setContentType(ContentTypes.APPLICATION_JSON);
 
 			ServletResponseUtil.write(
 				httpServletResponse,
 				JSONUtil.put(
-					"diffHtml", diffHtml
+					"diffs",
+					JSONUtil.put(
+						"source", sourceDiffsJSONObject
+					).put(
+						"target", targetDiffsJSONObject
+					)
 				).toString());
 		}
 		catch (Exception exception) {
@@ -86,6 +134,52 @@ public class CompareObjectEntryVersionsCMSServlet extends BaseCMSServlet {
 		}
 	}
 
+	private Map<String, String> _getFieldValues(
+			String languageId, long objectEntryId, int version)
+		throws Exception {
+
+		ObjectEntryVersion objectEntryVersion =
+			_objectEntryVersionLocalService.getObjectEntryVersion(
+				objectEntryId, version);
+
+		ObjectEntry objectEntry = ObjectEntry.unsafeToDTO(
+			objectEntryVersion.getContent());
+
+		Map<String, Object> properties = objectEntry.getProperties();
+
+		Object nestedProperties = properties.get("properties");
+
+		if (nestedProperties instanceof Map) {
+			properties = (Map<String, Object>)nestedProperties;
+		}
+
+		Map<String, String> fieldValues = new HashMap<>();
+
+		for (Map.Entry<String, Object> entry : properties.entrySet()) {
+			String name = entry.getKey();
+
+			if (name.endsWith("_i18n") || name.endsWith("RawText")) {
+				continue;
+			}
+
+			Object value = entry.getValue();
+
+			Object localizedValues = properties.get(name + "_i18n");
+
+			if (localizedValues instanceof Map) {
+				value = ((Map<String, Object>)localizedValues).get(languageId);
+			}
+
+			if (value == null) {
+				value = StringPool.BLANK;
+			}
+
+			fieldValues.put(name, String.valueOf(value));
+		}
+
+		return fieldValues;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CompareObjectEntryVersionsCMSServlet.class);
 
@@ -94,5 +188,11 @@ public class CompareObjectEntryVersionsCMSServlet extends BaseCMSServlet {
 
 	@Reference
 	private JSONFactory _jsonFactory;
+
+	@Reference
+	private ObjectEntryService _objectEntryService;
+
+	@Reference
+	private ObjectEntryVersionLocalService _objectEntryVersionLocalService;
 
 }
