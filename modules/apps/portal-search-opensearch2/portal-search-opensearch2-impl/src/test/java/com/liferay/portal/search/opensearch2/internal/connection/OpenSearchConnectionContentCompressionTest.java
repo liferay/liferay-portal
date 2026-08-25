8572f7c7c3ce62b292a6d696b8a92bb398a0153c
Selena Aungst <selena.aungst@liferay.com>
2026-08-25 12:20:24 -0700

    LPD-103633 portal-search-opensearch2-impl: Cover content compression.
    https://liferay.atlassian.net/browse/LPD-103633

---
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/connection/OpenSearchConnectionContentCompressionTest.java
@@ -0,0 +1,166 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.opensearch2.internal.connection;
+
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.net.InetSocketAddress;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.HttpStatus;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch.core.CountResponse;
+
+/**
+ * @author Selena Aungst
+ */
+public class OpenSearchConnectionContentCompressionTest {
+
+	@ClassRule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		_httpServer = HttpServer.create(new InetSocketAddress(0), 0);
+
+		HttpContext httpContext = _httpServer.createContext("/");
+
+		httpContext.setHandler(this::_handle);
+
+		_httpServer.start();
+	}
+
+	@After
+	public void tearDown() {
+		if (_openSearchConnection != null) {
+			_openSearchConnection.close();
+		}
+
+		_httpServer.stop(0);
+	}
+
+	@Test
+	public void testConnectWithCompressionDisabled() throws Exception {
+		_testConnect(false, null);
+	}
+
+	@Test
+	public void testConnectWithCompressionEnabled() throws Exception {
+		_testConnect(true, _GZIP);
+	}
+
+	private String _getCountJSON() {
+		return JSONUtil.put(
+			"_shards",
+			JSONUtil.put(
+				"failed", 0
+			).put(
+				"successful", 1
+			).put(
+				"total", 1
+			)
+		).put(
+			"count", _COUNT
+		).toString();
+	}
+
+	private byte[] _gzip(byte[] bytes) throws IOException {
+		ByteArrayOutputStream byteArrayOutputStream =
+			new ByteArrayOutputStream();
+
+		try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(
+				byteArrayOutputStream)) {
+
+			gzipOutputStream.write(bytes);
+		}
+
+		return byteArrayOutputStream.toByteArray();
+	}
+
+	private void _handle(HttpExchange httpExchange) throws IOException {
+		Headers requestHeaders = httpExchange.getRequestHeaders();
+
+		_acceptEncoding = requestHeaders.getFirst(HttpHeaders.ACCEPT_ENCODING);
+
+		Headers responseHeaders = httpExchange.getResponseHeaders();
+
+		byte[] bytes = _getCountJSON().getBytes(StandardCharsets.UTF_8);
+
+		if ((_acceptEncoding != null) && _acceptEncoding.contains(_GZIP)) {
+			bytes = _gzip(bytes);
+
+			responseHeaders.set(HttpHeaders.CONTENT_ENCODING, _GZIP);
+		}
+
+		responseHeaders.set(HttpHeaders.CONTENT_TYPE, "application/json");
+
+		httpExchange.sendResponseHeaders(HttpStatus.SC_OK, bytes.length);
+
+		try (OutputStream outputStream = httpExchange.getResponseBody()) {
+			outputStream.write(bytes);
+		}
+	}
+
+	private void _testConnect(
+			boolean compressionEnabled, String expectedAcceptEncoding)
+		throws Exception {
+
+		InetSocketAddress inetSocketAddress = _httpServer.getAddress();
+
+		String networkHostAddress =
+			"http://localhost:" + inetSocketAddress.getPort();
+
+		OpenSearchConnection.Builder openSearchConnectionBuilder =
+			new OpenSearchConnection.Builder();
+
+		openSearchConnectionBuilder.compressionEnabled(compressionEnabled);
+		openSearchConnectionBuilder.networkHostAddresses(
+			new String[] {networkHostAddress});
+
+		_openSearchConnection = openSearchConnectionBuilder.build();
+
+		_openSearchConnection.connect();
+
+		OpenSearchClient openSearchClient =
+			_openSearchConnection.getOpenSearchClient();
+
+		CountResponse countResponse = openSearchClient.count();
+
+		Assert.assertEquals(expectedAcceptEncoding, _acceptEncoding);
+		Assert.assertEquals(_COUNT, countResponse.count());
+	}
+
+	private static final long _COUNT = 7;
+
+	private static final String _GZIP = "gzip";
+
+	private String _acceptEncoding;
+	private HttpServer _httpServer;
+	private OpenSearchConnection _openSearchConnection;
+
+}
