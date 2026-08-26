f7996c3ec129d9faac9ff4f261adb08aed50462f
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-08-26 17:34:55 +0100

    LPD-103548 Block the interactions inside the page preview

--- modules/apps/layout/layout-content-web/src/main/resources/META-INF/resources/js/components/PagePreview.tsx
+++ modules/apps/layout/layout-content-web/src/main/resources/META-INF/resources/js/components/PagePreview.tsx
@@ -3,9 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {preventIframeNavigation} from '@liferay/layout-js-components-web';
 import React from 'react';
 
 import {config} from '../config';
+
+const BLOCKED_EVENTS = ['auxclick', 'click', 'submit'] as const;
 
 interface Props {
 	experienceId?: string;
@@ -22,11 +25,29 @@ export default function PagePreview({experienceId, languageId}: Props) {
 		params.set('segmentsExperienceId', experienceId);
 	}
 
+	const handleLoad = (event: React.SyntheticEvent<HTMLIFrameElement>) => {
+		preventIframeNavigation(event);
+
+		const iframe = event.target as HTMLIFrameElement;
+
+		const iframeDocument = iframe.contentDocument;
+
+		for (const type of BLOCKED_EVENTS) {
+			iframeDocument?.addEventListener(type, blockEvent, true);
+		}
+	};
+
 	return (
 		<iframe
 			className="version-history__preview"
+			onLoad={handleLoad}
 			src={`${config.getPagePreviewURL}?${params}`}
 			title={Liferay.Language.get('preview')}
 		/>
 	);
+}
+
+function blockEvent(event: Event) {
+	event.preventDefault();
+	event.stopImmediatePropagation();
 }
