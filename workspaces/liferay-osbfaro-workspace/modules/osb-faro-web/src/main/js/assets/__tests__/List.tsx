2f835d2a909710e9884c63de7faca25a10e77bec
Marcio Fonseca <marcio.fonseca@liferay.com>
2026-08-25 21:48:46 -0300

    LPD-103750 Update tests

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/assets/__tests__/List.tsx
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/assets/__tests__/List.tsx
@@ -15,7 +15,6 @@ jest.mock('@liferay/frontend-data-set-web', () => ({
 	...jest.requireActual('@liferay/frontend-data-set-web'),
 	FrontendDataSet: ({
 		additionalAPIURLParametersTransformer,
-		customDataRenderers,
 		emptyState,
 		filters,
 		groupedFilters,
@@ -25,7 +24,6 @@ jest.mock('@liferay/frontend-data-set-web', () => ({
 		views,
 	}: {
 		additionalAPIURLParametersTransformer?: (loadDataArgs: any) => any;
-		customDataRenderers?: {[key: string]: React.FC<any>};
 		emptyState?: {
 			description?: React.ReactNode;
 			image?: string;
@@ -72,24 +70,6 @@ jest.mock('@liferay/frontend-data-set-web', () => ({
 
 				<div data-testid="fds-fields">
 					{JSON.stringify(views?.[0]?.schema?.fields ?? null)}
-				</div>
-
-				<div data-testid="fds-object-type-cells">
-					{[undefined, 'content', 'file', 'unknown'].map((value) => {
-						const ObjectTypeCell =
-							customDataRenderers?.assetObjectTypeRenderer;
-
-						return (
-							ObjectTypeCell && (
-								<div
-									data-testid={`fds-object-type-cell-${value}`}
-									key={String(value)}
-								>
-									<ObjectTypeCell value={value} />
-								</div>
-							)
-						);
-					})}
 				</div>
 
 				{emptyState && (
@@ -569,31 +549,11 @@ describe('List', () => {
 		});
 	});
 
-	describe('object type column', () => {
+	describe('table columns', () => {
 		const getFields = () =>
 			JSON.parse(screen.getByTestId('fds-fields').textContent);
 
-		const getObjectTypeField = () => {
-			renderList();
-
-			return getFields().find(
-				(field: {fieldName: string}) => field.fieldName === 'objectType'
-			);
-		};
-
-		it('should add an object type column to the table', () => {
-			expect(getObjectTypeField()).toBeDefined();
-		});
-
-		it('should label the object type column "Object Type"', () => {
-			expect(getObjectTypeField().label).toBe('Object Type');
-		});
-
-		it('should make the object type column sortable', () => {
-			expect(getObjectTypeField().sortable).toBe(true);
-		});
-
-		it('should place the object type column after the type column', () => {
+		it('should not show an object type column', () => {
 			renderList();
 
 			const fieldNames = getFields().map(
@@ -603,43 +563,10 @@ describe('List', () => {
 			expect(fieldNames).toEqual([
 				'assetTitle',
 				'assetType',
-				'objectType',
 				'viewsMetric',
 				'impressionsMetric',
 				'downloadsMetric',
 			]);
-		});
-
-		it('should render the content object type as "Content"', () => {
-			renderList();
-
-			expect(
-				screen.getByTestId('fds-object-type-cell-content')
-			).toHaveTextContent('Content');
-		});
-
-		it('should render the file object type as "File"', () => {
-			renderList();
-
-			expect(
-				screen.getByTestId('fds-object-type-cell-file')
-			).toHaveTextContent('File');
-		});
-
-		it('should render nothing when the asset carries no object type', () => {
-			renderList();
-
-			expect(
-				screen.getByTestId('fds-object-type-cell-undefined')
-			).toHaveTextContent('');
-		});
-
-		it('should render nothing for an unrecognized object type', () => {
-			renderList();
-
-			expect(
-				screen.getByTestId('fds-object-type-cell-unknown')
-			).toHaveTextContent('');
 		});
 	});
 
@@ -779,7 +706,6 @@ describe('List', () => {
 		const SORTABLE_KEYS = [
 			'assetTitle',
 			'assetType',
-			'objectType',
 			'viewsMetric',
 			'impressionsMetric',
 			'downloadsMetric',
