99dd48cc437de9bcf2533732ada3265d1740b119
brookedalton <brooke.dalton@liferay.com>
2026-08-26 17:41:07 -0700

    LPD-103662 Check permissions before sharing a publication link

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/GetSharePublicationLinkMVCResourceCommand.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/GetSharePublicationLinkMVCResourceCommand.java
@@ -5,15 +5,25 @@
 
 package com.liferay.change.tracking.web.internal.portlet.action;
 
+import com.liferay.change.tracking.constants.CTActionKeys;
 import com.liferay.change.tracking.constants.CTPortletKeys;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.change.tracking.web.internal.helper.PublicationHelper;
+import com.liferay.change.tracking.web.internal.security.permission.resource.CTCollectionPermission;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.portlet.ResourceRequest;
 import jakarta.portlet.ResourceResponse;
@@ -45,6 +55,34 @@ public class GetSharePublicationLinkMVCResourceCommand
 		CTCollection ctCollection = _ctCollectionLocalService.getCTCollection(
 			ctCollectionId);
 
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			AuthTokenUtil.checkCSRFToken(
+				_portal.getHttpServletRequest(resourceRequest),
+				GetSharePublicationLinkMVCResourceCommand.class.getName());
+
+			CTCollectionPermission.check(
+				themeDisplay.getPermissionChecker(), ctCollection,
+				CTActionKeys.INVITE_USERS);
+		}
+		catch (PrincipalException principalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(principalException);
+			}
+
+			JSONPortletResponseUtil.writeJSON(
+				resourceRequest, resourceResponse,
+				JSONUtil.put(
+					"errorMessage",
+					_language.get(
+						themeDisplay.getLocale(),
+						"you-do-not-have-the-required-permissions")));
+
+			return;
+		}
+
 		boolean shareable = ParamUtil.getBoolean(resourceRequest, "shareable");
 
 		if (ctCollection.isShareable() != shareable) {
@@ -65,8 +103,17 @@ public class GetSharePublicationLinkMVCResourceCommand
 			));
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		GetSharePublicationLinkMVCResourceCommand.class);
+
 	@Reference
 	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private PublicationHelper _publicationHelper;
