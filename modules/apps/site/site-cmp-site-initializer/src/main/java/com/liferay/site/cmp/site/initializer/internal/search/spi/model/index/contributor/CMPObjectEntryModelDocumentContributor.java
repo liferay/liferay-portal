4ae26e278364e18cf21d999c9fa5e181c4fc74a8
shuyangzhou <shuyang.zhou@liferay.com>
2026-08-29 07:56:13 -0700

    Revert "LPD-99319 Gate the CMP document contributor on the App.CMP license"
    This reverts commit ff47ac02df99203c287c1cf843e1597b757530c8.

--- modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/search/spi/model/index/contributor/CMPObjectEntryModelDocumentContributor.java
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/search/spi/model/index/contributor/CMPObjectEntryModelDocumentContributor.java
@@ -16,8 +16,7 @@ import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.sql.dsl.expression.Predicate;
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.license.util.App;
-import com.liferay.portal.kernel.license.util.LicenseManagerUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
@@ -73,7 +72,9 @@ public class CMPObjectEntryModelDocumentContributor
 	private void _contribute(Document document, ObjectEntry objectEntry)
 		throws PortalException {
 
-		if (!LicenseManagerUtil.isAppEnabled(App.CMP)) {
+		if (!FeatureFlagManagerUtil.isEnabled(
+				objectEntry.getCompanyId(), "LPD-58677")) {
+
 			return;
 		}
 
