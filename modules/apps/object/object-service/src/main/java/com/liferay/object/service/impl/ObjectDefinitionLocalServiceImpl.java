9f49b53aa2c86e8ae6974c47fd4f2e8a525d50d0
shuyangzhou <shuyang.zhou@liferay.com>
2026-08-29 07:56:03 -0700

    Revert "LPD-103893 Fix object definition deletion with a composite key rule"
    This reverts commit 76678a598b681e8b46195dd300b4ccba3907bf93.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -26,7 +26,6 @@ import com.liferay.object.constants.ObjectDefinitionSettingConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
-import com.liferay.object.constants.ObjectValidationRuleSettingConstants;
 import com.liferay.object.definition.security.permission.resource.util.ObjectDefinitionResourcePermissionUtil;
 import com.liferay.object.definition.setting.util.ObjectDefinitionSettingUtil;
 import com.liferay.object.definition.tree.util.ObjectDefinitionTreeUtil;
@@ -87,7 +86,6 @@ import com.liferay.object.model.ObjectEntryTable;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFolder;
 import com.liferay.object.model.ObjectRelationship;
-import com.liferay.object.model.ObjectValidationRuleSetting;
 import com.liferay.object.model.impl.ObjectDefinitionImpl;
 import com.liferay.object.petra.sql.dsl.DynamicObjectDefinitionLocalizationTable;
 import com.liferay.object.petra.sql.dsl.DynamicObjectDefinitionLocalizationTableFactory;
@@ -117,7 +115,6 @@ import com.liferay.object.service.persistence.ObjectEntryPersistence;
 import com.liferay.object.service.persistence.ObjectFieldPersistence;
 import com.liferay.object.service.persistence.ObjectFolderPersistence;
 import com.liferay.object.service.persistence.ObjectRelationshipPersistence;
-import com.liferay.object.service.persistence.ObjectValidationRuleSettingPersistence;
 import com.liferay.object.system.SystemObjectDefinitionManager;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.lang.SafeCloseable;
@@ -667,15 +664,9 @@ public class ObjectDefinitionLocalServiceImpl
 			_objectLayoutLocalService.deleteObjectLayouts(
 				objectDefinition.getObjectDefinitionId());
 
-			_objectValidationRuleLocalService.deleteObjectValidationRules(
-				objectDefinition.getObjectDefinitionId());
-
 			for (ObjectRelationship objectRelationship :
 					_objectRelationshipPersistence.findByODI1_R(
 						objectDefinition.getObjectDefinitionId(), false)) {
-
-				_deleteCompositeKeyObjectValidationRule(
-					objectRelationship.getObjectFieldId2());
 
 				_objectRelationshipLocalService.deleteObjectRelationship(
 					objectRelationship);
@@ -688,6 +679,9 @@ public class ObjectDefinitionLocalServiceImpl
 				_objectRelationshipLocalService.deleteObjectRelationship(
 					objectRelationship);
 			}
+
+			_objectValidationRuleLocalService.deleteObjectValidationRules(
+				objectDefinition.getObjectDefinitionId());
 
 			_objectViewLocalService.deleteObjectViews(
 				objectDefinition.getObjectDefinitionId());
@@ -2223,21 +2217,6 @@ public class ObjectDefinitionLocalServiceImpl
 					objectDefinitionPersistence.getDataSource()),
 				dynamicObjectDefinitionTable.getTableName(), false,
 				objectField.getDBColumnName());
-		}
-	}
-
-	private void _deleteCompositeKeyObjectValidationRule(long objectFieldId)
-		throws PortalException {
-
-		ObjectValidationRuleSetting objectValidationRuleSetting =
-			_objectValidationRuleSettingPersistence.fetchByN_V(
-				ObjectValidationRuleSettingConstants.
-					NAME_COMPOSITE_KEY_OBJECT_FIELD_ID,
-				String.valueOf(objectFieldId));
-
-		if (objectValidationRuleSetting != null) {
-			_objectValidationRuleLocalService.deleteObjectValidationRule(
-				objectValidationRuleSetting.getObjectValidationRuleId());
 		}
 	}
 
@@ -4199,10 +4178,6 @@ public class ObjectDefinitionLocalServiceImpl
 
 	@Reference
 	private ObjectValidationRuleLocalService _objectValidationRuleLocalService;
-
-	@Reference
-	private ObjectValidationRuleSettingPersistence
-		_objectValidationRuleSettingPersistence;
 
 	@Reference
 	private ObjectViewLocalService _objectViewLocalService;
