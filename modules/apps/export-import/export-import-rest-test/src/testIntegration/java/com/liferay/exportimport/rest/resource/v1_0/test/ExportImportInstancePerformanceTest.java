faf415b93d6c200d7a9b200cb51efdad374c31d4
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-08-31 05:20:42 +0200

    LPD-99738 Let ExportImportInstancePerformanceTest run on CI
    ExportImportInstancePerformanceTest skipped execution on Jenkins via an Assume.assumeTrue(Validator.isNull(System.getenv("JENKINS_HOME"))) guard. Neither the test's history, nor its originating Jira tickets or pull requests, records why that guard was added, so nothing depends on it staying off CI. Removing it lets the test exercise its timing assertions automatically on CI instead of relying on a developer to run it locally by hand.
    
    Its closest sibling, ExportImportTaskResourcePerformanceTest, carried the identical guard and got the identical fix, but that change is split out to LPD-103968 (liferay-headless/liferay-portal#4191) per Jose's reviewer feedback on #4184, which asked for it to land separately from this ticket's core fix.

--- modules/apps/export-import/export-import-rest-test/src/testIntegration/java/com/liferay/exportimport/rest/resource/v1_0/test/ExportImportInstancePerformanceTest.java
+++ modules/apps/export-import/export-import-rest-test/src/testIntegration/java/com/liferay/exportimport/rest/resource/v1_0/test/ExportImportInstancePerformanceTest.java
@@ -30,8 +30,6 @@ import com.liferay.portal.background.task.model.BackgroundTask;
 import com.liferay.portal.background.task.service.BackgroundTaskLocalService;
 import com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskConstants;
 import com.liferay.portal.kernel.test.performance.PerformanceTimer;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.AssumeTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -39,7 +37,6 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.kernel.util.PropsValues;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -54,7 +51,6 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
-import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -70,13 +66,8 @@ public class ExportImportInstancePerformanceTest {
 
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new AssumeTestRule("assume"), new LiferayIntegrationTestRule());
-
-	public static void assume() {
-		Assume.assumeTrue(Validator.isNull(System.getenv("JENKINS_HOME")));
-	}
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
