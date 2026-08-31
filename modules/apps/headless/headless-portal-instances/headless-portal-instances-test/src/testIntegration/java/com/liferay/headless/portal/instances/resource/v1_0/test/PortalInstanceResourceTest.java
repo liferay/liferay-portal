8bd0da62d2a426e43dae971c0f44b26cd08f032c
Jorge Avalos <jorge.avalos@liferay.com>
2026-08-31 09:35:58 -0700

    LPD-92622 Drop the exported schema before deleting the configurations

--- modules/apps/headless/headless-portal-instances/headless-portal-instances-test/src/testIntegration/java/com/liferay/headless/portal/instances/resource/v1_0/test/PortalInstanceResourceTest.java
+++ modules/apps/headless/headless-portal-instances/headless-portal-instances-test/src/testIntegration/java/com/liferay/headless/portal/instances/resource/v1_0/test/PortalInstanceResourceTest.java
@@ -997,11 +997,11 @@ public class PortalInstanceResourceTest
 					nonexistentGroupConfiguration.getPid()));
 		}
 		finally {
+			_dropExportedSchema(companyId);
+
 			for (Configuration configuration : configurations) {
 				configuration.delete();
 			}
-
-			_dropExportedSchema(companyId);
 		}
 	}
 
