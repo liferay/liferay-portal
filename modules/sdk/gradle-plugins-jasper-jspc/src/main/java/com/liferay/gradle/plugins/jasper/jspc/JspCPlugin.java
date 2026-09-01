c8995393cece45c3712f1bb9d97b507fc29728ab
Debora Buriti <debora.buriti@liferay.com>
2026-09-01 14:30:52 -0300

    LPD-104342 Upgrade tomcat to 10.1.59 inside modules

--- modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
+++ modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
@@ -82,7 +82,7 @@ public class JspCPlugin implements Plugin<Project> {
 			"jakarta.servlet.jsp.jstl-api", "3.0.2");
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "org.apache.tomcat", "tomcat-jasper",
-			"10.1.57");
+			"10.1.59");
 		GradleUtil.addDependency(
 			project, CONFIGURATION_NAME, "org.glassfish.web",
 			"jakarta.servlet.jsp.jstl", "3.0.1", false);
