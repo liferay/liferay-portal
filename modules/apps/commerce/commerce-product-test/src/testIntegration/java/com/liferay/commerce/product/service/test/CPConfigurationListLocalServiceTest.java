3d0b63048a49c12c961d7d08f5ecb4334a7af451
Brian I. Kim <il.kim@liferay.com>
2026-08-31 21:12:07 -0700

    LPD-104150 Integration Test

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPConfigurationListLocalServiceTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPConfigurationListLocalServiceTest.java
@@ -248,6 +248,34 @@ public class CPConfigurationListLocalServiceTest {
 			WorkflowConstants.STATUS_APPROVED, cpConfigurationList.getStatus());
 	}
 
+	@Test
+	public void testUpdateExternalReferenceCode() throws Exception {
+		frutillaRule.scenario(
+			"Update the external reference code of a Product Configuration List"
+		).given(
+			"A Product Configuration List"
+		).when(
+			"Its external reference code is updated"
+		).then(
+			"The external reference code is changed"
+		);
+
+		CPConfigurationList cpConfigurationList = _addCPConfigurationList(
+			RandomTestUtil.randomString(), _commerceCatalog.getGroupId(), 0,
+			false, RandomTestUtil.randomString());
+
+		String externalReferenceCode = RandomTestUtil.randomString();
+
+		cpConfigurationList =
+			_cpConfigurationListLocalService.updateExternalReferenceCode(
+				cpConfigurationList.getCPConfigurationListId(),
+				externalReferenceCode);
+
+		Assert.assertEquals(
+			externalReferenceCode,
+			cpConfigurationList.getExternalReferenceCode());
+	}
+
 	@Rule
 	public final FrutillaRule frutillaRule = new FrutillaRule();
 
