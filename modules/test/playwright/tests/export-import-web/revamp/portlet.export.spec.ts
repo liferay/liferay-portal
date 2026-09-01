0f77c25971dce28f41220da8f6ee5fec02eb2880
Jaime Leon <jaime.leon@liferay.com>
2026-09-01 13:01:26 +0200

    LPD-104237 Cover the widget topper export and import entries

--- modules/test/playwright/tests/export-import-web/revamp/portlet.export.spec.ts
+++ modules/test/playwright/tests/export-import-web/revamp/portlet.export.spec.ts
@@ -5,9 +5,12 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
 import {documentLibraryPagesTest} from '../../../fixtures/documentLibraryPages.fixtures';
 import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
+import {pageViewModePagesTest} from '../../../fixtures/pageViewModePagesTest';
 import {productMenuPageTest} from '../../../fixtures/productMenuPageTest';
 import getRandomString from '../../../utils/getRandomString';
 import {stagingPageTest} from '../main/fixtures/stagingPageTest';
@@ -24,6 +27,17 @@ export const test = mergeTests(
 	stagingPageTest
 );
 
+const portletTest = mergeTests(
+	apiHelpersTest,
+	exportImportPagesTest,
+	featureFlagsTest({
+		'LPD-57655': {enabled: true},
+	}),
+	isolatedSiteTest,
+	loginTest(),
+	pageViewModePagesTest
+);
+
 test('Can export using the new navigation buttons', async ({
 	documentLibraryPage,
 	exportImportPage,
@@ -38,6 +52,47 @@ test('Can export using the new navigation buttons', async ({
 
 	await exportImportPage.export(exportName);
 });
+
+portletTest(
+	'Can export a portlet from its page topper menu',
+	{tag: '@LPD-104237'},
+	async ({apiHelpers, exportImportPage, page, site, widgetPagePage}) => {
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			groupId: site.id,
+			title: getRandomString(),
+			typeSettings:
+				'layout-template-id=1_column\ncolumn-1=com_liferay_document_library_web_portlet_DLPortlet\n',
+		});
+
+		await apiHelpers.headlessDelivery.postDocumentFolder(site.id);
+
+		await page.goto('/web' + site.friendlyUrlPath + layout.friendlyURL);
+
+		await widgetPagePage.clickOnAction('Documents and Media', 'Export');
+
+		await expect(
+			page.locator('.portlet-title-text', {
+				hasText: 'Export Documents and Media',
+			})
+		).toBeVisible();
+
+		await exportImportPage.clickNew();
+
+		await expect(
+			page.getByText('1 Item', {exact: true}).first()
+		).toBeVisible();
+
+		const exportName = getRandomString();
+
+		await exportImportPage.nameInput.fill(exportName);
+
+		await exportImportPage.exportButton.click();
+
+		await expect(
+			exportImportPage.taskStatusLabel(exportName)
+		).toBeVisible();
+	}
+);
 
 test(
 	'sanitizes a javascript: redirect parameter on the portlet export Cancel link',
--- modules/test/playwright/tests/export-import-web/revamp/portlet.import.spec.ts
+++ modules/test/playwright/tests/export-import-web/revamp/portlet.import.spec.ts
@@ -6,9 +6,12 @@
 import {expect, mergeTests} from '@playwright/test';
 import * as path from 'path';
 
+import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
 import {documentLibraryPagesTest} from '../../../fixtures/documentLibraryPages.fixtures';
 import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
+import {pageViewModePagesTest} from '../../../fixtures/pageViewModePagesTest';
 import {productMenuPageTest} from '../../../fixtures/productMenuPageTest';
 import getRandomString from '../../../utils/getRandomString';
 import {stagingPageTest} from '../main/fixtures/stagingPageTest';
@@ -23,6 +26,17 @@ export const test = mergeTests(
 	}),
 	loginTest(),
 	stagingPageTest
+);
+
+const portletTest = mergeTests(
+	apiHelpersTest,
+	exportImportPagesTest,
+	featureFlagsTest({
+		'LPD-57655': {enabled: true},
+	}),
+	isolatedSiteTest,
+	loginTest(),
+	pageViewModePagesTest
 );
 
 test('Can import using the new navigation buttons', async ({
@@ -47,3 +61,37 @@ test('Can import using the new navigation buttons', async ({
 		documentLibraryPage.page.getByRole('link', {name: 'LPS-205933'})
 	).toBeVisible();
 });
+
+portletTest(
+	'Can import a portlet from its page topper menu',
+	{tag: '@LPD-104237'},
+	async ({apiHelpers, exportImportPage, page, site, widgetPagePage}) => {
+		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+			groupId: site.id,
+			title: getRandomString(),
+			typeSettings:
+				'layout-template-id=1_column\ncolumn-1=com_liferay_document_library_web_portlet_DLPortlet\n',
+		});
+
+		await page.goto('/web' + site.friendlyUrlPath + layout.friendlyURL);
+
+		await widgetPagePage.clickOnAction('Documents and Media', 'Import');
+
+		await expect(
+			page.locator('.portlet-title-text', {
+				hasText: 'Import Documents and Media',
+			})
+		).toBeVisible();
+
+		await exportImportPage.newButton.click();
+
+		await exportImportPage.import({
+			folderPath: path.join(
+				__dirname,
+				'dependencies',
+				'documents.portlet.lar'
+			),
+			name: getRandomString(),
+		});
+	}
+);
