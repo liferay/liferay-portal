5d1f768a75090b7163cb8a3d10d2dc73747cc584
Mario Leandro <mario.leandro@liferay.com>
2026-09-01 10:16:14 -0300

    LPD-103691 Expose the navigation items of Server Administration

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/application/list/ServerAdminPanelApp.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/application/list/ServerAdminPanelApp.java
@@ -7,9 +7,21 @@ package com.liferay.server.admin.web.internal.application.list;
 
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.PanelAppNavigationItem;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -32,6 +44,29 @@ public class ServerAdminPanelApp extends BasePanelApp {
 	}
 
 	@Override
+	public List<PanelAppNavigationItem> getPanelAppNavigationItems(
+			HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return TransformUtil.unsafeTransformToList(
+			_TABS1_NAMES,
+			tabs1 -> new PanelAppNavigationItem(
+				_language.get(LocaleUtil.ENGLISH, tabs1),
+				PortletURLBuilder.create(
+					getPortletURL(httpServletRequest)
+				).setMVCRenderCommandName(
+					"/server_admin/view"
+				).setTabs1(
+					tabs1
+				).buildString(),
+				_language.get(themeDisplay.getLocale(), tabs1)));
+	}
+
+	@Override
 	public Portlet getPortlet() {
 		return _portlet;
 	}
@@ -40,6 +75,15 @@ public class ServerAdminPanelApp extends BasePanelApp {
 	public String getPortletId() {
 		return PortletKeys.SERVER_ADMIN;
 	}
+
+	private static final String[] _TABS1_NAMES = {
+		"resources", "log-levels", "properties", "database-migration",
+		"document-migration", "external-services", "friendly-urls", "script",
+		"shutdown", "production-readiness"
+	};
+
+	@Reference
+	private Language _language;
 
 	@Reference(
 		target = "(jakarta.portlet.name=" + PortletKeys.SERVER_ADMIN + ")"
---
+++ modules/apps/server/server-admin-web/src/test/java/com/liferay/server/admin/web/internal/application/list/ServerAdminPanelAppTest.java
@@ -0,0 +1,126 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.server.admin.web.internal.application.list;
+
+import com.liferay.application.list.PanelAppNavigationItem;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletURL;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import jakarta.portlet.PortletURL;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Mario Leandro
+ */
+public class ServerAdminPanelAppTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		ReflectionTestUtil.setFieldValue(
+			_serverAdminPanelApp, "_language", _language);
+
+		Mockito.when(
+			_httpServletRequest.getAttribute(WebKeys.THEME_DISPLAY)
+		).thenReturn(
+			_themeDisplay
+		);
+
+		Mockito.when(
+			_language.get(Mockito.eq(LocaleUtil.ENGLISH), Mockito.anyString())
+		).thenAnswer(
+			invocationOnMock -> invocationOnMock.getArgument(1)
+		);
+
+		Mockito.when(
+			_language.get(Mockito.eq(LocaleUtil.SPAIN), Mockito.anyString())
+		).thenAnswer(
+			invocationOnMock -> invocationOnMock.getArgument(1) + "-es"
+		);
+
+		Mockito.when(
+			_themeDisplay.getLocale()
+		).thenReturn(
+			LocaleUtil.SPAIN
+		);
+	}
+
+	@Test
+	public void testGetPanelAppNavigationItems() throws Exception {
+		List<PanelAppNavigationItem> panelAppNavigationItems =
+			_serverAdminPanelApp.getPanelAppNavigationItems(
+				_httpServletRequest);
+
+		String[] expectedTabs1Names = {
+			"resources", "log-levels", "properties", "database-migration",
+			"document-migration", "external-services", "friendly-urls",
+			"script", "shutdown", "production-readiness"
+		};
+
+		Assert.assertEquals(
+			panelAppNavigationItems.toString(), expectedTabs1Names.length,
+			panelAppNavigationItems.size());
+
+		for (int i = 0; i < panelAppNavigationItems.size(); i++) {
+			String tabs1 = expectedTabs1Names[i];
+
+			PanelAppNavigationItem panelAppNavigationItem =
+				panelAppNavigationItems.get(i);
+
+			Assert.assertEquals(
+				tabs1, panelAppNavigationItem.getCanonicalName());
+			Assert.assertEquals(
+				tabs1 + "-es", panelAppNavigationItem.getLabel());
+
+			String href = panelAppNavigationItem.getHref();
+
+			Assert.assertTrue(
+				href, href.contains("mvcRenderCommandName=/server_admin/view"));
+			Assert.assertTrue(href, href.contains("tabs1=" + tabs1));
+		}
+	}
+
+	private final HttpServletRequest _httpServletRequest = Mockito.mock(
+		HttpServletRequest.class);
+	private final Language _language = Mockito.mock(Language.class);
+
+	private final ServerAdminPanelApp _serverAdminPanelApp =
+		new ServerAdminPanelApp() {
+
+			@Override
+			public PortletURL getPortletURL(
+				HttpServletRequest httpServletRequest) {
+
+				return _portletURL;
+			}
+
+			private final PortletURL _portletURL = new MockLiferayPortletURL();
+
+		};
+
+	private final ThemeDisplay _themeDisplay = Mockito.mock(ThemeDisplay.class);
+
+}
