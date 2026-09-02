d1d66808fca1e960d76d42e978b35cfc5c0f6e33
Kenji Heigel <kenji.heigel@liferay.com>
2026-09-02 15:11:22 -0700

    LRCI-8140 Set up through the inherited setUp method
    One entry point keeps later setup in the same place rather than adding another named method beside it. The base class clears the cache in its own setUp, so the override chains to it the way every other overriding test in the module does.

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/clazz/group/JUnitBatchTestClassGroupTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/clazz/group/JUnitBatchTestClassGroupTest.java
@@ -30,7 +30,10 @@ public class JUnitBatchTestClassGroupTest
 	extends com.liferay.jenkins.results.parser.Test {
 
 	@Before
-	public void setUpGitRemotes() throws Exception {
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
 		setShellCommandOutput(
 			"git remote -v", mockShell(),
 			"upstream\tgit@github.com:liferay/liferay-portal.git (fetch)\n" +
