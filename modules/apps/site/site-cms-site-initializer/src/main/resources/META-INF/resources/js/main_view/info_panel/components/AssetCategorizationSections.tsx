7471891fd45e5200b9de85949a8de2e06bca2885
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-09-03 12:28:00 +0100

    LPD-104531 Group the categorization sections in a flush panel group

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/components/AssetCategorizationSections.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/components/AssetCategorizationSections.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayPanel from '@clayui/panel';
 import React, {useEffect, useMemo, useState} from 'react';
 
 import VocabularyService from '../../../common/services/VocabularyService';
@@ -78,7 +79,7 @@ export default function AssetCategorizationSections({
 	}, [assetLibraryId]);
 
 	return (
-		<>
+		<ClayPanel.Group flush>
 			<AssetCategories
 				cmsGroupId={cmsGroupId}
 				errorMessage={errorMessage}
@@ -126,6 +127,6 @@ export default function AssetCategorizationSections({
 					vocabularyId={funnelStageVocabularyId}
 				/>
 			)}
-		</>
+		</ClayPanel.Group>
 	);
 }
