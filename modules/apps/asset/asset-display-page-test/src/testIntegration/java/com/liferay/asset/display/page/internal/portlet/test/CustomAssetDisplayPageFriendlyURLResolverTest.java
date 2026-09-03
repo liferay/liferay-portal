d97d79b0d8fe752cbd7a2d43dda616adeaf02d21
Javier Moral <javier.moral@liferay.com>
2026-09-03 23:26:53 +0200

    LPD-104244 Test the explicit display page resolution in a Design Library

--- modules/apps/asset/asset-display-page-test/src/testIntegration/java/com/liferay/asset/display/page/internal/portlet/test/CustomAssetDisplayPageFriendlyURLResolverTest.java
+++ modules/apps/asset/asset-display-page-test/src/testIntegration/java/com/liferay/asset/display/page/internal/portlet/test/CustomAssetDisplayPageFriendlyURLResolverTest.java
@@ -6,9 +6,15 @@
 package com.liferay.asset.display.page.internal.portlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.depot.constants.DepotConstants;
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
 import com.liferay.layout.display.page.LayoutDisplayPageProvider;
 import com.liferay.layout.display.page.LayoutDisplayPageProviderRegistry;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.test.util.DisplayPageTemplateTestUtil;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectEntryFolderConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
@@ -22,23 +28,36 @@ import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutFriendlyURLComposite;
 import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.FeatureFlagTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.io.Serializable;
 
 import java.lang.reflect.Method;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -57,7 +76,9 @@ public class CustomAssetDisplayPageFriendlyURLResolverTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -118,6 +139,85 @@ public class CustomAssetDisplayPageFriendlyURLResolverTest {
 		Assert.assertEquals(1, actualObjectEntry.getVersion());
 	}
 
+	@FeatureFlag("LPD-57283")
+	@Test
+	@TestInfo("LPD-104244")
+	public void testGetLayoutDisplayPageObjectProviderLayout()
+		throws Exception {
+
+		FeatureFlagTestUtil.invokeFeatureFlagListeners(
+			TestPropsValues.getCompanyId(), true, "LPD-57283");
+
+		_testGetLayoutDisplayPageObjectProviderLayout();
+
+		_testGetLayoutDisplayPageObjectProviderLayoutWhenDisconnected();
+	}
+
+	private DepotEntry _addDesignLibraryDepotEntry() throws Exception {
+		DepotEntry depotEntry = _depotEntryLocalService.addDepotEntry(
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			DepotConstants.TYPE_DESIGN_LIBRARY,
+			ServiceContextTestUtil.getServiceContext());
+
+		_depotEntries.add(depotEntry);
+
+		return depotEntry;
+	}
+
+	private Layout _addDisplayPageTemplateLayout(long groupId, String name)
+		throws Exception {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			DisplayPageTemplateTestUtil.addDisplayPageTemplate(
+				groupId,
+				_portal.getClassNameId(_objectDefinition.getClassName()), null,
+				false, null, name, WorkflowConstants.STATUS_APPROVED);
+
+		return _layoutLocalService.getLayout(layoutPageTemplateEntry.getPlid());
+	}
+
+	private ObjectEntry _addObjectEntry() throws Exception {
+		return _objectEntryLocalService.addObjectEntry(
+			_group.getGroupId(), TestPropsValues.getUserId(),
+			_objectDefinition.getObjectDefinitionId(),
+			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
+			null,
+			HashMapBuilder.<String, Serializable>put(
+				"text", RandomTestUtil.randomString()
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private void _assertLayout(
+			Layout expectedLayout, String layoutFriendlyURL,
+			ObjectEntry objectEntry)
+		throws Exception {
+
+		LayoutFriendlyURLComposite layoutFriendlyURLComposite =
+			_friendlyURLResolver.getLayoutFriendlyURLComposite(
+				TestPropsValues.getCompanyId(), _group.getGroupId(), false,
+				StringBundler.concat(
+					_friendlyURLResolver.getURLSeparator(),
+					layoutFriendlyURL.substring(1), StringPool.SLASH,
+					_portal.getClassNameId(_objectDefinition.getClassName()),
+					StringPool.SLASH, objectEntry.getObjectEntryId()),
+				Collections.emptyMap(),
+				HashMapBuilder.<String, Object>put(
+					WebKeys.LOCALE, LocaleUtil.getDefault()
+				).build());
+
+		Layout layout = layoutFriendlyURLComposite.getLayout();
+
+		if (expectedLayout == null) {
+			Assert.assertNull(layout);
+
+			return;
+		}
+
+		Assert.assertEquals(expectedLayout.getPlid(), layout.getPlid());
+	}
+
 	private LayoutDisplayPageObjectProvider<?>
 			_getLayoutDisplayPageObjectProvider(
 				String friendlyURL, Map<String, String[]> params)
@@ -138,6 +238,53 @@ public class CustomAssetDisplayPageFriendlyURLResolverTest {
 			RandomTestUtil.randomLong(), friendlyURL, params);
 	}
 
+	private void _testGetLayoutDisplayPageObjectProviderLayout()
+		throws Exception {
+
+		ObjectEntry objectEntry = _addObjectEntry();
+
+		DepotEntry depotEntry = _addDesignLibraryDepotEntry();
+
+		_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+			depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		String name = RandomTestUtil.randomString();
+
+		Layout designLibraryLayout = _addDisplayPageTemplateLayout(
+			depotEntry.getGroupId(), name);
+
+		String layoutFriendlyURL = designLibraryLayout.getFriendlyURL();
+
+		_assertLayout(designLibraryLayout, layoutFriendlyURL, objectEntry);
+
+		Layout layout = _addDisplayPageTemplateLayout(
+			_group.getGroupId(), name);
+
+		_assertLayout(layout, layoutFriendlyURL, objectEntry);
+	}
+
+	private void _testGetLayoutDisplayPageObjectProviderLayoutWhenDisconnected()
+		throws Exception {
+
+		ObjectEntry objectEntry = _addObjectEntry();
+
+		DepotEntry depotEntry = _addDesignLibraryDepotEntry();
+
+		Layout designLibraryLayout = _addDisplayPageTemplateLayout(
+			depotEntry.getGroupId(), RandomTestUtil.randomString());
+
+		_assertLayout(null, designLibraryLayout.getFriendlyURL(), objectEntry);
+	}
+
+	@DeleteAfterTestRun
+	private final List<DepotEntry> _depotEntries = new ArrayList<>();
+
+	@Inject
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
+
+	@Inject
+	private DepotEntryLocalService _depotEntryLocalService;
+
 	@Inject(
 		filter = "component.name=com.liferay.asset.display.page.internal.portlet.CustomAssetDisplayPageFriendlyURLResolver"
 	)
@@ -149,6 +296,9 @@ public class CustomAssetDisplayPageFriendlyURLResolverTest {
 	@Inject
 	private LayoutDisplayPageProviderRegistry
 		_layoutDisplayPageProviderRegistry;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
 
 	private ObjectDefinition _objectDefinition;
 
