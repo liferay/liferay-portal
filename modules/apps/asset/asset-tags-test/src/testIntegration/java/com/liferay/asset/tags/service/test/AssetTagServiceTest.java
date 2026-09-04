2d7b48f472b70220ad1198cdfa114b7d922df5af
shuyangzhou <shuyang.zhou@liferay.com>
2026-09-03 21:37:47 -0700

    LPD-101861 Cover the AssetTag group ID getters with an empty array

--- modules/apps/asset/asset-tags-test/src/testIntegration/java/com/liferay/asset/tags/service/test/AssetTagServiceTest.java
+++ modules/apps/asset/asset-tags-test/src/testIntegration/java/com/liferay/asset/tags/service/test/AssetTagServiceTest.java
@@ -6,7 +6,10 @@
 package com.liferay.asset.tags.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetTagServiceUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -16,6 +19,9 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,7 +39,9 @@ public class AssetTagServiceTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -64,6 +72,50 @@ public class AssetTagServiceTest {
 		Assert.assertEquals(
 			initialTagsCount,
 			AssetTagLocalServiceUtil.getGroupTagsCount(_group.getGroupId()));
+	}
+
+	@Test
+	public void testGetGroupsTagsWithNoGroupIds() throws Exception {
+		_addTag();
+
+		List<AssetTag> assetTags = AssetTagServiceUtil.getGroupsTags(
+			new long[0]);
+
+		Assert.assertTrue(assetTags.toString(), assetTags.isEmpty());
+	}
+
+	@Test
+	public void testGetTagsCountWithNoGroupIds() throws Exception {
+		AssetTag assetTag = _addTag();
+
+		Assert.assertEquals(
+			0, AssetTagServiceUtil.getTagsCount(new long[0], null));
+		Assert.assertEquals(
+			0,
+			AssetTagServiceUtil.getTagsCount(new long[0], assetTag.getName()));
+	}
+
+	@Test
+	public void testGetTagsWithNoGroupIds() throws Exception {
+		AssetTag assetTag = _addTag();
+
+		List<AssetTag> assetTags = AssetTagServiceUtil.getTags(
+			new long[0], null, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertTrue(assetTags.toString(), assetTags.isEmpty());
+
+		assetTags = AssetTagServiceUtil.getTags(
+			new long[0], assetTag.getName(), QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS);
+
+		Assert.assertTrue(assetTags.toString(), assetTags.isEmpty());
+	}
+
+	private AssetTag _addTag() throws Exception {
+		return AssetTagLocalServiceUtil.addTag(
+			null, TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(),
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 	}
 
 	@DeleteAfterTestRun
