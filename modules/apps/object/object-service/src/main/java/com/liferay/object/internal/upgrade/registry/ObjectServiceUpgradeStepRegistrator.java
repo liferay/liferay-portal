06cbb49bf7e240948ecf7e02548ef8ef405ce3b6
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-09-04 12:01:45 +0200

    LPD-104380 Repair upgrade
    The faulty upgrade was released in 2025.Q3.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -759,6 +759,17 @@ public class ObjectServiceUpgradeStepRegistrator
 			"13.5.0", "13.6.0",
 			UpgradeProcessFactory.runSQL(
 				"delete from PLOEntry where key_ = 'model.resource.'"));
+
+		registry.register(
+			"13.5.0", "13.6.0",
+			UpgradeProcessFactory.runSQL(
+				StringBundler.concat(
+					"update ObjectField set readOnly = '",
+					ObjectFieldConstants.READ_ONLY_FALSE,
+					"' where readOnly not in ('",
+					ObjectFieldConstants.READ_ONLY_CONDITIONAL, "', '",
+					ObjectFieldConstants.READ_ONLY_FALSE, "', '",
+					ObjectFieldConstants.READ_ONLY_TRUE, "')")));
 	}
 
 	@Reference
