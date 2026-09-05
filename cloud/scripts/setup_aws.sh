3e5c0c16d98df4acdb5364c33cd63ccb19a7fdb2
Brian Chan <brian.chan@liferay.com>
2026-09-05 13:36:59 -0300

    LCD-53290 SF

--- cloud/scripts/setup_aws.sh
+++ cloud/scripts/setup_aws.sh
@@ -110,11 +110,11 @@ function _configure_s3_bucket {
 	local bucket_name="${1}"
 	local region="${2}"
 
-	alias_name="alias/tfstate-${bucket_name}"
-
 	local account_id
 
 	account_id="$(aws sts get-caller-identity --output text --query "Account")"
+
+	alias_name="alias/tfstate-${bucket_name}"
 
 	local kms_key_id
 
