cb6b7134c81f352ae640a97286f83ef592c63048
Gabriel Prates <gabsprates@gmail.com>
2026-09-07 13:35:47 +0200

    LPD-100257 Move objects in removed panel categories to Objects
    An object definition stores its panel category as a plain string, set from the object admin picker. The Applications panel reorganization dissolves ten of those categories, and a definition still pointing at one of them would render nowhere: the object keeps working through its APIs but disappears from the menu where the user expects it.
    
    A new upgrade step for schema version 13.7.0 moves every affected definition to Control Panel > Objects. The ten keys are pinned as string literals rather than constants because the constants that declared them are removed later in this reorganization, and pinning historical values is correct for an upgrade process regardless. Definitions with a blank key are untouched by construction, since blank already means the object is hidden from the menu.
    
    This must come before the reorganization deletes the categories.

---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/upgrade/v13_7_0/test/ObjectDefinitionPanelCategoryKeyUpgradeProcessTest.java
@@ -0,0 +1,112 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v13_7_0.test;
+
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Gabriel Prates
+ */
+@RunWith(Arquillian.class)
+public class ObjectDefinitionPanelCategoryKeyUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpgrade() throws Exception {
+		_blankObjectDefinition = _addObjectDefinition(StringPool.BLANK);
+		_mergedObjectDefinition = _addObjectDefinition(
+			"control_panel.search_tuning");
+		_removedObjectDefinition = _addObjectDefinition(
+			"applications_menu.applications.custom.apps");
+		_supportedObjectDefinition = _addObjectDefinition(
+			PanelCategoryKeys.CONTROL_PANEL_WORKFLOW);
+
+		for (UpgradeProcess upgradeProcess :
+				UpgradeTestUtil.getUpgradeSteps(
+					_upgradeStepRegistrator, new Version(13, 7, 0))) {
+
+			upgradeProcess.upgrade();
+		}
+
+		EntityCacheUtil.clearCache();
+
+		Assert.assertEquals(
+			StringPool.BLANK, _getPanelCategoryKey(_blankObjectDefinition));
+		Assert.assertEquals(
+			PanelCategoryKeys.CONTROL_PANEL_OBJECT,
+			_getPanelCategoryKey(_mergedObjectDefinition));
+		Assert.assertEquals(
+			PanelCategoryKeys.CONTROL_PANEL_OBJECT,
+			_getPanelCategoryKey(_removedObjectDefinition));
+		Assert.assertEquals(
+			PanelCategoryKeys.CONTROL_PANEL_WORKFLOW,
+			_getPanelCategoryKey(_supportedObjectDefinition));
+	}
+
+	private ObjectDefinition _addObjectDefinition(String panelCategoryKey)
+		throws Exception {
+
+		ObjectDefinition objectDefinition =
+			ObjectDefinitionTestUtil.addCustomObjectDefinition();
+
+		objectDefinition.setPanelCategoryKey(panelCategoryKey);
+
+		return _objectDefinitionLocalService.updateObjectDefinition(
+			objectDefinition);
+	}
+
+	private String _getPanelCategoryKey(ObjectDefinition objectDefinition) {
+		ObjectDefinition persistedObjectDefinition =
+			_objectDefinitionLocalService.fetchObjectDefinition(
+				objectDefinition.getObjectDefinitionId());
+
+		return persistedObjectDefinition.getPanelCategoryKey();
+	}
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _blankObjectDefinition;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _mergedObjectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _removedObjectDefinition;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _supportedObjectDefinition;
+
+	@Inject(
+		filter = "component.name=com.liferay.object.internal.upgrade.registry.ObjectServiceUpgradeStepRegistrator"
+	)
+	private UpgradeStepRegistrator _upgradeStepRegistrator;
+
+}
