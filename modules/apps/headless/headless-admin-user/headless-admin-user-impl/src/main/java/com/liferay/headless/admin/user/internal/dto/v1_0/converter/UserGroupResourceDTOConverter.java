870820780376b4d9f612c62bc9ce3efcd101e5a8
Carlos Correa <carlos.correa@liferay.com>
2026-09-08 12:36:11 +0200

    LPD-104832 Drop the service ranking of the User and UserGroup DTOConverters
    LPD-61121 gave them the maximum ranking so the Search API would pick them over the other converters of the same type. The default marker now serves that purpose, and a maximum ranking would prevent any module from replacing them.

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserGroupResourceDTOConverter.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserGroupResourceDTOConverter.java
@@ -36,8 +36,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	property = {
 		"default=true",
-		"dto.class.name=com.liferay.portal.kernel.model.UserGroup",
-		"service.ranking:Integer=" + Integer.MAX_VALUE
+		"dto.class.name=com.liferay.portal.kernel.model.UserGroup"
 	},
 	service = DTOConverter.class
 )
--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserResourceDTOConverter.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserResourceDTOConverter.java
@@ -90,8 +90,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	property = {
 		"application.name=Liferay.Headless.Admin.User", "default=true",
-		"dto.class.name=com.liferay.portal.kernel.model.User",
-		"service.ranking:Integer=" + Integer.MAX_VALUE, "version=v1.0"
+		"dto.class.name=com.liferay.portal.kernel.model.User", "version=v1.0"
 	},
 	service = DTOConverter.class
 )
