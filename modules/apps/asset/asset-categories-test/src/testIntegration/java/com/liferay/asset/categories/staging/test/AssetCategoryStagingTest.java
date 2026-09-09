d75b917642cf2e9d42c8d3820f1aa579f606c560
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-09-09 09:44:58 +0200

    LPD-102853 Move the staging category search test to its own package
    The reviewer pointed out that AssetCategoryStagingSearchTest was the
    only file of its kind (git ls-files **StagingSearchTest** matched
    nothing else), while a "staging test for an entity" already has an
    established home elsewhere in the codebase: <module>/staging/test/
    <Entity>StagingTest.java, used by AssetListEntryStagingTest,
    DepotEntryStagingTest, DDMFormInstanceStagingTest, and, within this
    same PR, FragmentEntryLinkStagingTest. This moves the new test into
    that same shape instead of inventing a one-off search/test variant.

--- modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/staging/test/AssetCategoryStagingTest.java
+++ modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/staging/test/AssetCategoryStagingTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.asset.categories.search.test;
+package com.liferay.asset.categories.staging.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetCategory;
@@ -56,7 +56,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 @Sync
-public class AssetCategoryStagingSearchTest {
+public class AssetCategoryStagingTest {
 
 	@ClassRule
 	@Rule
