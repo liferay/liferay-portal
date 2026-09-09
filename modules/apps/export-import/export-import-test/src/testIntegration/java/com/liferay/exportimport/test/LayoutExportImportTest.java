ce0b57f1ed53cb519a6b61b8e511dd28e3113368
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-09-09 19:26:54 +0200

    LPD-104854 Reuse UserTestUtil.setUser instead of hand-rolling it
    UserTestUtil.setUser(User) already performs the same PrincipalThreadLocal.setName plus PermissionThreadLocal.setPermissionChecker sequence this test was hand-rolling, and the same test class already imports and calls it in setUp(). Calling it here instead of duplicating the two lines removes the now-unused PermissionCheckerFactoryUtil import as well.

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
@@ -86,7 +86,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
@@ -1089,10 +1088,7 @@ public class LayoutExportImportTest extends BaseExportImportTestCase {
 				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
 					_company.getCompanyId())) {
 
-			PermissionThreadLocal.setPermissionChecker(
-				PermissionCheckerFactoryUtil.create(adminUser));
-
-			PrincipalThreadLocal.setName(adminUser.getUserId());
+			UserTestUtil.setUser(adminUser);
 
 			Group newCompanyGroup = GroupTestUtil.addGroup(
 				_company.getCompanyId(), adminUser.getUserId(),
