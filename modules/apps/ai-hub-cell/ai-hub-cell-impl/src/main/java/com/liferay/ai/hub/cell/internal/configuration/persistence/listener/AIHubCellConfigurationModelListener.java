4e424f5f351be2a7b11c7c936526a57fc75da895
Pedro Leite <pedro.leite@liferay.com>
2026-09-09 16:09:07 -0300

    LPD-105255 Grant the AI Hub Cell OAuth2 application the Bulk REST scope

--- modules/apps/ai-hub-cell/ai-hub-cell-impl/src/main/java/com/liferay/ai/hub/cell/internal/configuration/persistence/listener/AIHubCellConfigurationModelListener.java
+++ modules/apps/ai-hub-cell/ai-hub-cell-impl/src/main/java/com/liferay/ai/hub/cell/internal/configuration/persistence/listener/AIHubCellConfigurationModelListener.java
@@ -109,6 +109,7 @@ public class AIHubCellConfigurationModelListener
 				company.getPortalURL(0), 0, null, "AI Hub Cell", null,
 				Arrays.asList(), false,
 				Arrays.asList(
+					"Liferay.Bulk.REST.everything.write",
 					"Liferay.Headless.Admin.Taxonomy.everything.read",
 					"Liferay.Headless.Admin.User.everything.read",
 					"Liferay.Headless.Batch.Engine.everything",
