d77a0ee76319a9a7630874ccc6d6e1f7a9dd87e7
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2026-09-10 19:11:31 -0300

    LPD-105487 Remove testSpanish method from OpenSearchMultiLanguageSortTest and back to use BaseMultiLanguageSortTestCase https://liferay.atlassian.net/browse/LPD-105487

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/sort/OpenSearchMultiLanguageSortTest.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/sort/OpenSearchMultiLanguageSortTest.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.search.opensearch2.internal.sort;
 
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.search.opensearch2.internal.OpenSearchTestRule;
 import com.liferay.portal.search.opensearch2.internal.indexing.LiferayOpenSearchIndexingFixtureFactory;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
@@ -13,7 +12,6 @@ import com.liferay.portal.search.test.util.sort.BaseMultiLanguageSortTestCase;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.ClassRule;
-import org.junit.Test;
 
 /**
  * @author Vagner B.C
@@ -29,17 +27,6 @@ public class OpenSearchMultiLanguageSortTest
 	@ClassRule
 	public static final OpenSearchTestRule openSearchTestRule =
 		OpenSearchTestRule.INSTANCE;
-
-	@Override
-	@Test
-	public void testSpanish() {
-		testLocaleSort(
-			LocaleUtil.SPAIN,
-			new String[] {
-				"a", "é", "d", "c", "cu", "ch", "ll", "ña", "nu", "p", "e", "á"
-			},
-			"[a, á, c, ch, cu, d, e, é, ll, ña, nu, p]");
-	}
 
 	@Override
 	protected IndexingFixture createIndexingFixture() throws Exception {
