f80a0ce06706b095ce6620f59f73da42265bbd68
Alberto Chaparro <alberto.chaparro@liferay.com>
2026-09-11 01:23:50 +0200

    LPD-103532 Model the backport schema versions as dummy steps
    The backports register the classTypeKey upgrade as 6.0.0 to 6.0.1 on release-2026.q1 and as 6.1.0 to 6.1.1 on release-2026.q3. Master represented those versions by duplicating the 6.1.0 and 6.2.0 steps with a second source version, which forks the upgrade graph and repeats each step's construction. Registering 6.0.0 to 6.0.1 and 6.1.0 to 6.1.1 as dummy steps keeps the chain linear, matches how this registrator already models release-only versions, and is safe because the idempotent 6.2.0 to 6.2.1 step populates the column later in the same chain.

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/registry/LayoutPageTemplateServiceUpgradeStepRegistrator.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/registry/LayoutPageTemplateServiceUpgradeStepRegistrator.java
@@ -263,11 +263,7 @@ public class LayoutPageTemplateServiceUpgradeStepRegistrator
 			new com.liferay.layout.page.template.internal.upgrade.v6_0_0.
 				LayoutPageTemplateStructureRelUpgradeProcess());
 
-		registry.register(
-			"6.0.0", "6.1.0",
-			LayoutPageTemplateStructureRelElementVariationAudienceEntryRelTable.
-				create(),
-			LayoutPageTemplateStructureRelElementVariationTable.create());
+		registry.register("6.0.0", "6.0.1", new DummyUpgradeStep());
 
 		registry.register(
 			"6.0.1", "6.1.0",
@@ -275,12 +271,7 @@ public class LayoutPageTemplateServiceUpgradeStepRegistrator
 				create(),
 			LayoutPageTemplateStructureRelElementVariationTable.create());
 
-		registry.register(
-			"6.1.0", "6.2.0",
-			new com.liferay.layout.page.template.internal.upgrade.v6_2_0.
-				LayoutPageTemplateStructureRelUpgradeProcess(
-					_layoutLocalService, _segmentsExperienceLocalService,
-					_userLocalService));
+		registry.register("6.1.0", "6.1.1", new DummyUpgradeStep());
 
 		registry.register(
 			"6.1.1", "6.2.0",
