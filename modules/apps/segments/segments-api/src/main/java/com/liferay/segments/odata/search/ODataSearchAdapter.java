cff7b96266dc1bd3d3cd0b7d50089272d78ca44e
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2026-09-10 00:22:46 -0700

    LPD-105334 Add ODataSearchAdapter#searchPrimaryKeys

--- modules/apps/segments/segments-api/src/main/java/com/liferay/segments/odata/search/ODataSearchAdapter.java
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/odata/search/ODataSearchAdapter.java
@@ -6,7 +6,9 @@
 package com.liferay.segments.odata.search;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.filter.FilterParser;
 
@@ -27,5 +29,27 @@ public interface ODataSearchAdapter {
 			long companyId, FilterParser filterParser, String filterString,
 			String className, EntityModel entityModel, Locale locale)
 		throws PortalException;
+
+	public default long[] searchPrimaryKeys(
+			long companyId, FilterParser filterParser, String filterString,
+			String className, EntityModel entityModel, Locale locale,
+			String primaryKeyFieldName, int start, int end)
+		throws PortalException {
+
+		Hits hits = search(
+			companyId, filterParser, filterString, className, entityModel,
+			locale, start, end);
+
+		Document[] documents = hits.getDocs();
+
+		long[] primaryKeys = new long[documents.length];
+
+		for (int i = 0; i < documents.length; i++) {
+			primaryKeys[i] = GetterUtil.getLong(
+				documents[i].get(primaryKeyFieldName));
+		}
+
+		return primaryKeys;
+	}
 
 }
