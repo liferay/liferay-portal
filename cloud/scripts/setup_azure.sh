901a867cb3530b0ef3541bdadd0852919ea9d3d0
Carlos Alvarez <carlos.alvarez@liferay.com>
2026-09-10 13:37:05 -0300

    LCD-53217 Pass the Azure observability settings from the bootstrap into Helm chart

--- cloud/scripts/setup_azure.sh
+++ cloud/scripts/setup_azure.sh
@@ -210,6 +210,10 @@ function _get_observability_parameters {
 				value: ($platform_module_outputs.observability_identity_client_id.value // "")
 			},
 			{
+				name: "azure.prometheusWorkspaceEndpoint",
+				value: ($platform_module_outputs.prometheus_workspace_endpoint.value // "")
+			},
+			{
 				name: "azure.remoteWrite.dataCollectionRuleId",
 				value: ($platform_module_outputs.prometheus_data_collection_rule_id.value // "")
 			},
@@ -224,6 +228,18 @@ function _get_observability_parameters {
 			{
 				name: "cloudProvider",
 				value: "azure"
+			},
+			{
+				name: "grafana.grafana\\.ini.azure.workload_identity_client_id",
+				value: ($platform_module_outputs.observability_identity_client_id.value // "")
+			},
+			{
+				name: "grafana.grafana\\.ini.azure.workload_identity_tenant_id",
+				value: $tenant_id
+			},
+			{
+				name: "grafana.serviceAccount.annotations.azure\\.workload\\.identity/client-id",
+				value: ($platform_module_outputs.observability_identity_client_id.value // "")
 			}
 		]
 		| map(select(.value != ""))'
