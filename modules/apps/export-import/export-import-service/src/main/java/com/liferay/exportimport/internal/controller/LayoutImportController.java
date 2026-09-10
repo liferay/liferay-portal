857e22438c8e191f07904f15aa21fc75cdd69cc6
Gabor Komaromi <gabor.komaromi@liferay.com>
2026-09-10 15:05:04 +0200

    LPD-105235 Capture the publishing user during layout staging import

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
@@ -193,6 +193,14 @@ public class LayoutImportController implements ImportController {
 				portletDataContext = getPortletDataContext(
 					exportImportConfiguration, zipReader);
 
+				Map<String, String[]> parameterMap =
+					portletDataContext.getParameterMap();
+
+				ExportImportThreadLocal.setLastImportUserName(
+					MapUtil.getString(parameterMap, "lastImportUserName"));
+				ExportImportThreadLocal.setLastImportUserUuid(
+					MapUtil.getString(parameterMap, "lastImportUserUuid"));
+
 				_exportImportLifecycleManager.fireExportImportLifecycleEvent(
 					ExportImportLifecycleConstants.EVENT_LAYOUT_IMPORT_STARTED,
 					getProcessFlag(),
@@ -234,6 +242,10 @@ public class LayoutImportController implements ImportController {
 				throwable);
 
 			throw throwable;
+		}
+		finally {
+			ExportImportThreadLocal.setLastImportUserName(null);
+			ExportImportThreadLocal.setLastImportUserUuid(null);
 		}
 	}
 
