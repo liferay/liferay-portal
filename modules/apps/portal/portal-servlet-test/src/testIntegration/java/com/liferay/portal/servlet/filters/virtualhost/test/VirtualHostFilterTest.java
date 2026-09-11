74742180e8a5c55eaa260e7336c56549628e286c
Saurasish Basak <saurasish.basak@liferay.com>
2026-09-11 15:38:05 +0530

    LPD-105342 Test that a missing file entry URL is not forwarded
    The two new tests cover the file entry path forms that Documents and Media generates, while the third asserts that an unknown path under the documents mapping still reaches the page pipeline.

--- modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/virtualhost/test/VirtualHostFilterTest.java
+++ modules/apps/portal/portal-servlet-test/src/testIntegration/java/com/liferay/portal/servlet/filters/virtualhost/test/VirtualHostFilterTest.java
@@ -7,6 +7,7 @@ package com.liferay.portal.servlet.filters.virtualhost.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -93,6 +94,21 @@ public class VirtualHostFilterTest {
 		_portalUtil.setPortal(_portal);
 
 		_virtualHostFilter.destroy();
+	}
+
+	@Test
+	public void testProcessFilterDoesNotForwardMissingFileEntryURL() {
+		Assert.assertNull(
+			_getForwardedDocumentURL(
+				StringBundler.concat(
+					"/d/", RandomTestUtil.randomString(), StringPool.SLASH,
+					RandomTestUtil.randomString())));
+
+		Assert.assertNull(
+			_getForwardedDocumentURL(
+				StringBundler.concat(
+					StringPool.SLASH, RandomTestUtil.randomLong(), "/0/",
+					RandomTestUtil.randomString())));
 	}
 
 	@Test
@@ -190,7 +206,8 @@ public class VirtualHostFilterTest {
 
 			Assert.assertEquals(
 				"/group" + groupFriendlyURL + "/home",
-				_getForwardedURL(_privateLayoutSet, "/home"));
+				_getForwardedURL(
+					_getMockHttpServletRequest(_privateLayoutSet, "/home")));
 		}
 		catch (PortalException portalException) {
 			throw new RuntimeException(portalException);
@@ -227,11 +244,20 @@ public class VirtualHostFilterTest {
 
 			Assert.assertEquals(
 				"/web" + groupFriendlyURL + "/home",
-				_getForwardedURL(null, groupFriendlyURL + "/home"));
+				_getForwardedURL(
+					_getMockHttpServletRequest(
+						null, groupFriendlyURL + "/home")));
 		}
 		catch (PortalException portalException) {
 			throw new RuntimeException(portalException);
 		}
+	}
+
+	@Test
+	public void testProcessFilterForwardsUnknownDocumentsURL() {
+		Assert.assertNotNull(
+			_getForwardedDocumentURL(
+				StringPool.SLASH + RandomTestUtil.randomString()));
 	}
 
 	@Test
@@ -294,9 +320,17 @@ public class VirtualHostFilterTest {
 		}
 	}
 
-	private String _getForwardedURL(LayoutSet layoutSet, String requestURI) {
+	private String _getForwardedDocumentURL(String path) {
 		MockHttpServletRequest mockHttpServletRequest =
-			_getMockHttpServletRequest(layoutSet, requestURI);
+			_getMockHttpServletRequest("/documents" + path);
+
+		mockHttpServletRequest.setPathInfo(path);
+
+		return _getForwardedURL(mockHttpServletRequest);
+	}
+
+	private String _getForwardedURL(
+		MockHttpServletRequest mockHttpServletRequest) {
 
 		MockHttpServletResponse mockHttpServletResponse =
 			new MockHttpServletResponse();
@@ -316,24 +350,7 @@ public class VirtualHostFilterTest {
 	}
 
 	private String _getForwardedURL(String requestURI) {
-		MockHttpServletRequest mockHttpServletRequest =
-			_getMockHttpServletRequest(requestURI);
-
-		MockHttpServletResponse mockHttpServletResponse =
-			new MockHttpServletResponse();
-
-		_virtualHostFilter.init(new MockFilterConfig());
-
-		ReflectionTestUtil.invoke(
-			_virtualHostFilter, "processFilter",
-			new Class<?>[] {
-				HttpServletRequest.class, HttpServletResponse.class,
-				FilterChain.class
-			},
-			mockHttpServletRequest, mockHttpServletResponse,
-			new MockFilterChain());
-
-		return mockHttpServletResponse.getForwardedUrl();
+		return _getForwardedURL(_getMockHttpServletRequest(requestURI));
 	}
 
 	private String _getGroupFriendlyURL(LayoutSet layoutSet)
