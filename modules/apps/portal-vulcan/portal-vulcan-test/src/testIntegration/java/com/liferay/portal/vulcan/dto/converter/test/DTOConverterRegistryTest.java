8a7d9ea2ad6b0ea377b56a2db882508cf34e75d3
Carlos Correa <carlos.correa@liferay.com>
2026-09-11 14:46:09 +0200

    LPD-104832 Simplify and sort

--- modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/dto/converter/test/DTOConverterRegistryTest.java
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/dto/converter/test/DTOConverterRegistryTest.java
@@ -123,16 +123,15 @@ public class DTOConverterRegistryTest {
 	public void testGetDTOConverterWithDefaultProperty() throws Exception {
 		String dtoClassName = RandomTestUtil.randomString();
 
-		DTOConverter<?, ?> defaultDTOConverter = new TestDTOConverter();
-		DTOConverter<?, ?> nondefaultDTOConverter = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter = new TestDTOConverter();
 
-		try (AutoCloseable autoCloseable1 = _registerDTOConverter(
-				null, dtoClassName, nondefaultDTOConverter, null);
-			AutoCloseable autoCloseable2 = _registerDefaultDTOConverter(
-				dtoClassName, defaultDTOConverter, null)) {
+		try (AutoCloseable autoCloseable1 = _registerDefaultDTOConverter(
+				dtoClassName, dtoConverter, null);
+			AutoCloseable autoCloseable2 = _registerDTOConverter(
+				null, dtoClassName, new TestDTOConverter(), null)) {
 
 			Assert.assertSame(
-				defaultDTOConverter,
+				dtoConverter,
 				_dtoConverterRegistry.getDTOConverter(dtoClassName));
 		}
 	}
@@ -143,17 +142,16 @@ public class DTOConverterRegistryTest {
 
 		String dtoClassName = RandomTestUtil.randomString();
 
-		DTOConverter<?, ?> defaultDTOConverter = new TestDTOConverter();
-		DTOConverter<?, ?> highRankingDTOConverter = new TestDTOConverter();
+		DTOConverter<?, ?> dtoConverter = new TestDTOConverter();
 
-		try (AutoCloseable autoCloseable1 =
+		try (AutoCloseable autoCloseable1 = _registerDefaultDTOConverter(
+				dtoClassName, dtoConverter, null);
+			AutoCloseable autoCloseable2 =
 				_registerDTOConverterWithServiceRanking(
-					dtoClassName, highRankingDTOConverter, Integer.MAX_VALUE);
-			AutoCloseable autoCloseable2 = _registerDefaultDTOConverter(
-				dtoClassName, defaultDTOConverter, null)) {
+					dtoClassName, new TestDTOConverter(), Integer.MAX_VALUE)) {
 
 			Assert.assertSame(
-				defaultDTOConverter,
+				dtoConverter,
 				_dtoConverterRegistry.getDTOConverter(dtoClassName));
 		}
 	}
