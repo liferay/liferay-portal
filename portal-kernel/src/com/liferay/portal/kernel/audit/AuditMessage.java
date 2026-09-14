fc98988d064f0b0d3817aa809d7eed6aa5e73980
Christian Moura <christian.moura@liferay.com>
2026-09-14 15:19:37 -0300

    LPD-97723 Capture the correlation ID at audit message construction

--- portal-kernel/src/com/liferay/portal/kernel/audit/AuditMessage.java
+++ portal-kernel/src/com/liferay/portal/kernel/audit/AuditMessage.java
@@ -69,6 +69,7 @@ public class AuditMessage implements Serializable {
 
 		_clientHost = auditRequestThreadLocal.getClientHost();
 		_clientIP = auditRequestThreadLocal.getClientIP();
+		_correlationId = auditRequestThreadLocal.getCorrelationId();
 		_requestId = auditRequestThreadLocal.getRequestId();
 		_requestIdGenerated = auditRequestThreadLocal.isRequestIdGenerated();
 		_serverName = auditRequestThreadLocal.getServerName();
