b397ff27d47011b191333a6059a7d2b27a4af8b2
Nícolas Moura <nicolas.moura@liferay.com>
2026-09-14 17:09:14 -0300

    LPD-103300 Test that the default language comes from the label
    Service Builder derives getDefaultLanguageId from the first localized column declared on the entity, so moving the description column above the label would silently make every existing row report an empty default language. Nothing about that failure surfaces at compile time, and the locale fallback the generated OpenAPI relies on would degrade without any test turning red.

--- modules/apps/object/object-service/src/test/java/com/liferay/object/model/impl/ObjectFieldImplTest.java
+++ modules/apps/object/object-service/src/test/java/com/liferay/object/model/impl/ObjectFieldImplTest.java
@@ -8,9 +8,18 @@ package com.liferay.object.model.impl;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.model.ObjectField;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.language.LanguageImpl;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.portal.util.LocalizationImpl;
+
+import java.util.Collections;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,11 +34,38 @@ public class ObjectFieldImplTest {
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
 
+	@Before
+	public void setUp() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(new LanguageImpl());
+
+		LocalizationUtil localizationUtil = new LocalizationUtil();
+
+		localizationUtil.setLocalization(new LocalizationImpl());
+	}
+
 	@Test
 	public void testGetAttachmentDownloadActionKey() {
 		_testGetAttachmentDownloadActionKey("formatId", "DOWNLOAD_FORMAT_ID");
 		_testGetAttachmentDownloadActionKey(
 			"friendlyURLMapper", "DOWNLOAD_FRIENDLY_URL_MAPPER");
+	}
+
+	@Test
+	public void testGetDefaultLanguageId() {
+		ObjectField objectField = new ObjectFieldImpl();
+
+		objectField.setDescriptionMap(
+			Collections.singletonMap(
+				LocaleUtil.SPAIN, RandomTestUtil.randomString()),
+			LocaleUtil.SPAIN);
+		objectField.setLabelMap(
+			Collections.singletonMap(
+				LocaleUtil.GERMANY, RandomTestUtil.randomString()),
+			LocaleUtil.GERMANY);
+
+		Assert.assertEquals("de_DE", objectField.getDefaultLanguageId());
 	}
 
 	@Test
