bf8edc8a0f6e97d190ac4d29b4966653245fee83
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-09-14 12:00:53 +0200

    LPD-104909 Resolve only asset library groups on the user account route

--- modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/resource/v1_0/UserAccountResourceImpl.java
+++ modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/resource/v1_0/UserAccountResourceImpl.java
@@ -149,7 +149,7 @@ public class UserAccountResourceImpl extends BaseUserAccountResourceImpl {
 		Group group = _groupService.fetchGroupByExternalReferenceCode(
 			externalReferenceCode, contextCompany.getCompanyId());
 
-		if (group == null) {
+		if ((group == null) || !group.isDepot()) {
 			throw new NoSuchGroupException(
 				"No group exists with external reference code " +
 					externalReferenceCode);
