c26428b1237abe5dd8ae4c2b3a1c67369eadef7a
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-09-14 11:57:57 +0200

    LPD-104916 Check MANAGE_TAG on each asset library in putKeyword

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/KeywordResourceImpl.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/KeywordResourceImpl.java
@@ -340,14 +340,19 @@ public class KeywordResourceImpl
 	public Keyword putKeyword(Long keywordId, Keyword keyword)
 		throws Exception {
 
+		long[] assetLibraryGroupIds = TaxonomyGroupUtil.getAssetLibraryGroupIds(
+			keyword.getAssetLibraries(), contextCompany.getCompanyId());
+
+		if (ArrayUtil.isNotEmpty(keyword.getAssetLibraries())) {
+			_checkAssetLibraryGroupIdsPermission(assetLibraryGroupIds);
+		}
+
 		AssetTag assetTag = _assetTagService.updateTag(
 			keyword.getExternalReferenceCode(), keywordId, keyword.getName(),
 			null);
 
 		_assetTagGroupRelLocalService.setAssetTagGroupRels(
-			assetTag.getTagId(),
-			TaxonomyGroupUtil.getAssetLibraryGroupIds(
-				keyword.getAssetLibraries(), assetTag.getCompanyId()));
+			assetTag.getTagId(), assetLibraryGroupIds);
 
 		return _toKeyword(assetTag);
 	}
@@ -447,11 +452,7 @@ public class KeywordResourceImpl
 		long[] assetLibraryGroupIds = TaxonomyGroupUtil.getAssetLibraryGroupIds(
 			keyword.getAssetLibraries(), group.getCompanyId());
 
-		for (long assetLibraryGroupId : assetLibraryGroupIds) {
-			AssetTagsPermission.check(
-				PermissionThreadLocal.getPermissionChecker(),
-				assetLibraryGroupId, ActionKeys.MANAGE_TAG);
-		}
+		_checkAssetLibraryGroupIdsPermission(assetLibraryGroupIds);
 
 		AssetTag assetTag = _assetTagLocalService.addTag(
 			externalReferenceCode, contextUser.getUserId(), siteId,
@@ -461,6 +462,17 @@ public class KeywordResourceImpl
 			assetTag.getTagId(), assetLibraryGroupIds);
 
 		return assetTag;
+	}
+
+	private void _checkAssetLibraryGroupIdsPermission(
+			long[] assetLibraryGroupIds)
+		throws Exception {
+
+		for (long assetLibraryGroupId : assetLibraryGroupIds) {
+			AssetTagsPermission.check(
+				PermissionThreadLocal.getPermissionChecker(),
+				assetLibraryGroupId, ActionKeys.MANAGE_TAG);
+		}
 	}
 
 	private Page<Keyword> _getKeywordsPage(
