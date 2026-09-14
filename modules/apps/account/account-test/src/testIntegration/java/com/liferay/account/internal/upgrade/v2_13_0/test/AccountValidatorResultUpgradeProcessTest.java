54098bf98855ed1e0dd7d558f05dbdd54250e22b
Tancredi Covioli <tancredi.covioli@liferay.com>
2026-09-14 15:27:11 +0200

    LPD-105659 Adds integration tests for upgrade process

---
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/internal/upgrade/v2_13_0/test/AccountValidatorResultUpgradeProcessTest.java
@@ -0,0 +1,127 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.account.internal.upgrade.v2_13_0.test;
+
+import com.liferay.account.service.test.util.AccountEntryValidatorResultTestUtil;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.constants.ObjectRelationshipConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectRelationship;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.portal.kernel.cache.CacheRegistryUtil;
+import com.liferay.portal.kernel.dao.orm.EntityCache;
+import com.liferay.portal.kernel.dao.orm.FinderCache;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Tancredi Covioli
+ */
+@FeatureFlag("LPD-89850")
+@RunWith(Arquillian.class)
+public class AccountValidatorResultUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		AccountEntryValidatorResultTestUtil.getOrAddObjectDefinition(
+			AccountValidatorResultUpgradeProcessTest.class);
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_ACCOUNT", TestPropsValues.getCompanyId());
+
+		_objectRelationship =
+			_objectRelationshipLocalService.
+				fetchObjectRelationshipByExternalReferenceCode(
+					"L_ACCOUNT_TO_L_ACCOUNT_VALIDATOR_RESULTS",
+					TestPropsValues.getCompanyId(),
+					objectDefinition.getObjectDefinitionId());
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		_objectRelationshipLocalService.updateObjectRelationship(
+			_objectRelationship.getExternalReferenceCode(),
+			_objectRelationship.getObjectRelationshipId(),
+			_objectRelationship.getParameterObjectFieldId(),
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			_objectRelationship.isEdge(), _objectRelationship.getLabelMap(),
+			null);
+
+		_runUpgrade();
+
+		ObjectRelationship objectRelationship =
+			_objectRelationshipLocalService.getObjectRelationship(
+				_objectRelationship.getObjectRelationshipId());
+
+		Assert.assertEquals(
+			ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
+			objectRelationship.getDeletionType());
+	}
+
+	private void _runUpgrade() throws Exception {
+		UpgradeProcess[] upgradeProcesses = UpgradeTestUtil.getUpgradeSteps(
+			_upgradeStepRegistrator, new Version(2, 13, 0));
+
+		Assert.assertEquals(
+			Arrays.toString(upgradeProcesses), 1, upgradeProcesses.length);
+
+		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
+			upgradeProcess.upgrade();
+		}
+
+		CacheRegistryUtil.clear();
+
+		_entityCache.clearCache();
+		_finderCache.clearCache();
+	}
+
+	@Inject
+	private EntityCache _entityCache;
+
+	@Inject
+	private FinderCache _finderCache;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	private ObjectRelationship _objectRelationship;
+
+	@Inject
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.account.internal.upgrade.registry.AccountServiceUpgradeStepRegistrator))"
+	)
+	private UpgradeStepRegistrator _upgradeStepRegistrator;
+
+}
