b7a82804d93e1994ecf068e93b6e3f44094394b5
Javier Moral <javier.moral@liferay.com>
2026-09-14 15:41:06 +0200

    LPD-104653 Mirror the design library API package in the contributors
    The contributors implement com.liferay.design.library.resource.type.DesignLibraryResourceTypeContributor, so their own package must mirror the API package it comes from instead of stopping at com.liferay.<module>.web.internal.design.library.

--- modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/design/library/resource/type/StyleBookDesignLibraryResourceTypeContributor.java
+++ modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/design/library/resource/type/StyleBookDesignLibraryResourceTypeContributor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.style.book.web.internal.design.library;
+package com.liferay.style.book.web.internal.design.library.resource.type;
 
 import com.liferay.depot.model.DepotEntry;
 import com.liferay.design.library.resource.type.DesignLibraryResourceCreationItem;
