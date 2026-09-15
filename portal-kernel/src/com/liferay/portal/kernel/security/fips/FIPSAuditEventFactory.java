1cb781cf2f04050207bebec95ab7fa71033abaf5
Jorge García Jiménez <jorge.garcia@liferay.com>
2026-09-15 16:24:07 +0200

    LPD-100167 Name the audit field after the value it holds

--- portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSAuditEventFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/fips/FIPSAuditEventFactory.java
@@ -13,8 +13,8 @@ import com.liferay.portal.kernel.util.GetterUtil;
 public class FIPSAuditEventFactory {
 
 	public static FIPSAuditEvent createAuthAttemptFailure(
-		String attemptedUserId, String authenticationMethod,
-		int consecutiveFailureCount, String failureReason, String ipSource) {
+		String attemptedUserId, String authenticationMethod, String clientIP,
+		int consecutiveFailureCount, String failureReason) {
 
 		FIPSAuditEvent fipsAuditEvent = new FIPSAuditEvent(
 			"auth-attempt-failure", FIPSAuditEvent.Severity.WARNING);
@@ -24,11 +24,11 @@ public class FIPSAuditEventFactory {
 		fipsAuditEvent.put(
 			"authentication-method",
 			GetterUtil.getString(authenticationMethod));
+		fipsAuditEvent.put("client-ip", GetterUtil.getString(clientIP));
 		fipsAuditEvent.put(
 			"consecutive-failure-count", consecutiveFailureCount);
 		fipsAuditEvent.put(
 			"failure-reason", GetterUtil.getString(failureReason));
-		fipsAuditEvent.put("ip-source", GetterUtil.getString(ipSource));
 
 		return fipsAuditEvent;
 	}
