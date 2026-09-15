db84d51455ad8b81d8bbf7ad1c47affe02ee6a31
Jaime Leon <jaime.leon@liferay.com>
2026-09-15 15:29:27 +0200

    LPD-105770 Migrate the malicious LAR guard to integration

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LARValidationExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LARValidationExportImportTest.java
@@ -6,24 +6,32 @@
 package com.liferay.exportimport.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.bookmarks.constants.BookmarksPortletKeys;
+import com.liferay.exportimport.constants.ExportImportConstants;
 import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactoryUtil;
 import com.liferay.exportimport.kernel.configuration.constants.ExportImportConfigurationConstants;
 import com.liferay.exportimport.kernel.exception.LARFileException;
 import com.liferay.exportimport.kernel.exception.LayoutImportException;
+import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalServiceUtil;
 import com.liferay.exportimport.kernel.service.ExportImportLocalServiceUtil;
 import com.liferay.exportimport.test.util.lar.BaseExportImportTestCase;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StackTraceUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
@@ -32,10 +40,17 @@ import com.liferay.portal.kernel.zip.ZipReader;
 import com.liferay.portal.kernel.zip.ZipReaderFactory;
 import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.portal.kernel.zip.ZipWriterFactory;
+import com.liferay.portal.test.log.LogCapture;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.io.File;
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.util.Enumeration;
+import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -43,6 +58,9 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Jaime León
@@ -61,6 +79,37 @@ public class LARValidationExportImportTest extends BaseExportImportTestCase {
 		UserTestUtil.setUser(TestPropsValues.getUser());
 
 		super.setUp();
+	}
+
+	@Test
+	@TestInfo("LPS-57776")
+	public void testImportPortletInfoWithForbiddenClass() throws Exception {
+		larFile = _createRemoteCodeExecutionLARFile();
+
+		_replaceHeaderAttribute(
+			"schema-version",
+			ExportImportConstants.EXPORT_IMPORT_SCHEMA_VERSION);
+
+		Portlet portlet = PortletLocalServiceUtil.getPortletById(
+			TestPropsValues.getCompanyId(), BlogsPortletKeys.BLOGS_ADMIN);
+
+		PortletDataHandler portletDataHandler =
+			portlet.getPortletDataHandlerInstance();
+
+		_replaceManifestAttribute(
+			"portlet", "schema-version", portletDataHandler.getSchemaVersion());
+
+		try (LogCapture logCapture = getLogCapture(true)) {
+			_importPortletInfo();
+
+			Assert.fail();
+		}
+		catch (PortalException portalException) {
+			Assert.assertEquals(
+				StackTraceUtil.getStackTrace(portalException),
+				"java.lang.Comparable",
+				_getForbiddenClassName(portalException));
+		}
 	}
 
 	@Test
@@ -179,6 +228,37 @@ public class LARValidationExportImportTest extends BaseExportImportTestCase {
 		}
 	}
 
+	private File _createRemoteCodeExecutionLARFile() throws Exception {
+		String path =
+			"/com/liferay/exportimport/test/dependencies" +
+				"/blogs_remote_code_execution.lar";
+
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+		ZipWriter zipWriter = _zipWriterFactory.getZipWriter();
+
+		Enumeration<URL> enumeration = bundle.findEntries(path, "*", true);
+
+		while (enumeration.hasMoreElements()) {
+			URL url = enumeration.nextElement();
+
+			String entryPath = url.getPath();
+
+			if (entryPath.endsWith(StringPool.SLASH)) {
+				continue;
+			}
+
+			try (InputStream inputStream = url.openStream()) {
+				zipWriter.addEntry(
+					StringUtil.removeSubstring(
+						entryPath, path + StringPool.SLASH),
+					inputStream);
+			}
+		}
+
+		return zipWriter.getFile();
+	}
+
 	private void _exportPortletInfo() throws Exception {
 		larFile = ExportImportLocalServiceUtil.exportPortletInfoAsFile(
 			ExportImportConfigurationLocalServiceUtil.
@@ -193,7 +273,51 @@ public class LARValidationExportImportTest extends BaseExportImportTestCase {
 							getExportParameterMap(), StringPool.BLANK)));
 	}
 
+	private String _getForbiddenClassName(Throwable throwable) {
+		while (throwable != null) {
+			Class<?> clazz = throwable.getClass();
+
+			if (Objects.equals(
+					clazz.getName(),
+					"com.thoughtworks.xstream.security." +
+						"ForbiddenClassException")) {
+
+				return throwable.getMessage();
+			}
+
+			throwable = throwable.getCause();
+		}
+
+		return null;
+	}
+
+	private void _importPortletInfo() throws Exception {
+		Layout importedGroupLayout = LayoutTestUtil.addTypePortletLayout(
+			importedGroup);
+
+		ExportImportLocalServiceUtil.importPortletInfo(
+			ExportImportConfigurationLocalServiceUtil.
+				addDraftExportImportConfiguration(
+					TestPropsValues.getUserId(),
+					ExportImportConfigurationConstants.TYPE_IMPORT_PORTLET,
+					ExportImportConfigurationSettingsMapFactoryUtil.
+						buildImportPortletSettingsMap(
+							TestPropsValues.getUser(),
+							importedGroupLayout.getPlid(),
+							importedGroup.getGroupId(),
+							BlogsPortletKeys.BLOGS_ADMIN,
+							getImportParameterMap())),
+			larFile);
+	}
+
 	private void _replaceHeaderAttribute(String name, String value)
+		throws Exception {
+
+		_replaceManifestAttribute("header", name, value);
+	}
+
+	private void _replaceManifestAttribute(
+			String elementName, String name, String value)
 		throws Exception {
 
 		File file = FileUtil.createTempFile("lar");
@@ -209,9 +333,9 @@ public class LARValidationExportImportTest extends BaseExportImportTestCase {
 
 		Element rootElement = document.getRootElement();
 
-		Element headerElement = rootElement.element("header");
+		Element element = rootElement.element(elementName);
 
-		headerElement.addAttribute(name, value);
+		element.addAttribute(name, value);
 
 		ZipWriter zipWriter = _zipWriterFactory.getZipWriter(file);
 
