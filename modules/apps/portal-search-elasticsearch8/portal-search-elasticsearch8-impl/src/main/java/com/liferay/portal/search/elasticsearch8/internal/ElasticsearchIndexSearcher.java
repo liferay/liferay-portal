5458b464cfc41abb1ba311b8e6cb3484c5033848
Selena Aungst <selena.aungst@liferay.com>
2026-09-15 16:41:43 -0700

    LPD-105633 portal-search-elasticsearch8-impl: Honor the fallback to last page attribute.
    https://liferay.atlassian.net/browse/LPD-105633

--- modules/apps/portal-search-elasticsearch8/portal-search-elasticsearch8-impl/src/main/java/com/liferay/portal/search/elasticsearch8/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/portal-search-elasticsearch8/portal-search-elasticsearch8-impl/src/main/java/com/liferay/portal/search/elasticsearch8/internal/ElasticsearchIndexSearcher.java
@@ -621,7 +621,13 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 
 			Document[] documents = hits.getDocs();
 
-			if ((documents.length != 0) || (start == 0)) {
+			if ((documents.length != 0) || (start == 0) ||
+				!GetterUtil.getBoolean(
+					searchContext.getAttribute(
+						SearchContextAttributes.
+							ATTRIBUTE_KEY_FALLBACK_TO_LAST_PAGE),
+					true)) {
+
 				break;
 			}
 
