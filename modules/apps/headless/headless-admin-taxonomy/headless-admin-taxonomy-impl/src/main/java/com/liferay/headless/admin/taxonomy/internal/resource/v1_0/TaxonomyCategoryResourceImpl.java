1c662fa3b33a6b31b7e36f5504d02d9bae5ae517
Jürgen Kappler <jurgen.kappler@liferay.com>
2026-09-15 10:07:17 +0200

    LPD-105764 Publish asset category property deletions to live

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/TaxonomyCategoryResourceImpl.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/TaxonomyCategoryResourceImpl.java
@@ -1110,10 +1110,7 @@ public class TaxonomyCategoryResourceImpl
 				taxonomyCategory.getDescription(),
 				taxonomyCategory.getDescription_i18n()),
 			assetVocabularyId,
-			_merge(
-				_assetCategoryPropertyLocalService.getCategoryProperties(
-					persistedAssetCategory.getCategoryId()),
-				taxonomyCategory.getTaxonomyCategoryProperties()),
+			_toStringArray(taxonomyCategory.getTaxonomyCategoryProperties()),
 			ServiceContextBuilder.create(
 				persistedAssetCategory.getGroupId(), contextHttpServletRequest,
 				taxonomyCategory.getViewableByAsString()
