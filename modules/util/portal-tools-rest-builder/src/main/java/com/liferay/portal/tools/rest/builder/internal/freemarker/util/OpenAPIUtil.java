07fb1b210669f8899e76e1825d52c7516f855811
Rubén Pulido <ruben.pulido@liferay.com>
2026-09-15 14:05:44 +0200

    LPD-105650 Clone the subtypes of a discriminated external schema
    OpenAPIUtil.getAllExternalSchemas did not follow discriminator mappings and visited only the first allOf branch, so the subtypes were never cloned, and enqueueing an allOf branch under its parent's name overwrote the parent's entry.

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/util/OpenAPIUtil.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/util/OpenAPIUtil.java
@@ -11,6 +11,7 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.tools.rest.builder.internal.freemarker.tool.java.parser.util.OpenAPIParserUtil;
 import com.liferay.portal.tools.rest.builder.internal.yaml.config.ConfigYAML;
 import com.liferay.portal.tools.rest.builder.internal.yaml.openapi.Components;
+import com.liferay.portal.tools.rest.builder.internal.yaml.openapi.Discriminator;
 import com.liferay.portal.tools.rest.builder.internal.yaml.openapi.Info;
 import com.liferay.portal.tools.rest.builder.internal.yaml.openapi.Items;
 import com.liferay.portal.tools.rest.builder.internal.yaml.openapi.OpenAPIYAML;
@@ -97,6 +98,20 @@ public class OpenAPIUtil {
 			for (Map.Entry<String, Schema> entry : map.entrySet()) {
 				Schema schema = entry.getValue();
 
+				Discriminator discriminator = schema.getDiscriminator();
+
+				if ((discriminator != null) &&
+					(discriminator.getMapping() != null)) {
+
+					Map<String, String> mapping = discriminator.getMapping();
+
+					for (String mappingReference : mapping.values()) {
+						_addExternalReference(
+							allExternalSchemas, externalSchemas, queue,
+							mappingReference);
+					}
+				}
+
 				Map<String, Schema> propertySchemas = null;
 
 				Items items = schema.getItems();
@@ -114,9 +129,16 @@ public class OpenAPIUtil {
 				else if (schema.getAllOfSchemas() != null) {
 					List<Schema> allOfSchemas = schema.getAllOfSchemas();
 
-					queue.add(
-						Collections.singletonMap(
-							entry.getKey(), allOfSchemas.get(0)));
+					for (Schema allOfSchema : allOfSchemas) {
+						if (allOfSchema.getReference() != null) {
+							_addExternalReference(
+								allExternalSchemas, externalSchemas, queue,
+								allOfSchema.getReference());
+						}
+						else if (allOfSchema.getPropertySchemas() != null) {
+							queue.add(allOfSchema.getPropertySchemas());
+						}
+					}
 				}
 				else if (schema.getReference() != null) {
 					_addExternalReference(
