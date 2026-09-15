3b391d0e78def1bca2d3a23e1e1a3738a5743e5b
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-15 20:20:42 +0300

    LPD-105680 mcp-server-rest-test: Singular

--- modules/apps/mcp/mcp-server-rest-test/src/testIntegration/java/com/liferay/mcp/server/rest/internal/servlet/test/MCPServerPromptTest.java
+++ modules/apps/mcp/mcp-server-rest-test/src/testIntegration/java/com/liferay/mcp/server/rest/internal/servlet/test/MCPServerPromptTest.java
@@ -37,7 +37,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.log.LogCapture;
 import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.FeatureFlag;
-import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -65,7 +64,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Jaime León Rosado
  */
-@FeatureFlags(featureFlags = @FeatureFlag("LPD-63311"))
+@FeatureFlag("LPD-63311")
 @RunWith(Arquillian.class)
 public class MCPServerPromptTest {
 
