b65a03a4e71e01f4f1c441d89cc2935aa8a4c048
Kevin Tan <kevin.tan@liferay.com>
2026-09-16 12:53:43 -0700

    LPD-105736 Test resizing the first column from the left

--- modules/apps/data-engine/data-engine-js-components-web/test/js/core/utils/columnResizedHandler.js
+++ modules/apps/data-engine/data-engine-js-components-web/test/js/core/utils/columnResizedHandler.js
@@ -3,11 +3,79 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {handleResizeRight} from '../../../../src/main/resources/META-INF/resources/js/core/utils/columnResizedHandler';
+import {
+	handleResizeLeft,
+	handleResizeRight,
+} from '../../../../src/main/resources/META-INF/resources/js/core/utils/columnResizedHandler';
 
 const props = {};
 
 describe('core/utils/columnResizedHandler', () => {
+	describe('handleResizeLeft(props, state, index, columnTarget)', () => {
+		it('adds an empty column before the first field', () => {
+			const indexes = [
+				{
+					columnIndex: 0,
+					pageIndex: 0,
+					rowIndex: 0,
+				},
+			];
+			const state = {
+				pages: [
+					{
+						rows: [
+							{
+								columns: [
+									{
+										fields: [{}],
+										size: 6,
+									},
+								],
+							},
+						],
+					},
+				],
+			};
+
+			const result = handleResizeLeft(props, state, indexes, 2);
+
+			expect(result[0].rows[0].columns).toHaveLength(2);
+			expect(result[0].rows[0].columns[0].size).toEqual(2);
+			expect(result[0].rows[0].columns[1].size).toEqual(4);
+		});
+
+		it('does not shrink the first field to nothing', () => {
+			const indexes = [
+				{
+					columnIndex: 0,
+					pageIndex: 0,
+					rowIndex: 0,
+				},
+			];
+			const state = {
+				pages: [
+					{
+						rows: [
+							{
+								columns: [
+									{
+										fields: [{}],
+										size: 6,
+									},
+								],
+							},
+						],
+					},
+				],
+			};
+
+			const result = handleResizeLeft(props, state, indexes, 6);
+
+			expect(result[0].rows[0].columns).toHaveLength(1);
+			expect(result[0].rows[0].columns[0].size).toEqual(6);
+		});
+	});
+
 	describe('handleResizeRight(props, state, index, columnTarget)', () => {
 		it('resizes first field to the left', () => {
 			const indexes = [
