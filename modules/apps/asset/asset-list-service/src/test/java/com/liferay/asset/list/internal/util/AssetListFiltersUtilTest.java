acdaa4922980caf9dbb5850a89af7a4d00264996
Joshua Cords <joshua.cords@liferay.com>
2026-09-16 15:14:05 -0700

    LPD-104361 asset-list-service: Test the Contains All term handling
    https://liferay.atlassian.net/browse/LPD-104361
    
    The text operator assertions only checked that a match query came back, which held for either quantifier. They now cover the full operator and quantifier matrix, expecting a boolean must query holding one match query per term for "all" and a plain match query otherwise. A new test covers a single term, and another covers the localized common field path through Title.

--- modules/apps/asset/asset-list-service/src/test/java/com/liferay/asset/list/internal/util/AssetListFiltersUtilTest.java
+++ modules/apps/asset/asset-list-service/src/test/java/com/liferay/asset/list/internal/util/AssetListFiltersUtilTest.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.search.filter.TermFilter;
 import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.test.AssertUtils;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
@@ -826,37 +827,129 @@ public class AssetListFiltersUtilTest {
 			ObjectFieldConstants.BUSINESS_TYPE_TEXT,
 			ObjectFieldConstants.DB_TYPE_STRING, textFieldName);
 
-		String textFieldValue = RandomTestUtil.randomString();
+		String term1 = RandomTestUtil.randomString();
 
-		Query containsQuery = _assertNestedQuery(
-			BooleanClauseOccur.MUST,
-			_getFilterJSONObject("contains", textFieldName, textFieldValue),
-			textFieldName);
+		_assertMatchQuery(
+			_NESTED_TEXT_FIELD, term1,
+			_assertNestedQuery(
+				BooleanClauseOccur.MUST,
+				_getFilterJSONObject(
+					"contains", textFieldName, term1
+				).put(
+					"quantifier", "all"
+				),
+				textFieldName));
 
-		Assert.assertTrue(
-			containsQuery.toString(), containsQuery instanceof MatchQuery);
+		String term2 = RandomTestUtil.randomString();
 
-		Query containsWithQuantifierQuery = _assertNestedQuery(
-			BooleanClauseOccur.MUST,
-			_getFilterJSONObject(
-				"contains", textFieldName, textFieldValue
-			).put(
-				"quantifier", "any"
-			),
-			textFieldName);
+		String textFieldValue = term1 + StringPool.SPACE + term2;
 
-		Assert.assertTrue(
-			containsWithQuantifierQuery.toString(),
-			containsWithQuantifierQuery instanceof MatchQuery);
+		_assertAllTermsQuery(
+			_NESTED_TEXT_FIELD,
+			_assertNestedQuery(
+				BooleanClauseOccur.MUST,
+				_getFilterJSONObject(
+					"contains", textFieldName, textFieldValue
+				).put(
+					"quantifier", "all"
+				),
+				textFieldName),
+			term1, term2);
+		_assertMatchQuery(
+			_NESTED_TEXT_FIELD, textFieldValue,
+			_assertNestedQuery(
+				BooleanClauseOccur.MUST,
+				_getFilterJSONObject(
+					"contains", textFieldName, textFieldValue
+				).put(
+					"quantifier", "any"
+				),
+				textFieldName));
+		_assertAllTermsQuery(
+			_NESTED_TEXT_FIELD,
+			_assertNestedQuery(
+				BooleanClauseOccur.MUST_NOT,
+				_getFilterJSONObject(
+					"not-contains", textFieldName, textFieldValue
+				).put(
+					"quantifier", "all"
+				),
+				textFieldName),
+			term1, term2);
+		_assertMatchQuery(
+			_NESTED_TEXT_FIELD, textFieldValue,
+			_assertNestedQuery(
+				BooleanClauseOccur.MUST_NOT,
+				_getFilterJSONObject(
+					"not-contains", textFieldName, textFieldValue
+				).put(
+					"quantifier", "any"
+				),
+				textFieldName));
+	}
 
-		Query notContainsQuery = _assertNestedQuery(
-			BooleanClauseOccur.MUST_NOT,
-			_getFilterJSONObject("not-contains", textFieldName, textFieldValue),
-			textFieldName);
+	@Test
+	public void testFilterQueriesWithTitleContainsAllOperator() {
+		String term1 = RandomTestUtil.randomString();
+		String term2 = RandomTestUtil.randomString();
 
-		Assert.assertTrue(
-			notContainsQuery.toString(),
-			notContainsQuery instanceof MatchQuery);
+		String title = term1 + StringPool.SPACE + term2;
+
+		_assertAllTermsQuery(
+			"localized_title_en_US",
+			_assertCommonFieldQuery(
+				BooleanClauseOccur.MUST,
+				_getCommonFieldFilterJSONObject(
+					"contains", Field.TITLE, title
+				).put(
+					"quantifier", "all"
+				)),
+			term1, term2);
+		_assertMatchQuery(
+			"localized_title_en_US", title,
+			_assertCommonFieldQuery(
+				BooleanClauseOccur.MUST,
+				_getCommonFieldFilterJSONObject(
+					"contains", Field.TITLE, title
+				).put(
+					"quantifier", "any"
+				)));
+	}
+
+	private void _assertAllTermsQuery(
+		String expectedField, Query query, String... expectedTerms) {
+
+		Assert.assertTrue(query.toString(), query instanceof BooleanQuery);
+
+		BooleanQuery booleanQuery = (BooleanQuery)query;
+
+		List<BooleanClause<Query>> booleanClauses = booleanQuery.clauses();
+
+		Assert.assertEquals(
+			booleanClauses.toString(), expectedTerms.length,
+			booleanClauses.size());
+
+		List<String> terms = new ArrayList<>();
+
+		for (BooleanClause<Query> booleanClause : booleanClauses) {
+			Assert.assertEquals(
+				BooleanClauseOccur.MUST, booleanClause.getBooleanClauseOccur());
+
+			Query clauseQuery = booleanClause.getClause();
+
+			Assert.assertTrue(
+				clauseQuery.toString(), clauseQuery instanceof MatchQuery);
+
+			MatchQuery matchQuery = (MatchQuery)clauseQuery;
+
+			Assert.assertEquals(expectedField, matchQuery.getField());
+			Assert.assertNull(matchQuery.getType());
+
+			terms.add(matchQuery.getValue());
+		}
+
+		AssertUtils.assertEqualsSorted(
+			expectedTerms, terms.toArray(new String[0]));
 	}
 
 	private Filter _assertAssetFilter(
@@ -1367,6 +1460,9 @@ public class AssetListFiltersUtilTest {
 	private static final long _CLASS_TYPE_ID = RandomTestUtil.randomLong();
 
 	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
+
+	private static final String _NESTED_TEXT_FIELD =
+		"nestedFieldArray.value_text";
 
 	private static final MockedStatic<ObjectDefinitionLocalServiceUtil>
 		_objectDefinitionLocalServiceUtilMockedStatic = Mockito.mockStatic(
