6c2a01ff30644bb1ea8afadd3e4eaef64845cbce
Victor Galan <victor.galan@liferay.com>
2026-09-16 23:20:07 +0200

    LPD-105900 Split the segments criteria key into batch and real-time

--- modules/apps/audiences/audiences-api/src/main/java/com/liferay/audiences/constants/AudiencesCriteriaKeys.java
+++ modules/apps/audiences/audiences-api/src/main/java/com/liferay/audiences/constants/AudiencesCriteriaKeys.java
@@ -10,6 +10,8 @@ package com.liferay.audiences.constants;
  */
 public class AudiencesCriteriaKeys {
 
+	public static final String BATCH_SEGMENTS = "batch_segments";
+
 	public static final String BROWSER_NAME = "browser_name";
 
 	public static final String BROWSER_VERSION = "browser_version";
@@ -28,11 +30,11 @@ public class AudiencesCriteriaKeys {
 
 	public static final String PATHNAME = "pathname";
 
+	public static final String REAL_TIME_SEGMENTS = "real_time_segments";
+
 	public static final String REFERRER = "referrer";
 
 	public static final String REQUEST_PARAMETERS = "request_parameters";
-
-	public static final String SEGMENTS = "segments";
 
 	public static final String TIMEZONE = "timezone";
 
