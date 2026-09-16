5d32601a60c10e959cc0dc35c08c3b3f308eb7ca
Rubén Pulido <ruben.pulido@liferay.com>
2026-09-16 17:04:27 +0200

    LPD-105650 Test posting discriminated external schema values

--- modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/ReferencingTestEntityResourceTest.java
+++ modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/ReferencingTestEntityResourceTest.java
@@ -8,7 +8,10 @@ package com.liferay.portal.tools.rest.builder.test.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.ExternalChildTestEntity1;
+import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.ExternalChildTestEntity2;
 import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.ExternalScopedTestEntity;
+import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.ExternalTestEntity1;
 import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.ReferencingTestEntity;
 import com.liferay.portal.tools.rest.builder.test.client.scope.Scope;
 
@@ -33,14 +36,16 @@ public class ReferencingTestEntityResourceTest
 
 	@Override
 	@Test
-	@TestInfo("LPD-105194")
+	@TestInfo({"LPD-105194", "LPD-105650"})
 	public void testPostReferencingTestEntity() throws Exception {
 		super.testPostReferencingTestEntity();
+
+		_testPostReferencingTestEntityExternalChildTestEntity2();
 	}
 
 	@Override
 	protected String[] getAdditionalAssertFieldNames() {
-		return new String[] {"externalScopedTestEntity"};
+		return new String[] {"externalScopedTestEntity", "externalTestEntity1"};
 	}
 
 	@Override
@@ -59,6 +64,16 @@ public class ReferencingTestEntityResourceTest
 
 		referencingTestEntity.setExternalScopedTestEntity(
 			externalScopedTestEntity);
+
+		ExternalChildTestEntity1 externalChildTestEntity1 =
+			new ExternalChildTestEntity1();
+
+		externalChildTestEntity1.setExternalProperty(
+			RandomTestUtil.randomString());
+		externalChildTestEntity1.setType(
+			ExternalTestEntity1.Type.EXTERNAL_CHILD_TEST_ENTITY1);
+
+		referencingTestEntity.setExternalTestEntity1(externalChildTestEntity1);
 
 		return referencingTestEntity;
 	}
@@ -80,6 +95,29 @@ public class ReferencingTestEntityResourceTest
 		scope.setType(Scope.Type.SITE);
 
 		return scope;
+	}
+
+	private void _testPostReferencingTestEntityExternalChildTestEntity2()
+		throws Exception {
+
+		ReferencingTestEntity referencingTestEntity =
+			randomReferencingTestEntity();
+
+		ExternalChildTestEntity2 externalChildTestEntity2 =
+			new ExternalChildTestEntity2();
+
+		externalChildTestEntity2.setExternalReferenceCode(
+			RandomTestUtil.randomString());
+		externalChildTestEntity2.setScope(_randomScope());
+		externalChildTestEntity2.setType(
+			ExternalTestEntity1.Type.EXTERNAL_CHILD_TEST_ENTITY2);
+
+		referencingTestEntity.setExternalTestEntity1(externalChildTestEntity2);
+
+		assertEquals(
+			referencingTestEntity,
+			referencingTestEntityResource.postReferencingTestEntity(
+				referencingTestEntity));
 	}
 
 }
