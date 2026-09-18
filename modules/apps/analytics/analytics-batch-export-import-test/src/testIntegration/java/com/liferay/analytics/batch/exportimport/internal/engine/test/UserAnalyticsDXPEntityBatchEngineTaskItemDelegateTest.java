93b5feac9a78b0d6e031ad39a1a2cd76160864ee
Nilton Vieira <nilton.vieira@liferay.com>
2026-09-18 08:38:09 -0300

    LPD-104906 squash tests into one single test

--- modules/apps/analytics/analytics-batch-export-import-test/src/testIntegration/java/com/liferay/analytics/batch/exportimport/internal/engine/test/UserAnalyticsDXPEntityBatchEngineTaskItemDelegateTest.java
+++ modules/apps/analytics/analytics-batch-export-import-test/src/testIntegration/java/com/liferay/analytics/batch/exportimport/internal/engine/test/UserAnalyticsDXPEntityBatchEngineTaskItemDelegateTest.java
@@ -55,6 +55,7 @@ public class UserAnalyticsDXPEntityBatchEngineTaskItemDelegateTest {
 			CompanyLocalServiceUtil.getCompany(TestPropsValues.getCompanyId()));
 
 		_permissionChecker = PermissionThreadLocal.getPermissionChecker();
+		_user = UserTestUtil.addUser();
 	}
 
 	@After
@@ -63,16 +64,24 @@ public class UserAnalyticsDXPEntityBatchEngineTaskItemDelegateTest {
 	}
 
 	@Test
-	public void testReadIfAdministratorUser() throws Exception {
+	public void testRead() throws Exception {
+		_testReadIfAdministratorUser();
+		_testReadIfAnalyticsAdministratorUser();
+		_testReadIfRegularUser();
+	}
+
+	private Page<DXPEntity> _read() throws Exception {
+		return _batchEngineTaskItemDelegate.read(
+			null, Pagination.of(1, 1), null, Collections.emptyMap(), null);
+	}
+
+	private void _testReadIfAdministratorUser() throws Exception {
 		UserTestUtil.setUser(TestPropsValues.getUser());
 
 		Assert.assertNotNull(_read());
 	}
 
-	@Test
-	public void testReadIfAnalyticsAdministratorUser() throws Exception {
-		_user = UserTestUtil.addUser();
-
+	private void _testReadIfAnalyticsAdministratorUser() throws Exception {
 		Role role = _roleLocalService.getRole(
 			_user.getCompanyId(), RoleConstants.ANALYTICS_ADMINISTRATOR);
 
@@ -83,18 +92,17 @@ public class UserAnalyticsDXPEntityBatchEngineTaskItemDelegateTest {
 		Assert.assertNotNull(_read());
 	}
 
-	@Test(expected = PrincipalException.MustHavePermission.class)
-	public void testReadIfRegularUser() throws Exception {
-		_user = UserTestUtil.addUser();
-
+	private void _testReadIfRegularUser() throws Exception {
 		UserTestUtil.setUser(_user);
 
-		_read();
-	}
+		try {
+			_read();
 
-	private Page<DXPEntity> _read() throws Exception {
-		return _batchEngineTaskItemDelegate.read(
-			null, Pagination.of(1, 1), null, Collections.emptyMap(), null);
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			Assert.assertNotNull(principalException);
+		}
 	}
 
 	@Inject(
