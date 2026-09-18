c20fbf92ffc1ec48574dec157501afb02aeb2751
Timothy Bell <timothy.bell@liferay.com>
2026-09-18 15:26:08 -0500

    LCD-53479 Add restore sanitization extension on AWS

---
+++ cloud/helm/aws-infrastructure-provider/charts/liferay-subchart-backup-restore/scripts/sanitize-database.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+function main {
+	if [ -z "$(ls /restore-sanitization/*.sql 2>/dev/null)" ]
+	then
+		echo "No sanitization scripts are mounted. Skipping the database sanitization."
+
+		exit 0
+	fi
+
+	local sql_file
+
+	for sql_file in /restore-sanitization/*.sql
+	do
+		PGPASSWORD="${DATABASE_PASSWORD}" psql \
+			--dbname lportal \
+			--file "${sql_file}" \
+			--host "${DATABASE_ENDPOINT}" \
+			--port "${DATABASE_PORT}" \
+			--set ON_ERROR_STOP=1 \
+			--single-transaction \
+			--username "${DATABASE_USERNAME}"
+	done
+}
+
+main
