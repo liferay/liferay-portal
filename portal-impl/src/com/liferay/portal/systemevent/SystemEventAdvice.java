433a845f6b11cf332aae786f4e7377d71c8a4acc
Tina Tian <tina.tian@liferay.com>
2026-09-18 19:43:53 -0700

    LPD-104965 Revert LPD-35723 (1aea67ebe5bb0c8201a78e29de695f496dcd3543) "Mute system event during bulk delete to avoid massive event creation", the right way to skip nesting system event creation is ACTION_SKIP annotation, which the framework already provides and keeps the parent event while dropping the nested ones.

--- portal-impl/src/com/liferay/portal/systemevent/SystemEventAdvice.java
+++ portal-impl/src/com/liferay/portal/systemevent/SystemEventAdvice.java
@@ -14,7 +14,6 @@ import com.liferay.portal.kernel.aop.AopMethodInvocation;
 import com.liferay.portal.kernel.aop.ChainableMethodAdvice;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.mass.delete.MassDeleteCacheThreadLocal;
 import com.liferay.portal.kernel.model.AuditedModel;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.ClassedModel;
@@ -55,10 +54,6 @@ public class SystemEventAdvice extends ChainableMethodAdvice {
 			AopMethodInvocation aopMethodInvocation, Object[] arguments)
 		throws Throwable {
 
-		if (MassDeleteCacheThreadLocal.isMassDeleteMode()) {
-			return null;
-		}
-
 		SystemEvent systemEvent = aopMethodInvocation.getAdviceMethodContext();
 
 		if (systemEvent.action() != SystemEventConstants.ACTION_NONE) {
@@ -94,10 +89,6 @@ public class SystemEventAdvice extends ChainableMethodAdvice {
 			AopMethodInvocation aopMethodInvocation, Object[] arguments,
 			Object result)
 		throws Throwable {
-
-		if (MassDeleteCacheThreadLocal.isMassDeleteMode()) {
-			return result;
-		}
 
 		SystemEvent systemEvent = aopMethodInvocation.getAdviceMethodContext();
 
@@ -176,10 +167,6 @@ public class SystemEventAdvice extends ChainableMethodAdvice {
 	@Override
 	protected void duringFinally(
 		AopMethodInvocation aopMethodInvocation, Object[] arguments) {
-
-		if (MassDeleteCacheThreadLocal.isMassDeleteMode()) {
-			return;
-		}
 
 		SystemEvent systemEvent = aopMethodInvocation.getAdviceMethodContext();
 
