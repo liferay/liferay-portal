0b544d80ec68994f812fb56647b460d6b9f54b76
Gabriel Lima <gabriel.lima@liferay.com>
2026-09-21 10:09:35 -0300

    LPD-88086 Add test coverage for the style book editor's frontend token merge
    Covers EditStyleBookEntryDisplayContext's merge of a style book entry's custom frontend token definition into the registry-provided definitions returned to the editor.

---
+++ modules/apps/style-book/style-book-web-test/src/testIntegration/java/com/liferay/style/book/web/internal/display/context/test/EditStyleBookEntryDisplayContextTest.java
@@ -0,0 +1,154 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.style.book.web.internal.display.context.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.frontend.token.definition.util.FrontendTokenDefinitionUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderResponse;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.style.book.model.StyleBookEntry;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
+import com.liferay.style.book.test.util.FrontendTokenDefinitionTestUtil;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Gabriel Lima
+ */
+@RunWith(Arquillian.class)
+public class EditStyleBookEntryDisplayContextTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testGetFrontendTokenDefinitionsJSONArray() throws Exception {
+		String frontendTokenName = RandomTestUtil.randomString();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(_group.getGroupId());
+		serviceContext.setUserId(TestPropsValues.getUserId());
+
+		StyleBookEntry styleBookEntry =
+			_styleBookEntryLocalService.addStyleBookEntry(
+				null, TestPropsValues.getUserId(), _group.getGroupId(), false,
+				FrontendTokenDefinitionTestUtil.getFrontendTokenDefinition(
+					frontendTokenName),
+				StringPool.BLANK, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(), _THEME_ID_CLASSIC,
+				serviceContext);
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setParameter(
+			"styleBookEntryId",
+			String.valueOf(styleBookEntry.getStyleBookEntryId()));
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(
+			_companyLocalService.getCompany(TestPropsValues.getCompanyId()));
+		themeDisplay.setLanguageId(
+			LanguageUtil.getLanguageId(LocaleUtil.getDefault()));
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setRealUser(TestPropsValues.getUser());
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
+			new MockLiferayPortletRenderRequest(mockHttpServletRequest);
+
+		_mvcRenderCommand.render(
+			mockLiferayPortletRenderRequest,
+			new MockLiferayPortletRenderResponse());
+
+		Object editStyleBookEntryDisplayContext =
+			mockLiferayPortletRenderRequest.getAttribute(
+				"com.liferay.style.book.web.internal.display.context." +
+					"EditStyleBookEntryDisplayContext");
+
+		JSONArray frontendTokenDefinitionsJSONArray = ReflectionTestUtil.invoke(
+			editStyleBookEntryDisplayContext,
+			"_getFrontendTokenDefinitionsJSONArray", new Class<?>[0]);
+
+		Map<String, JSONObject> frontendTokenDefinitionJSONObjects =
+			JSONUtil.toJSONObjectMap(frontendTokenDefinitionsJSONArray, "id");
+
+		JSONObject frontendTokenDefinitionJSONObject =
+			frontendTokenDefinitionJSONObjects.get(_THEME_ID_CLASSIC);
+
+		List<String> frontendTokenNames =
+			FrontendTokenDefinitionUtil.getFrontendTokenNames(
+				frontendTokenDefinitionJSONObject);
+
+		Assert.assertTrue(frontendTokenNames.contains(frontendTokenName));
+	}
+
+	private static final String _THEME_ID_CLASSIC = "classic_WAR_classictheme";
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject(
+		filter = "component.name=com.liferay.style.book.web.internal.portlet.action.EditStyleBookEntryMVCRenderCommand"
+	)
+	private MVCRenderCommand _mvcRenderCommand;
+
+	@Inject
+	private StyleBookEntryLocalService _styleBookEntryLocalService;
+
+}
