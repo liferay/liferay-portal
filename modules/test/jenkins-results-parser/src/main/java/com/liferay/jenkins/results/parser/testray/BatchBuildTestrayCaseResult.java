f8421d6f71408d7db2a65f220c3c53c148dab3a2
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-22 15:51:19 -0700

    LRCI-8176 Check for a null build result before comparing it

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/BatchBuildTestrayCaseResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/BatchBuildTestrayCaseResult.java
@@ -506,13 +506,11 @@ public class BatchBuildTestrayCaseResult
 			if (result == null) {
 				testResultErrors = "Unable to finish build on CI";
 			}
-
-			if (result.equals("ABORTED")) {
+			else if (result.equals("ABORTED")) {
 				testResultErrors =
 					buildReport.getJobName() + " timed out after 2 hours";
 			}
-
-			if (result.equals("SUCCESS") || result.equals("UNSTABLE")) {
+			else if (result.equals("SUCCESS") || result.equals("UNSTABLE")) {
 				testResultErrors = "Unable to run test on CI";
 			}
 
