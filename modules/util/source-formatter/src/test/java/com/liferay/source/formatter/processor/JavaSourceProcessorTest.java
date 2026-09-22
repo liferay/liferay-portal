a7170f2d04db0ae53f1683270912b88f296df9c9
Alan Huang <alan.huang@liferay.com>
2026-09-22 12:58:39 +0800

    LPD-105896 Auto SF, sort Java terms

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaSourceProcessorTest.java
@@ -259,38 +259,6 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
-	public void testFeatureFlagsAnnotationTest() throws Exception {
-		test(
-			SourceProcessorTestParameters.create(
-				"FeatureFlagsAnnotationTest.testjava"
-			).addExpectedMessage(
-				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
-					"addProperties\" for feature flag",
-				22
-			).addExpectedMessage(
-				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
-					"addProperties\" for feature flag",
-				32
-			).addExpectedMessage(
-				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
-					"addProperties\" for feature flag",
-				42
-			));
-	}
-
-	@Test
-	public void testFetchContractCatch() throws Exception {
-		test(
-			"FetchContractCatch.testjava",
-			StringBundler.concat(
-				"Do not catch \"NoSuchUserNotificationEventException\" around ",
-				"the lookup \"userNotificationEventLocalService.getUser",
-				"NotificationEvent\" to signal a missing entity, call the ",
-				"null-tolerant fetch sibling and check for null instead"),
-			29);
-	}
-
-	@Test
 	public void testFIPSTLSVerification() throws Exception {
 		test(
 			SourceProcessorTestParameters.create(
@@ -343,6 +311,38 @@ public class JavaSourceProcessorTest extends BaseSourceProcessorTestCase {
 			"Missing check \"PropsValues.FIPS_ENABLED\" in if-statement " +
 				"condition in the same class as a trust manager, see LPD-93649",
 			17);
+	}
+
+	@Test
+	public void testFeatureFlagsAnnotationTest() throws Exception {
+		test(
+			SourceProcessorTestParameters.create(
+				"FeatureFlagsAnnotationTest.testjava"
+			).addExpectedMessage(
+				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
+					"addProperties\" for feature flag",
+				22
+			).addExpectedMessage(
+				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
+					"addProperties\" for feature flag",
+				32
+			).addExpectedMessage(
+				"Use annotation \"@FeatureFlags\" instead of \"PropsUtil." +
+					"addProperties\" for feature flag",
+				42
+			));
+	}
+
+	@Test
+	public void testFetchContractCatch() throws Exception {
+		test(
+			"FetchContractCatch.testjava",
+			StringBundler.concat(
+				"Do not catch \"NoSuchUserNotificationEventException\" around ",
+				"the lookup \"userNotificationEventLocalService.getUser",
+				"NotificationEvent\" to signal a missing entity, call the ",
+				"null-tolerant fetch sibling and check for null instead"),
+			29);
 	}
 
 	@Test
