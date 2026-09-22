d9ac64f02a2ed23997786ad37c37d87a7fca94ed
smottal <117469730+smottal@users.noreply.github.com>
2026-09-22 16:48:43 +0200

    LPD-106219 Add form style

--- modules/apps/site/site-pim-site-initializer/src/main/resources/META-INF/resources/js/EditPIMConnector.tsx
+++ modules/apps/site/site-pim-site-initializer/src/main/resources/META-INF/resources/js/EditPIMConnector.tsx
@@ -9,6 +9,7 @@ import ClayForm, {
 	ClaySelectWithOption,
 	ClayToggle,
 } from '@clayui/form';
+import ClayPanel from '@clayui/panel';
 import {RequiredMark, Toolbar} from '@liferay/site-cms-site-initializer';
 import {fetch, navigate, sub} from 'frontend-js-web';
 import React, {useEffect, useState} from 'react';
@@ -121,59 +122,79 @@ export default function EditPIMConnector({
 				</Toolbar.Item>
 			</Toolbar>
 
-			<div className="container-fluid container-fluid-max-xl mt-4">
+			<div className="container-fluid container-fluid-max-md p-0 p-md-4">
 				<ClayForm id="pimConnectorForm" onSubmit={handleSubmit}>
-					<ClayForm.Group>
-						<label htmlFor="pimConnectorName">
-							{Liferay.Language.get('name')}
+					<ClayPanel
+						aria-label="basic-info"
+						className="mb-4"
+						collapsable={false}
+						displayType="secondary"
+						role="group"
+					>
+						<div className="c-gap-4 d-flex flex-column p-4">
+							<h2 className="mb-0 py-2 text-6 text-dark">
+								{Liferay.Language.get('basic-info')}
+							</h2>
 
-							<RequiredMark />
-						</label>
+							<ClayForm.Group className="mb-0">
+								<label htmlFor="pimConnectorName">
+									{Liferay.Language.get('name')}
 
-						<ClayInput
-							id="pimConnectorName"
-							onChange={(event) => setName(event.target.value)}
-							required
-							type="text"
-							value={name}
-						/>
-					</ClayForm.Group>
+									<RequiredMark />
+								</label>
 
-					<ClayForm.Group>
-						<label htmlFor="pimConnectorKey">
-							{Liferay.Language.get('connector')}
+								<ClayInput
+									id="pimConnectorName"
+									onChange={(event) =>
+										setName(event.target.value)
+									}
+									required
+									type="text"
+									value={name}
+								/>
+							</ClayForm.Group>
 
-							<RequiredMark />
-						</label>
+							<ClayForm.Group className="mb-0">
+								<label htmlFor="pimConnectorKey">
+									{Liferay.Language.get('connector')}
 
-						<ClaySelectWithOption
-							id="pimConnectorKey"
-							onChange={(event) => setKey(event.target.value)}
-							options={[
-								{
-									disabled: true,
-									label: Liferay.Language.get(
-										'select-a-connector'
-									),
-									value: '',
-								},
-								...pimConnectors.map((pimConnector) => ({
-									label: pimConnector.name,
-									value: pimConnector.key,
-								})),
-							]}
-							required
-							value={key}
-						/>
-					</ClayForm.Group>
+									<RequiredMark />
+								</label>
 
-					<ClayForm.Group>
-						<ClayToggle
-							label={Liferay.Language.get('active')}
-							onToggle={setActive}
-							toggled={active}
-						/>
-					</ClayForm.Group>
+								<ClaySelectWithOption
+									id="pimConnectorKey"
+									onChange={(event) =>
+										setKey(event.target.value)
+									}
+									options={[
+										{
+											disabled: true,
+											label: Liferay.Language.get(
+												'select-a-connector'
+											),
+											value: '',
+										},
+										...pimConnectors.map(
+											(pimConnector) => ({
+												label: pimConnector.name,
+												value: pimConnector.key,
+											})
+										),
+									]}
+									required
+									value={key}
+								/>
+							</ClayForm.Group>
+
+							<ClayForm.Group className="mb-0">
+								<ClayToggle
+									label={Liferay.Language.get('active')}
+									onToggle={setActive}
+									toggled={active}
+								/>
+							</ClayForm.Group>
+						</div>
+					</ClayPanel>
 				</ClayForm>
 			</div>
 		</>
