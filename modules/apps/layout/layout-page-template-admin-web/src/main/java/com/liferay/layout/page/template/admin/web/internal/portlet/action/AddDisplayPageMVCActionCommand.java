96966c7c6ed414e66803e12428480fb1863145ea
Javier Moral <javier.moral@liferay.com>
2026-09-23 15:11:33 +0200

    LPD-106071 Return to the Design Library after creating a display page template

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddDisplayPageMVCActionCommand.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/AddDisplayPageMVCActionCommand.java
@@ -6,6 +6,7 @@
 package com.liferay.layout.page.template.admin.web.internal.portlet.action;
 
 import com.liferay.asset.kernel.NoSuchClassTypeException;
+import com.liferay.design.library.util.DesignLibraryUtil;
 import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
 import com.liferay.layout.page.template.admin.web.internal.handler.LayoutPageTemplateEntryExceptionRequestHandlerUtil;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateConstants;
@@ -18,6 +19,7 @@ import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
@@ -88,6 +90,19 @@ public class AddDisplayPageMVCActionCommand extends BaseMVCActionCommand {
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
+
+		Group scopeGroup = themeDisplay.getScopeGroup();
+
+		if (DesignLibraryUtil.isDesignLibraryScope(scopeGroup)) {
+			return HttpComponentsUtil.addParameters(
+				_portal.getLayoutFullURL(draftLayout, themeDisplay),
+				"p_l_back_url",
+				DesignLibraryUtil.getDesignLibraryResourcesURL(
+					scopeGroup, _portal.getHttpServletRequest(actionRequest)),
+				"p_l_back_url_title",
+				scopeGroup.getDescriptiveName(themeDisplay.getLocale()),
+				"p_l_mode", Constants.EDIT);
+		}
 
 		return HttpComponentsUtil.addParameters(
 			_portal.getLayoutFullURL(draftLayout, themeDisplay), "p_l_back_url",
