f066c027ab734fb4f391426cbd616a4f7fb9a3f1
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-23 12:37:24 +0300

    LPD-100297 export-import-rest-impl: Add entity model to make preview sites sortable from client

---
+++ modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/odata/entity/v1_0/PreviewSiteEntityModel.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.exportimport.rest.internal.odata.entity.v1_0;
+
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.Map;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class PreviewSiteEntityModel implements EntityModel {
+
+	public PreviewSiteEntityModel() {
+		_entityFieldsMap = EntityModel.toEntityFieldsMap(
+			new StringEntityField(
+				"descriptiveName", locale -> "descriptiveName"));
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		return _entityFieldsMap;
+	}
+
+	private final Map<String, EntityField> _entityFieldsMap;
+
+}
