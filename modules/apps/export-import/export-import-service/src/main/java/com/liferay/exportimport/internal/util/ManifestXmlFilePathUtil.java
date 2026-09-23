943a1c3fef054c7d1e49e7dcc3f15ad0ccb0e6f5
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-23 11:24:48 +0300

    LPD-100297 export-import-service: Add utility for resolving manifest paths

---
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/util/ManifestXmlFilePathUtil.java
@@ -0,0 +1,48 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.exportimport.internal.util;
+
+import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+
+/**
+ * @author Petteri Karttunen
+ */
+public class ManifestXmlFilePathUtil {
+
+	public static final String MANIFEST_XML_FILE_PATH = "/manifest.xml";
+
+	public static String getExportManifestXmlFilePath(
+		PortletDataContext portletDataContext) {
+
+		if (!GroupExportImportParameterUtil.isGroupScoped(portletDataContext)) {
+			return MANIFEST_XML_FILE_PATH;
+		}
+
+		return ExportImportPathUtil.getRootPath(portletDataContext) +
+			MANIFEST_XML_FILE_PATH;
+	}
+
+	public static String getImportManifestXmlFilePath(long sourceGroupId) {
+		return StringBundler.concat(
+			StringPool.FORWARD_SLASH, ExportImportPathUtil.PATH_PREFIX_GROUP,
+			StringPool.FORWARD_SLASH, sourceGroupId, MANIFEST_XML_FILE_PATH);
+	}
+
+	public static String getImportManifestXmlFilePath(
+		PortletDataContext portletDataContext) {
+
+		if (!GroupExportImportParameterUtil.isGroupScoped(portletDataContext)) {
+			return MANIFEST_XML_FILE_PATH;
+		}
+
+		return ExportImportPathUtil.getSourceRootPath(portletDataContext) +
+			MANIFEST_XML_FILE_PATH;
+	}
+
+}
