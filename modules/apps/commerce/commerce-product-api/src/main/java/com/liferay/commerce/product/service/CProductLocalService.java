c0377d9a5ff97baf3f38eb1eb3708a939a2c850c
Alessio Rendina <alessio.rendina@liferay.com>
2026-09-23 14:39:26 +0200

    LPD-93768 Autogenerated (buildService)

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CProductLocalService.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CProductLocalService.java
@@ -18,7 +18,6 @@ import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.PersistedModel;
-import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.BaseLocalService;
@@ -26,7 +25,6 @@ import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.change.tracking.CTService;
 import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
-import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Isolation;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
@@ -108,7 +106,6 @@ public interface CProductLocalService
 	 * @throws PortalException
 	 */
 	@Indexable(type = IndexableType.DELETE)
-	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public CProduct deleteCProduct(CProduct cProduct) throws PortalException;
 
 	/**
@@ -363,4 +360,4 @@ public interface CProductLocalService
 		throws E;
 
 }
-// LIFERAY-SERVICE-BUILDER-HASH:-591142694
+// LIFERAY-SERVICE-BUILDER-HASH:-540418836
