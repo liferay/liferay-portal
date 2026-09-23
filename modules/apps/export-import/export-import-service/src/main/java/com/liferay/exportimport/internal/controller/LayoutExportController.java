ce20fd2083580bea32f7cf3ed621d3663ea41ba3
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-09-23 11:58:09 +0300

    LPD-100297 export-import-service: Wire group export-import to layout controllers

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutExportController.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutExportController.java
@@ -8,7 +8,9 @@ package com.liferay.exportimport.internal.controller;
 import com.liferay.asset.link.model.adapter.StagedAssetLink;
 import com.liferay.exportimport.constants.ExportImportConstants;
 import com.liferay.exportimport.controller.PortletExportController;
+import com.liferay.exportimport.internal.lar.GroupExporter;
 import com.liferay.exportimport.internal.lar.PermissionExporter;
+import com.liferay.exportimport.internal.util.ManifestXmlFilePathUtil;
 import com.liferay.exportimport.kernel.controller.ExportController;
 import com.liferay.exportimport.kernel.controller.ExportImportController;
 import com.liferay.exportimport.kernel.lar.ExportImportDateUtil;
@@ -35,6 +37,7 @@ import com.liferay.portal.kernel.model.LayoutPrototype;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
 import com.liferay.portal.kernel.service.LayoutSetLocalService;
 import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalService;
@@ -279,6 +282,11 @@ public class LayoutExportController implements ExportController {
 		rootElement.addElement("site-portlets");
 		rootElement.addElement("site-services");
 
+		GroupExporter groupExporter = new GroupExporter(
+			_exportImportHelper, _groupService, _portletDataContextFactory);
+
+		groupExporter.addGroupsElement(portletDataContext, rootElement);
+
 		// Export the group
 
 		LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
@@ -341,7 +349,11 @@ public class LayoutExportController implements ExportController {
 		}
 
 		portletDataContext.addZipEntry(
-			"/manifest.xml", document.formattedString());
+			ManifestXmlFilePathUtil.getExportManifestXmlFilePath(
+				portletDataContext),
+			document.formattedString());
+
+		groupExporter.exportGroups(portletDataContext, this::doExport);
 
 		ZipWriter zipWriter = portletDataContext.getZipWriter();
 
@@ -423,6 +435,9 @@ public class LayoutExportController implements ExportController {
 
 	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private GroupService _groupService;
 
 	@Reference
 	private Language _language;
--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
@@ -9,6 +9,8 @@ import com.liferay.asset.link.model.adapter.StagedAssetLink;
 import com.liferay.exportimport.configuration.ExportImportServiceConfiguration;
 import com.liferay.exportimport.constants.ExportImportConstants;
 import com.liferay.exportimport.controller.PortletImportController;
+import com.liferay.exportimport.internal.lar.GroupImporter;
+import com.liferay.exportimport.internal.util.ManifestXmlFilePathUtil;
 import com.liferay.exportimport.kernel.controller.ExportImportController;
 import com.liferay.exportimport.kernel.controller.ImportController;
 import com.liferay.exportimport.kernel.exception.LARFileException;
@@ -37,6 +39,7 @@ import com.liferay.exportimport.lar.PermissionImporter;
 import com.liferay.exportimport.portlet.data.handler.provider.PortletDataHandlerProvider;
 import com.liferay.exportimport.portlet.element.handler.PortletElementHandler;
 import com.liferay.exportimport.portlet.element.handler.PortletElementHandlerFactory;
+import com.liferay.exportimport.report.service.ExportImportReportEntryLocalService;
 import com.liferay.layout.admin.kernel.visibility.LayoutVisibilityManager;
 import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.exception.LayoutPrototypeException;
@@ -56,7 +59,9 @@ import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.plugin.Version;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
 import com.liferay.portal.kernel.service.LayoutSetLocalService;
 import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalService;
@@ -145,8 +150,9 @@ public class LayoutImportController implements ImportController {
 
 			try (ZipReader zipReader = _zipReaderFactory.getZipReader(file)) {
 				validateFile(
-					layoutSet.getCompanyId(), targetGroupId, parameterMap,
-					zipReader);
+					layoutSet.getCompanyId(), targetGroupId,
+					ManifestXmlFilePathUtil.MANIFEST_XML_FILE_PATH,
+					parameterMap, zipReader);
 
 				PortletDataContext portletDataContext = getPortletDataContext(
 					exportImportConfiguration, zipReader);
@@ -275,8 +281,9 @@ public class LayoutImportController implements ImportController {
 
 			try (ZipReader zipReader = _zipReaderFactory.getZipReader(file)) {
 				validateFile(
-					layoutSet.getCompanyId(), targetGroupId, parameterMap,
-					zipReader);
+					layoutSet.getCompanyId(), targetGroupId,
+					ManifestXmlFilePathUtil.MANIFEST_XML_FILE_PATH,
+					parameterMap, zipReader);
 
 				PortletDataContext portletDataContext = getPortletDataContext(
 					exportImportConfiguration, zipReader);
@@ -460,13 +467,13 @@ public class LayoutImportController implements ImportController {
 	}
 
 	protected void validateFile(
-			long companyId, long groupId, Map<String, String[]> parameterMap,
-			ZipReader zipReader)
+			long companyId, long groupId, String manifestXmlFilePath,
+			Map<String, String[]> parameterMap, ZipReader zipReader)
 		throws Exception {
 
 		// XML
 
-		String xml = zipReader.getEntryAsString("/manifest.xml");
+		String xml = zipReader.getEntryAsString(manifestXmlFilePath);
 
 		if (xml == null) {
 			throw new LARFileException(LARFileException.TYPE_MISSING_MANIFEST);
@@ -776,8 +783,10 @@ public class LayoutImportController implements ImportController {
 		// LAR validation
 
 		validateFile(
-			companyId, portletDataContext.getGroupId(), parameterMap,
-			portletDataContext.getZipReader());
+			companyId, portletDataContext.getGroupId(),
+			ManifestXmlFilePathUtil.getImportManifestXmlFilePath(
+				portletDataContext),
+			parameterMap, portletDataContext.getZipReader());
 
 		// Source and target group id
 
@@ -976,6 +985,14 @@ public class LayoutImportController implements ImportController {
 		if (_log.isInfoEnabled()) {
 			_log.info("Importing layouts takes " + stopWatch.getTime() + " ms");
 		}
+
+		GroupImporter groupImporter = new GroupImporter(
+			_classNameLocalService, _exportImportHelper,
+			_exportImportReportEntryLocalService, _groupLocalService,
+			_groupService, _portletDataContextFactory);
+
+		groupImporter.importGroups(
+			portletDataContext, this::_importFile, userId);
 	}
 
 	private void _validateLayoutPrototypes(
@@ -1078,6 +1095,9 @@ public class LayoutImportController implements ImportController {
 		LayoutImportController.class);
 
 	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
 	private ConfigurationProvider _configurationProvider;
 
 	@Reference
@@ -1090,7 +1110,14 @@ public class LayoutImportController implements ImportController {
 	private ExportImportLifecycleManager _exportImportLifecycleManager;
 
 	@Reference
+	private ExportImportReportEntryLocalService
+		_exportImportReportEntryLocalService;
+
+	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private GroupService _groupService;
 
 	@Reference
 	private Language _language;
