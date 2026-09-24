3df3da358f64fc844feacfe97429c9c83f6a047b
Mikel Lorza <mikel.lorza@liferay.com>
2026-09-24 14:32:38 +0200

    LPD-106876 Add tests

---
+++ modules/apps/wiki/wiki-engine-text/src/test/java/com/liferay/wiki/engine/text/internal/TextEngineTest.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.wiki.engine.text.internal;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.wiki.model.WikiPage;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Mikel Lorza
+ */
+public class TextEngineTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testConvert() {
+		WikiPage wikiPage = Mockito.mock(WikiPage.class);
+
+		Mockito.when(
+			wikiPage.getContent()
+		).thenReturn(
+			"a <b>bold</b> & 1 < 2"
+		);
+
+		TextEngine textEngine = new TextEngine();
+
+		Assert.assertEquals(
+			"<pre>a &lt;b&gt;bold&lt;/b&gt; &amp; 1 &lt; 2</pre>",
+			textEngine.convert(wikiPage, null, null, null));
+	}
+
+}
